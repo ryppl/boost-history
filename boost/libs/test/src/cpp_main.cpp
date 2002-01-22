bfdcc042707ae2d767c7047833404b5807faf964
Gennadiy Rozental <rogeeff@gmail.com>
2002-01-22 05:44:28 +0000

    test library revision 2 initial commit
    SVN-Revision: 12425

--- boost/libs/test/src/cpp_main.cpp
+++ boost/libs/test/src/cpp_main.cpp
@@ -13,78 +13,84 @@
 //  may elect to do so because header-only implementation requires no library
 //  build support. (Suggested by Ed Brey)
 
+//  header dependencies are deliberately restricted to reduce coupling.
+
+// LOCAL
+#include <boost/test/execution_monitor.hpp>
+
+// BOOST
+#include <boost/cstdlib.hpp>
+#include <boost/config.hpp>
+
+// STL
+#include <iostream>
+
+#ifdef BOOST_NO_STDC_NAMESPACE
+namespace std { using ::getenv; using ::atoi; }
+#endif
+
+int cpp_main( int argc, char * argv[] );  // prototype for user's cpp_main()
+
+namespace
+{
+
+class cpp_main_caller : public boost::execution_monitor {
+public:
+    cpp_main_caller( int argc, char ** argv )
+    : m_argc( argc ), m_argv( argv )    {}
+    
+    int         function()              { return cpp_main( m_argc, m_argv ); }
+    
+private:
+    int         m_argc;
+    char **     m_argv;
+};
+
+}
+
+// ************************************************************************** //
+// **************                   cpp main                   ************** //
+// ************************************************************************** //
+
+int main( int argc, char * argv[] )
+{
+    cpp_main_caller caller( argc, argv );
+    
+    int result;
+    
+    try {
+        result = caller.execute();
+        
+        //  Some prefer a confirming message when all is well, while others don't
+        //  like the clutter.  Use an environmental variable to avoid command
+        //  line argument modifications; for use in production programs
+        //  that's a no-no in some organizations.
+        char const* p = std::getenv( "BOOST_CPP_MAIN_CONFIRMATION" );
+        if( !p || std::atoi(p) != 0 ) { 
+            std::cout << std::flush << "no errors detected" << std::endl; 
+        }
+    }
+    catch( boost::execution_exception const& exex ) {
+        std::cout << "\n*** Error: " << exex.what() << std::endl;
+        result = exex.code();
+    }
+    
+    if( result != 0 && result != boost::exit_success ) {
+        std::cout << "\n**** error return code " << result << std::endl;
+        std::cerr << "**********  errors detected; see standard output for details  ***********" << std::endl;
+    }
+
+    return result;
+}
+
+//____________________________________________________________________________//
+
 //  Revision History
+//   16 Nov 01 Minor message fix (Gennadiy)
 //    4 Jun 01 Rewrite to use exception_monitor
 //   26 Feb 01 Numerous changes suggested during formal review.
 //   25 Jan 01 catch_exceptions.hpp code factored out.
 //   22 Jan 01 Remove test_tools dependencies to reduce coupling.
 //    5 Nov 00 Initial boost version (Beman Dawes)
 
-//  header dependencies are deliberately restricted to reduce coupling.
-#include <boost/test/execution_monitor.hpp>
-#include <boost/cstdlib.hpp>
-#include <boost/config.hpp>
-#include <iostream>
-
-# ifdef BOOST_NO_STDC_NAMESPACE
-    namespace std { using ::getenv; using ::atoi; }
-# endif
-
-//  It is easiest (because cpp_main.cpp is sometimes used as a header)
-//  to always compile execution_monitor.cpp in the same translation unit.  
-#include <libs/test/src/execution_monitor.cpp>
-
-int cpp_main( int argc, char * argv[] );  // prototype for user's cpp_main()
-
-namespace
-{
-  class cpp_main_caller : public boost::execution_monitor
-  {
-   public:
-    cpp_main_caller( int argc, char ** argv )
-                       : _argc(argc), _argv(argv) {}
-
-    int  function() { return cpp_main( _argc, _argv ); }
-
-   private:
-    int           _argc;
-    char **       _argv;
-  };
-}
-
-//  main()  ------------------------------------------------------------------//
-
-int main( int argc, char * argv[] )
-{
-  cpp_main_caller caller( argc, argv );
-
-  int result;
-  
-  try
-  {
-    result = caller.execute();
-
-    //  Some prefer a confirming message when all is well, while others don't
-    //  like the clutter.  Use an environmental variable to avoid command
-    //  line argument modifications; for use in production programs
-    //  that's a no-no in some organizations.
-    const char * p = std::getenv( "BOOST_CPP_MAIN_CONFIRMATION" );
-    if ( !p || std::atoi(p) != 0 ) 
-      { std::cout << std::flush << "no errors detected" << std::endl; }
-  }
-  catch ( const boost::execution_exception & exex )
-  {
-    std::cout << "\n** Error: " << exex.what() << std::endl;
-    result = exex.code();
-  }
-
-  if ( result != 0 && result != boost::exit_success )
-  {
-    std::cout << std::endl << "**** error return code "
-              << result << std::endl;
-    std::cerr
-      << "**********  errors detected; see stdout for details  ***********"
-      << std::endl;
-  }
-  return result;
-}
+// EOF
--- boost/libs/test/src/test_main.cpp
+++ boost/libs/test/src/test_main.cpp
@@ -23,81 +23,77 @@
 
 //  header dependencies are deliberately kept minimal to reducing coupling.
 
-
-#include <boost/config.hpp>           // for broken compiler workarounds
-#include <boost/cstdlib.hpp>          // for exit codes
+// LOCAL
 #include <boost/test/unit_test.hpp>   // for unit_test framework
-#include <cstring>                    // for strcmp
-#include <iostream>                   // for cout, cerr
-#include <cstdlib>                    // for atoi
+#include <boost/test/unit_test_result.hpp>
 
-# ifdef BOOST_NO_STDC_NAMESPACE
-    namespace std { using ::getenv; using ::atoi; using :: strcmp; }
-# endif
+// BOOST
+#include <boost/cstdlib.hpp>          // for exit codes
+
+// STL
+#include <iostream>                   // for cout, cerr
 
 int test_main( int argc, char * argv[] );  // prototype for user's test_main()
 
-namespace
-{
-  int      argc_;
-  char **  argv_;
-  int      test_main_result;
-
-  void call_test_main()
-  {
-    test_main_result = test_main( argc_, argv_ );
-
-    // translate a test_main non-success return into a unit_test error
-    BOOST_TEST( test_main_result == 0
-                || test_main_result == boost::exit_success );
-  }
-
-}
-
-//  main()  ------------------------------------------------------------------//
-
-int main( int argc, char ** argv )
-{
-  //  set the log level from environment or command line
-  const char * p = std::getenv( "BOOST_LOGLEVEL" );
-  if ( p ) boost::set_loglevel( std::atoi( p ) );
-  for ( int i = 1; i < argc; ++i ) // scan for command line switch
-  {
-    if ( std::strcmp( argv[i], "--loglevel=" ) == 0 )
+namespace {
+    int      argc_;
+    char **  argv_;
+    int      test_main_result;
+    
+    void call_test_main()
     {
-      boost::set_loglevel( std::atoi( argv[i]+11 ) );
-      // remove command line entry so test_main() needn't worry about it.
-      // note: C++ standard (3.6.1/2) requires argv[argc] be 0. 
-      for ( int j = i; j < argc; ++j ) { argv[j] = argv[j+1]; }
-      --argc;
-      --i;
+        test_main_result = test_main( argc_, argv_ );
+        
+        // translate a test_main non-success return into a unit_test error
+        BOOST_TEST( test_main_result == 0
+            || test_main_result == boost::exit_success );
     }
-  }
-
-  //  set up and execute the test
-
-  argc_ = argc;
-  argv_ = argv;
-
-  boost::test_suite test( argv[0] );
-  test.add( BOOST_TEST_CASE( &call_test_main ) );
-
-  int run_result = test.run();
-  
-  if(run_result != 0 || 
-     (test_main_result != 0 &&
-      test_main_result != boost::exit_success))
-  {
-      std::cerr << "*** Failures in test programm \"" << 
-                   argv[0] << "\"" << std::endl;
-      return ( test_main_result != 0
-           && test_main_result != boost::exit_success )
-        ? test_main_result
-        : run_result;
-  }
-  else
-  {
-      return 0;
-  }
-
+    
 }
+
+// ************************************************************************** //
+// **************                   test main                  ************** //
+// ************************************************************************** //
+
+using namespace boost::unit_test_framework;
+
+int main( int argc, char* argv[] )
+{
+    std::string         loglevel;
+    bool                no_result_code;
+    result_report_type  report_type;
+
+    // 1. set the log level
+    unit_test_log::instance().set_log_threshold_level_by_name(
+        retrieve_framework_parameter( LOGLEVEL, &argc, argv ).data() );
+
+    // 2. set the result code flag
+    no_result_code = retrieve_framework_parameter( NO_RESULT_CODE, &argc, argv ) == "no";
+
+    // 3. set the report type
+    std::string report_type_to_set = retrieve_framework_parameter( RESULT_REPORT, &argc, argv );
+
+    report_type = report_type_to_set == report_type_names[NO_REPORT] ? NO_REPORT : CONFIRMATION_REPORT;
+
+    unit_test_log::instance().set_log_threshold_level_by_name( loglevel.data() );
+
+    //  set up the test   
+    argc_ = argc;
+    argv_ = argv;
+    boost::unit_test_framework::detail::grinning_ptr<test_case> test_main_tc( BOOST_TEST_CASE( &call_test_main ) );
+
+    // start testing
+    test_main_tc->run();
+    
+    // report result
+    if( report_type == CONFIRMATION_REPORT ) {
+        unit_test_result::instance().confirmation_report( std::cout );
+    }
+
+    // return code
+    return no_result_code ? 0 : 
+        ( test_main_result != 0 && test_main_result != boost::exit_success ? 
+            test_main_result : unit_test_result::instance().result_code() );
+}
+
+// EOF
