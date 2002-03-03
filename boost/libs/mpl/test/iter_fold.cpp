f9f884807e343c6e8195ac99e5615227461b06a0
Dave Abrahams <dave@boostpro.com>
2002-03-03 23:43:39 +0000

    execute() test
    SVN-Revision: 13058

--- boost/libs/mpl/test/iter_fold.cpp
+++ boost/libs/mpl/test/iter_fold.cpp
@@ -20,7 +20,13 @@
 #include <boost/mpl/aux_/pred.hpp>
 #include <boost/mpl/select_type.hpp>
 #include <boost/mpl/type_list.hpp>
+#include <cassert>
+#include <algorithm>
+#include <cstddef>
 
+#if defined(BOOST_INTEL_CXX_VERSION) && BOOST_INTEL_CXX_VERSION < 600
+# pragma warning (disable: 985) // identifier was truncated in debug information
+#endif 
 namespace mpl = boost::mpl;
 
 struct choose1
@@ -33,6 +39,12 @@ struct choose1
             (3 * sizeof(X) > sizeof(Y))
             , X
             , Y>::type type;
+        
+        static void execute(std::size_t** x)
+        {
+            *(*x)++ = sizeof(X);
+            *(*x)++ = sizeof(Y);
+        }
     };
 };
 
@@ -46,30 +58,57 @@ struct choose2
             (sizeof(X) == 2*sizeof(Y))
             , X
             , Y>::type type;
+
+        static void execute(std::size_t** x)
+        {
+            *(*x)++ = sizeof(X);
+            *(*x)++ = sizeof(Y);
+        }
     };
 };
 
 
-template <class T> struct undefined;
-
 int main()
 {
-
     typedef mpl::type_list<char[2], char[4], char[8], char[10]> types;
+
+    std::size_t sizes1[9] = {0};
+    std::size_t const sizes1_expected[9] = {
+        1, 2
+        , 1, 4
+        , 4, 8
+        , 4, 10
+        , 0
+    };
+
     
-    typedef mpl::iter_fold<
-          types
-        , char
-        , choose1
-        >::type result1;
+    std::size_t sizes2[17] = {0};
+    std::size_t const sizes2_expected[17] = {
+        1, 2
+        , 1, 4
+        , 4, 8
+        , 4, 10
+        
+        , 10, 4
+        , 8, 4
+        , 4, 8
+        , 2, 8
+        , 0
+    };
+    
+    typedef mpl::iter_fold<types, char, choose1> loop1;
+    std::size_t* out = sizes1;
+    loop1::execute(&out);
+    assert(std::equal(sizes1, sizes1 + 9, sizes1_expected));
+    
+    typedef mpl::iter_fold<types, char, choose1, choose2> loop2;
 
-    typedef mpl::iter_fold<
-          types
-        , char
-        , choose1
-        , choose2
-        >::type result2;
-
-    BOOST_STATIC_ASSERT((boost::is_same<result1,char[4]>::value));
-    BOOST_STATIC_ASSERT((boost::is_same<result2,char[8]>::value));
+    std::size_t* out2 = sizes2;
+    loop2::execute(&out2);
+    assert(std::equal(sizes2, sizes2 + 17, sizes2_expected));
+    
+    BOOST_STATIC_ASSERT((boost::is_same<loop1::type,char[4]>::value));
+    BOOST_STATIC_ASSERT((boost::is_same<loop2::type,char[8]>::value));
+    
+    return 0;
 }
