5014354a1fe7a881ee67f0bb6c2002b2f9e4d78f
Dave Abrahams <dave@boostpro.com>
2002-03-04 02:15:51 +0000

    Unrolling (baffled)!
    SVN-Revision: 13064

--- boost/boost/mpl/iter_fold.hpp
+++ boost/boost/mpl/iter_fold.hpp
@@ -26,87 +26,11 @@ namespace mpl {
 
 namespace aux {
 
-template <
-      typename Iterator
-    , typename LastIterator
-    , typename State
-    , typename ForwardOp
-    , typename BackwardOp
-    >
-struct iter_fold_impl_more;
-
-template <
-    typename State
-    >
-struct iter_fold_impl_done;
-
-template <
-      typename Iterator
-    , typename LastIterator
-    , typename State
-    , typename ForwardOp
-    , typename BackwardOp
-    >
-struct iter_fold_impl
-    : select_type<
-      is_same<Iterator,LastIterator>::value
-      , iter_fold_impl_done<State>
-      , iter_fold_impl_more<Iterator, LastIterator, State, ForwardOp, BackwardOp>
-        >::type
-{
-};
-    
-template<
-      typename Iterator
-    , typename LastIterator
-    , typename State
-    , typename ForwardOp
-    , typename BackwardOp
-    >
-struct iter_fold_impl_more
-{
- private:
-    typedef apply2<ForwardOp,State,Iterator> forward;
-    
-    typedef iter_fold_impl<
-        typename Iterator::next
-        , LastIterator
-        , typename forward::type
-        , ForwardOp
-        , BackwardOp
-        > recursion;
-
-    typedef typename recursion::type inner_t;
-    
-    typedef apply2<
-          BackwardOp
-        , Iterator
-        , inner_t
-        > backward;
-
- public:
-    typedef typename backward::type type;
-
-    static void execute()
-    {
-        forward::execute();
-        recursion::execute();
-        backward::execute();
-    }
-
-    template <class T>
-    static void execute(T x)
-    {
-        forward::execute(x);
-        recursion::execute(x);
-        backward::execute(x);
-    }
-};
-
-template<typename State>
-struct iter_fold_impl_done
+template <class Iterator, class State>
+struct iter_fold_done
 {
     typedef State type;
+    typedef Iterator iterator;
 
     static void execute()
     {
@@ -118,10 +42,122 @@ struct iter_fold_impl_done
     }
 };
 
+#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+template <class Iterator, class LastIterator, class ForwardOp, class State>
+struct iter_fold_next : apply2<ForwardOp,State,Iterator>
+{
+    typedef typename Iterator::next iterator;
+};
+
+template <class LastIterator, class ForwardOp, class State>
+struct iter_fold_next<LastIterator, LastIterator, ForwardOp, State>
+    : iter_fold_done<LastIterator,State>
+{
+};
+
+template <class Iterator, class LastIterator, class State, class BackwardOp>
+struct iter_fold_prev : apply2<BackwardOp,Iterator,State>
+{
+};
+
+template <class LastIterator, class State, class BackwardOp>
+struct iter_fold_prev<LastIterator,LastIterator,State,BackwardOp>
+    : iter_fold_done<LastIterator,State>
+{
+    typedef State type;
+};
+#else 
+template <class Iterator, class ForwardOp, class State>
+struct iter_fold_more : apply2<ForwardOp,State,Iterator>
+{
+    typedef typename Iterator::next iterator;
+};
+
+template <class Iterator, class LastIterator, class ForwardOp, class State>
+struct iter_fold_next
+    : select_type<is_same<Iterator,LastIterator>::value
+                  , iter_fold_more<Iterator,ForwardOp,State>
+                  , iter_fold_done<Iterator,State>
+      >::type
+{};
+
+template <class Iterator, class LastIterator, class State, class BackwardOp>
+struct iter_fold_prev
+    : select_type<is_same<Iterator,LastIterator>::value
+                  , apply2<BackwardOp,Iterator,State>
+                  , iter_fold_done<Iterator,State>
+      >::type
+{};
+#endif
+
+template<
+      typename Iterator
+    , typename LastIterator
+    , typename State
+    , typename ForwardOp
+    , typename BackwardOp
+    >
+struct iter_fold_impl
+{
+ private:
+    typedef iter_fold_next<Iterator,LastIterator,ForwardOp,State> next1;
+    typedef iter_fold_next<typename next1::iterator,LastIterator,ForwardOp,typename next1::type> next2;
+    typedef iter_fold_next<typename next2::iterator,LastIterator,ForwardOp,typename next2::type> next3;
+    typedef iter_fold_next<typename next3::iterator,LastIterator,ForwardOp,typename next3::type> next4;
+    
+    typedef iter_fold_impl<
+        typename next4::iterator
+        , LastIterator
+        , typename next4::type
+        , ForwardOp
+        , BackwardOp
+        > recursion;
+
+    typedef typename recursion::type inner_t;
+    
+    typedef iter_fold_prev<typename next3::iterator,LastIterator,inner_t,BackwardOp> prev4;
+    typedef iter_fold_prev<typename next2::iterator,LastIterator,typename prev4::type,BackwardOp> prev3;
+    typedef iter_fold_prev<typename next1::iterator,LastIterator,typename prev3::type,BackwardOp> prev2;
+    typedef iter_fold_prev<Iterator,LastIterator,typename prev2::type,BackwardOp> prev1;
+ public:
+    typedef typename prev1::type type;
+
+    static void execute()
+    {
+        next1::execute();
+        next2::execute();
+        next3::execute();
+        next4::execute();
+        
+        recursion::execute();
+        
+        prev4::execute();
+        prev3::execute();
+        prev2::execute();
+        prev1::execute();
+    }
+
+    template <class T>
+    static void execute(T x)
+    {
+        next1::execute(x);
+        next2::execute(x);
+        next3::execute(x);
+        next4::execute(x);
+        
+        recursion::execute(x);
+        
+        prev4::execute(x);
+        prev3::execute(x);
+        prev2::execute(x);
+        prev1::execute(x);
+    }
+};
+
 struct select2nd
 {
     template <class T, class U>
-    struct apply : iter_fold_impl_done<U>
+    struct apply : iter_fold_done<T,U>
     {
     };
 };
