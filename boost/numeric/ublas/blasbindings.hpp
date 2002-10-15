35e43b3c4d2bb2807256a7a5dd1feaeaa9acbf71
Toon Knapen <toon.knapen@gmail.com>
2002-10-15 12:21:32 +0000

    moved to boost/numeric/blasbindings
    SVN-Revision: 578

--- boost/numeric/ublas/blasbindings.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef boost_ublas_blasbindings_hpp
-#define boost_ublas_blasbindings_hpp
-
-#include <math/blas1.hpp>
-#include <math/blas2.hpp>
-#include <math/blas3.hpp>
-
-
-#endif // boost_ublas_blasbindings_hpp
