2d9e504cfcefd81cb583406915f465d7a49ebe6d
Toon Knapen <toon.knapen@gmail.com>
2002-10-17 08:45:53 +0000

    traits to handle ublas allocators (unbounded)
    SVN-Revision: 591

---
+++ boost/numeric/blasbindings/storage_traits/ublas.hpp
@@ -0,0 +1,35 @@
+// (-*- C++ -*- header)  
+
+#ifndef BOOST_BLASBINDINGS_STORAGE_TRAITS_UBLAS_H
+#define BOOST_BLASBINDINGS_STORAGE_TRAITS_UBLAS_H
+
+#include <cstddef> 
+#include <boost/numeric/blasbindings/storage_traits.hpp> 
+#include <boost/numeric/ublas/storage.hpp>
+
+namespace boost { namespace numeric { namespace blasbindings {
+
+  // ublas::unbounded<> 
+  template <typename T>
+  struct storage_traits<boost::numeric::ublas::unbounded_array<T> > {
+    typedef boost::numeric::ublas::unbounded_array<T> allocator_type;
+    typedef typename allocator_type::pointer          pointer; 
+
+    static pointer storage (allocator_type& a) 
+    { return a.begin() ; }
+
+  }; 
+
+  template <typename T>
+  struct storage_traits<boost::numeric::ublas::unbounded_array<T> const> {
+    typedef boost::numeric::ublas::unbounded_array<T> const allocator_type;
+    typedef typename allocator_type::pointer const          pointer; 
+
+    static pointer storage (allocator_type& a) 
+    { return a.begin() ; }
+
+  }; 
+
+}}}  
+
+#endif // BOOST_BLASBINDINGS_STORAGE_TRAITS_UBLAS_H
