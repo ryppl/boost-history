af4c735408ea82dfab8f65c50b3d82bf2aa98f2f
Toon Knapen <toon.knapen@gmail.com>
2002-10-25 20:46:07 +0000

    replaced by boost::numeric::bindings::blas
    SVN-Revision: 617

--- boost/numeric/blasbindings/blas_traits/std_valarray.hpp
+++
@@ -1,9 +0,0 @@
-// (-*- C++ -*- header)  
-
-#ifndef BOOST_BLASBINDINGS_BLAS_TRAITS_STD_VALARRAY_H
-#define BOOST_BLASBINDINGS_BLAS_TRAITS_STD_VALARRAY_H
-
-#include <boost/numeric/blasbindings/blas_traits.hpp>
-#include <boost/numeric/blasbindings/storage_traits/std_valarray.hpp> 
-
-#endif // BOOST_BLASBINDINGS_BLAS_TRAITS_STD_VALARRAY_H
