5c926add862d8f244b5ed84341bca88fd9c151a1
Craig Henderson <cdm.henderson@googlemail.com>
2002-10-30 22:49:33 +0000

    Multiple pattern search
    SVN-Revision: 644

--- boost/sequence_algo/boyer_moore.hpp
+++ boost/sequence_algo/boyer_moore.hpp
@@ -14,12 +14,16 @@
 
 #include <algorithm>
 #include <boost/config.hpp>
+#include <boost/type_traits.hpp>
 
 namespace boost {
 
     template<typename Iter>
     struct boyer_moore_traits
     {
+// MSVC6 chokes on using the iterator_traits, so this class must be
+// specialised for that compiler
+#if !(defined(BOOST_MSVC)  &&  (_MSC_VER <= 1300))
       private:
         // the alphabet type is the type that is used to represent an
         // element in the sequence
@@ -38,31 +42,48 @@ namespace boost {
         // types for T
         BOOST_STATIC_CONSTANT(unsigned long,
                               alphabet_size = (1<<(sizeof(alphabet_type)*8))-1);
+#endif
     };
 
 
     namespace detail {
 
         // small helper function to initialise the pattern working array
-        template<typename Traits, typename Array, typename Pattern>
+        template<unsigned long alphabet_size, typename Array, typename Pattern>
         void init_pattern(Array &array, Pattern pattern, std::ptrdiff_t len)
         {
             std::ptrdiff_t loop;
 
-            for (loop=0; loop<Traits::alphabet_size; ++loop)
+            for (loop=0; loop<=alphabet_size; ++loop)
                 array[loop] = len;
             --len;
             for (loop=0; loop<len; ++pattern, ++loop)
                 array[*pattern] = len - loop;
         }
 
+        template<typename Val1, typename Val2>
+        struct select_value
+        {
+            Val1 v1_;
+            Val2 v2_;
+
+            select_value(Val1 v1, Val2 v2) : v1_(v1), v2_(v2) { }
+            operator Val1()                             { return v1_; }
+            operator Val2()                             { return v2_; }
+        };
+
     }   // namespace detail
 
 
-    template<typename ItIn1, typename ItIn2>
-    ItIn1 boyer_moore_search(ItIn1 begin_search,  ItIn1 end_search,
-                             ItIn2 begin_pattern, ItIn2 end_pattern)
+    template<typename Res, typename ItIn1, typename ItIn2, typename ItOut>
+    void
+    boyer_moore_search(ItIn1 begin_search,  ItIn1 end_search,
+                       ItIn2 begin_pattern, ItIn2 end_pattern,
+                       ItOut output,
+                       std::size_t max_count = 0)
     {
+        std::size_t count = 0;
+
         // calculate the length of the pattern and the search sequence
         std::ptrdiff_t len_pattern = std::distance<>(begin_pattern, end_pattern);
         std::ptrdiff_t len_search  = std::distance<>(begin_search, end_search);
@@ -70,15 +91,15 @@ namespace boost {
         // if the pattern is longer than the search sequence, then we're not
         // going to find the pattern, so return immediately
         if (len_pattern > len_search)
-            return end_search;
+            return;
 
         // create and initialise the pattern alphabet
         typedef boyer_moore_traits<ItIn1> traits;
-        std::ptrdiff_t array[traits::alphabet_size] = { 0 };
-        detail::init_pattern<traits>(array, begin_pattern, len_pattern);
+        std::ptrdiff_t array[traits::alphabet_size+1] = { 0 };
+        detail::init_pattern<traits::alphabet_size>(array, begin_pattern, len_pattern);
 
         std::ptrdiff_t loop = len_pattern - 1;
-        while (loop < len_search)
+        while ((count < max_count  ||  max_count == 0)  &&  loop < len_search)
         {
             // start at the end of the search sequence
             ItIn1 it_search = begin_search;
@@ -90,9 +111,7 @@ namespace boost {
 
             // store the last element for use later
             // (this is just a simple optimisation)
-            typename
-            std::iterator_traits<ItIn1>::value_type
-            current = *it_search;
+            typename traits::alphabet_type current = *it_search;
 
             // work from the end of the pattern to the beginning and compare
             // each element with the search sequence
@@ -105,17 +124,20 @@ namespace boost {
             // if all of the elements match, then return the iterator pointing
             // to the start of the pattern within the search sequence
             if (it_patt == begin_pattern  &&  *it_search == *it_patt)
-                return it_search;
+            {
+                detail::select_value<ItIn1, std::ptrdiff_t> val(it_search, loop - len_pattern + 1);
+                *output++ = val;
+                ++count;
+            }
 
             // skip over this position and continue around the loop
             loop += array[current];
         }
 
-        // if we get this far, we have not found the pattern, so return the
-        // end of the search sequence
-        return end_search;
+        return;
     }
 
 }       // namespace boost
 
 #endif  // BOOST_BOYER_MOORE_H
+
--- libs/sequence_algo/test/boyer_moore/boyer_moore.cpp
+++ libs/sequence_algo/test/boyer_moore/boyer_moore.cpp
@@ -11,22 +11,144 @@
 
 #include "boost/sequence_algo/boyer_moore.hpp"
 #include <assert.h>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <string.h>
 
-std::string::const_iterator boyer_moore(const std::string &text, const char *pattern)
+// MSVC6 chokes on using the iterator_traits, so this class must be
+// specialised for that compiler
+#if defined(BOOST_MSVC)  &&  (_MSC_VER <= 1300)
+struct boost::boyer_moore_traits<std::string::const_iterator>
 {
-    const char *it = pattern + strlen(pattern);
-    return boost::boyer_moore_search<>(text.begin(), text.end(), pattern, it);
+    typedef
+    std::string::value_type
+    alphabet_type;
+
+    BOOST_STATIC_CONSTANT(unsigned long,
+                          alphabet_size = (1<<(sizeof(alphabet_type)*8))-1);
+};
+
+struct boost::boyer_moore_traits<std::wstring::const_iterator>
+{
+    typedef
+    std::wstring::value_type
+    alphabet_type;
+
+    BOOST_STATIC_CONSTANT(unsigned long,
+                          alphabet_size = (1<<(sizeof(alphabet_type)*8))-1);
+};
+#endif
+
+
+
+// wrappers for common usage
+inline
+std::string::const_iterator
+boyer_moore(const std::string &text,
+            const std::string &pattern)
+{
+    std::vector<std::string::const_iterator> vec;
+    boost::boyer_moore_search<std::string::const_iterator,
+                              std::string::const_iterator,
+                              std::string::const_iterator>(text.begin(),
+                                                           text.end(),
+                                                           pattern.begin(),
+                                                           pattern.end(),
+                                                           std::back_inserter<>(vec));
+    return (vec.size() == 0)? text.end() : vec.front();
 }
 
-std::string::const_iterator boyer_moore(const char *text, const char *pattern)
+inline
+std::wstring::const_iterator
+boyer_moore(const std::wstring &text,
+            const std::wstring &pattern)
+{
+    std::vector<std::wstring::const_iterator> vec;
+    boost::boyer_moore_search<std::wstring::const_iterator,
+                              std::wstring::const_iterator,
+                              std::wstring::const_iterator>(text.begin(),
+                                                            text.end(),
+                                                            pattern.begin(),
+                                                            pattern.end(),
+                                                            std::back_inserter<>(vec));
+    return (vec.size() == 0)? text.end() : vec.front();
+}
+
+inline
+std::string::const_iterator
+boyer_moore(const std::string             &text,
+            const std::string::value_type *pattern)
+{
+    std::string p(pattern);
+    std::vector<std::string::const_iterator> vec;
+    boost::boyer_moore_search<std::string::const_iterator,
+                              std::string::const_iterator,
+                              std::string::const_iterator>(text.begin(),
+                                                           text.end(),
+                                                           p.begin(),
+                                                           p.end(),
+                                                           std::back_inserter<>(vec));
+    return (vec.size() == 0)? text.end() : vec.front();
+}
+
+inline
+std::wstring::const_iterator
+boyer_moore(const std::wstring             &text,
+            const std::wstring::value_type *pattern)
+{
+    std::wstring p(pattern);
+    std::vector<std::wstring::const_iterator> vec;
+    boost::boyer_moore_search<std::wstring::const_iterator,
+                              std::wstring::const_iterator,
+                              std::wstring::const_iterator>(text.begin(),
+                                                            text.end(),
+                                                            p.begin(),
+                                                            p.end(),
+                                                            std::back_inserter<>(vec));
+    return (vec.size() == 0)? text.end() : vec.front();
+}
+
+inline
+const std::string::value_type *
+boyer_moore(const std::string::value_type *text,
+            const std::string::value_type *pattern)
 {
     std::string t(text);
     std::string p(pattern);
-    return boost::boyer_moore_search<>(t.begin(), t.end(), p.begin(), p.end());
+    std::vector<std::ptrdiff_t> vec;
+    boost::boyer_moore_search<std::ptrdiff_t,
+                              std::string::const_iterator,
+                              std::string::const_iterator>(t.begin(),
+                                                           t.end(),
+                                                           p.begin(),
+                                                           p.end(),
+                                                           std::back_inserter<>(vec));
+    return (vec.size() == 0)?
+               text + std::char_traits<std::string::value_type>::length(text)
+             : text + vec.front();
 }
+
+inline
+const std::wstring::value_type *
+boyer_moore(const std::wstring::value_type *text,
+            const std::wstring::value_type *pattern)
+{
+    std::wstring t(text);
+    std::wstring p(pattern);
+    std::vector<std::ptrdiff_t> vec;
+    boost::boyer_moore_search<std::ptrdiff_t,
+                              std::wstring::const_iterator,
+                              std::wstring::const_iterator>(t.begin(),
+                                                            t.end(),
+                                                            p.begin(),
+                                                            p.end(),
+                                                            std::back_inserter<>(vec));
+    return (vec.size() == 0)?
+               text + std::char_traits<std::wstring::value_type>::length(text)
+             : text + vec.front();
+}
+
 
 int main(int, char *[])
 {
@@ -34,12 +156,78 @@ int main(int, char *[])
     std::string pattern("example");
 
     std::string::const_iterator it;
-    it = boost::boyer_moore_search<>(text.begin(), text.end(), pattern.begin(), pattern.end());
-    it = boyer_moore(text, "this");         assert(it != text.end());
-    it = boyer_moore(text, "is an ex");     assert(it != text.end());
-    it = boyer_moore(text, "find this");    assert(it == text.end());
-    it = boyer_moore(text, "fo");           assert(it != text.end());
-    it = boyer_moore(text, "is");           assert(it != text.end());
-    std::advance(it, 2);
-    it = boyer_moore(&(*it), "is");         assert(it != text.end());
+
+    std::vector<std::string::const_iterator> vec;
+    boost::boyer_moore_search<std::string::const_iterator,
+                              std::string::const_iterator,
+                              std::string::const_iterator>(text.begin(),
+                                                           text.end(),
+                                                           pattern.begin(),
+                                                           pattern.end(),
+                                                           std::back_inserter<>(vec));
+    assert(vec.size() > 0);
+    assert(*boyer_moore("this is an example foo bar", "example") != 0);
+    it = boyer_moore(text, pattern);                            assert(it != text.end());
+    it = boyer_moore(text, "this");                             assert(it != text.end());
+    it = boyer_moore(text, "is an ex");                         assert(it != text.end());
+    it = boyer_moore(text, "find this");                        assert(it == text.end());
+    it = boyer_moore(text, "fo");                               assert(it != text.end());
+
+    std::string is("is");
+    vec.clear();
+    boost::boyer_moore_search<std::string::const_iterator,
+                              std::string::const_iterator,
+                              std::string::const_iterator>(text.begin(),
+                                                           text.end(),
+                                                           is.begin(),
+                                                           is.end(),
+                                                           std::back_inserter<>(vec));
+    assert(vec.size() == 2);
+
+
+    text = "humpty dumpty sat on a wall, "
+           "humpty dumpty had a great fall, "
+           "all the kings horses and all the kings men, "
+           "couldn't put humpty together again.";
+    it = boyer_moore(text, "together again");                   assert(it != text.end());
+    it = boyer_moore(text, "Together");                         assert(it == text.end());
+    assert(text.find("together") != std::string::npos);
+
+
+#if defined(BOOST_MSVC)
+{
+    std::wstring wtext(L"this is an example foo bar");
+    std::wstring wpattern(L"example");
+
+    std::wstring::const_iterator wit;
+    assert(*boyer_moore(L"this is an example foo bar", L"example") != 0);
+
+    std::vector<std::ptrdiff_t> vec;
+    boost::boyer_moore_search<std::ptrdiff_t,
+                              std::wstring::const_iterator,
+                              std::wstring::const_iterator>(wtext.begin(),
+                                                            wtext.end(),
+                                                            wpattern.begin(),
+                                                            wpattern.end(),
+                                                            std::back_inserter<>(vec));
+    assert(vec.size() > 0);
+
+    wit = boyer_moore(wtext, wpattern);      assert(wit != wtext.end());
+    wit = boyer_moore(wtext, L"this");       assert(wit != wtext.end());
+    wit = boyer_moore(wtext, L"is an ex");   assert(wit != wtext.end());
+    wit = boyer_moore(wtext, L"find this");  assert(wit == wtext.end());
+    wit = boyer_moore(wtext, L"fo");         assert(wit != wtext.end());
+    wit = boyer_moore(wtext, L"is");         assert(wit != wtext.end());
+    std::advance(wit, 2);
+    wit = boyer_moore(&(*wit), L"is");       assert(wit != wtext.end());
+}
+#endif
+
+    std::cout << std::endl
+              << "Boyer Moore Search Test"
+              << std::endl
+              << "Finished."
+              << std::endl;
+
+    return 0;
 }
