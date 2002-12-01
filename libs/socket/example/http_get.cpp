49959d00f1cc4ccff2cbdb6de0f04928b390b661
Hugo Duncan <hugo.duncan@alcan.com>
2002-12-01 00:17:52 +0000

    added address_info class and http_get example
    SVN-Revision: 733

---
+++ libs/socket/example/http_get.cpp
@@ -0,0 +1,83 @@
+// Copyright (C) 2002 Hugo Duncan
+
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee,
+// provided that the above copyright notice appear in all copies and
+// that both that copyright notice and this permission notice appear
+// in supporting documentation.  Hugo Duncan makes no representations
+// about the suitability of this software for any purpose.
+// It is provided "as is" without express or implied warranty.
+
+#ifdef _MSC_VER
+#pragma warning (disable: 4786 4305)
+  // 4786 truncated debug symbolic name
+  // 4305 truncation from const double to float
+#endif
+
+#if defined(__BORLANDC__)
+#pragma hdrstop
+#endif
+
+#include "boost/socket/address_info.hpp"
+#include "boost/socket/connector_socket.hpp"
+#include "boost/socket/data_socket.hpp"
+#include "boost/socket/socketstream.hpp"
+#include "boost/socket/any_protocol.hpp"
+#include "boost/socket/any_address.hpp"
+
+#include <iostream>
+
+#ifdef _MSC_VER
+#pragma warning (push, 4)
+#pragma warning (disable: 4786 4305)
+#endif
+
+
+int main(int argc, char** argv)
+{
+  if (argc!=3)
+  {
+    std::cerr << "usage: http_get hostname page" << std::endl;
+    return 1;
+  }
+
+  boost::socket::address_info_list list(argv[1],"http");
+
+  boost::socket::address_info_list::iterator i=list.begin(), iend=list.end();
+  if (i==iend)
+  {
+    std::cerr << "host " << argv[1] << " not found\n";
+    return 2;
+  }
+  std::cout << std::distance(i,iend) << " addresses found\n";
+
+  boost::socket::connector<> connector;
+  boost::socket::connector<>::data_connection_t connection;
+
+  connector.connect(connection, i->protocol(), i->address());
+
+  boost::socket::basic_socket_stream<char> s(connection);
+
+  s << "GET /" << argv[2] << " HTTP/1.0\r\n\r\n";
+  s.flush();
+
+  std::string result;
+
+
+  s >> result;
+  while (!s.eof())
+  {
+    std::cout << result << " ";
+    s >> result;
+  }
+
+  if (connection.is_valid())
+    std::cerr << "\n\nData connection was not closed properly" << std::endl;
+
+  return 0;
+}
+
+
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ libs/socket/src/address_info.cpp
@@ -0,0 +1,110 @@
+// Copyright (C) 2002 Hugo Duncan
+
+// Permission to use, copy, modify, distribute and sell this software
+// and its documentation for any purpose is hereby granted without fee,
+// provided that the above copyright notice appear in all copies and
+// that both that copyright notice and this permission notice appear
+// in supporting documentation.  Hugo Duncan makes no representations
+// about the suitability of this software for any purpose.
+// It is provided "as is" without express or implied warranty.
+
+#ifdef _MSC_VER
+#pragma warning (disable: 4786 4305)
+  // 4786 truncated debug symbolic name
+  // 4305 truncation from const double to float
+#endif
+
+#if defined(__BORLANDC__)
+#pragma hdrstop
+#endif
+
+#include "boost/socket/address_info.hpp"
+#include "boost/socket/any_protocol.hpp"
+#include "boost/socket/any_address.hpp"
+
+#ifdef USES_WINSOCK2
+#include <Winsock2.h>
+#include <Ws2tcpip.h>
+#else
+#endif
+
+#ifdef _MSC_VER
+#pragma warning (push, 4)
+#pragma warning (disable: 4786 4305)
+#endif
+
+namespace boost
+{
+  namespace socket
+  {
+
+    any_protocol address_info::protocol() const
+    {
+      return any_protocol(m_addrinfo->ai_socktype,
+                          m_addrinfo->ai_protocol,
+                          m_addrinfo->ai_family);
+    }
+
+    any_address address_info::address() const
+    {
+      return any_address(m_addrinfo->ai_family,
+                         m_addrinfo->ai_addr,
+                         m_addrinfo->ai_addrlen);
+    }
+
+    const int address_info::flags() const
+    {
+      return m_addrinfo->ai_flags;
+    }
+
+    ::addrinfo const* address_info::next() const
+    {
+      return m_addrinfo->ai_next;
+    }
+
+    address_info_list::address_info_list(const char* name,
+                                         const char* service,
+                                         const int   flags,
+                                         const int   family,
+                                         const int   socktype,
+                                         const int   protocol)
+        : m_addrinfo(0)
+    {
+      get_addrinfo(name,service,flags,family,socktype,protocol);
+    }
+
+    address_info_list::~address_info_list()
+    {
+      ::freeaddrinfo(m_addrinfo);
+    }
+
+    void address_info_list::get_addrinfo(
+      const char* name,
+      const char* service,
+      const int   flags,
+      const int   family,
+      const int   socktype,
+      const int   protocol )
+    {
+      int result;
+      ::addrinfo hint;
+
+      memset(&hint, 0, sizeof(hint));
+      hint.ai_flags = flags;
+      hint.ai_family = family;
+      hint.ai_socktype = socktype;
+      hint.ai_protocol = protocol;
+
+      result = ::getaddrinfo(name, service, &hint, &m_addrinfo);
+      if (result != 0)
+      {
+        throw socket_exception("getaddrinfo",result);
+      }
+    }
+
+  }// namespace
+}// namespace
+
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
