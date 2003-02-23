fd12dec247f1b70b5a475f14d980fc85d8f77dd8
Douglas Gregor <doug.gregor@gmail.com>
2003-02-23 00:26:46 +0000

    - use result_of<> - don't do that crazy argument deduction stuff--keep it simple! - only include the nullary operator() when there is a result_type in T - generate for any number of arguments
    SVN-Revision: 1038

---
+++ boost/detail/ref_iterate.hpp
@@ -0,0 +1,13 @@
+#if !defined(BOOST_PP_IS_ITERATING)
+# error Boost.ref - do not include this file!
+#endif
+
+#if BOOST_PP_ITERATION() > 0
+
+template<BOOST_PP_ENUM_PARAMS(BOOST_PP_ITERATION(),typename T)>
+typename ::boost::result_of<T(BOOST_PP_ENUM_PARAMS(BOOST_PP_ITERATION(),T))>::type
+operator()(BOOST_PP_ENUM_BINARY_PARAMS(BOOST_PP_ITERATION(),T,& a)) const
+{
+  return get()(BOOST_PP_ENUM_PARAMS(BOOST_PP_ITERATION(),a));
+}
+#endif
