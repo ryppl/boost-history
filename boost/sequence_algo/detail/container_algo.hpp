af7c70bba2e91f4d18b18e2eccf2e5b329163f1f
Thorsten Jørgen Ottosen <thorsten.ottosen@gmail.com>
2003-03-02 00:33:27 +0000

    container_traits.hpp
    SVN-Revision: 1076

--- boost/sequence_algo/detail/container_algo.hpp
+++ boost/sequence_algo/detail/container_algo.hpp
@@ -1,284 +1,183 @@
+// (C) Copyright Thorsten Ottosen 2002-2003. Permission to copy, use, modify,
+// sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
+
+// (C) Copyright Jeremy Siek 2001. Permission to copy, use, modify,
+// sell and distribute this software is granted provided this
+// copyright notice appears in all copies. This software is provided
+// "as is" without express or implied warranty, and with no claim as
+// to its suitability for any purpose.
+
 
 #include "../algo_traits.hpp"
 
 namespace boost
 { 
 
-#if 1
-    //#ifdef  BOOST_NO_FUNCTION_TEMPLATE_ORDERING
-    // We need the tags because the compiler will not know how to
-    // choose between begin( pair<T1,T2>& ) and begin( Container& )...
-    // Instead we rely on full class specialization which is
-    // supported by most compilers
+    ///////////////////////////////////////////////////////////////////////////
+    // begin/end for stl containers
+    ///////////////////////////////////////////////////////////////////////////
+    
+//     namespace detail
+//     {
+// 	template <typename Container>
+// 	inline typename
+// 	container_traits<Container>::iterator
+// 	begin(Container& c, container_algo_container_tag const&)
+// 	{
+// 	    return c.begin();
+// 	}
+	
+// 	template <typename Container>
+// 	inline typename
+// 	container_traits<Container>::const_iterator
+// 	begin(const Container& c, container_algo_container_tag const&)
+// 	{
+// 	    return c.begin();
+// 	}
+	
+// 	template <typename Container>
+// 	inline typename
+// 	container_traits<Container>::iterator
+// 	end(Container& c, container_algo_container_tag const&)
+// 	{
+//             return c.end();
+//         }
 
-    namespace detail 
-    {
+//         template <typename Container>
+//         inline typename        
+//         container_traits<Container>::const_iterator
+//         end(const Container& c, container_algo_container_tag const&)
+//         {
+//             return c.end();
+//         }
 
-        
-        
-        // tag classes for dispatch 
-        class container_algo_pair_tag
-        {
-        };
-        class container_algo_container_tag
-        {
-        };
-        class container_algo_array_tag
-        {
-        };
-        class container_algo_iter_tag
-        {
-        };
+//         template <typename Container>
+//         inline typename
+//         container_traits<Container>::size_type
+//         size(const Container& c, container_algo_container_tag const&)
+//         {
+//             return c.size();
+//         }
 
-        template <typename Container>
-        struct container_algo_make_tag
-        {
-            typedef container_algo_container_tag       tag;
-            typedef typename Container::size_type      size_type;
-            typedef typename Container::iterator       result_type;
-            typedef typename Container::const_iterator const_result_type;
-        };
+//         // for pair_tag
+//         template <typename Iter1, typename Iter2>
+//         inline typename
+//         container_traits< std::pair<Iter1,Iter2> >::iterator
+//         begin(const std::pair<Iter1, Iter2>& p, container_algo_pair_tag const&)
+//         {
+//             return p.first;
+//         }
 
-        template <typename T>
-        struct container_algo_make_tag< std::pair<T,T> >
-        {
-            typedef container_algo_pair_tag            tag;
-            typedef typename iterator_traits<T>::difference_type size_type;
-            typedef T                                  result_type;
-            typedef T                                  const_result_type;
-        };
-        template <typename T>
-        struct container_algo_make_tag< const std::pair<T,T> >
-        : public container_algo_make_tag< std::pair<T,T> >
-        {
-        };
+//         template <typename Iter1, typename Iter2>
+//         inline typename
+//         container_traits< std::pair<Iter1, Iter2> >::const_iterator
+//         end(const std::pair<Iter1, Iter2>& p, container_algo_pair_tag const&)
+//         {
+//             return p.second;
+//         }
 
-        template< typename T, size_t sz >
-        struct container_algo_make_tag< T[sz] >
-        {
-            typedef container_algo_array_tag                  tag;
-//            typedef typename array_traits<T[sz]>::size_type   size_type;
-//            typedef typename array_traits<T[sz]>::iterator    result_type;
-            typedef typename algo_traits<T[sz]>::size_type   size_type; 
-            typedef typename algo_traits<T[sz]>::iterator     result_type;
-            typedef const result_type                         const_result_type; 
-        };
+//         template <typename Iter1, typename Iter2>
+//         inline typename
+//         container_traits< std::pair<Iter1,Iter2> >::size_type
+//         size(const std::pair<Iter1, Iter2>& p, container_algo_pair_tag const&)
+//         {
+//             return std::distance(p.first, p.second);
+//         }
 
-        template< typename Iterator >
-        struct container_algo_make_tag;
+// #ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
 
-        // for container_tag
-        template <typename Container>
-        inline typename
-        container_algo_make_tag<Container>::result_type
-        begin(Container& c, container_algo_container_tag const&)
-        {
-            return c.begin();
-        }
+//         // for array_tag
+//         template< typename T, std::size_t sz >
+//         inline typename 
+//         container_traits<T[sz]>::iterator
+//         begin( T (&t)[sz], const container_algo_array_tag& )
+//         {
+//             return t;
+//         }
 
-        template <typename Container>
-        inline typename
-        container_algo_make_tag<Container>::const_result_type
-        begin(const Container& c, container_algo_container_tag const&)
-        {
-            return c.begin();
-        }
+//         template< typename T, std::size_t sz >
+//         inline typename
+// 	container_traits<T[sz]>::const_iterator 
+//         begin( const T (&t)[sz], const container_algo_array_tag&  )
+//         {
+//             return t;
+//         }
 
-        template <typename Container>
-        inline typename
-        container_algo_make_tag<Container>::result_type
-        end(Container& c, container_algo_container_tag const&)
-        {
-            return c.end();
-        }
+//         template< typename T, std::size_t sz >
+//         inline typename
+//         container_traits<T[sz]>::iterator 
+//         end( T (&t)[sz], const container_algo_array_tag&  )
+//         {
+//             return t + sz;
+//         }
 
-        template <typename Container>
-        inline typename        
-        container_algo_make_tag<Container>::const_result_type
-        end(const Container& c, container_algo_container_tag const&)
-        {
-            return c.end();
-        }
+//         template< typename T, std::size_t sz >
+//         inline typename
+//         container_traits<T[sz]>::const_iterator 
+//         end( const T (&t)[sz], const container_algo_array_tag&  )
+//         {
+//             return t + sz;
+//         }
 
-        template <typename Container>
-        inline typename
-        container_algo_make_tag<Container>::size_type
-        size(const Container& c, container_algo_container_tag const&)
-        {
-            return c.size();
-        }
+//         template< typename T, std::size_t sz >
+//         inline typename
+//         container_traits<T[sz]>::size_type
+//         size( const T (&t)[sz], const container_algo_array_tag&  )
+//         {
+//             return sz;
+//         }
 
-        // for pair_tag
-        template <typename Iter1, typename Iter2>
-        inline typename
-        container_algo_make_tag< std::pair<Iter1,Iter2> >::result_type
-        begin(const std::pair<Iter1, Iter2>& p, container_algo_pair_tag const&)
-        {
-            return p.first;
-        }
+// #endif // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
 
-        template <typename Iter1, typename Iter2>
-        inline typename
-        container_algo_make_tag< std::pair<Iter1, Iter2> >::const_result_type
-        end(const std::pair<Iter1, Iter2>& p, container_algo_pair_tag const&)
-        {
-            return p.second;
-        }
+//     } // namespace detail
 
-        template <typename Iter1, typename Iter2>
-        inline typename
-        container_algo_make_tag< std::pair<Iter1,Iter2> >::size_type
-        size(const std::pair<Iter1, Iter2>& p, container_algo_pair_tag const&)
-        {
-            return std::distance(p.first, p.second);
-        }
+//     template <typename T>
+//     inline typename
+//     container_traits<T>::iterator
+//     begin(T& t)
+//     {
+//         return begin(t, typename 
+//                              container_traits<T>::tag()); 
+//     }
 
-        // for array_tag
-        template< typename T, size_t sz >
-        inline typename 
-        container_algo_make_tag<T[sz]>::result_type
-        begin( T t[sz] )
-        {
-            return begin( t );
-        }
+//     template <typename T>
+//     inline typename
+//     container_traits<T>::const_iterator
+//     begin(const T& t)
+//     {
+//         return begin(t, typename 
+//                              container_traits<T>::tag()); 
+//     }
 
-        template< typename T, size_t sz >
-        inline typename
-        container_algo_make_tag<T[sz]>::result_type 
-        begin( const T t[sz] )
-        {
-            return begin( t );
-        }
+//     template <typename T>
+//     inline typename
+//     container_traits<T>::iterator
+//     end(T& t)
+//     {
+//         return end(t, typename 
+//                            container_traits<T>::tag()); 
+//     }
 
-        template< typename T, size_t sz >
-        inline typename
-        container_algo_make_tag<T[sz]>::result_type 
-        end( T t[sz] )
-        {
-            return end( t );
-        }
+//     template <typename T>
+//     inline typename
+//     container_traits<T>::const_iterator
+//     end(const T& t)
+//     {
+//         return end(t, typename 
+//                            container_traits<T>::tag());
+//     }
 
-        template< typename T, size_t sz >
-        inline typename
-        container_algo_make_tag<T[sz]>::result_type 
-        end( const T t[sz] )
-        {
-            return end( t );
-        }
+//     template <typename T>
+//     inline typename
+//     container_traits<T>::size_type
+//     size(const T& t)
+//     {
+//         return size(t, typename 
+//                             container_traits<T>::tag()); 
+//     }
 
-        template< typename T, size_t sz >
-        inline typename
-        container_algo_make_tag<T[sz]>::size_type
-        size( const T t[sz] )
-        {
-            return size( t );
-        }
-
-    } // namespace detail
-
-    template <typename T>
-    inline typename
-    detail::container_algo_make_tag<T>::result_type
-    begin(T& t)
-    {
-        return detail::begin(t, typename 
-                             detail::container_algo_make_tag<T>::tag()); 
-    }
-
-    template <typename T>
-    inline typename
-    detail::container_algo_make_tag<T>::const_result_type
-    begin(const T& t)
-    {
-        return detail::begin(t, typename 
-                             detail::container_algo_make_tag<T>::tag()); 
-    }
-
-    template <typename T>
-    inline typename
-    detail::container_algo_make_tag<T>::result_type
-    end(T& t)
-    {
-        return detail::end(t, typename 
-                           detail::container_algo_make_tag<T>::tag()); 
-    }
-
-    template <typename T>
-    inline typename
-    detail::container_algo_make_tag<T>::const_result_type
-    end(const T& t)
-    {
-        return detail::end(t, typename 
-                           detail::container_algo_make_tag<T>::tag());
-    }
-
-    template <typename T>
-    inline typename
-    detail::container_algo_make_tag<T>::size_type
-    size(const T& t)
-    {
-        return detail::size(t, typename 
-                            detail::container_algo_make_tag<T>::tag()); 
-    }
-
-
-
-#else
-    // The compiler will handle it fine like this
-
-    template <typename Container>
-    typename Container::iterator
-    begin(Container& c)
-    {
-        return c.begin();
-    }
-
-    template <typename Container>
-    typename Container::const_iterator
-    begin(const Container& c)
-    {
-        return c.begin();
-    }
-
-    template <typename Container>
-    typename Container::iterator
-    end(Container& c)
-    {
-        return c.end();
-    }
-
-    template <typename Container>
-    typename Container::const_iterator
-    end(const Container& c)
-    {
-        return c.end();
-    }
-
-    template <typename Container>
-    typename Container::size_type
-    size(const Container& c)
-    {
-        return c.size();
-    }
-
-    template <typename Iter1, typename Iter2>
-    Iter1 begin(const std::pair<Iter1, Iter2>& p)
-    {
-        return p.first;
-    }
-
-    template <typename Iter1, typename Iter2>
-    Iter2 end(const std::pair<Iter1, Iter2>& p)
-    {
-        return p.second;
-    }
-
-    template <typename Iter1, typename Iter2>
-    typename boost::detail::iterator_traits<Iter1>::difference_type
-    size(const std::pair<Iter1, Iter2>& p)
-    {
-        return std::distance(p.first, p.second);
-    }
-#endif
 
 } // namespace 'boost'
