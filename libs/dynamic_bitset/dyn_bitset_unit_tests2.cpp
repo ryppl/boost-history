fdcc8a4c8fcbace7ac472ce51164b2a3e64fd5e3
Gennaro Prota <gennaro.prota@yahoo.com>
2003-04-24 15:22:41 +0000

    additional tests for >>= and <<=,  small fixes
    SVN-Revision: 1175

--- libs/dynamic_bitset/dyn_bitset_unit_tests2.cpp
+++ libs/dynamic_bitset/dyn_bitset_unit_tests2.cpp
@@ -6,20 +6,18 @@
 // purpose.
 
 
-#include <cmath> // for pow
-#include "boost/dynamic_bitset.hpp"
-
 #include "bitset_test.hpp"
+#include "boost/dynamic_bitset.hpp"
 #include "boost/config.hpp" // for BOOST_HAS_LONG_LONG
 
 template <typename Block>
 void run_test_cases()
 {
-  typedef bitset_test< boost::dynamic_bitset<Block> > Tests;
-
-  std::string long_string(101, '0');
-  for (std::size_t i = 0; i < long_string.size(); ++i)
-    long_string[i] = '0' + (i % 2);
+  typedef boost::dynamic_bitset<Block> bitset_type;
+  typedef bitset_test< bitset_type > Tests;
+  const int bits_per_block = bitset_type::bits_per_block;
+  
+  std::string long_string = get_long_string();
 
   //=====================================================================
   // Test operator&=
@@ -100,6 +98,19 @@ void run_test_cases()
     boost::dynamic_bitset<Block> b(std::string("1010"));
     Tests::shift_left_assignment(b, pos);
   }
+  {
+    // test with both multiple and
+    // non multiple of bits_per_block
+    const int how_many = 10;
+    for (int i = 1; i <= how_many; ++i) {
+        std::size_t     multiple = i * bits_per_block;
+        std::size_t non_multiple = multiple - 1;
+        boost::dynamic_bitset<Block> b(long_string);
+
+        Tests::shift_left_assignment(b, multiple);
+        Tests::shift_left_assignment(b, non_multiple);
+    }
+  }
   { // case pos == size()/2
     std::size_t pos = long_string.size() / 2;
     boost::dynamic_bitset<Block> b(long_string);
@@ -116,6 +127,20 @@ void run_test_cases()
     std::size_t pos = 0;
     boost::dynamic_bitset<Block> b(std::string("1010"));
     Tests::shift_right_assignment(b, pos);
+  }
+  {
+    // test with both multiple and
+    // non multiple of bits_per_block
+    const int how_many = 10;
+    for (int i = 1; i <= how_many; ++i) {
+        std::size_t     multiple = i * bits_per_block;
+        std::size_t non_multiple = multiple - 1;
+        boost::dynamic_bitset<Block> b(long_string);
+          
+        Tests::shift_right_assignment(b, multiple);
+        Tests::shift_right_assignment(b, non_multiple);
+    }
+      
   }
   { // case pos == size()/2
     std::size_t pos = long_string.size() / 2;
@@ -240,4 +265,3 @@ test_main(int argc, char*[])
 
   return EXIT_SUCCESS;
 }
-
