a2b671c0b8080536f1d2e9f71ae1fc7439381fd0
Toon Knapen <toon.knapen@gmail.com>
2003-06-02 10:07:47 +0000

    traits definition for matrices
    SVN-Revision: 1299

---
+++ boost/numeric/bindings/traits/matrix_traits.hpp
@@ -0,0 +1,108 @@
+// (-*- C++ -*- header)  
+
+/*
+ * 
+ * Copyright (c) 2002, 2003 Kresimir Fresl and Toon Knapen 
+ *
+ * Permission to copy, modify, use and distribute this software 
+ * for any non-commercial or commercial purpose is granted provided 
+ * that this license appear on all copies of the software source code.
+ *
+ * Authors assume no responsibility whatsoever for its use and makes 
+ * no guarantees about its quality, correctness or reliability.
+ *
+ * First author acknowledges the support of the Faculty of Civil 
+ * Engineering, University of Zagreb, Croatia.
+ *
+ */
+
+#ifndef BOOST_NUMERIC_BINDINGS_TRAITS_MATRIX_TRAITS_HPP
+#define BOOST_NUMERIC_BINDINGS_TRAITS_MATRIX_TRAITS_HPP
+
+namespace boost { namespace numeric { namespace bindings { namespace traits {
+
+  /// matrix_traits<> generic version: 
+  /// There is default implemented since there is no
+  /// reasonable default. Most matrix libraries
+  /// provide totally different functions.
+  template <typename M>
+  struct matrix_traits {
+    // typedefs:
+    //   value_type
+    //   pointer
+    //   matrix_structure 
+    //   ordering_type
+    // static functions:
+    //   pointer storage()
+    //   int storage_size()
+    //   int size1()
+    //   int size2()
+    //   int leading_dimension()  // not all matrix types 
+    // symmetric/hermitian typedefs:
+    //   uplo_type 
+  }; 
+
+  // matrix structure tags:
+  struct general_t {}; 
+  struct symmetric_t {}; 
+  struct symmetric_packed_t {}; 
+  struct hermitian_t {}; 
+  struct hermitian_packed_t {}; 
+  // triangular, banded etc. 
+  struct unknown_structure_t {}; 
+
+  // storage ordering tags: 
+  struct row_major_t {};
+  struct column_major_t {};
+
+  // upper/lower tags:
+  struct upper_t {};
+  struct lower_t {};
+
+
+  // free accessor functions
+  template <typename M>
+  inline
+  typename matrix_traits<M>::pointer matrix_storage (M& m) { 
+    return matrix_traits<M>::storage (m); 
+  }
+  
+  template <typename M>
+  inline
+  int matrix_size1 (M& m) { 
+    return matrix_traits<M>::size1 (m); 
+  }
+  
+  template <typename M>
+  inline
+  int matrix_size2 (M& m) { 
+    return matrix_traits<M>::size2 (m); 
+  }
+  
+  template <typename M>
+  inline
+  int matrix_storage_size (M& m) { 
+    return matrix_traits<M>::storage_size (m); 
+  }
+  
+  template <typename M>
+  inline
+  int matrix_stride1 (M& m) { 
+    return matrix_traits<M>::stride1 (m); 
+  }
+  
+  template <typename M>
+  inline
+  int matrix_stride2 (M& m) { 
+    return matrix_traits<M>::stride2 (m); 
+  }
+  
+  template <typename M>
+  inline
+  int leading_dimension (M& m) { 
+    return matrix_traits<M>::leading_dimension (m); 
+  }
+  
+}}}}  
+
+#endif // BOOST_NUMERIC_BINDINGS_TRAITS_MATRIX_TRAITS_HPP
