d9abe9f513527a120aa388b8782ea8ce9959e69b
Toon Knapen <toon.knapen@gmail.com>
2003-06-03 14:39:20 +0000

    complex types in C++ are now complex_f and complex_d
    SVN-Revision: 1312

--- boost/numeric/bindings/traits/type.hpp
+++ boost/numeric/bindings/traits/type.hpp
@@ -1,12 +1,15 @@
 #ifndef boost_numeric_bindings_type_hpp
 #define boost_numeric_bindings_type_hpp
 
+// This header provides typedefs to float complex and double complex.
+// This makes it possible to redefine the complex class being used.
+
 #include <complex>
 
 namespace boost { namespace numeric { namespace bindings { namespace traits {
 
-  typedef std::complex< float >  float_complex ;
-  typedef std::complex< double > double_complex ;
+  typedef std::complex< float >  complex_f ;
+  typedef std::complex< double > complex_d ; 
 
 }}}}
 
