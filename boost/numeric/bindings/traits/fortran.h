8263360607b824fa954409c1211e1cfa88ab9794
Toon Knapen <toon.knapen@gmail.com>
2003-06-04 10:11:28 +0000

    added COMO
    SVN-Revision: 1337

--- boost/numeric/bindings/traits/fortran.h
+++ boost/numeric/bindings/traits/fortran.h
@@ -2,14 +2,14 @@
 //  distribute this software is granted provided this copyright notice appears
 //  in all copies. This software is provided "as is" without express or implied
 //  warranty, and with no claim as to its suitability for any purpose.
-//  Copyright (C) 2002, 2003 Si-Lab b.v.b.a. and Toon Knapen 
+//  Copyright (C) 2002, 2003 Si-Lab b.v.b.a., Toon Knapen and Kresimir Fresl 
 
 #ifndef BOOST_NUMERIC_BINDINGS_TRAITS_FORTRAN_H
 #define BOOST_NUMERIC_BINDINGS_TRAITS_FORTRAN_H
 
 // First we need to know what the conventions for linking
 // C with Fortran is on this platform/toolset
-#if defined(__GNUC__) || defined(__ICC) || defined(__sgi)
+#if defined(__GNUC__) || defined(__ICC) || defined(__sgi) || defined(__COMO__)
 #define BIND_FORTRAN_LOWERCASE_UNDERSCORE
 #elif defined(__IBMCPP__)
 #define BIND_FORTRAN_LOWERCASE
