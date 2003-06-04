ecd661bbbf2afeb4b73bde5e80c66dbe5b4916a1
Toon Knapen <toon.knapen@gmail.com>
2003-06-04 08:50:21 +0000

    uses free functions since easier to use (less typedefs) and portable Also uses blas2_overloads now
    SVN-Revision: 1329

--- boost/numeric/bindings/blas/blas2.hpp
+++ boost/numeric/bindings/blas/blas2.hpp
@@ -2,27 +2,25 @@
 //  distribute this software is granted provided this copyright notice appears
 //  in all copies. This software is provided "as is" without express or implied
 //  warranty, and with no claim as to its suitability for any purpose.
-//  Copyright Toon Knapen and Kresimir Fresl
+//  Copyright Toon Knapen and Kresimir Fresl 2003
 
 #ifndef BOOST_BINDINGS_BLAS_BLAS2_HPP
 #define BOOST_BINDINGS_BLAS_BLAS2_HPP
 
-#include <cassert> 
-
-#include <boost/numeric/bindings/blas/blaspp.hpp>
-#include <boost/numeric/bindings/traits/traits.hpp>
+#include <boost/numeric/bindings/blas/blas2_overloads.hpp>
+#include <boost/numeric/bindings/traits/vector_traits.hpp>
+#include <boost/numeric/bindings/traits/matrix_traits.hpp>
 #include <boost/numeric/bindings/traits/transpose.hpp>
 #include <boost/numeric/ublas/traits.hpp>
 #include <boost/static_assert.hpp>
 #include <boost/type_traits.hpp>
+#include <cassert> 
 
 namespace boost { namespace numeric { namespace bindings { namespace blas {
 
   // y <- alpha * op (A) * x + beta * y
   // op (A) == A || A^T || A^H
-  template < 
-    typename matrix_type, typename vector_type_x, typename vector_type_y 
-    >
+  template < typename matrix_type, typename vector_type_x, typename vector_type_y >
   void gemv(const char TRANS, 
 	    const typename traits::matrix_traits<matrix_type>::value_type &alpha, 
 	    const matrix_type &a, 
@@ -35,39 +33,30 @@ namespace boost { namespace numeric { namespace bindings { namespace blas {
     typedef traits::vector_traits< const vector_type_x > xvtraits ; 
     typedef traits::vector_traits< vector_type_y > yvtraits ; 
     typedef typename mtraits::value_type value_type ;
-    typedef typename traits::value_traits< value_type >::value_type bind_type ;
 
     // precondition: matrix_type must be dense or dense_proxy
     BOOST_STATIC_ASSERT( ( boost::is_same< typename mtraits::matrix_structure,
                                            boost::numeric::bindings::traits::general_t
                            >::value ) ) ;
 
-    const int m = mtraits::size1( a ) ;
-    const int n = mtraits::size2( a ) ;
-    assert ( xvtraits::size( x ) >= (TRANS == traits::NO_TRANSPOSE ? n : m) ) ; 
-    assert ( yvtraits::size( y ) >= (TRANS == traits::NO_TRANSPOSE ? m : n) ) ; 
-    const int lda = mtraits::leading_dimension( a ) ; 
-    const int stride_x = xvtraits::stride( x ) ;
-    const int stride_y = yvtraits::stride( y ) ;
+    const int m = traits::matrix_size1( a ) ;
+    const int n = traits::matrix_size2( a ) ;
+    assert ( traits::vector_size( x ) >= (TRANS == traits::NO_TRANSPOSE ? n : m) ) ; 
+    assert ( traits::vector_size( y ) >= (TRANS == traits::NO_TRANSPOSE ? m : n) ) ; 
+    const int lda = traits::leading_dimension( a ) ; 
+    const int stride_x = traits::vector_stride( x ) ;
+    const int stride_y = traits::vector_stride( y ) ;
 
-    const value_type *a_ptr = mtraits::storage( a ) ;
-    const value_type *x_ptr = xvtraits::storage( x ) ;
-    value_type *y_ptr = yvtraits::storage( y ) ;
+    const value_type *a_ptr = traits::matrix_storage( a ) ;
+    const value_type *x_ptr = traits::vector_storage( x ) ;
+    value_type *y_ptr = traits::vector_storage( y ) ;
 
-    blas< value_type >::gemv( &TRANS, &m, &n, 
-			      (const bind_type*)&alpha, 
-			      (const bind_type*)a_ptr, &lda, 
-			      (const bind_type*)x_ptr, &stride_x, 
-			      (const bind_type*)&beta, 
-			      (bind_type*)y_ptr, &stride_y 
-			      );
+    detail::gemv( TRANS, m, n, alpha, a_ptr, lda, x_ptr, stride_x, beta, y_ptr, stride_y );
   }
 
 
   // y <- alpha * A * x + beta * y 
-  template < 
-    typename matrix_type, typename vector_type_x, typename vector_type_y 
-    >
+  template < typename matrix_type, typename vector_type_x, typename vector_type_y >
   inline 
   void gemv(const typename traits::matrix_traits<matrix_type>::value_type &alpha, 
 	    const matrix_type &a, 
@@ -81,9 +70,7 @@ namespace boost { namespace numeric { namespace bindings { namespace blas {
 
 
   // y <- A * x
-  template < 
-    typename matrix_type, typename vector_type_x, typename vector_type_y 
-    >
+  template < typename matrix_type, typename vector_type_x, typename vector_type_y >
   inline 
   void gemv(const matrix_type &a, const vector_type_x &x, vector_type_y &y)
   {
