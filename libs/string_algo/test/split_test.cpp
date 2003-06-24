01ee86c913ba0c488eee23c8a3a278c2af20ded7
Pavol Droba <droba@topmail.sk>
2003-06-24 13:47:38 +0000

    test fixed
    SVN-Revision: 1390

--- libs/string_algo/test/split_test.cpp
+++ libs/string_algo/test/split_test.cpp
@@ -46,6 +46,15 @@ void iterator_test()
     vector<string> tokens;
     vector< vector<int> > vtokens;
 
+    ifind_all(
+        tokens,
+        str2,
+        "xx" );
+
+    BOOST_REQUIRE( tokens.size()==2 );
+    BOOST_CHECK( tokens[0]==string("Xx") );
+    BOOST_CHECK( tokens[1]==string("xX") );
+
     // find_all tests
     find_all(
         tokens,
@@ -56,14 +65,6 @@ void iterator_test()
     BOOST_CHECK( tokens[0]==string("xx") );
     BOOST_CHECK( tokens[1]==string("xx") );
 
-    ifind_all(
-        tokens,
-        pch1,
-        "xx" );
-
-    BOOST_REQUIRE( tokens.size()==2 );
-    BOOST_CHECK( tokens[0]==string("xx") );
-    BOOST_CHECK( tokens[1]==string("xx") );
 
     find_all(
         vtokens,
