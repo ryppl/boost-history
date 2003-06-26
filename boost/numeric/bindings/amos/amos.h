34da5e7cc0012147c9181f8577681cde6f1b9686
Toon Knapen <toon.knapen@gmail.com>
2003-06-26 08:58:49 +0000

    adapted to new binding scheme as in blas and atlas
    SVN-Revision: 1394

--- boost/numeric/bindings/amos/amos.h
+++ boost/numeric/bindings/amos/amos.h
@@ -2,65 +2,28 @@
 //  distribute this software is granted provided this copyright notice appears
 //  in all copies. This software is provided "as is" without express or implied
 //  warranty, and with no claim as to its suitability for any purpose.
+//
 //  Copyright Toon Knapen
 
-#ifndef boost_numeric_bindings_amos_amos_h
-#define boost_numeric_bindings_amos_amos_h
+#ifndef BOOST_NUMERIC_BINDINGS_AMOS_AMOS_H
+#define BOOST_NUMERIC_BINDINGS_AMOS_AMOS_H
 
-#ifdef __cplusplus
-#ifndef boost_numeric_bindings_amos_amos_hpp
-#error "include amos.hpp instead"
-#endif
-#endif
-
-#include <boost/numeric/bindings/traits/fortran.h>
-
-#if defined(BIND_FORTRAN_LOWERCASE_UNDERSCORE)
-
-#define AMOS_DBESI dbesi_
-#define AMOS_CBESI cbesi_
-#define AMOS_ZBESI zbesi_
-
-#define AMOS_DBESJ dbesj_
-#define AMOS_CBESJ cbesj_
-#define AMOS_ZBESJ zbesj_
-
-#define AMOS_DBESY dbesy_
-#define AMOS_CBESY cbesy_
-#define AMOS_ZBESY zbesy_
-
-#elif defined( BIND_FORTRAN_LOWERCASE )
-
-#define AMOS_DBESI dbesi
-#define AMOS_CBESI cbesi
-#define AMOS_ZBESI zbesi
-
-#define AMOS_DBESJ dbesj
-#define AMOS_CBESJ cbesj
-#define AMOS_ZBESJ zbesj
-
-#define AMOS_DBESY dbesy
-#define AMOS_CBESY cbesy
-#define AMOS_ZBESY zbesy
-
-#else
-#error do not how to link with fortran
-#endif
-
+#include <boost/numeric/bindings/amos/amos_names.h>
 #include <boost/numeric/bindings/traits/type.h>
+
 extern "C"
 {
-  void AMOS_DBESI(const double*   z,                  const double* fnu, const int* kode, const int* n, double*   cy,             int* nz           ) ;
-  void AMOS_CBESI(const fcomplex* z,                  const float* fnu,  const int* kode, const int* n, fcomplex* cy,             int* nz, int* ierr) ;
+  void AMOS_DBESI(const double*     z,                const double* fnu, const int* kode, const int* n, double*     cy,           int* nz           ) ;
+  void AMOS_CBESI(const fcomplex_t* z,                const float*  fnu, const int* kode, const int* n, fcomplex_t* cy,           int* nz, int* ierr) ;
   void AMOS_ZBESI(const double* zr, const double* zi, const double* fnu, const int* kode, const int* n, double* cyr, double* cyi, int* nz, int* ierr) ;
 
-  void AMOS_DBESJ(const double  * z,                  const double* fnu,                  const int* n, double*   cy,             int* nz           ) ;
-  void AMOS_CBESJ(const fcomplex* z,                  const float* fnu,  const int* kode, const int* n, fcomplex* cy,             int* nz, int* ierr) ;
+  void AMOS_DBESJ(const double  * z,                  const double* fnu,                  const int* n, double*     cy,             int* nz           ) ;
+  void AMOS_CBESJ(const fcomplex_t* z,                const float*  fnu, const int* kode, const int* n, fcomplex_t* cy,             int* nz, int* ierr) ;
   void AMOS_ZBESJ(const double* zr, const double* zi, const double* fnu, const int* kode, const int* n, double* cyr, double* cyi, int* nz, int* ierr) ;
 
-  void AMOS_DBESY(const double  * z,                  const double* fnu,                  const int* n, double*   cy                                                              ) ;
-  void AMOS_CBESY(const fcomplex* z,                  const float* fnu,  const int* kode, const int* n, fcomplex* cy,             int* nz, fcomplex *cwrk,               int* ierr) ;
+  void AMOS_DBESY(const double  * z,                  const double* fnu,                  const int* n, double*     cy                                                            ) ;
+  void AMOS_CBESY(const fcomplex_t* z,                const float*  fnu, const int* kode, const int* n, fcomplex_t* cy,           int* nz, fcomplex_t *cwrk,             int* ierr) ;
   void AMOS_ZBESY(const double* zr, const double* zi, const double* fnu, const int* kode, const int* n, double* cyr, double* cyi, int* nz, double *cwrkr, double *cwrki, int* ierr) ;
 }
 
-#endif // boost_numeric_bindings_amos_amos_hpp
+#endif // BOOST_NUMERIC_BINDINGS_AMOS_AMOS_HPP
--- boost/numeric/bindings/amos/amos.hpp
+++ boost/numeric/bindings/amos/amos.hpp
@@ -4,154 +4,80 @@
 //  warranty, and with no claim as to its suitability for any purpose.
 //  Copyright Toon Knapen
 
-#ifndef boost_numeric_bindings_amos_amos_hpp
-#define boost_numeric_bindings_amos_amos_hpp
+#ifndef BOOST_NUMERIC_BINDINGS_AMOS_AMOS_HPP
+#define BOOST_NUMERIC_BINDINGS_AMOS_AMOS_HPP
 
-#ifndef __cplusplus
-#error "include directly amos_h"
-#endif
-
-#include <boost/numeric/bindings/amos/amos.h>
-#include <boost/numeric/bindings/traits/value_traits.hpp>
-#include <boost/numeric/bindings/traits/traits.hpp>
-#include <boost/numeric/bindings/traits/value_traits.hpp>
+#include <boost/numeric/bindings/amos/amos_overloads.hpp>
+#include <boost/numeric/bindings/traits/type_traits.hpp>
+#include <boost/static_assert.hpp>
 
 namespace boost { namespace numeric { namespace bindings { namespace amos {
 
-  namespace detail
+  template < typename vector_type, typename value_type >
+  int besi(const value_type&                                             z,   // std::complex< float > or std::complex< double >
+           const typename traits::type_traits< value_type >::real_type   fnu, // float or double
+           int                                                           kode, 
+           vector_type&                                                  cy, 
+           int&                                                          nz) 
   {
-    using namespace ::boost::numeric::bindings::traits ;
-    //
-    // [CZ]BESI
-    //
-    template < typename value_type > 
-    void besi(const typename value_traits< value_type >::value_type * z, 
-              const typename value_traits< value_type >::value_type * fnu, 
-              const int                                             * kode, 
-              const int                                             * n, 
-              typename value_traits< value_type >::value_type       * cy, 
-              int                                                   * nz, 
-              int                                                   * error)
-    { assert( 0 ) ; } // no bindings for this value_type
+#ifndef BOOST_NUMERIC_BINDINGS_POOR_MANS_TRAITS
+    BOOST_STATIC_ASSERT( ( boost::is_same< value_type, typename traits::vector_traits<vector_type>::value_type >::value ) ) ;
+#else
+    BOOST_STATIC_ASSERT( ( boost::is_same< value_type, typename vector_type::value_type >::value ) ) ;
+#endif
 
-    template <>
-    void besi< float >(const fcomplex * z, const fcomplex * fnu, const int * kode, const int * n, fcomplex* cy, int * nz, int * error) ;
+    int n = traits::vector_size( cy ) ;
+    value_type * cy_ptr = traits::vector_storage( cy ) ;
 
-    template <>
-    void besi< double >(const dcomplex * z, const dcomplex * fnu, const int * kode, const int * n, dcomplex* cy, int * nz, int * error) ;
-
-    template <>
-    void besi< std::complex< float > >(const fcomplex * z, const fcomplex * fnu, const int * kode, const int * n, fcomplex* cy, int * nz, int * error) ;
-
-    template <>
-    void besi< std::complex< double > >(const dcomplex * z, const dcomplex * fnu, const int * kode, const int * n, dcomplex* cy, int * nz, int * error) ;
-
-    //
-    // [CZ]BESJ
-    //
-    template < typename value_type > 
-    void besj(const typename value_traits< value_type >::value_type * z, 
-              const typename value_traits< value_type >::value_type * fnu, 
-              const int                                             * kode, 
-              const int                                             * n, 
-              typename value_traits< value_type >::value_type       * cy, 
-              int                                                   * nz, 
-              int                                                   * error)
-    { assert( 0 ) ; } // no bindings for this value_type
-
-    template <>
-    void besj< float >(const fcomplex * z, const fcomplex * fnu, const int * kode, const int * n, fcomplex* cy, int * nz, int * error) ;
-    
-    template <>
-    void besj< double >(const dcomplex * z, const dcomplex * fnu, const int * kode, const int * n, dcomplex* cy, int * nz, int * error) ;
-
-    template <>
-    void besj< std::complex< float > >(const fcomplex * z, const fcomplex * fnu, const int * kode, const int * n, fcomplex* cy, int * nz, int * error) ;
-    
-    template <>
-    void besj< std::complex< double > >(const dcomplex * z, const dcomplex * fnu, const int * kode, const int * n, dcomplex* cy, int * nz, int * error) ;
-
-
-    //
-    // [CZ]BESY
-    //
-    template < typename value_type > // actually bind_type
-    void besy(const typename value_traits< value_type >::value_type * z, 
-              const typename value_traits< value_type >::value_type * fnu, 
-              const int                                             * kode, 
-              const int                                             * n, 
-              typename value_traits< value_type >::value_type       * cy, 
-              int                                                   * nz, 
-              typename value_traits< value_type >::value_type       * wrk, 
-              int                                                   * error)
-    { assert( 0 ) ; } // no bindings for this value_type
-
-    template <>
-    void besy< float >(const float * z, const float * fnu, const int * kode, const int * n, float* cy, int * nz, float * wrk, int * error) ;
-    
-    template <>
-    void besy< double >(const double * z, const double * fnu, const int * kode, const int * n, double* cy, int * nz, double * wrk, int * error) ;
-
-    template <>
-    void besy< std::complex< float > >(const float * z, const float * fnu, const int * kode, const int * n, float* cy, int * nz, float * wrk, int * error) ;
-    
-    template <>
-    void besy< std::complex< double > >(const double * z, const double * fnu, const int * kode, const int * n, double* cy, int * nz, double * wrk, int * error) ;
-  }
-
-  template < typename vector_type >
-  int besi(const typename vector_type::value_type&                                             z,   // std::complex< float > or std::complex< double >
-           const typename traits::value_traits< typename vector_type::value_type >::value_type fnu, // float or double
-           int                                                                                 kode, 
-           vector_type&                                                                        cy, 
-           int&                                                                                nz) 
-  {
-    typedef typename vector_type::value_type                                        value_type ;
-    typedef typename traits::value_traits< value_type >::value_type                 bind_type ;
-    typedef typename boost::numeric::bindings::traits::vector_traits< vector_type > vector_traits ;
-    int n = vector_traits::size( cy ) ;
-    const value_type * cy_ptr = vector_traits::storage( cy ) ;
     int error = 0 ;
-    detail::besi< value_type >( (const bind_type*)&z, (const bind_type*)&fnu, &kode, &n, (bind_type*)cy_ptr, &nz, &error ) ;
+    detail::besi( z, fnu, kode, n, cy_ptr, nz, error ) ;
     return error ;
   }
 
-  template < typename vector_type >
-  int besj(const typename vector_type::value_type&                                              z, 
-           const typename traits::value_traits< typename vector_type::value_type >::value_type  fnu, 
-           int                                                                                  kode, 
-           vector_type&                                                                         cy, 
-           int&                                                                                 nz) 
+  template < typename vector_type, typename value_type >
+  int besj(const value_type&                                            z, 
+           const typename traits::type_traits< value_type >::real_type  fnu, 
+           int                                                          kode, 
+           vector_type&                                                 cy, 
+           int&                                                         nz) 
   {
-    typedef typename vector_type::value_type                           value_type ;
-    typedef typename traits::value_traits< value_type >::value_type    bind_type ;
-    typedef typename traits::vector_traits< vector_type >              vector_traits ;
-    int n = vector_traits::size( cy ) ;
-    const value_type * cy_ptr = vector_traits::storage( cy ) ;
+#ifndef BOOST_NUMERIC_BINDINGS_POOR_MANS_TRAITS
+    BOOST_STATIC_ASSERT( ( boost::is_same< value_type, typename traits::vector_traits<vector_type>::value_type >::value ) ) ;
+#else
+    BOOST_STATIC_ASSERT( ( boost::is_same< value_type, typename vector_type::value_type >::value ) ) ;
+#endif
+
+    int n = traits::vector_size( cy ) ;
+    value_type * cy_ptr = traits::vector_storage( cy ) ;
+
     int error = 0 ;
-    detail::besj< value_type >( (const bind_type*)&z, (const bind_type*)&fnu, &kode, &n, (bind_type*)cy_ptr, &nz, &error ) ;
+    detail::besj( z, fnu, kode, n, cy_ptr, nz, error ) ;
     return error ;
   }
 
-  template < typename vector_type >
-  int besy(const typename vector_type::value_type&                                             z, 
-           const typename traits::value_traits< typename vector_type::value_type >::value_type fnu, 
-           int                                                                                 kode, 
-           vector_type&                                                                        cy, 
-           int&                                                                                nz) 
+  template < typename vector_type, typename value_type >
+  int besy(const value_type&                                           z, 
+           const typename traits::type_traits< value_type >::real_type fnu, 
+           int                                                         kode, 
+           vector_type&                                                cy, 
+           int&                                                        nz) 
   {
-    typedef typename vector_type::value_type                          value_type ;
-    typedef typename traits::value_traits< value_type >::value_type   bind_type ;
-    typedef typename traits::vector_traits< vector_type >             vector_traits ;
-    int n = vector_traits::size( cy ) ;
-    const value_type * cy_ptr = vector_traits::storage( cy ) ;
+#ifndef BOOST_NUMERIC_BINDINGS_POOR_MANS_TRAITS
+    BOOST_STATIC_ASSERT( ( boost::is_same< value_type, typename traits::vector_traits<vector_type>::value_type >::value ) ) ;
+#else
+    BOOST_STATIC_ASSERT( ( boost::is_same< value_type, typename vector_type::value_type >::value ) ) ;
+#endif
+
+    int n = traits::vector_size( cy ) ;
+    const value_type * cy_ptr = traits::vector_storage( cy ) ;
+
+    int error = 0 ;
     value_type * cwrk = new value_type[n];
-    int error = 0 ;
-    detail::besy< value_type >( (const bind_type*)&z, (const bind_type*)&fnu, &kode, &n, (bind_type*)cy_ptr, &nz, cwrk, &error ) ;
+    detail::besy( z, fnu, kode, n, cy_ptr, nz, cwrk, error ) ;
     delete[] cwrk ;
     return error ;
   }
 
 }}}}
 
-#endif // boost_numeric_bindings_amos_amos_hpp
+#endif // BOOST_NUMERIC_BINDINGS_AMOS_AMOS_HPP
