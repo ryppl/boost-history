262f58fd3b68de330d21df09414c6cfcca4eb063
Kresimir Fresl <fresl@grad.hr>
2003-09-04 10:39:22 +0000

    vector_reference and c_vector added; cosmetic changes
    SVN-Revision: 1485

--- boost/numeric/bindings/traits/ublas_vector.hpp
+++ boost/numeric/bindings/traits/ublas_vector.hpp
@@ -1,17 +1,15 @@
-// (-*- C++ -*- header)  
-
 /*
  * 
- * Copyright (c) Kresimir Fresl 2002 
+ * Copyright (c) 2002, 2003 Kresimir Fresl, Toon Knapen and Karl Meerbergen
  *
  * Permission to copy, modify, use and distribute this software 
  * for any non-commercial or commercial purpose is granted provided 
  * that this license appear on all copies of the software source code.
  *
- * Author assumes no responsibility whatsoever for its use and makes 
+ * Authors assume no responsibility whatsoever for its use and makes 
  * no guarantees about its quality, correctness or reliability.
  *
- * Author acknowledges the support of the Faculty of Civil Engineering, 
+ * KF acknowledges the support of the Faculty of Civil Engineering, 
  * University of Zagreb, Croatia.
  *
  */
@@ -23,8 +21,10 @@
 
 #ifndef BOOST_NUMERIC_BINDINGS_POOR_MANS_TRAITS
 
-#include <boost/numeric/ublas/vector.hpp>
-#include <boost/numeric/bindings/traits/detail/ublas_vector_closure.hpp>
+#ifndef BOOST_UBLAS_HAVE_BINDINGS
+#  include <boost/numeric/ublas/vector.hpp>
+#endif 
+
 
 namespace boost { namespace numeric { namespace bindings { namespace traits {
 
@@ -33,43 +33,71 @@ namespace boost { namespace numeric { namespace bindings { namespace traits {
   struct vector_detail_traits< boost::numeric::ublas::vector<T, ArrT>, V > 
   : default_vector_traits< V, T > 
   {
-    BOOST_STATIC_ASSERT( (::boost::is_same< boost::numeric::ublas::vector<T, ArrT>, typename boost::remove_const<V>::type >::value) );
+#ifndef BOOST_NUMERIC_BINDINGS_NO_SANITY_CHECK
+    BOOST_STATIC_ASSERT( (boost::is_same< boost::numeric::ublas::vector<T, ArrT>, typename boost::remove_const<V>::type >::value) );
+#endif
 
+    typedef boost::numeric::ublas::vector<T, ArrT>          identifier_type; 
     typedef V                                               vector_type;
     typedef typename default_vector_traits< V, T >::pointer pointer;
-    typedef typename detail::generate_const<V,ArrT>::type   array_type ;
 
     static pointer storage (vector_type& v) {
+      typedef typename detail::generate_const<V,ArrT>::type array_type ;
       return vector_traits<array_type>::storage (v.data()); 
+    }
+  }; 
+
+  // ublas::vector_reference<>
+  template <typename V, typename VR>
+  struct vector_detail_traits< boost::numeric::ublas::vector_reference<V>, VR > 
+  : default_vector_traits< VR, typename V::value_type > 
+  {
+#ifndef BOOST_NUMERIC_BINDINGS_NO_SANITY_CHECK
+    BOOST_STATIC_ASSERT( (boost::is_same< boost::numeric::ublas::vector_reference<V>, typename boost::remove_const<VR>::type >::value) );
+#endif
+
+    typedef boost::numeric::ublas::vector_reference<V>             identifier_type; 
+    typedef VR                                                     vector_type;
+    typedef typename V::value_type                                 value_type ;
+    typedef typename default_vector_traits<VR,value_type>::pointer pointer; 
+
+  private:
+    typedef typename detail::generate_const<VR,V>::type vct_t;
+
+  public:
+    static pointer storage (vector_type& v) {
+      return vector_traits<vct_t>::storage (v.expression()); 
+    }
+    static int stride (vector_type& v) {
+      return vector_traits<vct_t>::stride (v.expression()); 
     }
   }; 
 
   // ublas::vector_range<>
   template <typename V, typename VR>
-  struct vector_detail_traits<boost::numeric::ublas::vector_range<V>, VR > 
+  struct vector_detail_traits< boost::numeric::ublas::vector_range<V>, VR > 
   : default_vector_traits< VR, typename V::value_type > 
   {
-    BOOST_STATIC_ASSERT( (::boost::is_same< boost::numeric::ublas::vector_range<V>, typename boost::remove_const<VR>::type >::value) );
+#ifndef BOOST_NUMERIC_BINDINGS_NO_SANITY_CHECK
+    BOOST_STATIC_ASSERT( (boost::is_same< boost::numeric::ublas::vector_range<V>, typename boost::remove_const<VR>::type >::value) );
+#endif
 
+    typedef boost::numeric::ublas::vector_range<V>                 identifier_type; 
+    typedef VR                                                     vector_type;
     typedef typename V::value_type                                 value_type ;
-    typedef boost::numeric::ublas::vector_range<V>                 vector_type                ; 
-    typedef VR                                                     c_vector_type; 
     typedef typename default_vector_traits<VR,value_type>::pointer pointer; 
-    typedef typename detail::generate_const<VR,V>::type                    v_type; 
 
   private:
-    typedef typename c_vector_type::vector_closure_type                          closure_t; 
-    typedef detail::closure_traits<typename detail::generate_const<VR,closure_t>::type > cl_traits; 
+    typedef typename detail::generate_const<VR, typename VR::vector_closure_type>::type v_type; 
 
   public:
-    static pointer storage (c_vector_type& vr) {
-      typename cl_traits::type& vt = cl_traits::get (vr.data()); 
-      pointer ptr = vector_traits<v_type>::storage (vt); 
-      ptr += vr.start() * vector_traits<v_type>::stride (vt);
+    static pointer storage (vector_type& vr) {
+      pointer ptr = vector_traits<v_type>::storage (vr.data()); 
+      ptr += vr.start() * vector_traits<v_type>::stride (vr.data());
       return ptr; 
     }
-    static int stride (c_vector_type& vr) {
-      return vector_traits<v_type>::stride (cl_traits::get (vr.data())); 
+    static int stride (vector_type& vr) {
+      return vector_traits<v_type>::stride (vr.data()); 
     }
   }; 
 
@@ -79,31 +107,50 @@ namespace boost { namespace numeric { namespace bindings { namespace traits {
   struct vector_detail_traits<boost::numeric::ublas::vector_slice<V>, VS > 
   : default_vector_traits< VS, typename V::value_type > 
   {
-    BOOST_STATIC_ASSERT( (::boost::is_same< boost::numeric::ublas::vector_slice<V>, typename boost::remove_const<VS>::type >::value) );
+#ifndef BOOST_NUMERIC_BINDINGS_NO_SANITY_CHECK
+    BOOST_STATIC_ASSERT( (boost::is_same< boost::numeric::ublas::vector_slice<V>, typename boost::remove_const<VS>::type >::value) );
+#endif
 
+    typedef boost::numeric::ublas::vector_slice<V>                 identifier_type; 
+    typedef VS                                                     vector_type;
     typedef typename V::value_type                                 value_type ;
-    typedef boost::numeric::ublas::vector_slice<V>                 vector_type                ; 
-    typedef VS                                                     c_vector_type; 
     typedef typename default_vector_traits<VS,value_type>::pointer pointer; 
-    typedef typename detail::generate_const<VS,V>::type                    v_type; 
 
   private:
-    typedef typename c_vector_type::vector_closure_type                          closure_t; 
-    typedef detail::closure_traits<typename detail::generate_const<VS,closure_t>::type > cl_traits; 
-    typedef typename V::value_type value_type ;
+    typedef typename detail::generate_const<VS, typename VS::vector_closure_type>::type v_type; 
 
   public:
-    static pointer storage (c_vector_type& vs) {
-      typename cl_traits::type& vt = cl_traits::get (vs.data()); 
-      pointer ptr = vector_traits<v_type>::storage (vt); 
-      ptr += vs.start() * vector_traits<v_type>::stride (vt);
+    static pointer storage (vector_type& vs) {
+      pointer ptr = vector_traits<v_type>::storage (vs.data()); 
+      ptr += vs.start() * vector_traits<v_type>::stride (vs.data());
       return ptr; 
     }
-    static int stride (c_vector_type& vs) {
-      return vs.stride() 
-	* vector_traits<v_type>::stride (cl_traits::get (vs.data())); 
+    static int stride (vector_type& vs) {
+      return vs.stride() * vector_traits<v_type>::stride (vs.data()); 
     }
   }; 
+
+
+#ifndef BOOST_NUMERIC_BINDINGS_FORTRAN 
+  
+  // (undocumented) ublas::c_vector<>
+  template <typename T, std::size_t N, typename V>
+  struct vector_detail_traits< boost::numeric::ublas::c_vector<T, N>, V > 
+  : default_vector_traits< V, T > 
+  {
+#ifndef BOOST_NUMERIC_BINDINGS_NO_SANITY_CHECK
+    BOOST_STATIC_ASSERT( (boost::is_same< boost::numeric::ublas::c_vector<T,N>, typename boost::remove_const<V>::type >::value) );
+#endif
+
+    typedef boost::numeric::ublas::c_vector<T,N>         identifier_type; 
+    typedef V                                            vector_type;
+    typedef typename default_vector_traits<V,T>::pointer pointer;
+
+    static pointer storage (vector_type& v) { return v.data(); }
+  }; 
+
+#endif // BOOST_NUMERIC_BINDINGS_FORTRAN 
+
 
 }}}}  
 
