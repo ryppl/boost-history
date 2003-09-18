7420ab840aed785b92faab0ef1ce14c04b4e4dc1
Douglas Gregor <doug.gregor@gmail.com>
2003-09-18 19:57:52 +0000

    tribool.hpp: 	- Fix operator==, operator!= to have the semantics given in the 	  truth tables 	- Hide indeterminate_t in the detail namespace 	- Doxygenate the source, so we can generate reference           documentation from it via BoostBook.
    SVN-Revision: 1572

--- boost/tribool.hpp
+++ boost/tribool.hpp
@@ -10,75 +10,63 @@
 //
 // This software is provided "as is" without express or implied warranty,
 // and with no claim as to its suitability for any purpose.
- 
-// For more information, see http://www.boost.org
 
+// For more information, see http://www.boost.org
 #ifndef BOOST_TRIBOOL_HPP
 #define BOOST_TRIBOOL_HPP
 
 namespace boost {
 
+/// INTERNAL ONLY
+namespace detail {
 /**
- * A type used only to uniquely identify the 'indeterminate' 
+ * INTERNAL ONLY
+ *
+ * \brief A type used only to uniquely identify the 'indeterminate'
  * function/keyword.
  */
 struct indeterminate_t {};
+}
 
 // Forward declaration of tribool class
 class tribool;
 
 /**
+ * INTERNAL ONLY
  * The type of the 'indeterminate' keyword. This has the same type as the
  * function 'indeterminate' so that we can recognize when the keyword is
  * used.
  */
-typedef bool (*indeterminate_keyword_t)(tribool, indeterminate_t);
-
-// Declaration for the indeterminate function
-inline bool 
-indeterminate(tribool x, 
-              indeterminate_t dummy = indeterminate_t());
-
-// Equality comparisons among tribools and bools ----------------------
-inline tribool operator==(tribool x, bool y);
-inline tribool operator==(bool x, tribool y);
-inline tribool operator==(indeterminate_keyword_t, tribool x);
-inline tribool operator==(tribool x, indeterminate_keyword_t);
-inline tribool operator==(tribool x, tribool y);
-// End Equality comparisons among tribools and bools ------------------
-
-// Inequality comparisons among tribools and bools --------------------
-inline tribool operator!=(tribool x, bool y);
-inline tribool operator!=(bool x, tribool y);
-inline tribool operator!=(indeterminate_keyword_t, tribool x);
-inline tribool operator!=(tribool x, indeterminate_keyword_t);
-inline tribool operator!=(tribool x, tribool y);
-// End Inequality comparisons among tribools and bools ----------------
-
-// Logical AND operations among tribools and bools --------------------
-inline tribool operator&&(tribool x, bool y);
-inline tribool operator&&(bool x, tribool y);
-inline tribool operator&&(indeterminate_keyword_t, tribool x);
-inline tribool operator&&(tribool x, indeterminate_keyword_t);
-inline tribool operator&&(tribool x, tribool y);
-// End Logical AND operations among tribools and bools ----------------
-
-
-// Logical OR operations among tribools and bools ---------------------
-inline tribool operator||(tribool x, bool y);
-inline tribool operator||(bool x, tribool y);
-inline tribool operator||(indeterminate_keyword_t, tribool x);
-inline tribool operator||(tribool x, indeterminate_keyword_t);
-inline tribool operator||(tribool x, tribool y);
-// End Logical OR operations among tribools and bools ------------------
+typedef bool (*indeterminate_keyword_t)(tribool, detail::indeterminate_t);
 
 /**
- * A 3-state boolean type. 3-state boolean values are either true, false, or
+ * \brief Keyword and test function for the indeterminate tribool value
+ *
+ * The \c indeterminate function has a dual role. It's first role is
+ * as a unary function that tells whether the tribool value is in the
+ * "indeterminate" state. It's second role is as a keyword
+ * representing the indeterminate (just like "true" and "false"
+ * represent the true and false states). If you do not like the name
+ * "indeterminate", and would prefer to use a different name, see the
+ * macro BOOST_TRIBOOL_THIRD_STATE.
+ *
+ * \returns @p x.value == tribool::indeterminate_value
+ * \throws nothrow
+ */
+inline bool
+indeterminate(tribool x,
+              detail::indeterminate_t dummy = detail::indeterminate_t());
+
+/**
+ * \brief A 3-state boolean type.
+ *
+ * 3-state boolean values are either true, false, or
  * indeterminate.
  */
 class tribool
 {
 private:
+  /// INTERNAL ONLY
   struct dummy {
     void nonnull() {};
   };
@@ -89,22 +77,22 @@ public:
   /**
    * Construct a new 3-state boolean value with the value 'false'.
    *
-   * \throws nothing
+   * \throws nothrow
    */
   tribool() : value(false_value) {}
 
   /**
-   * Construct a new 3-state boolean value with the given boolean value.
+   * Construct a new 3-state boolean value with the given boolean
+   * value, which may be \c true or \c false.
    *
-   * \param value the value of the 3-state boolean
-   * \throws nothing
+   * \throws nothrow
    */
   tribool(bool value) : value(value? true_value : false_value) {}
 
   /**
    * Construct a new 3-state boolean value with an indeterminate value.
    *
-   * \throws nothing
+   * \throws nothrow
    */
   tribool(indeterminate_keyword_t) : value(indeterminate_value) {}
 
@@ -113,24 +101,11 @@ public:
    * boolean context only when the 3-state boolean is definitely true.
    *
    * \returns true if the 3-state boolean is true, false otherwise
-   * \throws nothing
+   * \throws nothrow
    */
   operator safe_bool() const
   {
     return value == true_value? &dummy::nonnull : 0;
-  }
-
-  /**
-   * Return the logical negation of the given 3-state boolean. 
-   *
-   * \returns the logical negation of the 3-state boolean value
-   * \throws nothing
-   */
-  tribool operator!() const
-  {
-    return value == false_value? tribool(true)
-          :value == true_value? tribool(false)
-          :tribool(indeterminate);
   }
 
   /**
@@ -142,95 +117,78 @@ public:
 
 // Check if the given tribool has an indeterminate value. Also doubles as a
 // keyword for the 'indeterminate' value
-inline bool indeterminate(tribool x, indeterminate_t)
+inline bool indeterminate(tribool x, detail::indeterminate_t)
 {
   return x.value == tribool::indeterminate_value;
 }
 
-// Equality comparisons among tribools and bools ----------------------
-inline tribool operator==(tribool x, bool y)
+/**
+ * \brief Computes the logical negation of a tribool
+ *
+ * \returns the logical negation of the tribool, according to the
+ * table:
+ *  <table border=1>
+ *    <tr>
+ *      <th><center><code>!</code></center></th>
+ *      <th/>
+ *    </tr>
+ *    <tr>
+ *      <th><center>false</center></th>
+ *      <td><center>true</center></td>
+ *    </tr>
+ *    <tr>
+ *      <th><center>true</center></th>
+ *      <td><center>false</center></td>
+ *    </tr>
+ *    <tr>
+ *      <th><center>indeterminate</center></th>
+ *      <td><center>indeterminate</center></td>
+ *    </tr>
+ *  </table>
+ * \throws nothrow
+ */
+inline tribool operator!(tribool x)
 {
-  return x && y || !x && !y;
+  return x.value == tribool::false_value? tribool(true)
+        :x.value == tribool::true_value? tribool(false)
+        :tribool(indeterminate);
 }
 
-inline tribool operator==(bool x, tribool y)
-{
-  return x && y || !x && !y;
-}
-   
-inline tribool operator==(indeterminate_keyword_t, tribool x)
-{
-  return indeterminate(x);
-}
-        
-inline tribool operator==(tribool x, indeterminate_keyword_t)
-{
-  return indeterminate(x);
-}
-    
-inline tribool operator==(tribool x, tribool y)
-{
-  if (x == indeterminate || y == indeterminate)
-    return indeterminate;
-  else
-    return x && y || !x && !y;
-}
-// End Equality comparisons among tribools and bools ------------------
-
-// Inequality comparisons among tribools and bools --------------------
-inline tribool operator!=(tribool x, bool y)
-{
-  return x && !y || !x && y;
-}
-               
-inline tribool operator!=(bool x, tribool y)
-{
-  return x && !y || !x && y;
-}
- 
-inline tribool operator!=(indeterminate_keyword_t, tribool x)
-{
-  return !indeterminate(x);
-}
-
-inline tribool operator!=(tribool x, indeterminate_keyword_t)
-{
-  return !indeterminate(x);
-}
-
-inline tribool operator!=(tribool x, tribool y)
-{
-  if (x == indeterminate || y == indeterminate)
-    return indeterminate;
-  else
-    return !(x && y || !x && !y);
-}
-// End Inequality comparisons among tribools and bools ----------------
-
-// Logical AND operations among tribools and bools --------------------
-inline tribool operator&&(tribool x, bool y)
-{
-  return y? x : tribool(false);
-}
-
-inline tribool operator&&(bool x, tribool y)
-{
-  return x? y : tribool(false);
-}
-
-inline tribool operator&&(indeterminate_keyword_t, tribool x)
-{
-  return !x? tribool(false) : tribool(indeterminate);
-}
-
-inline tribool operator&&(tribool x, indeterminate_keyword_t)
-{
-  return !x? tribool(false) : tribool(indeterminate);
-}
-
+/**
+ * \brief Computes the logical conjuction of two tribools
+ *
+ * \returns the result of logically ANDing the two tribool values,
+ * according to the following table:
+ *       <table border=1>
+ *           <tr>
+ *             <th><center><code>&amp;&amp;</code></center></th>
+ *             <th><center>false</center></th>
+ *             <th><center>true</center></th>
+ *             <th><center>indeterminate</center></th>
+ *           </tr>
+ *           <tr>
+ *             <th><center>false</center></th>
+ *             <td><center>false</center></td>
+ *             <td><center>false</center></td>
+ *             <td><center>false</center></td>
+ *           </tr>
+ *           <tr>
+ *             <th><center>true</center></th>
+ *             <td><center>false</center></td>
+ *             <td><center>true</center></td>
+ *             <td><center>indeterminate</center></td>
+ *           </tr>
+ *           <tr>
+ *             <th><center>indeterminate</center></th>
+ *             <td><center>false</center></td>
+ *             <td><center>indeterminate</center></td>
+ *             <td><center>indeterminate</center></td>
+ *           </tr>
+ *       </table>
+ * \throws nothrow
+ */
 inline tribool operator&&(tribool x, tribool y)
 {
-  
   if (static_cast<bool>(!x) || static_cast<bool>(!y))
     return false;
   else if (static_cast<bool>(x) && static_cast<bool>(y))
@@ -238,30 +196,64 @@ inline tribool operator&&(tribool x, tribool y)
   else
     return indeterminate;
 }
-// End Logical AND operations among tribools and bools ----------------
 
+/**
+ * \overload
+ */
+inline tribool operator&&(tribool x, bool y)
+{ return y? x : tribool(false); }
 
-// Logical OR operations among tribools and bools ---------------------
-inline tribool operator||(tribool x, bool y)
-{
-  return y? tribool(true) : x;
-}
+/**
+ * \overload
+ */
+inline tribool operator&&(bool x, tribool y)
+{ return x? y : tribool(false); }
 
-inline tribool operator||(bool x, tribool y)
-{
-  return x? tribool(true) : y;
-}
+/**
+ * \overload
+ */
+inline tribool operator&&(indeterminate_keyword_t, tribool x)
+{ return !x? tribool(false) : tribool(indeterminate); }
 
-inline tribool operator||(indeterminate_keyword_t, tribool x)
-{
-  return x? tribool(true) : tribool(indeterminate);
-}
+/**
+ * \overload
+ */
+inline tribool operator&&(tribool x, indeterminate_keyword_t)
+{ return !x? tribool(false) : tribool(indeterminate); }
 
-inline tribool operator||(tribool x, indeterminate_keyword_t)
-{
-  return x? tribool(true) : tribool(indeterminate);
-}
-
+/**
+ * \brief Computes the logical disjunction of two tribools
+ *
+ * \returns the result of logically ORing the two tribool values,
+ * according to the following table:
+ *       <table border=1>
+ *           <tr>
+ *             <th><center><code>||</code></center></th>
+ *             <th><center>false</center></th>
+ *             <th><center>true</center></th>
+ *             <th><center>indeterminate</center></th>
+ *           </tr>
+ *           <tr>
+ *             <th><center>false</center></th>
+ *             <td><center>false</center></td>
+ *             <td><center>true</center></td>
+ *             <td><center>indeterminate</center></td>
+ *           </tr>
+ *           <tr>
+ *             <th><center>true</center></th>
+ *             <td><center>true</center></td>
+ *             <td><center>true</center></td>
+ *             <td><center>true</center></td>
+ *           </tr>
+ *           <tr>
+ *             <th><center>indeterminate</center></th>
+ *             <td><center>indeterminate</center></td>
+ *             <td><center>true</center></td>
+ *             <td><center>indeterminate</center></td>
+ *           </tr>
+ *       </table>
+ *  \throws nothrow
+ */
 inline tribool operator||(tribool x, tribool y)
 {
   if (static_cast<bool>(!x) && static_cast<bool>(!y))
@@ -271,17 +263,173 @@ inline tribool operator||(tribool x, tribool y)
   else
     return indeterminate;
 }
-// End Logical OR operations among tribools and bools ------------------
+
+/**
+ * \overload
+ */
+inline tribool operator||(tribool x, bool y)
+{ return y? tribool(true) : x; }
+
+/**
+ * \overload
+ */
+inline tribool operator||(bool x, tribool y)
+{ return x? tribool(true) : y; }
+
+/**
+ * \overload
+ */
+inline tribool operator||(indeterminate_keyword_t, tribool x)
+{ return x? tribool(true) : tribool(indeterminate); }
+
+/**
+ * \overload
+ */
+inline tribool operator||(tribool x, indeterminate_keyword_t)
+{ return x? tribool(true) : tribool(indeterminate); }
+
+
+/**
+ * \brief Compare tribools for equality
+ *
+ * \returns the result of comparing two tribool values, according to
+ * the following table:
+ *       <table border=1>
+ *          <tr>
+ *            <th><center><code>==</code></center></th>
+ *            <th><center>false</center></th>
+ *            <th><center>true</center></th>
+ *            <th><center>indeterminate</center></th>
+ *          </tr>
+ *          <tr>
+ *            <th><center>false</center></th>
+ *            <td><center>true</center></td>
+ *            <td><center>false</center></td>
+ *            <td><center>indeterminate</center></td>
+ *          </tr>
+ *          <tr>
+ *            <th><center>true</center></th>
+ *            <td><center>false</center></td>
+ *            <td><center>true</center></td>
+ *            <td><center>indeterminate</center></td>
+ *          </tr>
+ *          <tr>
+ *            <th><center>indeterminate</center></th>
+ *            <td><center>indeterminate</center></td>
+ *            <td><center>indeterminate</center></td>
+ *            <td><center>indeterminate</center></td>
+ *          </tr>
+ *      </table>
+ * \throws nothrow
+ */
+inline tribool operator==(tribool x, tribool y)
+{
+  if (indeterminate(x) || indeterminate(y))
+    return indeterminate;
+  else
+    return x && y || !x && !y;
+}
+
+/**
+ * \overload
+ */
+inline tribool operator==(tribool x, bool y) { return x == tribool(y); }
+
+/**
+ * \overload
+ */
+inline tribool operator==(bool x, tribool y) { return tribool(x) == y; }
+
+/**
+ * \overload
+ */
+inline tribool operator==(indeterminate_keyword_t, tribool x)
+{ return tribool(indeterminate) == x; }
+
+/**
+ * \overload
+ */
+inline tribool operator==(tribool x, indeterminate_keyword_t)
+{ return tribool(indeterminate) == x; }
+
+/**
+ * \brief Compare tribools for inequality
+ *
+ * \returns the result of comparing two tribool values for inequality,
+ * according to the following table:
+ *       <table border=1>
+ *           <tr>
+ *             <th><center><code>!=</code></center></th>
+ *             <th><center>false</center></th>
+ *             <th><center>true</center></th>
+ *             <th><center>indeterminate</center></th>
+ *           </tr>
+ *           <tr>
+ *             <th><center>false</center></th>
+ *             <td><center>false</center></td>
+ *             <td><center>true</center></td>
+ *             <td><center>indeterminate</center></td>
+ *           </tr>
+ *           <tr>
+ *             <th><center>true</center></th>
+ *             <td><center>true</center></td>
+ *             <td><center>false</center></td>
+ *             <td><center>indeterminate</center></td>
+ *           </tr>
+ *           <tr>
+ *             <th><center>indeterminate</center></th>
+ *             <td><center>indeterminate</center></td>
+ *             <td><center>indeterminate</center></td>
+ *             <td><center>indeterminate</center></td>
+ *           </tr>
+ *       </table>
+ * \throws nothrow
+ */
+inline tribool operator!=(tribool x, tribool y)
+{
+  if (indeterminate(x) || indeterminate(y))
+    return indeterminate;
+  else
+    return !(x && y || !x && !y);
+}
+
+/**
+ * \overload
+ */
+inline tribool operator!=(tribool x, bool y) { return x != tribool(y); }
+
+/**
+ * \overload
+ */
+inline tribool operator!=(bool x, tribool y) { return tribool(x) != y; }
+
+/**
+ * \overload
+ */
+inline tribool operator!=(indeterminate_keyword_t, tribool x)
+{ return tribool(indeterminate) != x; }
+
+/**
+ * \overload
+ */
+inline tribool operator!=(tribool x, indeterminate_keyword_t)
+{ return x != tribool(indeterminate); }
 
 } // end namespace boost
 
-#define BOOST_TRIBOOL_THIRD_STATE(Name)					\
-namespace boost {							\
-inline bool Name(tribool x, indeterminate_t dummy = indeterminate_t())	\
-{									\
-  return x.value == tribool::indeterminate_value;			\
-}									\
-} // end namespace boost
+/**
+ * \brief Declare a new name for the third state of a tribool
+ *
+ * Use this macro to declare a new name for the third state of a
+ * tribool. This state can have any number of new names (in addition
+ * to \c indeterminate), all of which will be equivalent.
+ */
+#define BOOST_TRIBOOL_THIRD_STATE(Name)                         \
+inline bool                                                     \
+Name(boost::tribool x,                                          \
+     boost::detail::indeterminate_t dummy =                     \
+       boost::detail::indeterminate_t())                        \
+{ return x.value == boost::tribool::indeterminate_value; }
 
 #endif // BOOST_TRIBOOL_HPP
 
