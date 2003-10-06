93d35579724069472e9c3f3add6b079e93df40fe
Bruno da Silva de Oliveira (bounce) <nicodemus@esss.com.br>
2003-10-06 19:10:51 +0000

    This commit was manufactured by cvs2svn to create tag 'version_0-9-27'.
    SVN-Revision: 20267

---
+++ tags/version_0-9-27/boost/libs/python/pyste/src/Pyste/exporters.py
@@ -0,0 +1,10 @@
+# Copyright Bruno da Silva de Oliveira 2003. Use, modification and 
+# distribution is subject to the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE_1_0.txt or copy at 
+# http:#www.boost.org/LICENSE_1_0.txt)
+
+
+# a list of Exporter instances
+exporters = []
+
+current_interface = None # the current interface file being processed
