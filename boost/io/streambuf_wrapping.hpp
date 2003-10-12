8ceda34e859951b0511a2adf044ccadcd8fb67d3
Daryle Walker <dwalker07@yahoo.com>
2003-10-12 21:42:25 +0000

    Moved common member functions and data to a new base class (again)
    SVN-Revision: 1666

--- boost/io/streambuf_wrapping.hpp
+++ boost/io/streambuf_wrapping.hpp
@@ -1,11 +1,11 @@
 //  Boost io/streambuf_wrapping.hpp header file  -----------------------------//
 
 //  (C) Copyright Daryle Walker 2003.  Permission to copy, use, modify, sell and
-//  distribute this software is granted provided this copyright notice appears 
-//  in all copies.  This software is provided "as is" without express or implied 
-//  warranty, and with no claim as to its suitability for any purpose. 
+//  distribute this software is granted provided this copyright notice appears
+//  in all copies.  This software is provided "as is" without express or implied
+//  warranty, and with no claim as to its suitability for any purpose.
 
-//  See http://www.boost.org for updates, documentation, and revision history. 
+//  See http://www.boost.org for updates, documentation, and revision history.
 
 #ifndef BOOST_IO_STREAMBUF_WRAPPING_HPP
 #define BOOST_IO_STREAMBUF_WRAPPING_HPP
@@ -14,6 +14,7 @@
 
 #include <boost/utility/base_from_member.hpp>  // for boost::base_from_member
 
+#include <ios>      // for std::basic_ios
 #include <istream>  // for std::basic_istream and std::basic_iostream
 #include <ostream>  // for std::basic_ostream
 
@@ -24,17 +25,82 @@ namespace io
 {
 
 
+//  Implementation detail stuff  ---------------------------------------------//
+
+namespace detail
+{
+
+    // Base class (template) for common member functions and data
+    template < class StreamBuf >
+    class basic_wrapping_ios
+        : private ::boost::base_from_member< StreamBuf >
+        , public virtual ::std::basic_ios< typename StreamBuf::char_type,
+            typename StreamBuf::traits_type >
+    {
+        typedef ::boost::base_from_member< StreamBuf >  pbase_type;
+
+        typedef ::std::basic_ios< typename StreamBuf::char_type,
+          typename StreamBuf::traits_type >  vbase_type;
+
+    public:
+        typedef StreamBuf  streambuf_type;
+
+        typedef typename StreamBuf::char_type  char_type;
+        typedef typename StreamBuf::int_type    int_type;
+
+        typedef typename StreamBuf::pos_type        pos_type;
+        typedef typename StreamBuf::off_type        off_type;
+        typedef typename StreamBuf::traits_type  traits_type;
+
+        streambuf_type *  rdbuf()
+        { return &this->pbase_type::member; }
+
+        streambuf_type const *  rdbuf() const
+        { return &this->pbase_type::member; }
+
+        bool  is_using_internal_streambuf() const
+        { return this->rdbuf() == this->vbase_type::rdbuf(); }
+
+    protected:
+        explicit  basic_wrapping_ios( streambuf_type const &s )
+            : pbase_type( s )
+            { this->vbase_type::init( &this->pbase_type::member ); }
+
+        basic_wrapping_ios()
+            : pbase_type()
+            { this->vbase_type::init( &this->pbase_type::member ); }
+
+        template < typename T1 >
+        explicit  basic_wrapping_ios( T1 x1 )
+            : pbase_type( x1 )
+            { this->vbase_type::init( &this->pbase_type::member ); }
+
+        template < typename T1, typename T2 >
+        basic_wrapping_ios( T1 x1, T2 x2 )
+            : pbase_type( x1, x2 )
+            { this->vbase_type::init( &this->pbase_type::member ); }
+
+        template < typename T1, typename T2, typename T3 >
+        basic_wrapping_ios( T1 x1, T2 x2, T3 x3 )
+            : pbase_type( x1, x2, x3 )
+            { this->vbase_type::init( &this->pbase_type::member ); }
+
+    };  // boost::io::detail::basic_wrapping_ios
+
+}  // namespace detail
+
+
 //  Streambuf-wrapping stream class template declarations  -------------------//
 
 // Use a macro to template the templates!
 #define BOOST_PRIVATE_WRAPPER( Stream ) \
     template < class StreamBuf > \
     class basic_wrapping_##Stream \
-        : private boost::base_from_member< StreamBuf > \
-        , public std::basic_##Stream < typename StreamBuf::char_type, \
+        : public detail::basic_wrapping_ios< StreamBuf > \
+        , public ::std::basic_##Stream < typename StreamBuf::char_type, \
             typename StreamBuf::traits_type > \
     { \
-        typedef ::boost::base_from_member< StreamBuf >  pbase_type; \
+        typedef detail::basic_wrapping_ios< StreamBuf >  pbase_type; \
         typedef ::std::basic_##Stream< typename StreamBuf::char_type, \
           typename StreamBuf::traits_type >  base_type; \
     public: \
@@ -44,30 +110,24 @@ namespace io
         typedef typename StreamBuf::pos_type           pos_type; \
         typedef typename StreamBuf::off_type           off_type; \
         typedef typename StreamBuf::traits_type     traits_type; \
-        streambuf_type *  rdbuf() \
-        { return &this->pbase_type::member; } \
-        streambuf_type const *  rdbuf() const \
-        { return &this->pbase_type::member; } \
-        bool  is_using_internal_streambuf() const \
-        { return this->rdbuf() == this->base_type::rdbuf(); } \
     protected: \
         explicit  basic_wrapping_##Stream ( streambuf_type const &s ) \
-            : pbase_type( s ), base_type( &this->pbase_type::member ) \
+            : pbase_type( s ), base_type( this->pbase_type::rdbuf() ) \
             {} \
         basic_wrapping_##Stream () \
-            : pbase_type(), base_type( &this->pbase_type::member ) \
+            : pbase_type(), base_type( this->pbase_type::rdbuf() ) \
             {} \
         template < typename T1 > \
         explicit  basic_wrapping_##Stream ( T1 x1 ) \
-            : pbase_type( x1 ), base_type( &this->pbase_type::member ) \
+            : pbase_type( x1 ), base_type( this->pbase_type::rdbuf() ) \
             {} \
         template < typename T1, typename T2 > \
         basic_wrapping_##Stream ( T1 x1, T2 x2 ) \
-            : pbase_type( x1, x2 ), base_type( &this->pbase_type::member ) \
+            : pbase_type( x1, x2 ), base_type( this->pbase_type::rdbuf() ) \
             {} \
         template < typename T1, typename T2, typename T3 > \
         basic_wrapping_##Stream ( T1 x1, T2 x2, T3 x3 ) \
-            : pbase_type( x1, x2, x3 ), base_type( &this->pbase_type::member ) \
+            : pbase_type( x1, x2, x3 ), base_type( this->pbase_type::rdbuf() ) \
             {} \
     }
 
