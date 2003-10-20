a74e76eb640d8a6a1977e240e2318e39ea4db3af
Eric Friedman <ebf@users.sourceforge.net>
2003-10-20 00:00:28 +0000

    Fixed sort to work with final MPL code.
    SVN-Revision: 1685

--- boost/mpl/sort_fwd.hpp
+++ boost/mpl/sort_fwd.hpp
@@ -1,5 +1,5 @@
 //-----------------------------------------------------------------------------
-// boost mpl/sort.hpp header file
+// boost mpl/sort_fwd.hpp header file
 // See http://www.boost.org for updates, documentation, and revision history.
 //-----------------------------------------------------------------------------
 //
@@ -14,40 +14,31 @@
 // suitability of this software for any purpose. It is provided "as is" 
 // without express or implied warranty.
 
-#ifndef BOOST_MPL_SORT_HPP_INCLUDED
-#define BOOST_MPL_SORT_HPP_INCLUDED
+#ifndef BOOST_MPL_SORT_FWD_HPP_INCLUDED
+#define BOOST_MPL_SORT_FWD_HPP_INCLUDED
 
-#include "boost/mpl/sort_fwd.hpp"
-#include "boost/mpl/aux_/sort_impl.hpp"
-#include "boost/mpl/lambda.hpp"
-#include "boost/mpl/aux_/sequence_tag.hpp"
+#include "boost/mpl/less.hpp"
+#include "boost/mpl/placeholders.hpp"
 #include "boost/mpl/aux_/void_spec.hpp"
-#include "boost/mpl/aux_/lambda_support.hpp"
 
 namespace boost {
 namespace mpl {
 
+template< typename Tag > struct sort_traits;
+
 BOOST_MPL_AUX_AGLORITHM_NAMESPACE_BEGIN
 
-template < typename Sequence, typename Predicate >  // see sort_fwd.hpp
-struct sort
-{
-private:
-    typedef typename lambda<Predicate>::type pred_;
-
-public:
-    typedef typename sort_traits<
-          typename BOOST_MPL_AUX_SEQUENCE_TAG(Sequence)
-        >::template algorithm<
-          Sequence, pred_
-        >::type type;
-
-    BOOST_MPL_AUX_LAMBDA_SUPPORT(2,sort,(Sequence,Predicate))
-};
+template <
+      typename BOOST_MPL_AUX_VOID_SPEC_PARAM(Sequence)
+    , typename Predicate = less<_,_>
+    >
+struct sort;
 
 BOOST_MPL_AUX_AGLORITHM_NAMESPACE_END
+
+BOOST_MPL_AUX_ALGORITHM_VOID_SPEC(1, sort)
 
 } // namespace mpl
 } // namespace boost
 
-#endif // BOOST_MPL_SORT_HPP_INCLUDED
+#endif // BOOST_MPL_SORT_FWD_HPP_INCLUDED
