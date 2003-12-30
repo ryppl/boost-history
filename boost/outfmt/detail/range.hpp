a5fd8e2ffe3b14622a5d0fd718bdd60dd0bcc421
Reece H. Dunn (bounce) <msclrhd@hotmail.com>
2003-12-30 21:33:31 +0000

    bug fixes
    SVN-Revision: 1853

--- boost/outfmt/detail/range.hpp
+++ boost/outfmt/detail/range.hpp
@@ -27,8 +27,11 @@
                std::pair< RangeT, RangeT >( r1, r2 )
             {
             }
-            template< typename RangeT2 >
-            inline           range_t( const range_t< RangeT2 > & r ):
+            inline           range_t( const std::pair< RangeT, RangeT > & r ):
+               std::pair< RangeT, RangeT >( r )
+            {
+            }
+            inline           range_t( const range_t & r ):
                std::pair< RangeT, RangeT >( r )
             {
             }
@@ -46,20 +49,20 @@
       template< class Container >
       range_t< typename Container::iterator >    range( Container & c )
       {
-         return( range_t< ForwardIterator >( c.begin(), c.end()));
+         return( range( c.begin(), c.end()));
       }
 
       template< typename T, std::size_t n >
       range_t< const T * >                       range( const T a[ n ] )
       {
-         return( range_t< ForwardIterator >( a, a + n ));
+         return( range( a, a + n ));
       }
 
       template< typename T, std::size_t n >
       range_t< const T * >                       range( const T a[ n ], std::size_t off )
       {
          // boost::precondition<>::test( boost::between( off, 0, n ), "range: invalid offset" );
-         return( range_t< ForwardIterator >( a + off, a + n ));
+         return( range( a + off, a + n ));
       }
 
       template< typename T, std::size_t n >
@@ -67,7 +70,7 @@
       {
          // boost::precondition<>::test( boost::between( off,       0, n ), "range: invalid offset" );
          // boost::precondition<>::test( boost::between( off + len, 0, n ), "range: invalid offset/length" );
-         return( range_t< ForwardIterator >( a + off, a + off + len ));
+         return( range( a + off, a + off + len ));
       }
    }}
 #endif
