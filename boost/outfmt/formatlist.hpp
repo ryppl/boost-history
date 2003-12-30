adc6675810393e9304a61a6b57a62979151c266e
Reece H. Dunn (bounce) <msclrhd@hotmail.com>
2003-12-30 21:19:59 +0000

    replaced formatlist_t with aliases to formatob_t equivalents
    SVN-Revision: 1852

--- boost/outfmt/formatlist.hpp
+++ boost/outfmt/formatlist.hpp
@@ -1,4 +1,4 @@
-// (C) Copyright 2003: Reece H. Dunn 
+// (C) Copyright 2003: Reece H. Dunn
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1200)
 #  pragma once
@@ -10,221 +10,126 @@
 
    namespace boost { namespace io
    {
-      template
-      <
-         typename ForwardIterator, typename FormatType = char *,
-         class    FmtObject = boost::io::basic_object
-      >
-      class formatlist_t: public detail::list_object< FormatType, formatlist_t
-                                                                  <
-                                                                     ForwardIterator,
-                                                                     FormatType,
-                                                                     FmtObject
-                                                                  >, FmtObject >
-      {
-         private:
-            ForwardIterator            first;
-            ForwardIterator            last;
-         public:
-            inline ForwardIterator               begin() const 
-            {
-               return( first );
-            }
-            inline ForwardIterator               end() const 
-            {
-               return( last );
-            }
-         public:
-            typedef ForwardIterator                        iterator;
-         public: // constructors
-            inline           formatlist_t( const formatlist_t & fl ):
-               detail::list_object< FormatType, formatlist_t, FmtObject >( fl ),
-               first( fl.first ),
-               last(  fl.last )
-            {
-            }
-            inline           formatlist_t( ForwardIterator f, ForwardIterator l ):
-               first( f ),
-               last(  l )
-            {
-            }
-            inline           formatlist_t( ForwardIterator f, ForwardIterator l, FmtObject o ):
-               detail::list_object< FormatType, formatlist_t, FmtObject >( o ),
-               first( f ),
-               last(  l )
-            {
-            }
-      };
-
-#     if !defined(BOOST_IOFM_NO_BASIC_STREAM)
-         template< typename CharT, class TraitsT, typename ForwardIterator, typename FormatType, typename FmtObject >
-         inline std::basic_ostream< CharT, TraitsT > & operator<<
-         (
-            std::basic_ostream< CharT, TraitsT > & os,
-            const formatlist_t< ForwardIterator, FormatType, FmtObject > & fl
-         )
-         {
-            return( fl.write( os, fl.begin(), fl.end()));
-         }
-
-         template< typename CharT, class TraitsT, typename ForwardIterator, typename FormatType, typename FmtObject >
-         inline std::basic_istream< CharT, TraitsT > & operator>>
-         (
-            std::basic_istream< CharT, TraitsT > & is,
-            const formatlist_t< ForwardIterator, FormatType, FmtObject > & fl
-         )
-         {
-            boost::io::detail::input_helper< std::istream >
-                                                 in( is );
-            fl.read( in,
-               const_cast< formatlist_t< ForwardIterator, FormatType, FmtObject > & >( fl ).begin(),
-               const_cast< formatlist_t< ForwardIterator, FormatType, FmtObject > & >( fl ).end()
-            );
-            return( is );
-         }
-#     else
-         template< typename ForwardIterator, typename FormatType, typename FmtObject >
-         inline std::ostream & operator<<
-         (
-            std::ostream & os,
-            const formatlist_t< ForwardIterator, FormatType, FmtObject > & fl
-         )
-         {
-            return( fl.write( os, fl.begin(), fl.end()));
-         }
-
-         template< typename ForwardIterator, typename FormatType, typename FmtObject >
-         inline std::istream & operator>>
-         (
-            std::istream & is,
-            const formatlist_t< ForwardIterator, FormatType, FmtObject > & fl
-         )
-         {
-            boost::io::detail::input_helper< std::istream >
-                                                 in( is );
-            fl.read( in,
-               const_cast< formatlist_t< ForwardIterator, FormatType, FmtObject > & >( fl ).begin(),
-               const_cast< formatlist_t< ForwardIterator, FormatType, FmtObject > & >( fl ).end()
-            );
-            return( is );
-         }
-#     endif
+      // shorthand manipulators for sequence-style objects
 
       // list format manipulators -- iterator range variants
 
       template< typename ForwardIterator >
-      inline formatlist_t< ForwardIterator >
-                                                 formatlist
+      inline formatob_t< range_t< ForwardIterator >,
+                         char *,
+                         range_object< char * >
+                       >                         formatlist
                                                  (
                                                     ForwardIterator first,
                                                     ForwardIterator last
                                                  )
       {
-         return( formatlist_t< ForwardIterator >( first, last ));
+         return( formatob( range( first, last ), rangefmt()));
       }
 
       template< typename FormatType, typename ForwardIterator >
-      inline formatlist_t< ForwardIterator, FormatType >
-                                                 formatlistex
+      inline formatob_t< range_t< ForwardIterator >,
+                         FormatType,
+                         range_object< FormatType >
+                       >                         formatlistex
                                                  (
                                                     ForwardIterator first,
                                                     ForwardIterator last
                                                  )
       {
-         return( formatlist_t< ForwardIterator, FormatType >( first, last ));
+         return( formatob( range( first, last ), rangefmtex< FormatType >()));
       }
 
       template< typename ForwardIterator, class FmtObject >
-      inline formatlist_t< ForwardIterator, typename FmtObject::format_type, FmtObject >
-                                                 formatlistout
+      inline formatob_t< range_t< ForwardIterator >,
+                         typename FmtObject::format_type,
+                         range_object< typename FmtObject::format_type, FmtObject >
+                       >                         formatlistout
                                                  (
                                                     ForwardIterator first,
                                                     ForwardIterator last,
                                                     const FmtObject & o
                                                  )
       {
-         return( formatlist_t< ForwardIterator, BOOST_DEDUCED_TYPENAME FmtObject::format_type, FmtObject >
-         (
-            first,
-            last,
-            o
-         ));
+         return( formatob( range( first, last ), rangefmt( o )));
       }
 
       // list format manipulators -- container variants
 
       template< class Container >
-      inline formatlist_t< typename Container::iterator > // unknown error in VC7
-                                                 format
+      inline formatob_t< Container,
+                         char *,
+                         container_object< char * >
+                       >                         format
                                                  (
                                                     Container & c
                                                  )
       {
-         return( formatlist_t< BOOST_DEDUCED_TYPENAME Container::iterator >( c.begin(), c.end()));
+         return( formatob( c, containerfmt()));
       }
 
       template< typename FormatType, class Container >
-      inline formatlist_t< typename Container::iterator, FormatType >
-                                                 formatex
+      inline formatob_t< Container,
+                         FormatType,
+                         container_object< FormatType >
+                       >                         formatex
                                                  (
                                                     Container & c
                                                  )
       {
-         return( formatlist_t< BOOST_DEDUCED_TYPENAME Container::iterator, FormatType >( c.begin(), c.end()));
+         return( formatob( c, containerfmtex< FormatType >()));
       }
 
       template< class Container, class FmtObject >
-      inline formatlist_t< typename Container::iterator, typename FmtObject::format_type, FmtObject >
-                                                 formatout
+      inline formatob_t< Container,
+                         typename FmtObject::format_type,
+                         container_object< typename FmtObject::format_type, FmtObject >
+                       >                         formatout
                                                  (
                                                     Container & c,
                                                     const FmtObject & o
                                                  )
       {
-         return( formatlist_t< BOOST_DEDUCED_TYPENAME Container::iterator, BOOST_DEDUCED_TYPENAME FmtObject::format_type, FmtObject >
-         (
-            c.begin(),
-            c.end(),
-            o
-         ));
+         return( formatob( c, containerfmt( o )));
       }
 
       // list format manipulators -- iterator range variants #2
 
       template< typename ForwardIterator >
-      inline formatlist_t< ForwardIterator >
-                                                 formatrange
+      inline formatob_t< std::pair< ForwardIterator, ForwardIterator >,
+                         char *,
+                         range_object< char * >
+                       >                         formatrange
                                                  (
                                                     std::pair< ForwardIterator, ForwardIterator > & ip
                                                  )
       {
-         return( formatlist_t< ForwardIterator >( ip.first, ip.second ));
+         return( formatob( ip, rangefmt()));
       }
 
       template< typename FormatType, typename ForwardIterator >
-      inline formatlist_t< ForwardIterator, FormatType >
-                                                 formatrangeex
+      inline formatob_t< std::pair< ForwardIterator, ForwardIterator >,
+                         FormatType,
+                         range_object< FormatType >
+                       >                         formatrangeex
                                                  (
                                                     std::pair< ForwardIterator, ForwardIterator > & ip
                                                  )
       {
-         return( formatlist_t< ForwardIterator, FormatType >( ip.first, ip.second ));
+         return( formatob( ip, rangefmtex< FormatType >()));
       }
 
       template< typename ForwardIterator, class FmtObject >
-      inline formatlist_t< ForwardIterator, typename FmtObject::format_type, FmtObject >
-                                                 formatrangeout
+      inline formatob_t< std::pair< ForwardIterator, ForwardIterator >,
+                         typename FmtObject::format_type,
+                         range_object< typename FmtObject::format_type, FmtObject >
+                       >                         formatrangeout
                                                  (
                                                     std::pair< ForwardIterator, ForwardIterator > & ip,
                                                     const FmtObject & o
                                                  )
       {
-         return( formatlist_t< ForwardIterator, BOOST_DEDUCED_TYPENAME FmtObject::format_type, FmtObject >
-         (
-            ip.first,
-            ip.second,
-            o
-         ));
+         return( formatob( ip, rangefmt( o )));
       }
    }}
 #endif
