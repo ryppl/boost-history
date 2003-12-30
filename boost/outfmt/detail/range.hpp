2189f834a72f3dab80fe77baa471463791384f39
Reece H. Dunn (bounce) <msclrhd@hotmail.com>
2003-12-30 19:52:42 +0000

    updated to interoperate with std::pair
    SVN-Revision: 1848

--- boost/outfmt/detail/range.hpp
+++ boost/outfmt/detail/range.hpp
@@ -6,30 +6,30 @@
 
 #ifndef BOOST_IOFM_DETAIL_RANGE_T_HPP
 #define BOOST_IOFM_DETAIL_RANGE_T_HPP
-#  include <iomanip>
-
    namespace boost { namespace io
    {
       template< typename RangeT >
-      class range_t: public boost::noncopyable
+      class range_t: public std::pair< RangeT, RangeT >
       {
          public:
-            RangeT                     first;
-            RangeT                     last;
+            inline RangeT                        begin() const
+            {
+               return( first );
+            }
+            inline RangeT                        end() const
+            {
+               return( second );
+            }
+         private:
+            inline           range_t();
          public:
-            inline           range_t():
-               first(),
-               last()
-            {
-            }
             inline           range_t( const RangeT & r1, const RangeT & r2 ):
-               first( r1 ),
-               last(  r2 )
+               std::pair< RangeT, RangeT >( r1, r2 )
             {
             }
-            inline           range_t( const range_t & r ):
-               first( r.first ),
-               last(  r.last )
+            template< typename RangeT2 >
+            inline           range_t( const range_t< RangeT2 > & r ):
+               std::pair< RangeT, RangeT >( r )
             {
             }
       };
@@ -39,34 +39,35 @@
       template< typename ForwardIterator >
       range_t< ForwardIterator >                 range( ForwardIterator f, ForwardIterator l )
       {
+         // boost::precondition<>::test( f <= l, "invalid range" );
          return( range_t< ForwardIterator >( f, l ));
       }
 
       template< class Container >
       range_t< typename Container::iterator >    range( Container & c )
       {
-         return( range( c.begin(), c.end()));
+         return( range_t< ForwardIterator >( c.begin(), c.end()));
       }
 
       template< typename T, std::size_t n >
       range_t< const T * >                       range( const T a[ n ] )
       {
-         return( range( a, a + n ));
+         return( range_t< ForwardIterator >( a, a + n ));
       }
 
       template< typename T, std::size_t n >
       range_t< const T * >                       range( const T a[ n ], std::size_t off )
       {
-         // pre: 0 <= off < n
-         return( range( a + off, a + n ));
+         // boost::precondition<>::test( boost::between( off, 0, n ), "range: invalid offset" );
+         return( range_t< ForwardIterator >( a + off, a + n ));
       }
 
       template< typename T, std::size_t n >
       range_t< const T * >                       range( const T a[ n ], std::size_t off, std::size_t len )
       {
-         // pre: 0 <= off < n
-         // pre: 0 <= ( off + len ) < n
-         return( range( a + off, a + off + len ));
+         // boost::precondition<>::test( boost::between( off,       0, n ), "range: invalid offset" );
+         // boost::precondition<>::test( boost::between( off + len, 0, n ), "range: invalid offset/length" );
+         return( range_t< ForwardIterator >( a + off, a + off + len ));
       }
    }}
 #endif
