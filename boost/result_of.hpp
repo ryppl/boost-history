7fe357bad764910c05031e567df8a47cee49d3cc
Douglas Gregor <doug.gregor@gmail.com>
2004-01-05 21:27:28 +0000

    Fix some bugs with result_of and bring it up to conformance with the TR
    SVN-Revision: 1861

--- boost/result_of.hpp
+++ boost/result_of.hpp
@@ -4,6 +4,7 @@
 #include <boost/type_traits/ice.hpp>
 #include <boost/type.hpp>
 #include <boost/preprocessor.hpp>
+#include <boost/detail/workaround.hpp>
 
 #ifndef BOOST_RESULT_OF_NUM_ARHS
 #  define BOOST_RESULT_OF_NUM_ARGS 10
@@ -39,13 +40,19 @@ struct get_result_of<F, FArgs, true>
 template<typename F, typename FArgs>
 struct get_result_of<F, FArgs, false>
 {
-//  typedef typename F::template result_of<FArgs>::type type;
+  typedef typename F::template result<FArgs>::type type;
+};
+
+template<typename F>
+struct get_result_of<F, F(), false>
+{
+  typedef void type;
 };
 
 template<typename F, typename FArgs>
 struct result_of : get_result_of<F, FArgs, (has_result_type<F>::value)> {};
 
-}
+} // end namespace detail
 
 #define BOOST_PP_ITERATION_PARAMS_1 (3,(0,BOOST_RESULT_OF_NUM_ARGS,<boost/detail/result_of_iterate.hpp>))
 #include BOOST_PP_ITERATE()
