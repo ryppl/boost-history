ac0bef7c295436ee6d63c41f96773d8fe826d6a9
Reece H. Dunn (bounce) <msclrhd@hotmail.com>
2004-02-29 17:13:35 +0000

    improved test script
    SVN-Revision: 2008

--- libs/outfmt/test/generate.sh
+++ libs/outfmt/test/generate.sh
@@ -1,14 +1,29 @@
 #!/bin/sh
 
+##### Configuration
+
 tools="gcc msvc"
 
-for tool in $tools ; do     # for each toolset
-   echo testing for: $tool  #    inform the user we are testing this toolset
-   bjam $tool               #    rebuild the regression tests
+##### [1]: Perform the tests
 
-   cd run                   #    go into the directory with the built tools
-   for exec in *.exe ; do   #    for each test program
-      ./$exec               #       run it!
+for tool in $tools ; do      # for each toolset
+   ##### [1a]: Build
+
+   echo testing outfmt for $tool...
+   bjam -a $tool 2>&1 | tee build.$tool.log > nul
+
+   ##### [1b]: Run
+
+   cd run                    #    go into the directory with the built tools
+   for exec in *.exe ; do    #    for each test program
+      ./$exec                #       run it!
    done
-   cd ..                    #    go back to the build directory :)
+   cd ..                     #    go back to the build directory :)
 done
+
+##### [2]: Post-Test Clean Up
+
+mv run/results.xml results.xml
+rm -r run
+rm -r bin
+
