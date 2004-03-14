daf1737ef6e6c8d3ac31eed71f8bb53763301d96
Gennaro Prota <gennaro.prota@yahoo.com>
2004-03-14 18:18:26 +0000

    - new license reference - changed EXIT_SUCCESS to 0 - little change to make string tests faster - parenthesized max usages - removed some warnings - typo fixes - added copy assigment operator tests - added swap (member) function tests
    SVN-Revision: 2051

--- libs/dynamic_bitset/dyn_bitset_unit_tests1.cpp
+++ libs/dynamic_bitset/dyn_bitset_unit_tests1.cpp
@@ -1,14 +1,16 @@
-// (C) Copyright Jeremy Siek 2001.
-// Permission to copy, use, modify, sell and distribute this software
-// is granted provided this copyright notice appears in all
-// copies. This software is provided "as is" without express or
-// implied warranty, and with no claim as to its suitability for any
-// purpose.
+// --------------------------------------------------------
+//        (C) Copyright Jeremy Siek   2001.
+//        (C) Copyright Gennaro Prota 2003 - 2004.
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+// -----------------------------------------------------------
 
 
 #include "bitset_test.hpp"
 #include "boost/dynamic_bitset.hpp"
-
 #include "boost/limits.hpp"
 #include "boost/config.hpp"
 
@@ -23,7 +25,7 @@ void run_string_tests(const String& s
 
   // bitset length determined by the string-related arguments
   std::size_t i;
-  for (i = 0; i <= len/2 ; /*++i*/ i += step) {
+  for (i = 0; i <= len/2 ; i += step) {
     Tests::from_string(s, i, len/2);     // len/2 - i bits
     Tests::from_string(s, i, len);       // len - i   bits
     Tests::from_string(s, i, 1 + len*2); // len - i   bits
@@ -53,9 +55,10 @@ void run_test_cases( BOOST_EXPLICIT_TEMPLATE_TYPE(Block) )
   std::size_t   n,
                 ul_width    = std::numeric_limits<unsigned long>::digits,
                 block_width = std::numeric_limits<Block>::digits;
+  const unsigned long ulong_max = (std::numeric_limits<unsigned long>::max)();
   unsigned long numbers[]   = { 0, 1, 40247,
-                                std::numeric_limits<unsigned long>::max() >> 1,
-                                std::numeric_limits<unsigned long>::max() };
+                                ulong_max >> 1,
+                                ulong_max };
 
   //=====================================================================
   // Test construction from unsigned long
@@ -98,11 +101,11 @@ void run_test_cases( BOOST_EXPLICIT_TEMPLATE_TYPE(Block) )
       run_string_tests<Tests>(get_long_string());
 
 # if !defined BOOST_NO_STD_WSTRING
-      // I need to decide what to do for non "C" locale here. On
+      // I need to decide what to do for non "C" locales here. On
       // one hand I should have better tests. On the other one
       // I don't want tests for dynamic_bitset to cope with locales,
       // ctype::widen, etc. (but that's what you deserve when you
-      // don't separate concerns at the library level)
+      // don't separate concerns at the library level) - gps
       //
       run_string_tests<Tests>(
           std::wstring(L"11111000000111111111010101010101010101010111111"));
@@ -128,10 +131,10 @@ void run_test_cases( BOOST_EXPLICIT_TEMPLATE_TYPE(Block) )
     Tests::from_block_range(blocks);
   }
   {
-    std::vector<Block> blocks(101);
-    for (typename std::vector<Block>::size_type i = 0;
-         i < blocks.size(); ++i)
-      blocks[i] = i;
+    const unsigned int n = (std::numeric_limits<unsigned char>::max());
+    std::vector<Block> blocks(n);
+    for (typename std::vector<Block>::size_type i = 0; i < n; ++i)
+      blocks[i] = static_cast<Block>(i);
     Tests::from_block_range(blocks);
   }
   //=====================================================================
@@ -151,16 +154,49 @@ void run_test_cases( BOOST_EXPLICIT_TEMPLATE_TYPE(Block) )
   //=====================================================================
   // Test assignment operator
   {
-    boost::dynamic_bitset<Block> a, b;
+    bitset_type a, b;
     Tests::assignment_operator(a, b);
   }
   {
-    boost::dynamic_bitset<Block> a(std::string("1")), b(std::string("0"));
+    bitset_type a(std::string("1")), b(std::string("0"));
     Tests::assignment_operator(a, b);
   }
   {
-    boost::dynamic_bitset<Block> a(long_string), b(long_string);
+    bitset_type a(long_string), b(long_string);
     Tests::assignment_operator(a, b);
+  }
+  {
+    bitset_type a;
+    bitset_type b(long_string); // b greater than a, a empty
+    Tests::assignment_operator(a, b);
+  }
+  {
+    bitset_type a(std::string("0"));
+    bitset_type b(long_string); // b greater than a
+    Tests::assignment_operator(a, b);
+  }
+  //=====================================================================
+  // Test swap
+  {
+    bitset_type a;
+    bitset_type b(std::string("1"));
+    Tests::swap(a, b);
+    Tests::swap(b, a);
+    Tests::swap(a, a);
+  }
+  {
+    bitset_type a;
+    bitset_type b(long_string);
+    Tests::swap(a, b);
+    Tests::swap(b, a);
+  }
+  {
+    bitset_type a(std::string("0"));
+    bitset_type b(long_string);
+    Tests::swap(a, b);
+    Tests::swap(b, a);
+    Tests::swap(a, a);
+    Tests::swap(b, b);
   }
   //=====================================================================
   // Test resize
@@ -253,10 +289,19 @@ void run_test_cases( BOOST_EXPLICIT_TEMPLATE_TYPE(Block) )
   }
   {
     boost::dynamic_bitset<Block> a(std::string("1"));
-    std::vector<Block> blocks(101);
-    for (typename std::vector<Block>::size_type i = 0;
-         i < blocks.size(); ++i)
-      blocks[i] = i;
+    const unsigned int n = (std::numeric_limits<unsigned char>::max)();
+    std::vector<Block> blocks(n);
+    for (typename std::vector<Block>::size_type i = 0; i < n; ++i)
+      blocks[i] = static_cast<Block>(i);
+    Tests::append_block_range(a, blocks);
+  }
+  {
+    boost::dynamic_bitset<Block> a;
+    a.append(Block(1));
+    a.append(Block(2));
+    Block x[] = {3, 4, 5};
+    std::size_t sz = sizeof(x) / sizeof(x[0]);
+    std::vector<Block> blocks(x, x + sz);
     Tests::append_block_range(a, blocks);
   }
   {
@@ -300,5 +345,5 @@ test_main(int, char*[])
   run_test_cases<unsigned long long>();
 # endif
 
-  return EXIT_SUCCESS;
+  return 0;
 }
