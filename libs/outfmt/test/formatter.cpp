28cf40ed7d702e5310ecd8287efce0e4cad1aa3b
Reece H. Dunn (bounce) <msclrhd@hotmail.com>
2004-03-14 16:05:31 +0000

    VC6 and type modularization support
    SVN-Revision: 2048

--- libs/outfmt/test/formatter.cpp
+++ libs/outfmt/test/formatter.cpp
@@ -1,7 +1,7 @@
 // (C) Copyright 2004: Reece H. Dunn
 
-#include <iostream>
 #include <boost/outfmt/formatlist.hpp>
+#include <boost/outfmt/stl/list.hpp>   // std::list support
 
 #include "test_engine.hpp"
 
@@ -20,25 +20,25 @@ int main()
 
    // boost::io::formatter_t< ... >::format
 
-   test.formatter( boost::io::format( l ).format( " : " ),             "[ A : B : C ]", "format( sep )" );
-   test.formatter( boost::io::format( l ).format( "{ ", " }" ),        "{ A, B, C }",   "format( open, close )" );
-   test.formatter( boost::io::format( l ).format( "{ ", " }", " : " ), "{ A : B : C }", "format( open, close, sep )" );
+   test_formatter( test, boost::io::format( l ).format( " : " ),             "[ A : B : C ]", "format( sep )" );
+   test_formatter( test, boost::io::format( l ).format( "{ ", " }" ),        "{ A, B, C }",   "format( open, close )" );
+   test_formatter( test, boost::io::format( l ).format( "{ ", " }", " : " ), "{ A : B : C }", "format( open, close, sep )" );
 
    // boost::io::openclose_formatter< ... >
 
-   test.formatter( boost::io::format( l ).format( boost::io::openclose_formatter< char * >()),             "[ A, B, C ]", "format( openclose_formatter())" );
-   test.formatter( boost::io::format( l ).format( boost::io::openclose_formatter< char * >( "{ ", " }" )), "{ A, B, C }", "format( openclose_formatter( open, close ))" );
-   test.formatter( boost::io::format( l ).format( boost::io::openclose_formatter< char * >( ocfmt )),      "< A, B, C >", "format( openclose_formatter( openclose_formatter ))" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::openclose_formatter< char * >()),             "[ A, B, C ]", "format( openclose_formatter())" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::openclose_formatter< char * >( "{ ", " }" )), "{ A, B, C }", "format( openclose_formatter( open, close ))" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::openclose_formatter< char * >( ocfmt )),      "< A, B, C >", "format( openclose_formatter( openclose_formatter ))" );
 
    // boost::io::formatter< ... >
 
-   test.formatter( boost::io::format( l ).format( boost::io::formatter< char * >()),                    "[ A, B, C ]",   "format( formatter())" );
-   test.formatter( boost::io::format( l ).format( boost::io::formatter< char * >( " : " )),             "[ A : B : C ]", "format( formatter( sep ))" );
-   test.formatter( boost::io::format( l ).format( boost::io::formatter< char * >( "{ ", " }" )),        "{ A, B, C }",   "format( formatter( open, close ))" );
-   test.formatter( boost::io::format( l ).format( boost::io::formatter< char * >( "{ ", " }", " : " )), "{ A : B : C }", "format( formatter( open, close, sep ))" );
-   test.formatter( boost::io::format( l ).format( boost::io::formatter< char * >( ocfmt )),             "< A, B, C >",   "format( formatter( openclose_formatter ))" );
-   test.formatter( boost::io::format( l ).format( boost::io::formatter< char * >( ocfmt, " : " )),      "< A : B : C >", "format( formatter( openclose_formatter, sep ))" );
-   test.formatter( boost::io::format( l ).format( boost::io::formatter< char * >( fmt )),               "< A | B | C >", "format( formatter( formatter ))" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::formatter< char * >()),                    "[ A, B, C ]",   "format( formatter())" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::formatter< char * >( " : " )),             "[ A : B : C ]", "format( formatter( sep ))" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::formatter< char * >( "{ ", " }" )),        "{ A, B, C }",   "format( formatter( open, close ))" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::formatter< char * >( "{ ", " }", " : " )), "{ A : B : C }", "format( formatter( open, close, sep ))" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::formatter< char * >( ocfmt )),             "< A, B, C >",   "format( formatter( openclose_formatter ))" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::formatter< char * >( ocfmt, " : " )),      "< A : B : C >", "format( formatter( openclose_formatter, sep ))" );
+   test_formatter( test, boost::io::format( l ).format( boost::io::formatter< char * >( fmt )),               "< A | B | C >", "format( formatter( formatter ))" );
 
    return( 0 );
 }
--- libs/outfmt/test/stl_io.cpp
+++ libs/outfmt/test/stl_io.cpp
@@ -1,7 +1,20 @@
 // (C) Copyright 2004: Reece H. Dunn
 
-#include <iostream>
-#include <boost/outfmt/stl_io.hpp>
+#include <boost/outfmt/stl/vector.hpp>     // std::vector
+#include <boost/outfmt/stl/list.hpp>       // std::list
+#include <boost/outfmt/stl/deque.hpp>      // std::deque
+#include <boost/outfmt/stl/map.hpp>        // std::map; std::multimap
+#include <boost/outfmt/stl/set.hpp>        // std::set; std::multiset
+#include <boost/outfmt/stl/pair.hpp>       // std::pair
+
+#if defined(BOOST_IOFM_HASH_CONTAINERS)
+#  include <boost/outfmt/stl/hash_map.hpp> // stdext::hash_map; stdext::hash_multimap
+#  include <boost/outfmt/stl/hash_set.hpp> // stdext::hash_set; stdext::hash_multiset
+#endif
+
+#if defined(BOOST_HAS_SLIST)
+#   include <boost/outfmt/stl/slist.hpp>   // stdext::slist
+#endif
 
 #include "test_engine.hpp"
 
@@ -16,7 +29,7 @@ int main()
       v[ 1 ] = 6;
       v[ 2 ] = 9;
 
-      test.stlio( v, "[ 3, 6, 9 ]", "std::vector" );
+      test_stlio( test, v, "[ 3, 6, 9 ]", "std::vector" );
    }
 
    // std::list
@@ -26,7 +39,7 @@ int main()
       l.push_back( 'B' );
       l.push_back( 'C' );
 
-      test.stlio( l, "[ A, B, C ]", "std::list" );
+      test_stlio( test, l, "[ A, B, C ]", "std::list" );
    }
 
    // std::deque
@@ -36,7 +49,7 @@ int main()
       dq.push_back( 'B' );
       dq.push_back( 'C' );
 
-      test.stlio( dq, "[ A, B, C ]", "std::deque" );
+      test_stlio( test, dq, "[ A, B, C ]", "std::deque" );
    }
 
    // std::map
@@ -46,7 +59,7 @@ int main()
       m[ 'L' ] =  7;
       m[ 'S' ] = -5;
 
-      test.stlio( m, "[ ( A, 3 ), ( L, 7 ), ( S, -5 ) ]", "std::map" );
+      test_stlio( test, m, "[ ( A, 3 ), ( L, 7 ), ( S, -5 ) ]", "std::map" );
    }
 
    // std::multimap
@@ -56,7 +69,7 @@ int main()
       mm.insert( std::pair< char, int >( 'L',  7 ));
       mm.insert( std::pair< char, int >( 'S', -5 ));
 
-      test.stlio( mm, "[ ( A, 3 ), ( L, 7 ), ( S, -5 ) ]", "std::multimap" );
+      test_stlio( test, mm, "[ ( A, 3 ), ( L, 7 ), ( S, -5 ) ]", "std::multimap" );
    }
 
    // std::set
@@ -69,7 +82,7 @@ int main()
       s.insert( 'D' );
       s.insert( 'E' );
 
-      test.stlio( s, "[ A, B, C, D, E ]", "std::set" );
+      test_stlio( test, s, "[ A, B, C, D, E ]", "std::set" );
    }
 
    // std::multiset
@@ -82,20 +95,20 @@ int main()
       ms.insert( 'D' );
       ms.insert( 'E' );
 
-      test.stlio( ms, "[ A, A, B, C, D, E ]", "std::multiset" );
+      test_stlio( test, ms, "[ A, A, B, C, D, E ]", "std::multiset" );
    }
 
    // std::pair
    {
       std::pair< char, float >         p( 'A', 2.759f );
-      test.stlio( p, "( A, 2.759 )", "std::pair" );
+      test_stlio( test, p, "( A, 2.759 )", "std::pair" );
    }
 
    // std::string
    {
       std::pair< std::string, std::string >
                                        p( "Abschied Nehmen", "Farewell" );
-      test.stlio( p, "( \"Abschied Nehmen\", \"Farewell\" )", "std::string" );
+      test_stlio( test, p, "( \"Abschied Nehmen\", \"Farewell\" )", "std::string" );
    }
 
 #  if defined(BOOST_IOFM_HASH_CONTAINERS)
@@ -107,7 +120,7 @@ int main()
          hm[ 'L' ] = 7;
          hm[ 'S' ] = -5;
 
-         test.stlio( hm, "[ ( A, 3 ), ( L, 7 ), ( S, -5 ) ]", "hash_map" );
+         test_stlio( test, hm, "[ ( A, 3 ), ( L, 7 ), ( S, -5 ) ]", "hash_map" );
       }
 
       // hash_multimap
@@ -118,7 +131,7 @@ int main()
          hmm.insert( std::pair< char, int >( 'L',  7 ));
          hmm.insert( std::pair< char, int >( 'S', -5 ));
 
-         test.stlio( hmm, "[ ( A, 3 ), ( L, 7 ), ( S, -5 ) ]", "hash_multimap" );
+         test_stlio( test, hmm, "[ ( A, 3 ), ( L, 7 ), ( S, -5 ) ]", "hash_multimap" );
       }
 
       // hash_set
@@ -134,11 +147,11 @@ int main()
 
          // [hack]: is there a better way to test this?
 #        if defined(BOOST_DINKUMWARE_STDLIB) && (BOOST_DINKUMWARE_STDLIB == 313)
-            test.stlio( hs, "[ A, C, E, B, D ]", "hash_set" );
+            test_stlio( test, hs, "[ A, C, E, B, D ]", "hash_set" );
 #        elif defined(BOOST_DINKUMWARE_STDLIB) && (BOOST_DINKUMWARE_STDLIB < 313)
-            test.stlio( hs, "[ B, D, A, C, E ]", "hash_set" );
+            test_stlio( test, hs, "[ B, D, A, C, E ]", "hash_set" );
 #        else
-            test.stlio( hs, "[ A, B, C, D, E ]", "hash_set" );
+            test_stlio( test, hs, "[ A, B, C, D, E ]", "hash_set" );
 #        endif
       }
 
@@ -155,11 +168,11 @@ int main()
 
          // [hack]: is there a better way to test this?
 #        if defined(BOOST_DINKUMWARE_STDLIB) && (BOOST_DINKUMWARE_STDLIB == 313)
-            test.stlio( hms, "[ A, A, C, E, B, D ]", "hash_multiset" );
+            test_stlio( test, hms, "[ A, A, C, E, B, D ]", "hash_multiset" );
 #        elif defined(BOOST_DINKUMWARE_STDLIB) && (BOOST_DINKUMWARE_STDLIB < 313)
-            test.stlio( hms, "[ B, D, A, A, C, E ]", "hash_multiset" );
+            test_stlio( test, hms, "[ B, D, A, A, C, E ]", "hash_multiset" );
 #        else
-            test.stlio( hms, "[ A, A, B, C, D, E ]", "hash_multiset" );
+            test_stlio( test, hms, "[ A, A, B, C, D, E ]", "hash_multiset" );
 #        endif
       }
 #  else
@@ -177,7 +190,7 @@ int main()
       sl.push_front( 'B' );
       sl.push_front( 'A' );
 
-      test.stlio( sl, "[ A, B, C ]", "slist" );
+      test_stlio( test, sl, "[ A, B, C ]", "slist" );
    }
 #  else
       test.unsupported( "slist", "library does not support 'slist' extension class" );
