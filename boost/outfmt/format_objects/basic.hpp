d236630c070d718b85015c5c0b228aa3e58c7a77
Reece H. Dunn (bounce) <msclrhd@hotmail.com>
2004-03-16 23:48:36 +0000

    string support
    SVN-Revision: 2057

--- boost/outfmt/format_objects/basic.hpp
+++ boost/outfmt/format_objects/basic.hpp
@@ -8,6 +8,75 @@
 
    namespace boost { namespace io
    {
+      namespace detail
+      {
+         class simple_object
+         {
+            public:
+               template< typename T, class InputStream >
+               static inline bool                read( InputStream & is, T & value )
+               {
+                  is >> value;
+                  return( is.isgood());
+               }
+               template< typename T, class OutputStream >
+               static inline OutputStream &      write( OutputStream & os, const T & value )
+               {
+                  return( os << value );
+               }
+         };
+
+         // special handling for string types
+
+         class string_object
+         {
+            public:
+            template< typename T, class InputStream >
+            static inline bool                   read( InputStream & is, T & s )
+            {
+               s.erase();
+               is.skipws();
+               if( !is.match( '"' )) return( false );
+
+               typename InputStream::char_type
+                                       ch  = '\0';
+               typename InputStream::char_type
+                                       sch = '"';
+
+               while( is.getch( ch ) && !is.eq( ch, sch ))
+               {
+                  if( is.eq( ch, '\\' ) && is.getch( ch ))
+                  {
+                     // only escape \ and " characters
+                     if( !is.eq( ch, '\\' ) && !is.eq( ch, '"' ))
+                        s.push_back( '\\' );
+
+                     s.push_back( ch );
+                  }
+                  else                 s.push_back( ch );
+               }
+
+               return( true );
+            }
+            template< typename T, class OutputStream >
+            static inline OutputStream &         write( OutputStream & os, const T & value )
+            {
+               os << '"';
+               T::const_iterator       last = value.end();
+               for( T::const_iterator i = value.begin(); i != last; ++i )
+               {
+                  switch( *i ) // only escape \ and " characters
+                  {
+                     case '"':    os << "\\\""; break;
+                     case '\\':   os << "\\\\"; break;
+                     default:     os << *i;     break;
+                  }
+               }
+               return( os << '"' );
+            }
+         };
+      }
+
       class basic_object
       {
          public:
@@ -18,13 +87,26 @@
             template< typename T, class InputStream >
             inline bool                          read( InputStream & is, T & value ) const
             {
-               is >> value;
-               return( is.isgood());
+               return
+               (
+                  BOOST_DEDUCED_TYPENAME select
+                  <
+                     is_std_string< T >, detail::string_object,
+                     mpl::true_,         detail::simple_object
+                  >::type::read( is, value )
+               );
             }
             template< typename T, class OutputStream >
             inline OutputStream &                write( OutputStream & os, const T & value ) const
             {
-               return( os << value );
+               return
+               (
+                  BOOST_DEDUCED_TYPENAME select
+                  <
+                     is_std_string< T >, detail::string_object,
+                     mpl::true_,         detail::simple_object
+                  >::type::write( os, value )
+               );
             }
          public:
             inline           basic_object()
