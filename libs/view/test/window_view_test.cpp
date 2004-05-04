cbc7601221c1f6f77145b15fd20d1103fc033db0
Roland Richter <roland.richter@jku.at>
2004-05-04 11:50:44 +0000

    Added image tests.
    SVN-Revision: 2161

--- libs/view/test/window_view_test.cpp
+++ libs/view/test/window_view_test.cpp
@@ -6,6 +6,8 @@
 
 #include <boost/view/window_view.hpp>
 
+#include <boost/tupple/iterator_tupple.hpp>
+
 
 void test1()
 {
@@ -27,12 +29,12 @@ void test1()
     BOOST_CHECK( wv[1] == v[ (i+1)%N ] );
     BOOST_CHECK( wv[2] == v[ (i+2)%N ] );
 
-    wv.rotate();
+    wv.shift();
   }
 
   for( int i = 0; i < N+1; ++i )
   {
-    wv.rotate(-1);
+    wv.shift(-1);
 
     BOOST_CHECK( wv.front() == v[ (N-i)%N ] );
     BOOST_CHECK( wv.back() == v[ (N-i+2)%N ] );
@@ -75,7 +77,7 @@ void test2()
   BOOST_CHECK( *wit == v[4] ); ++wit;
   BOOST_CHECK( *wit == v[3] );
 
-  wvc.rotate();
+  wvc.shift();
   BOOST_CHECK( wvc.front() == v[3] );
   BOOST_CHECK( wvc.back() == v[1+1] );
   BOOST_CHECK( wvc[0] == v[3] );
@@ -92,6 +94,94 @@ void test2()
     BOOST_CHECK( *wit == v[ (4-i+N)%N ] );
     BOOST_CHECK( wvd[i] == v[ (4-i+N)%N ] );
   }
+}
+
+
+void image_test()
+{
+  typedef unsigned char PixelType; 
+
+  typedef std::vector<PixelType> ImageType;
+  typedef std::vector<PixelType>::iterator ImageIterator;
+
+  int width = 11; int height = 9;
+  ImageType image( width * height );
+
+  int stride = width;
+
+  // First approach:
+
+  ImageIterator rowBegin = image.begin();
+  ImageIterator rowEnd   = rowBegin + stride;
+
+  // Iterates vertically down the image.
+  for( ; rowEnd != image.end(); rowBegin += stride, rowEnd += stride )
+  {
+    boost::view::window_view<ImageType> row( image, rowBegin, rowEnd );
+
+    // Iterates horizontally over one row.
+    for( boost::view::window_view<ImageType>::iterator pixel = row.begin();
+         pixel != row.end(); ++pixel )
+    {
+      *pixel = 0;
+    }
+  }
+ 
+  // Second approach:
+
+  // The end of the image at the lower right corner.
+  // Note that this is necessary because the view's iterator type is cyclic_iterator.
+  boost::view::window_view<ImageType>::iterator imageEnd =
+    boost::view::window_view<ImageType>( image, image.begin(), image.end() ).end();
+
+  // The first row starts at the first pixel and has 'stride' pixels.
+  boost::view::window_view<ImageType> row( image, image.begin(), image.begin() + stride );
+
+  for( ; row.end() != imageEnd; row.shift( stride ) )
+  {
+    for( boost::view::window_view<ImageType>::iterator pixel = row.begin();
+         pixel != row.end(); ++pixel )
+    {
+      *pixel = 0;
+    }
+  }
+
+}
+
+
+void image_with_tuple_test()
+{
+  typedef unsigned char PixelType; 
+
+  typedef std::vector<PixelType> ImageType;
+  typedef std::vector<PixelType>::iterator ImageIterator;
+
+  int width = 11; int height = 9;
+  int stride = width;
+  ImageType image( width * height );
+
+  // Third approach:
+
+  typedef boost::tupple::iterator_tuple<ImageIterator,ImageIterator> RowType;
+
+  PixelType value = 0;
+
+  RowType row( image.begin(), image.begin() + stride );
+  RowType::difference_type nextRow( stride, stride );
+
+  for( ; row.first() != image.end(); row += nextRow )
+  {
+    for( ImageIterator pixel = row.first(); pixel != row.last(); ++pixel )
+    {
+      *pixel = value++;
+    }
+  }
+
+  value = 0;
+  for( ImageIterator it = image.begin(); it != image.end(); ++it )
+  {
+    BOOST_CHECK( *it == value++ );
+  }
 
 }
 
@@ -100,6 +190,9 @@ int test_main(int, char *[])
 {
   test1();
   test2();
+
+  image_test();
+  image_with_tuple_test();
 
   bool error_on_purpose = false;
   //BOOST_CHECK( error_on_purpose );
