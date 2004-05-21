62c8a06c3418196029961e18c5027ec36d732dd8
Reece H. Dunn (bounce) <msclrhd@hotmail.com>
2004-05-21 00:22:25 +0000

    moved to the Boost License 1.0
    SVN-Revision: 2186

--- boost/outfmt/detail/format_deducer.hpp
+++ boost/outfmt/detail/format_deducer.hpp
@@ -1,4 +1,6 @@
 // (C) Copyright Jonathan Turkanis 2003-2004.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef BOOST_IOFM_DETAIL_FormatDeducer_HPP
 #define BOOST_IOFM_DETAIL_FormatDeducer_HPP
--- boost/outfmt/stl/pair.hpp
+++ boost/outfmt/stl/pair.hpp
@@ -1,4 +1,6 @@
 // (C) Copyright 2004: Reece H. Dunn
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef BOOST_IOFM_STL_Pair_HPP
 #define BOOST_IOFM_STL_Pair_HPP
