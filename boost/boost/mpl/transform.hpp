3444222443929d9126b6c3326b728880bf89960e
Aleksey Gurtovoy <agurtovoy@meta-comm.com>
2004-08-21 05:16:52 +0000

    transform fixes
    SVN-Revision: 24633

--- boost/boost/mpl/transform.hpp
+++ boost/boost/mpl/transform.hpp
@@ -5,9 +5,9 @@
 // Copyright (c) Aleksey Gurtovoy 2000-2004
 // Copyright (c) David Abrahams 2003-2004
 //
-// Use, modification and distribution are subject to the Boost Software 
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy 
-// at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
 //
 // See http://www.boost.org/libs/mpl for documentation.
 
@@ -20,6 +20,7 @@
 #include <boost/mpl/pair_view.hpp>
 #include <boost/mpl/is_sequence.hpp>
 #include <boost/mpl/if.hpp>
+#include <boost/mpl/lambda.hpp>
 #include <boost/mpl/bind.hpp>
 #include <boost/mpl/or.hpp>
 #include <boost/mpl/not.hpp>
@@ -39,9 +40,9 @@ struct transform1_impl
     : fold< 
           Seq
         , typename In::state
-        , bind2< typename In::operation
+        , bind2< typename lambda< typename In::operation >::type
             , _1
-            , bind1<Op, _2>
+            , bind1< typename lambda<Op>::type, _2>
             > 
         >
 {
@@ -56,9 +57,9 @@ struct reverse_transform1_impl
     : reverse_fold< 
           Seq
         , typename In::state
-        , bind2< typename In::operation
+        , bind2< typename lambda< typename In::operation >::type
             , _1
-            , bind1<Op, _2>
+            , bind1< typename lambda<Op>::type, _2>
             > 
         >
 {
@@ -95,9 +96,9 @@ struct reverse_transform2_impl
     : reverse_fold< 
           pair_view<Seq1,Seq2>
         , typename In::state
-        , bind2< typename In::operation
+        , bind2< typename lambda< typename In::operation >::type
             , _1
-            , bind2<Op
+            , bind2< typename lambda< Op >::type
                 , bind1<first<>,_2>
                 , bind1<second<>,_2>
                 >
@@ -116,20 +117,24 @@ BOOST_MPL_AUX_AGLORITHM_NAMESPACE_BEGIN
 template<
       typename BOOST_MPL_AUX_NA_PARAM(Seq1)
     , typename BOOST_MPL_AUX_NA_PARAM(Seq2OrOperation)
-    , typename BOOST_MPL_AUX_NA_PARAM(Operation)
+    , typename BOOST_MPL_AUX_NA_PARAM(OperationOrInserter)
+    , typename BOOST_MPL_AUX_NA_PARAM(Inserter)
     >
 struct transform
   : if_<
-          or_< is_na<Operation>, not_< is_sequence<Seq2OrOperation> > >
-        , transform1<Seq1,Seq2OrOperation>
-        , transform2<Seq1,Seq2OrOperation,Operation>
+          or_<
+              is_na<OperationOrInserter>
+            , not_< is_sequence<Seq2OrOperation> >
+            >
+        , transform1<Seq1,Seq2OrOperation,OperationOrInserter>
+        , transform2<Seq1,Seq2OrOperation,OperationOrInserter,Inserter>
         >::type
 {
 };
 
 BOOST_MPL_AUX_AGLORITHM_NAMESPACE_END
 
-BOOST_MPL_AUX_NA_ALGORITHM_SPEC(3, transform)
+BOOST_MPL_AUX_NA_ALGORITHM_SPEC(4, transform)
 
 }}
 
