70498d5856c3bf83956c9f19f07f5405b20f1779
Aleksey Gurtovoy <agurtovoy@meta-comm.com>
2004-08-24 22:06:43 +0000

    make transform_view explicit metafunction
    SVN-Revision: 24724

--- boost/boost/mpl/transform_view.hpp
+++ boost/boost/mpl/transform_view.hpp
@@ -2,11 +2,11 @@
 #ifndef BOOST_MPL_TRANSFORM_VIEW_HPP_INCLUDED
 #define BOOST_MPL_TRANSFORM_VIEW_HPP_INCLUDED
 
-// Copyright Aleksey Gurtovoy 2000-2002
+// Copyright Aleksey Gurtovoy 2000-2004
 //
-// Use, modification and distribution are subject to the Boost Software 
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy 
-// at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
 //
 // See http://www.boost.org/libs/mpl for documentation.
 
@@ -19,8 +19,7 @@
 #include <boost/mpl/aux_/transform_iter.hpp>
 #include <boost/mpl/aux_/na_spec.hpp>
 
-namespace boost {
-namespace mpl {
+namespace boost { namespace mpl {
 
 template<
       typename BOOST_MPL_AUX_NA_PARAM(Sequence)
@@ -35,13 +34,13 @@ struct transform_view
  
  public:
     struct tag;
+    typedef transform_view type;
     typedef aux::transform_iter< first_,last_,f_ > begin;
     typedef aux::transform_iter< last_,last_,f_ > end;
 };
 
 BOOST_MPL_AUX_NA_SPEC(2, transform_view)
 
-} // namespace mpl
-} // namespace boost
+}}
 
 #endif // BOOST_MPL_TRANSFORM_VIEW_HPP_INCLUDED
