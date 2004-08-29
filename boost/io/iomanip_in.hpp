230b773f73913ba042683890642ed4fdd9bfca3c
Daryle Walker <dwalker07@yahoo.com>
2004-08-29 22:01:12 +0000

    Removed input- and output-specific repeated-character manipulator types
    SVN-Revision: 2284

--- boost/io/iomanip_in.hpp
+++ boost/io/iomanip_in.hpp
@@ -25,87 +25,23 @@ namespace io
 
 //  Forward declarations  ----------------------------------------------------//
 
-template < typename Ch = char >
-    class multi_basic_skipper;
-
-class multi_skipper;
-
-multi_skipper  multi_skipl( std::streamsize count, bool final_sync = false );
+repeat_char  multi_skipl( std::streamsize count, bool final_sync = false );
 
 template < typename Ch, class Tr >
     std::basic_istream<Ch, Tr> &  skipl( std::basic_istream<Ch, Tr> &is );
 
 
-//  I/O-manipulator operator function declarations  --------------------------//
-
-template < typename Ch, class Tr >
-    std::basic_istream<Ch, Tr> &  operator >>( std::basic_istream<Ch, Tr> &is,
-     multi_basic_skipper<Ch> const &s );
-
-template < typename Ch, class Tr >
-    std::basic_istream<Ch, Tr> &  operator >>( std::basic_istream<Ch, Tr> &is,
-     multi_skipper const &s );
-
-
-//  I/O-manipulator object class (template) declarations  --------------------//
-
-template < typename Ch >
-class multi_basic_skipper
-    : private repeat_ch<Ch>
-{
-    typedef repeat_ch<Ch>  base_type;
-
-public:
-    // Template argument
-    typedef Ch  char_type;
-
-    // Lifetime management
-    multi_basic_skipper( char_type c, std::streamsize count,
-     bool final_sync = false );
-
-    // Accessors
-    using base_type::repeated_char;
-    using base_type::repeat_count;
-    using base_type::will_synchronize_afterwards;
-
-    // Operator
-    template < class Tr >
-    void  operator ()( ::std::basic_istream<Ch, Tr> &is ) const;
-
-};  // boost::io::multi_basic_skipper
-
-class multi_skipper
-    : private repeat_char
-{
-    typedef repeat_char  base_type;
-
-public:
-    // Lifetime management
-    multi_skipper( char c, ::std::streamsize count, bool final_sync = false );
-
-    // Accessors
-    using base_type::repeated_char;
-    using base_type::repeat_count;
-    using base_type::will_synchronize_afterwards;
-
-    // Operator
-    template < typename Ch, class Tr >
-    void  operator ()( ::std::basic_istream<Ch, Tr> &is ) const;
-
-};  // boost::io::multi_skipper
-
-
 //  I/O-manipulator function (template) definitions  -------------------------//
 
 inline
-multi_skipper
+repeat_char
 multi_skipl
 (
     std::streamsize  count,
     bool             final_sync  // = false
 )
 {
-    return multi_skipper( '\n', count, final_sync );
+    return repeat_char( '\n', count, final_sync );
 }
 
 template < typename Ch, class Tr >
@@ -117,82 +53,6 @@ skipl
 )
 {
     return is >> multi_skipl( 1 );
-}
-
-
-//  I/O-manipulator object class (template) member function definitions  -----//
-
-template < typename Ch >
-inline
-multi_basic_skipper<Ch>::multi_basic_skipper
-(
-    char_type        c,
-    std::streamsize  count,
-    bool             final_sync  // = false
-)
-    : base_type( c, count, final_sync )
-{
-}
-
-template < typename Ch >
-template < class Tr >
-inline
-void
-multi_basic_skipper<Ch>::operator ()
-(
-    std::basic_istream<Ch, Tr> &  is
-) const
-{
-    this->base_type::operator ()( is );
-}
-
-inline
-multi_skipper::multi_skipper
-(
-    char             c,
-    std::streamsize  count,
-    bool             final_sync  // = false
-)
-    : base_type( c, count, final_sync )
-{
-}
-
-template < typename Ch, class Tr >
-inline
-void
-multi_skipper::operator ()
-(
-    std::basic_istream<Ch, Tr> &  is
-) const
-{
-    this->base_type::operator ()( is );
-}
-
-
-//  I/O-manipulator operator function definitions  ---------------------------//
-
-template < typename Ch, class Tr >
-inline
-std::basic_istream<Ch, Tr> &
-operator >>
-(
-    std::basic_istream<Ch, Tr> &     is,
-    multi_basic_skipper<Ch> const &  s
-)
-{
-    return s( is ), is;
-}
-
-template < typename Ch, class Tr >
-inline
-std::basic_istream<Ch, Tr> &
-operator >>
-(
-    std::basic_istream<Ch, Tr> &  is,
-    multi_skipper const &         s
-)
-{
-    return s( is ), is;
 }
 
 
--- boost/io/iomanip_out.hpp
+++ boost/io/iomanip_out.hpp
@@ -25,87 +25,23 @@ namespace io
 
 //  Forward declarations  ----------------------------------------------------//
 
-template < typename Ch = char >
-    class multi_basic_newer;
-
-class multi_newer;
-
-multi_newer    multi_newl( std::streamsize count, bool final_flush = false );
+repeat_char  multi_newl( std::streamsize count, bool final_flush = false );
 
 template < typename Ch, class Tr >
     std::basic_ostream<Ch, Tr> &  newl( std::basic_ostream<Ch, Tr> &os );
 
 
-//  I/O-manipulator operator function declarations  --------------------------//
-
-template < typename Ch, class Tr >
-    std::basic_ostream<Ch, Tr> &  operator <<( std::basic_ostream<Ch, Tr> &os,
-     multi_basic_newer<Ch> const &n );
-
-template < typename Ch, class Tr >
-    std::basic_ostream<Ch, Tr> &  operator <<( std::basic_ostream<Ch, Tr> &os,
-     multi_newer const &n );
-
-
-//  I/O-manipulator object class (template) declarations  --------------------//
-
-template < typename Ch >
-class multi_basic_newer
-    : private repeat_ch<Ch>
-{
-    typedef repeat_ch<Ch>  base_type;
-
-public:
-    // Template argument
-    typedef Ch  char_type;
-
-    // Lifetime management
-    multi_basic_newer( char_type c, std::streamsize count,
-     bool final_flush = false );
-
-    // Accessors
-    using base_type::repeated_char;
-    using base_type::repeat_count;
-    using base_type::will_synchronize_afterwards;
-
-    // Operator
-    template < class Tr >
-    void  operator ()( ::std::basic_ostream<Ch, Tr> &os ) const;
-
-};  // boost::io::multi_basic_newer
-
-class multi_newer
-    : private repeat_char
-{
-    typedef repeat_char  base_type;
-
-public:
-    // Lifetime management
-    multi_newer( char c, ::std::streamsize count, bool final_flush = false );
-
-    // Accessors
-    using base_type::repeated_char;
-    using base_type::repeat_count;
-    using base_type::will_synchronize_afterwards;
-
-    // Operator
-    template < typename Ch, class Tr >
-    void  operator ()( ::std::basic_ostream<Ch, Tr> &os ) const;
-
-};  // boost::io::multi_newer
-
-
 //  I/O-manipulator function (template) definitions  -------------------------//
 
 inline
-multi_newer
+repeat_char
 multi_newl
 (
     std::streamsize  count,
     bool             final_flush  // = false
 )
 {
-    return multi_newer( '\n', count, final_flush );
+    return repeat_char( '\n', count, final_flush );
 }
 
 template < typename Ch, class Tr >
@@ -117,82 +53,6 @@ newl
 )
 {
     return os << multi_newl( 1 );
-}
-
-
-//  I/O-manipulator object class (template) member function definitions  -----//
-
-template < typename Ch >
-inline
-multi_basic_newer<Ch>::multi_basic_newer
-(
-    char_type        c,
-    std::streamsize  count,
-    bool             final_flush  // = false
-)
-    : base_type( c, count, final_flush )
-{
-}
-
-template < typename Ch >
-template < class Tr >
-inline
-void
-multi_basic_newer<Ch>::operator ()
-(
-    std::basic_ostream<Ch, Tr> &  os
-) const
-{
-    this->base_type::operator ()( os );
-}
-
-inline
-multi_newer::multi_newer
-(
-    char             c,
-    std::streamsize  count,
-    bool             final_flush  // = false
-)
-    : base_type( c, count, final_flush )
-{
-}
-
-template < typename Ch, class Tr >
-inline
-void
-multi_newer::operator ()
-(
-    std::basic_ostream<Ch, Tr> &  os
-) const
-{
-    this->base_type::operator ()( os );
-}
-
-
-//  I/O-manipulator operator function definitions  ---------------------------//
-
-template < typename Ch, class Tr >
-inline
-std::basic_ostream<Ch, Tr> &
-operator <<
-(
-    std::basic_ostream<Ch, Tr> &   os,
-    multi_basic_newer<Ch> const &  n
-)
-{
-    return n( os ), os;
-}
-
-template < typename Ch, class Tr >
-inline
-std::basic_ostream<Ch, Tr> &
-operator <<
-(
-    std::basic_ostream<Ch, Tr> &  os,
-    multi_newer const &           n
-)
-{
-    return n( os ), os;
 }
 
 
