23c68262a632e548c51f9968fae2e8d6cc8d09d3
Dave Abrahams <dave@boostpro.com>
2004-09-20 12:49:23 +0000

    Copyright
    SVN-Revision: 7237

--- langbinding/branches/langbinding/boost/langbinding/function/config.hpp
+++ langbinding/branches/langbinding/boost/langbinding/function/config.hpp
@@ -1,9 +1,13 @@
-#ifndef BOOST_LANGBINDING_FUNCTION_CONFIG_040916_HPP
-#define BOOST_LANGBINDING_FUNCTION_CONFIG_040916_HPP
+// Copyright David Abrahams 2004. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#ifndef CONFIG_DWA2004918_HPP
+# define CONFIG_DWA2004918_HPP
+
+# include <boost/langbinding/aux_/config.hpp>
 
 # ifndef BOOST_LANGBINDING_FUNCTION_MAX_ARITY
 #  define BOOST_LANGBINDING_FUNCTION_MAX_ARITY 5
 # endif
 
-#endif // BOOST_LANGBINDING_FUNCTION_CONFIG_040916_HPP
-
+#endif // CONFIG_DWA2004918_HPP
