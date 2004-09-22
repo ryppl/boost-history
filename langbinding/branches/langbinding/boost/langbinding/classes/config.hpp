4e239d425d5384ab82ce098b68c12cffd6a69328
Daniel Wallin <daniel@boostpro.com>
2004-09-22 11:42:14 +0000

    inheritance from BPL
    SVN-Revision: 7238

---
+++ langbinding/branches/langbinding/boost/langbinding/classes/config.hpp
@@ -0,0 +1,11 @@
+// Copyright Daniel Wallin 2004. Use, modification and distribution is
+// subject to the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_LANGBINDING_CLASSES_CONFIG_040922_HPP
+#define BOOST_LANGBINDING_CLASSES_CONFIG_040922_HPP
+
+# include <boost/langbinding/config.hpp>
+
+#endif // BOOST_LANGBINDING_CLASSES_CONFIG_040922_HPP
+
