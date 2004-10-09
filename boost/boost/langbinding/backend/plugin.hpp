f7f2325511665f6c839b383887b5dc3081f5c9df
Dave Abrahams <dave@boostpro.com>
2004-10-09 23:59:20 +0000

    Add comments.
    SVN-Revision: 25644

--- boost/boost/langbinding/backend/plugin.hpp
+++ boost/boost/langbinding/backend/plugin.hpp
@@ -12,21 +12,27 @@
 
 namespace boost { namespace langbinding { namespace backend { 
 
+// Base class for plugins implementing backend language-specific
+// functionality.
 class BOOST_LANGBINDING_DECL plugin
 {
  public:
     plugin()
-      : id_(count++)
+      : id_(count++)            // assign a unique ID
     {}
     
     virtual ~plugin();
-    
-    virtual void* call(
-        void* function
-      , call_xxx_data& signature_constants
-      , void* result_storage
-      , void* const* arg_storage) const = 0;
 
+    // Implements a call to a backend function.  Used to supply the
+    // interface for overridable virtual functions on wrapped classes.
+    virtual void* call(
+        void* function                          // backend-specific function data
+      , call_xxx_data& signature_constants      // constant data associated with the call signature.
+      , void* result_storage                    // raw memory in which to store the result.
+      , void* const* arg_storage                // an array of pointers to the arguments
+    ) const = 0;
+
+    // Find an override for the named function on the given class instance.
     virtual override
     find_override(char const* function_name, class_instance const&) const = 0;
 
