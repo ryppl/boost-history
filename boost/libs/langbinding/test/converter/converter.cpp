90fcf60ecf65679bcac1cd4fa20b6c1b5e3a4c1a
Daniel Wallin <daniel@boostpro.com>
2004-10-11 16:22:23 +0000

    *** empty log message ***
    SVN-Revision: 25670

--- boost/libs/langbinding/test/converter/converter.cpp
+++ boost/libs/langbinding/test/converter/converter.cpp
@@ -10,42 +10,8 @@
 using boost::langbinding::util::type_id;
 using namespace boost::langbinding::converter;
 
-struct int_ : converter<int_, std::string, int>
-{
-    static void construct(std::string const& src, void* storage)
-    {
-        new (storage) int(boost::lexical_cast<int>(src));
-    }
-
-    static void* convertible(std::string const& src)
-    {
-        try
-        {
-            boost::lexical_cast<int>(src);
-        }
-        catch (boost::bad_lexical_cast&)
-        {
-            return 0;
-        }
-
-        return yes;
-    }
-};
-
 int main()
 {
-    int_ register_int;
-
-    std::string arg("42");
-    arg_conversion result = registry::convert(
-        &arg
-      , registry::get(type_id<int>())->rvalue_converters
-    );
-
-    assert(result.convertible);
-
-    int x;
-    result.construct(result.source, &x);
-    assert(x == 42);
+    // rewrite
 }
 
