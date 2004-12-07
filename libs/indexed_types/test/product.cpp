b4d6e53a179778294f348398ffa2c83725412d04
Larry Joe Evans <cppljevans@suddenlink.net>
2004-12-07 00:45:19 +0000

    Accommodate change to product.hpp
    SVN-Revision: 2395

--- libs/indexed_types/test/product.cpp
+++ libs/indexed_types/test/product.cpp
@@ -3,80 +3,44 @@
 //
 #include "boost/indexed_types/product.hpp"
 
-enum index { a, b, c, last_ };
+enum indices { a, b, c };
 
-template<index> struct field_ndx;
+namespace boost{namespace indexed_types{
+template<>
+struct enum_size<indices>{static unsigned const value=c+1;};
+}}//exit boost::indexed_types namespace
+
+template<indices> struct field_ndx;
 
 template<> struct field_ndx<a> 
 {
-    static index const our_ndx=a;
+    static indices const our_ndx=a;
 };
 template<> struct field_ndx<b> 
 {
-    static index const our_ndx=b;
+    static indices const our_ndx=b;
 };
 template<> struct field_ndx<c> 
 {
-    static index const our_ndx=c;
+    static indices const our_ndx=c;
 };
 
-template<unsigned i>
+template<indices i>
 struct field_types
 {
-    typedef field_ndx<index(i)> type;
+    typedef field_ndx<i> type;
 };
 
 template<>
 struct field_types<c>
 //Purpose:
-//  Illustrate duplication of types in 
-//  SeqOfTypes(template arg to product)
+//  Illustrate repetition of types in 
+//  TypeMap (template arg to product)
 //  is possible.
 {
     typedef field_ndx<b> type;
 };
 
-//Acknowlegement:
-//  The following upto the //==================== comment line
-//  was from email with headers:
-/*
-From: 	Aleksey Gurtovoy <agurtovoy@meta-comm.com>
-To: 	"'Larry Evans'" <cppljevans@cox-internet.com>
-Subject: RE: typo mpl_paper.pdf  in 2.2.4 ?
-Date: 	Fri, 3 Dec 2004 18:52:27 -0600 
- */
-//
-
-#include <boost/mpl/transform.hpp>
-#include <boost/mpl/range_c.hpp>
-#include <boost/mpl/vector.hpp>
-#include <boost/mpl/back_inserter.hpp>
-
-template< typename Index > struct index_range;
-
-template<
-      template< unsigned > class FieldType
-    >
-struct make_field_type
-{
-    template< typename Index > struct apply
-    {
-        typedef typename FieldType<Index::value>::type type;
-    };
-};
-
-template<> struct index_range<index>
-    : boost::mpl::range_c<int,a,last_>
-{
-};
-
-    typedef boost::mpl::transform<
-          index_range<index>::type
-        , make_field_type<field_types>
-        , boost::mpl::back_inserter< boost::mpl::vector<> >
-        >::type field_seq;
-
-//=======================================        
 #include <boost/test/unit_test.hpp>
 #include <boost/type_traits/is_same.hpp>
 using namespace boost;
@@ -84,11 +48,8 @@ using namespace boost;
 namespace itps = indexed_types;
 
 
-void field_seq_test()
+void field_types_test()
 {
-    BOOST_CHECK((is_same<field_types<a>::type,mpl::at_c<field_seq,a>::type >::value));
-    BOOST_CHECK((is_same<field_types<b>::type,mpl::at_c<field_seq,b>::type >::value));
-    BOOST_CHECK((is_same<field_types<c>::type,mpl::at_c<field_seq,c>::type >::value));
     BOOST_CHECK((is_same<field_types<a>::type,field_ndx<a> >::value));
     BOOST_CHECK((is_same<field_types<b>::type,field_ndx<b> >::value));
     BOOST_CHECK((is_same<field_types<c>::type,field_ndx<b> >::value));
@@ -96,20 +57,20 @@ void field_seq_test()
 
 void factor_test()
 {
-    BOOST_CHECK((is_same<itps::factor<field_seq,a+1>::type,field_ndx<a> >::value));
-    BOOST_CHECK((is_same<itps::factor<field_seq,b+1>::type,field_ndx<b> >::value));
-    BOOST_CHECK((is_same<itps::factor<field_seq,c+1>::type,field_ndx<b> >::value));
+    BOOST_CHECK((is_same<itps::factor<indices,field_types,a+1>::type,field_ndx<a> >::value));
+    BOOST_CHECK((is_same<itps::factor<indices,field_types,b+1>::type,field_ndx<b> >::value));
+    BOOST_CHECK((is_same<itps::factor<indices,field_types,c+1>::type,field_ndx<b> >::value));
 }
 
 void project_test()
 {
-    itps::product<field_seq> r;
+    itps::product<indices,field_types> r;
     field_ndx<a>*pa = &r.project<a>();
     field_ndx<b>*pb = &r.project<b>();
     field_ndx<b>*pc = &r.project<c>();
-    BOOST_CHECK((pa == &r.::itps::factor<field_seq,a+1>::my_field));
-    BOOST_CHECK((pb == &r.::itps::factor<field_seq,b+1>::my_field));
-    BOOST_CHECK((pc == &r.::itps::factor<field_seq,c+1>::my_field));
+    BOOST_CHECK((pa == &r.::itps::factor<indices,field_types,a+1>::my_field));
+    BOOST_CHECK((pb == &r.::itps::factor<indices,field_types,b+1>::my_field));
+    BOOST_CHECK((pc == &r.::itps::factor<indices,field_types,c+1>::my_field));
 }
 
 namespace test = unit_test_framework;
@@ -117,7 +78,7 @@ namespace test = unit_test_framework;
 test::test_suite* init_unit_test_suite(int argc, char* argv[])
 {
     test::test_suite* test = BOOST_TEST_SUITE("dependent function tests");
-    test->add(BOOST_TEST_CASE(&field_seq_test));
+    test->add(BOOST_TEST_CASE(&field_types_test));
     test->add(BOOST_TEST_CASE(&factor_test));
     test->add(BOOST_TEST_CASE(&project_test));
     return test;
