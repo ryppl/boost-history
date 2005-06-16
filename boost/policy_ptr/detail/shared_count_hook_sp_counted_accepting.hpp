811326cf08df1fa85a9adecbf23424a1d85807b8
Larry Joe Evans <cppljevans@suddenlink.net>
2005-06-16 21:36:21 +0000

    end renaming to shared_count_hook* from shared_count*
    SVN-Revision: 2637

---
+++ boost/policy_ptr/detail/shared_count_hook_sp_counted_accepting.hpp
@@ -0,0 +1,175 @@
+//specialization of shared_count_hook for collecting cycles using precise pointer location
+#ifndef BOOST_POLICY_PTR_DETAIL_SHARED_COUNT_HOOK_SP_COUNTED_ACCEPTING_HPP_LJE20050514
+#define BOOST_POLICY_PTR_DETAIL_SHARED_COUNT_HOOK_SP_COUNTED_ACCEPTING_HPP_LJE20050514
+#include "boost/policy_ptr/detail/shared_count.hpp"
+#include "boost/policy_ptr/detail/sp_counted_accepting.hpp"
+#include "boost/fields_visitor/fields_visitor.hpp"
+#ifdef CONTAINER_SINGLE_OBJ_ID
+#include "boost/utility/obj_id.hpp"
+#endif
+
+namespace boost
+{
+namespace policy_ptr
+{
+namespace detail
+{
+
+template<class Referent>
+class sp_counted_impl_hook<Referent,sp_counted_accepting>
+  : public sp_counted_accepting
+/**@brief
+ *  Specialization for collecting cycles (must be declared before
+ *  used [indirectly] in next declaration ).
+ */
+{
+    
+ protected:
+ 
+    typedef Referent ref_type;
+    
+    virtual ref_type* typed_ref_start(void)const
+    =0
+    ;
+    
+    sp_counted_impl_hook( Referent* p )
+    {}
+    
+    //void dispose_hook()
+    //  is defined in sp_counted_accepting
+    
+      void
+    accept_each(shared_count_visitor_abs& a_visitor)const
+    ;
+};
+  
+template<>
+class shared_count_hook<sp_counted_accepting>
+  : public shared_count_base<sp_counted_accepting>
+#ifdef CONTAINER_SINGLE_OBJ_ID
+  , public utility::obj_id
+#endif
+  , public fields_visitor::field_registrar<shared_count_visitor_abs>
+/**@brief
+ *  Same as general template (defined in shared_count.hpp)
+ *  but, in addition, allows collection of cycles by precisely
+ *  locating smart pointers via the field_registrar superclass.
+ */
+{
+    friend class shared_count_visitor_abs;
+    
+        typedef 
+      shared_count_base<sp_counted_accepting>
+    count_super_type;
+    
+      typedef fields_visitor::field_registrar<shared_count_visitor_abs> 
+    registrar_super_type;
+    
+ public:
+    shared_count_hook(void)
+      : count_super_type()
+      , registrar_super_type(this)
+    { }
+    
+    shared_count_hook(shared_count_hook const & r)
+      : count_super_type(r)
+      , registrar_super_type(this)
+    { }
+    
+    template<class Y>
+    shared_count_hook(Y* p)
+      : count_super_type(p, checked_deleter<Y>() )
+      , registrar_super_type(this)
+    { }
+
+ private:
+      sp_counted_accepting*
+    get_counter(void)
+    {
+        return this->pi_;
+    }   
+};
+
+class shared_count_visitor_abs
+{
+ public:
+        virtual
+    ~shared_count_visitor_abs(void)
+    {}
+      void
+    visit_field(shared_count_hook<sp_counted_accepting>& a_count)
+    {
+        if(a_count.is_valid())
+        {
+            visit_count(a_count);
+        }
+    }
+
+      template
+      < class X
+      >    
+      void
+    visit_field(X& a_field)
+    {
+          typedef typename fields_visitor::field_iterator_of
+          < shared_count_visitor_abs
+          , X
+          > 
+        iter_type;
+        iter_type a_iter(&a_field);
+        for(; !a_iter.empty(); a_iter.increment())
+        {
+            a_iter.accept(*this);
+        }
+    }
+    
+ protected:
+ 
+         static 
+      sp_counted_accepting*
+    get_counter(shared_count_hook<sp_counted_accepting>& a_count)
+    {
+        return a_count.get_counter();
+    }  
+    
+ private:    
+ 
+        virtual
+      void
+    visit_count(shared_count_hook<sp_counted_accepting>& a_count)
+    =0
+    ;
+};
+
+template<class Referent>
+      void
+sp_counted_impl_hook<Referent,sp_counted_accepting>::
+    accept_each(shared_count_visitor_abs& a_visitor)const
+    {
+          typedef typename fields_visitor::field_iterator_of
+          < shared_count_visitor_abs
+          , Referent
+          > 
+        iter_type;
+        Referent* pref=this->typed_ref_start();
+        iter_type a_iter(pref);
+        for(; !a_iter.empty(); a_iter.increment())
+        {
+            a_iter.accept(a_visitor);
+        }
+    }
+    
+}//exit detail namespace
+}//exit policy_ptr namespace
+}//exit boost namespace
+//----------------------------------------
+//ChangeLog:
+//  2005-06-15: Larry Evans
+//    WHAT:
+//      Renamed from shared_count_sp_counted_accepting and renamed
+//      several classes.
+//    WHY:
+//      Part of changes described in shared_count.hpp ChangeLog.
+//      These changed were needed because of, amoung other changes,
+//      a change in the inheritance heirarchy.
+#endif
---
+++ boost/policy_ptr/detail/shared_count_hook_sp_counted_tagged.hpp
@@ -0,0 +1,74 @@
+//specialization of shared_count_hook for collecting cycles using "tagged" shared_count
+#ifndef BOOST_POLICY_PTR_DETAIL_SHARED_COUNT_HOOK_SP_COUNTED_TAGGED_HPP_INCLUDED
+#define BOOST_POLICY_PTR_DETAIL_SHARED_COUNT_HOOK_SP_COUNTED_TAGGED_HPP_INCLUDED
+#include "boost/policy_ptr/detail/shared_count.hpp"
+#include "boost/policy_ptr/detail/sp_counted_impl_hook_sp_counted_tagged.hpp"
+
+namespace boost
+{
+namespace policy_ptr
+{
+namespace detail
+{
+
+int const shared_count_id = 0x2C35F101; //value of "tag"
+
+template<>
+class shared_count_hook<sp_counted_tagged>
+  : public shared_count_base<sp_counted_tagged>
+/**@brief
+ *  Same as general template (defined in shared_count.hpp)
+ *  but, in addition, add's a "tag" (shared_count_id),
+ *  which is zeroed in DTOR.
+ */
+{
+        typedef 
+      shared_count_base<sp_counted_tagged>
+    count_super_type;
+    
+ protected:
+ 
+    int id_; //the "tag"
+
+    shared_count_hook(void)
+      : count_super_type()
+      , id_(shared_count_id)
+   { 
+   }
+    
+    shared_count_hook(shared_count_hook const & r)
+      : count_super_type(r)
+      , id_(shared_count_id)
+    { 
+    }
+    
+    template<class Y>
+    shared_count_hook(Y* p)
+      : count_super_type(p, checked_deleter<Y>() )
+      , id_(shared_count_id)
+    {
+    }
+
+    template<class P, class D> 
+    shared_count_hook(P p, D d)
+      : count_super_type(p,d)
+    { }
+    
+    ~shared_count_hook()
+    {
+        id_ = 0;
+    }
+    
+};
+
+}//exit detail namespace
+}//exit policy_ptr namespace
+}//exit boost namespace
+//----------------------------------------
+//ChangeLog:
+//  2005-06-15: Larry Evans
+//    WHAT:
+//      renamed from shared_count_sp_counted_tagged.
+//    WHY:
+//      part of changes described in shared_count.hpp ChangeLog.
+#endif
