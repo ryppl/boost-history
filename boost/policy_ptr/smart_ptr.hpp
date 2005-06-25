64f47784a46aff49839686d7722bab1bb995995c
Larry Joe Evans <cppljevans@suddenlink.net>
2005-06-25 12:49:45 +0000

    Finish soln to shared_count_hook_sp_counted_accepting.hpp problem mentioned in 2005-06-23 ChangeLog entry for that file.
    SVN-Revision: 2651

--- boost/policy_ptr/smart_ptr.hpp
+++ boost/policy_ptr/smart_ptr.hpp
@@ -16,7 +16,6 @@
 #include <boost/type_traits/is_class.hpp>
 #include <boost/type_traits/same_traits.hpp>
 #include <boost/type_traits/is_const.hpp>
-#include <boost/type_traits/remove_cv.hpp>
 #include <boost/mpl/identity.hpp>
 #include <boost/mpl/if.hpp>
 #include <boost/mpl/apply.hpp>
@@ -24,11 +23,8 @@
 #include <boost/mpl/list.hpp>
 #include <boost/mpl/find_if.hpp>
 #include <boost/mpl/eval_if.hpp>
-#include <boost/mpl/has_xxx.hpp>
-#include <boost/mpl/and.hpp>
 #include <boost/mpl/not.hpp>
 #include <boost/mpl/empty_base.hpp>
-#include <boost/preprocessor/repetition/enum_params.hpp>
 #include <boost/static_assert.hpp>
 #include <boost/type_traits/is_convertible.hpp>
 #include <boost/utility/enable_if.hpp>
@@ -63,21 +59,6 @@ namespace boost
     };
     //------------------------------------------------------------------------
 }   // namespace boost
-//----------------------------------------------------------------------------
-// Utility Macros
-#ifndef BOOST_NO_MEMBER_TEMPLATE_FRIENDS
-# define BOOST_SP_CONVERT_ARGUMENT(Policy, obj, field)                       \
-    ((obj).field)
-# define BOOST_SP_DECLARE_TEMPLATE_FRIEND(Param, type, arity)                \
-    template <BOOST_PP_ENUM_PARAMS(arity, class Param)>                      \
-    friend class type;                   
-# define BOOST_SP_PRIVATE private
-#else // BOOST_NO_MEMBER_TEMPLATE_FRIENDS
-# define BOOST_SP_CONVERT_ARGUMENT(policy, obj, field)                       \
-    (reinterpret_cast<policy const&>(obj).field)
-# define BOOST_SP_DECLARE_TEMPLATE_FRIEND(Param, type, arity)
-# define BOOST_SP_PRIVATE public
-#endif // BOOST_NO_MEMBER_TEMPLATE_FRIENDS
 //----------------------------------------------------------------------------
 // Configuration Macros
 #ifdef BOOST_SP_INVARIANT_CHECKING
@@ -130,12 +111,6 @@ namespace boost
         private:
             Ptr& ptr_;
             move_source(const Ptr&);
-        };
-
-        //--------------------------------------------------------------------
-        struct empty_policy
-        {
-            typedef mpl::void_ policy_category;
         };
 
         //--------------------------------------------------------------------
@@ -211,74 +186,15 @@ namespace boost
     }   // namespace detail
     }   // namespace policy_ptr
 
-    //------------------------------------------------------------------------
-    // Type generator smart_ptr
-    template <
-        class P1 = policy_ptr::detail::empty_policy,
-        class P2 = policy_ptr::detail::empty_policy,
-        class P3 = policy_ptr::detail::empty_policy,
-        class P4 = policy_ptr::detail::empty_policy
-    >
-    struct smart_ptr
-    {
-        template <typename T>
-        class to;
-        BOOST_SP_DECLARE_TEMPLATE_FRIEND(to, T, 1)
-        BOOST_SP_DECLARE_TEMPLATE_FRIEND(smart_ptr, Q, 4)
-    BOOST_SP_PRIVATE:
-        template <typename SmartPtr>
-        static typename SmartPtr::stored_type 
-        clone(const SmartPtr& ptr, const typename SmartPtr::stored_type& p)
-        {
-            return ptr.do_clone(p);
-        }
-
-        template <typename SmartPtr>
-        static typename SmartPtr::stored_type 
-        clone(const SmartPtr& ptr, typename SmartPtr::stored_type& p)
-        {
-            return ptr.do_clone(p);
-        }
-    };
+}//exit boost namespace
+#include "boost/policy_ptr/detail/is_smart_ptr.hpp"
+namespace boost
+{
     //------------------------------------------------------------------------
     namespace policy_ptr 
     {
     namespace detail
     {
-        //BOOST_MPL_HAS_XXX_TRAIT_NAMED_DEF(has_enclosing_class, enclosing_class, true)
-        BOOST_MPL_HAS_XXX_TRAIT_NAMED_DEF(has_first_policy, first_policy, true)
-        BOOST_MPL_HAS_XXX_TRAIT_NAMED_DEF(has_second_policy, second_policy, true)
-        BOOST_MPL_HAS_XXX_TRAIT_NAMED_DEF(has_third_policy, third_policy, true)
-        BOOST_MPL_HAS_XXX_TRAIT_NAMED_DEF(has_fourth_policy, fourth_policy, true)
-        BOOST_MPL_HAS_XXX_TRAIT_NAMED_DEF(has_element_type, element_type, true)
-
-        template<class SmartPtr>
-        struct is_smart_ptr_impl 
-            : is_same<
-                  SmartPtr,
-                  typename 
-                  smart_ptr<
-                      typename SmartPtr::first_policy,
-                      typename SmartPtr::second_policy,
-                      typename SmartPtr::third_policy,
-                      typename SmartPtr::fourth_policy
-                  >::template to<typename SmartPtr::element_type>
-              >
-            { };
-
-        template<class SmartPtr>
-        struct is_smart_ptr
-            : mpl::and_< // Default arity of and_ is 5.
-                  mpl::and_<
-                      has_first_policy<SmartPtr>,
-                      has_second_policy<SmartPtr>,
-                      has_third_policy<SmartPtr>,
-                      has_fourth_policy<SmartPtr>,
-                      has_element_type<SmartPtr>
-                  >,
-                  is_smart_ptr_impl<typename remove_cv<SmartPtr>::type>
-              >
-            { };
 
         template<class P1, class P2, class P3, class P4, class SmartPtr>
         struct is_const_smart_ptr_impl 
