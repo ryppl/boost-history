b15f749ec228b42fa1590b775857bbd3cf2633b8
Ion Gaztañaga <igaztanaga@gmail.com>
2006-01-12 17:09:21 +0000

    Add new casts until next boost includes pointer_cast utilities
    SVN-Revision: 2809

--- boost/shmem/detail/generic_cast.hpp
+++ boost/shmem/detail/generic_cast.hpp
@@ -34,6 +34,42 @@ inline T do_static_cast_impl(const U r, const boost::true_type)
    return static_cast<T>(r);  
 }
 
+template<class T, class U> 
+inline T do_dynamic_cast_impl(const U &r, const boost::false_type)
+{  
+   return dynamic_pointer_cast<typename T::value_type>(r);  
+}
+
+template<class T, class U> 
+inline T do_dynamic_cast_impl(const U r, const boost::true_type)
+{  
+   return dynamic_cast<T>(r);  
+}
+
+template<class T, class U> 
+inline T do_reinterpret_cast_impl(const U &r, const boost::false_type)
+{  
+   return reinterpret_pointer_cast<typename T::value_type>(r);  
+}
+
+template<class T, class U> 
+inline T do_reinterpret_cast_impl(const U r, const boost::true_type)
+{  
+   return reinterpret_cast<T>(r);  
+}
+
+template<class T, class U> 
+inline T do_const_cast_impl(const U &r, const boost::false_type)
+{  
+   return const_pointer_cast<typename T::value_type>(r);  
+}
+
+template<class T, class U> 
+inline T do_const_cast_impl(const U r, const boost::true_type)
+{  
+   return const_cast<T>(r);  
+}
+
 }  //namespace detail {
 
 template<class T, class U> 
@@ -42,6 +78,23 @@ inline T do_static_cast(const U &r)
    return detail::do_static_cast_impl<T>(r, typename boost::is_pointer<U>::type());  
 }
 
+template<class T, class U> 
+inline T do_dynamic_cast(const U &r)
+{  
+   return detail::do_dynamic_cast_impl<T>(r, typename boost::is_pointer<U>::type());  
+}
+
+template<class T, class U> 
+inline T do_reinterpret_cast(const U &r)
+{  
+   return detail::do_reinterpret_cast_impl<T>(r, typename boost::is_pointer<U>::type());  
+}
+
+template<class T, class U> 
+inline T do_const_cast(const U &r)
+{  
+   return detail::do_const_cast_impl<T>(r, typename boost::is_pointer<U>::type());  
+}
 
 
 }}   //namespace boost { namespace shmem { 
