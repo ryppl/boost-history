6efc32d78a871d25182afd2a21ad666430a301b2
Ion Gaztañaga <igaztanaga@gmail.com>
2006-01-12 17:14:54 +0000

    Added possibility to release the lock
    SVN-Revision: 2810

--- boost/shmem/sync/lock.hpp
+++ boost/shmem/sync/lock.hpp
@@ -75,141 +75,149 @@ template <typename Mutex>
 class scoped_lock : private noncopyable
 {
 public:
-    typedef Mutex mutex_type;
-    friend class boost::shmem::shared_condition;
+   typedef Mutex mutex_type;
+   friend class boost::shmem::shared_condition;
 
-    explicit scoped_lock(Mutex& mx, bool initially_locked=true)
-        : m_mutex(mx), m_locked(false)
-    {
-        if (initially_locked) lock();
-    }
-    ~scoped_lock()
-    {
-        if (m_locked) unlock();
-    }
+   explicit scoped_lock(Mutex& mx, bool initially_locked=true)
+      : m_mutex(mx), m_locked(false)
+      {  if (initially_locked) lock(); }
+   ~scoped_lock()
+      {  if (m_locked) unlock(); }
 
-    void lock()
-    {
-        if (m_locked) throw boost::shmem::lock_exception();
-        lock_ops<Mutex>::lock(m_mutex);
-        m_locked = true;
-    }
-    void unlock()
-    {
-        if (!m_locked) throw boost::shmem::lock_exception();
-        lock_ops<Mutex>::unlock(m_mutex);
-        m_locked = false;
-    }
+   void lock()
+   {
+      if (m_locked) throw boost::shmem::lock_exception();
+      lock_ops<Mutex>::lock(m_mutex);
+      m_locked = true;
+   }
 
-    bool locked() const { return m_locked; }
-    operator const void*() const { return m_locked ? this : 0; }
+   void unlock()
+   {
+      if (!m_locked) throw boost::shmem::lock_exception();
+      lock_ops<Mutex>::unlock(m_mutex);
+      m_locked = false;
+   }
 
-private:
-    Mutex& m_mutex;
-    bool m_locked;
+   bool locked() const { return m_locked; }
+
+   operator const void*() const { return m_locked ? this : 0; }
+
+   void release()
+      {  m_locked = false; }
+
+   private:
+   Mutex& m_mutex;
+   bool m_locked;
 };
 
 template <typename TryMutex>
 class scoped_try_lock : private noncopyable
 {
 public:
-    typedef TryMutex mutex_type;
-    friend class boost::shmem::shared_condition;
+   typedef TryMutex mutex_type;
+   friend class boost::shmem::shared_condition;
 
-    explicit scoped_try_lock(TryMutex& mx)
-        : m_mutex(mx), m_locked(false)
-    {
-        try_lock();
-    }
-    scoped_try_lock(TryMutex& mx, bool initially_locked)
-        : m_mutex(mx), m_locked(false)
-    {
-        if (initially_locked) lock();
-    }
-    ~scoped_try_lock()
-    {
-        if (m_locked) unlock();
-    }
+   explicit scoped_try_lock(TryMutex& mx)
+      : m_mutex(mx), m_locked(false)
+      {  try_lock(); }
 
-    void lock()
-    {
-        if (m_locked) throw boost::shmem::lock_exception();
-        lock_ops<TryMutex>::lock(m_mutex);
-        m_locked = true;
-    }
-    bool try_lock()
-    {
-        if (m_locked) throw boost::shmem::lock_exception();
-        return (m_locked = lock_ops<TryMutex>::trylock(m_mutex));
-    }
-    void unlock()
-    {
-        if (!m_locked) throw boost::shmem::lock_exception();
-        lock_ops<TryMutex>::unlock(m_mutex);
-        m_locked = false;
-    }
+   scoped_try_lock(TryMutex& mx, bool initially_locked)
+      : m_mutex(mx), m_locked(false)
+      {  if (initially_locked) lock(); }
 
-    bool locked() const { return m_locked; }
-    operator const void*() const { return m_locked ? this : 0; }
+   ~scoped_try_lock()
+      {  if (m_locked) unlock(); }
 
-private:
+   void lock()
+   {
+      if (m_locked) throw boost::shmem::lock_exception();
+      lock_ops<TryMutex>::lock(m_mutex);
+      m_locked = true;
+   }
 
-    TryMutex& m_mutex;
-    bool m_locked;
+   bool try_lock()
+   {
+      if (m_locked) throw boost::shmem::lock_exception();
+      return (m_locked = lock_ops<TryMutex>::trylock(m_mutex));
+   }
+
+   void unlock()
+   {
+      if (!m_locked) throw boost::shmem::lock_exception();
+      lock_ops<TryMutex>::unlock(m_mutex);
+      m_locked = false;
+   }
+
+   bool locked() const { return m_locked; }
+
+   operator const void*() const { return m_locked ? this : 0; }
+
+   void release()
+      {  m_locked = false; }
+
+   private:
+
+   TryMutex& m_mutex;
+   bool m_locked;
 };
 
 template <typename TimedMutex>
 class scoped_timed_lock : private noncopyable
 {
 public:
-    typedef TimedMutex mutex_type;
+   typedef TimedMutex mutex_type;
 
-    scoped_timed_lock(TimedMutex& mx, const xtime& xt)
-        : m_mutex(mx), m_locked(false)
-    {
-        timed_lock(xt);
-    }
-    scoped_timed_lock(TimedMutex& mx, bool initially_locked)
-        : m_mutex(mx), m_locked(false)
-    {
-        if (initially_locked) lock();
-    }
-    ~scoped_timed_lock()
-    {
-        if (m_locked) unlock();
-    }
+   scoped_timed_lock(TimedMutex& mx, const xtime& xt)
+      : m_mutex(mx), m_locked(false)
+   {
+      timed_lock(xt);
+   }
+   scoped_timed_lock(TimedMutex& mx, bool initially_locked)
+      : m_mutex(mx), m_locked(false)
+   {
+      if (initially_locked) lock();
+   }
+   ~scoped_timed_lock()
+      {  if (m_locked) unlock(); }
 
-    void lock()
-    {
-        if (m_locked) throw lock_exception();
-        lock_ops<TimedMutex>::lock(m_mutex);
-        m_locked = true;
-    }
-    bool try_lock()
-    {
-        if (m_locked) throw lock_exception();
-        return (m_locked = lock_ops<TimedMutex>::trylock(m_mutex));
-    }
-    bool timed_lock(const xtime& xt)
-    {
-        if (m_locked) throw lock_exception();
-        return (m_locked = lock_ops<TimedMutex>::timedlock(m_mutex, xt));
-    }
-    void unlock()
-    {
-        if (!m_locked) throw lock_exception();
-        lock_ops<TimedMutex>::unlock(m_mutex);
-        m_locked = false;
-    }
+   void lock()
+   {
+      if (m_locked) throw lock_exception();
+      lock_ops<TimedMutex>::lock(m_mutex);
+      m_locked = true;
+   }
 
-    bool locked() const { return m_locked; }
-    operator const void*() const { return m_locked ? this : 0; }
+   bool try_lock()
+   {
+      if (m_locked) throw lock_exception();
+      return (m_locked = lock_ops<TimedMutex>::trylock(m_mutex));
+   }
 
-private:
-    friend class boost::shmem::shared_condition;
+   bool timed_lock(const xtime& xt)
+   {
+      if (m_locked) throw lock_exception();
+      return (m_locked = lock_ops<TimedMutex>::timedlock(m_mutex, xt));
+   }
 
-    TimedMutex& m_mutex;
-    bool m_locked;
+   void unlock()
+   {
+      if (!m_locked) throw lock_exception();
+      lock_ops<TimedMutex>::unlock(m_mutex);
+      m_locked = false;
+   }
+
+   bool locked() const { return m_locked; }
+
+   operator const void*() const { return m_locked ? this : 0; }
+
+   void release()
+      {  m_locked = false; }
+
+   private:
+   friend class boost::shmem::shared_condition;
+
+   TimedMutex& m_mutex;
+   bool m_locked;
 };
 
 } // namespace shmem
