789ea519f4dd3249d4bb40e65ab6e40fe0db5e5d
Marcin Kalicinski <kalita@poczta.onet.pl>
2006-02-14 22:09:44 +0000

    no message
    SVN-Revision: 2874

--- boost/profiler.hpp
+++ boost/profiler.hpp
@@ -5,5 +5,6 @@
 #include <boost/profiler/detail/context_iterators.hpp>
 #include <boost/profiler/detail/dump.hpp>
 #include <boost/profiler/detail/macros.hpp>
+#include <boost/current_function.hpp>
 
 #endif
