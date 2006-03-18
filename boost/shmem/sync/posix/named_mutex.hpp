fdadebf1fae9e3b0607fdb6abf2020c21f8a81c7
Ion Gaztañaga <igaztanaga@gmail.com>
2006-03-18 11:05:38 +0000

    Used a named semaphore to emulate the name mutex
    SVN-Revision: 2887

--- boost/shmem/sync/posix/named_mutex.hpp
+++ boost/shmem/sync/posix/named_mutex.hpp
@@ -23,30 +23,26 @@ namespace detail {
    {
       enum CreationType {  open_only, open_or_create, create_only  };
 
-      mut_construct_func_t(pthread_mutex_t *&mut)
+      mut_construct_func_t(sem_t *&mut)
          :  mp_mut(mut){}
 
       bool operator()(const shared_memory::segment_info_t * info, bool created) const
       {   
-         mp_mut = reinterpret_cast<pthread_mutex_t *>(info->get_user_ptr());
+         mp_mut = reinterpret_cast<sem_t *>(info->get_user_ptr());
          switch(m_create){
             case open_only:   
-               mp_mut = reinterpret_cast<pthread_mutex_t *>(info->get_user_ptr());
+               mp_mut = reinterpret_cast<sem_t *>(info->get_user_ptr());
             break;
 
             case open_or_create:
                if(created){
-                  BOOST_TRY{
-                     detail::mutexattr_wrapper mut_attr;
-                     detail::mutex_initializer mut(*mp_mut, mut_attr);
-                     mut.release();
+                 if(sem_init(mp_sem, 1, 1) == 0){
                      return true;
                   }
-                  BOOST_CATCH(...){
-                     mp_mut = 0;
+                  else{
+                     mp_sem = 0;
                      return false;
                   }
-                  BOOST_CATCH_END
                }
             break;
 
@@ -62,22 +58,22 @@ namespace detail {
       }
 
       CreationType   m_create;
-      pthread_mutex_t *       &mp_mut; 
+      sem_t *       &mp_mut; 
    };
 
    struct mut_destroy_func_t
    {
-      mut_destroy_func_t(pthread_mutex_t *&mut)
+      mut_destroy_func_t(sem_t *&mut)
          :  mp_mut(mut){}
 
       void operator()(const shared_memory::segment_info_t * info, bool last) const
       {  
          if(last){
-            int res = pthread_mutex_destroy(mp_mut);
+            int res = sem_destroy(mp_mut);
             assert(res  == 0);
          }
       }
-      pthread_mutex_t *       &mp_mut; 
+      sem_t *       &mp_mut; 
    };
 }
 
@@ -87,18 +83,18 @@ inline named_mutex::~named_mutex()
 }
 
 inline named_mutex::named_mutex() 
-   : mp_mut((pthread_mutex_t*)0)
+   : mp_mut((sem_t*)0)
 { }
 
 inline bool named_mutex::create(const char *name)
 {
-   if(mp_mut != (pthread_mutex_t*)0)
+   if(mp_mut != (sem_t*)0)
       return false;
 
    detail::mut_construct_func_t func(mp_mut);
    func.m_create        = detail::mut_construct_func_t::create_only;
 
-   if(!m_shmem.create_with_func(name, sizeof(pthread_mutex_t), func)){
+   if(!m_shmem.create_with_func(name, sizeof(sem_t), func)){
       return false;
    }
 
@@ -107,13 +103,13 @@ inline bool named_mutex::create(const char *name)
 
 inline bool named_mutex::open_or_create(const char *name)
 {
-   if(mp_mut != (pthread_mutex_t*)0)
+   if(mp_mut != (sem_t*)0)
       return false;
 
    detail::mut_construct_func_t func(mp_mut);
    func.m_create        = detail::mut_construct_func_t::open_or_create;
 
-   if(!m_shmem.open_or_create_with_func(name, sizeof(pthread_mutex_t), func)){
+   if(!m_shmem.open_or_create_with_func(name, sizeof(sem_t), func)){
       return false;
    }
 
@@ -122,7 +118,7 @@ inline bool named_mutex::open_or_create(const char *name)
 
 inline bool named_mutex::open(const char *name)
 {
-   if(mp_mut != (pthread_mutex_t*)0){
+   if(mp_mut != (sem_t*)0){
       return false;
    }
 
@@ -138,29 +134,29 @@ inline bool named_mutex::open(const char *name)
 
 inline void named_mutex::close()
 {
-   if(mp_mut != (pthread_mutex_t*)0){
+   if(mp_mut != (sem_t*)0){
       m_shmem.close_with_func(detail::mut_destroy_func_t(mp_mut));
-      mp_mut = (pthread_mutex_t*)0;
+      mp_mut = (sem_t*)0;
    }
 }
 
 inline void named_mutex::lock()
 {
-   if(pthread_mutex_lock(mp_mut) != 0){
+   if(sem_wait(mp_mut) != 0){
       throw lock_exception();
    }
 }
 
 inline void named_mutex::unlock()
 {
-   if(pthread_mutex_unlock(mp_mut) != 0){
+   if(sem_post(mp_mut) != 0){
       throw lock_exception();
    }
 }
 
 inline bool named_mutex::try_lock()
 {
-   int res = pthread_mutex_trylock(mp_mut);
+   int res = sem_trywait(mp_mut);
    assert(res == 0 || errno == EAGAIN);
    if(!(res == 0 || errno == EAGAIN)){
       throw lock_exception();
@@ -174,7 +170,7 @@ inline bool named_mutex::timed_lock(const xtime &xt)
       timespec tspec;
       xtime_to_timespec(xt, tspec);
 
-      int res = sem_timedwait(mp_mut, &tspec);
+      int res = sem_timedwait((mp_mut, &tspec);
       assert(res == 0 || errno == ETIMEDOUT);
 
       if(!(res == 0 || errno == ETIMEDOUT)){
