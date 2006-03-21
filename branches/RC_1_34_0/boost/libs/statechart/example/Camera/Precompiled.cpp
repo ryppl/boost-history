4cf36d8bd1d3866312cd5b066ec3e2821a64b0f3
Jeff Garland <jeff@crystalclearsoftware.com>
2006-03-21 02:26:31 +0000

    This commit was manufactured by cvs2svn to create branch 'RC_1_34_0'.
    SVN-Revision: 33417

---
+++ branches/RC_1_34_0/boost/libs/statechart/example/Camera/Precompiled.cpp
@@ -0,0 +1,9 @@
+//////////////////////////////////////////////////////////////////////////////
+// (c) Copyright Andreas Huber Doenni 2002-2005
+// Distributed under the Boost Software License, Version 1.0. (See accompany-
+// ing file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//////////////////////////////////////////////////////////////////////////////
+
+
+
+#include "Precompiled.hpp"
