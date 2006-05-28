3c32e4a6e44e8f64faf1abf08f9571eca32e186c
Dave Abrahams <dave@boostpro.com>
2006-05-28 15:58:54 +0000

    set up SoC 2006 structure
    SVN-Revision: 5010

---
+++ SOC/soc-import/boost/soc/2006/scripts/projects.py
@@ -0,0 +1,16 @@
+# Copyright David Abrahams 2006. Distributed under the Boost
+# Software License, Version 1.0. (See accompanying
+# file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+# tuples (project-name, student-id, mentor-id)
+projects = [
+    ('mincut-maxflow','stephan.diederich','gregor'),
+    ('concurrency','matthew.calabrese','dave'),
+    ('process','julio-manuel.vidal','jeff.garland'),
+    ('tree', 'bernhard.reiter', 'rene'),
+    ('tr1-math', 'xiaogang.zhang', 'john.maddock'),
+    ('graph', 'zampelli', 'jeremy.siek'),
+    ('misc', 'matias.capeletto', 'joaquin.munoz'),
+    ('fsm-viewer', 'tibuleac.ioana', 'andreas.donni'),
+    ('modules', 'lally.singh', 'joel'),
+    ('coroutine','giovanni.deretta', 'eric')]
