aec38f5a746f5d61c6f2e49c743bdba278dc9290
Lally Singh <lally.singh@gmail.com>
2006-06-07 09:24:45 +0000

    Still working on getting it to build with Wave.  Grrr.
    SVN-Revision: 5048

--- SOC/soc-import/boost/soc/2006/modules/trunk/main.cpp
+++ SOC/soc-import/boost/soc/2006/modules/trunk/main.cpp
@@ -1,6 +1,6 @@
 #include "driver/driver.h"
 
 
-int main (int args, char ** argv) {
+int main (int args, const char ** argv) {
 	return Driver::execute(args, argv);
 }
--- SOC/soc-import/boost/soc/2006/modules/trunk/parser/parse.cpp
+++ SOC/soc-import/boost/soc/2006/modules/trunk/parser/parse.cpp
@@ -1,12 +1,19 @@
 /*
 Open questions:
-1. How do I pass parameters into decl_grammar (the OutputDelegate)?
+1. How do I pass parameters into decl_grammar (& the OutputDelegate)?
+2. This grammar now takes in a scanner that does cpp lexing for us.
 */
 
 #define BOOST_SPIRIT_DEBUG
 #include <boost/spirit/core.hpp>
 #include <boost/spirit/iterator.hpp>
 #include <boost/spirit/symbols.hpp>
+#include <boost/spirit/utility.hpp>
+#include <boost/wave/token_ids.hpp>
+#include <boost/wave/grammars/cpp_grammar_gen.hpp>
+#include <boost/wave/util/pattern_parser.hpp>
+#include <boost/wave/cpp_exceptions.hpp>
+
 #include <iostream>
 #include <fstream>
 
@@ -14,136 +21,54 @@ Open questions:
 #include <sys/stat.h>
 #include <sys/mman.h>
 
+#include "parser/generator.h"
+
 using namespace std;
 using namespace boost::spirit;
+using namespace boost::wave;
 
-
-//
-// This is _certainly_ staying in a .cpp file, to avoid recompiling
-// all this metacode.
 struct decl_grammar : public grammar<decl_grammar> {
 	template<typename ScannerT>
 	struct definition {
-		rule<ScannerT>  id, decl, type, var_decl, type_decl, body;
-		symbols<> cv_qual, lifetime_qual, sign_qual, size_qual, scope_qual;
-		symbols<> reserved_word;
+		rule<ScannerT>  decl, var_decl, type_decl, id, semi;
 		
 		definition (decl_grammar const& self) {
-			cout << "definition(self)" << endl;
-			body = lexeme_d [
-			          ( ch_p('{') >> ( ~ch_p('{') & ~ch_p('}') ) >> '}' ) 
-				    ]
-			        | ( ch_p('{') >> body >> '}')
-			     ;
 
-			reserved_word = 
-				"asm", "auto", "bool", "break", "case", "catch",
-				"char", "class", "const", "const_cast",
-				"continue", "default", "delete", "do", "double",
-				"dynamic_cast", "else", "enum", "explicit",
-				"extern", "false", "float", "for", "friend",
-				"goto", "if", "inline", "int", "long", "mutable",
-				"namespace", "new", "operator", "private",
-				"protected", "public", "register",
-				"reinterpret_cast", "return", "short", "signed",
-				"sizeof", "static", "static_cast", "struct",
-				"switch", "template", "this", "throw", "true",
-				"try", "typedef", "typeid", "typename", "union",
-				"unsigned", "using", "virtual", "void",
-				"volatile", "wchar_t", "while"; 
-
-			// essentially a direct copy from Hartmut Kaiser's c_grammar.cpp
-			id =
-			 lexeme_d[
-                    ((alpha_p | '_') >> *(alnum_p | '_' ))
-                    // this part prevents reserved words, but allows
-                    // identifiers beginning with reserved_words.
-                   - (reserved_word >> anychar_p - (alnum_p | '_' ))
-                ];
-			
-			cv_qual = "const", "volatile";
-			lifetime_qual = "static", "auto";
-			sign_qual = "unsigned", "signed";
-			size_qual = "long", "short";
-			scope_qual = "register", "extern";
-			
-			type = id | ( *(cv_qual | lifetime_qual | sign_qual 
-			                | size_qual | scope_qual) 
-			              >> ("float" | "double" | "int" | size_qual 
-			                  | sign_qual) );
-			
 			// these are obviously insufficient for production use, but that'll
 			// come later.
-			var_decl = type >> id >> ';';
+			
+			id = ch_p(T_IDENTIFIER);
+			semi = ch_p(T_SEMICOLON);
+			
+			var_decl = id >> id >> semi;
+			
 			// completely ignoring typedef and function defs right now.
-			type_decl = ( str_p("struct")  | str_p ("class") ) >> id 
-			             >> !body >> ';';
+			type_decl = ( ch_p(T_STRUCT)  | ch_p(T_CLASS) ) >> id 
+						>> semi;
 			
 			decl = *(var_decl | type_decl);
-			
-			BOOST_SPIRIT_DEBUG_RULE(id);
-			BOOST_SPIRIT_DEBUG_RULE(var_decl);
-			BOOST_SPIRIT_DEBUG_RULE(type_decl);
-			BOOST_SPIRIT_DEBUG_RULE(body);
-			//BOOST_SPIRIT_DEBUG_NODE(reserved_word);
-			BOOST_SPIRIT_DEBUG_RULE(decl);
-
 		}
 		rule<ScannerT> const& start () { return var_decl; }
 	};
 };
 
+static std::vector<std::string> execute (context_t&, OutputDelegate&);
+
 std::vector<std::string>
 Generator::
 execute() {
-	return execute(c,d);
+	return ::execute(c,d);
 }
 
 static
 std::vector<std::string>
 execute (context_t& ctx, OutputDelegate& del) {
-//char *start, char *end) {
 	std::vector<std::string>  result;
 	decl_grammar g;
-	context_t::iterator_type first, last;
-	first = ctx.begin ();
-	last = ctx.end ();
-	if (parse (first, last, g, space_p).full) {
+	if (parse (ctx.begin (), ctx.end (), g, space_p).full) {
 		puts ("parsed");
 	} else {
 		puts ("not parsed.");
 	}
 	return result;
 }
-
-/*
-int main (int args, char ** argv) {
-	decl_grammar g;
-	int fd;
-	struct stat st;
-	char * buf;
-	if (args < 2) {
-		puts ("interactive mode");
-		buf = new char[16384];
-		while (!feof(stdin)) {
-			printf (">");
-			gets(buf);
-			if (strlen(buf))
-				execute(buf, buf+strlen(buf));
-		}
-	} else {
-		if (   ((fd = open (argv[1], O_RDONLY)) < 0)
-		    || (fstat (fd, &st) < 0)
-		    || ((buf = (char*) mmap (0, st.st_size, PROT_READ, 0, fd, 0)) < 0))
-		{
-			perror (argv[1]);
-			return 1;
-		} else {
-			printf ("reading input from %s\n", argv[1]);
-			execute(buf, buf+st.st_size);
-		}
-	}
-
-	return 0;
-}
-*/
