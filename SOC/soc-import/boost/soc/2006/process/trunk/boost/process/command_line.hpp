2eba9c5aa0b3cde47243f0282fcacefaeeaa599d
Julio Merino <julio+github@meroh.net>
2006-07-03 15:59:47 +0000

    Simple implementation of the command_line class and some tests for it. This is mostly to ensure that the build system works and that I'm correctly using unit_test_framework.
    SVN-Revision: 5225

--- SOC/soc-import/boost/soc/2006/process/trunk/boost/process/command_line.hpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/boost/process/command_line.hpp
@@ -12,12 +12,46 @@
 #if !defined(BOOST_PROCESS_COMMAND_LINE_HPP)
 #define BOOST_PROCESS_COMMAND_LINE_HPP
 
+#include <string>
+#include <vector>
+
 namespace boost {
     namespace process {
 
         class command_line {
+        public:
+            typedef std::vector< std::string > arguments_vector;
+
+        private:
+            std::string m_executable;
+            arguments_vector m_arguments;
 
         public:
+            command_line(const std::string& executable) :
+                m_executable(executable)
+            {
+            }
+
+            command_line&
+            argument(const std::string& argument)
+            {
+                m_arguments.push_back(argument);
+                return *this;
+            }
+
+            const arguments_vector&
+            get_arguments(void)
+                const
+            {
+                return m_arguments;
+            }
+
+            const std::string&
+            get_executable(void)
+                const
+            {
+                return m_executable;
+            }
         };
 
     } // namespace process
--- SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/command_line_test.cpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/command_line_test.cpp
@@ -13,13 +13,54 @@
 #include <iostream>
 
 #include <boost/process/command_line.hpp>
+#include <boost/test/unit_test.hpp>
 
 namespace bp = ::boost::process;
+namespace but = ::boost::unit_test;
 
-int
-main(void)
+// ------------------------------------------------------------------------
+
+static void
+test_arguments_addition(void)
 {
-    bp::command_line cl;
+    bp::command_line cl("program");
+    cl.argument("first").argument("second").argument("third");
+    bp::command_line::arguments_vector args = cl.get_arguments();
+    BOOST_CHECK(args.size() == 3);
+    BOOST_CHECK(args[0] == "first");
+    BOOST_CHECK(args[1] == "second");
+    BOOST_CHECK(args[2] == "third");
+}
 
-    return EXIT_SUCCESS;
+// ------------------------------------------------------------------------
+
+static void
+test_arguments_empty(void)
+{
+    bp::command_line cl("program");
+    bp::command_line::arguments_vector args = cl.get_arguments();
+    BOOST_CHECK(args.size() == 0);
+}
+
+// ------------------------------------------------------------------------
+
+static void
+test_executable(void)
+{
+    bp::command_line cl("program");
+    BOOST_CHECK(cl.get_executable() == "program");
+}
+
+// ------------------------------------------------------------------------
+
+but::test_suite *
+init_unit_test_suite(int argc, char* argv[])
+{
+    but::test_suite* test = BOOST_TEST_SUITE("command_line test suite");
+
+    test->add(BOOST_TEST_CASE(&test_executable));
+    test->add(BOOST_TEST_CASE(&test_arguments_empty));
+    test->add(BOOST_TEST_CASE(&test_arguments_addition));
+
+    return test;
 }
