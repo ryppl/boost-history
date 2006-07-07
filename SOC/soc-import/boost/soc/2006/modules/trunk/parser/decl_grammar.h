942db413daacedc96b3defa71869c7aee9f5963e
Lally Singh <lally.singh@gmail.com>
2006-07-07 21:05:13 +0000

    parser/decl_grammar.h: Last evolution before switching back to Hannibal.
    SVN-Revision: 5247

--- SOC/soc-import/boost/soc/2006/modules/trunk/parser/decl_grammar.h
+++ SOC/soc-import/boost/soc/2006/modules/trunk/parser/decl_grammar.h
@@ -7,6 +7,8 @@
 #include <boost/spirit/iterator.hpp>
 #include <boost/spirit/symbols.hpp>
 #include <boost/spirit/utility.hpp>
+// #include <boost/spirit/phoenix/core.hpp>
+// #include <boost/spirit/phoenix/core/variable.hpp>
 #include <boost/wave/token_ids.hpp>
 #include <boost/wave/grammars/cpp_grammar_gen.hpp>
 #include <boost/wave/util/pattern_parser.hpp>
@@ -23,22 +25,22 @@ modular c++ constructs.
 struct decl_grammar : public boost::spirit::grammar<decl_grammar> {
 	mutable std::vector<std::string> m_ids;
 	// iterator splicing engine.
-	mutable OutputDelegate& m_del;
+	mutable OutputDelegate* m_del;
 	
-	decl_grammar (OutputDelegate& del) : m_del(del) {}
+	decl_grammar (OutputDelegate& del) : m_del(&del) {}
 	
 	template<typename ScannerT>
 	struct definition {
         typedef boost::spirit::rule<ScannerT> rule_type;
 
 		rule_type translation_unit, declaration, inner_decl,
-		          access_specifier, skip_block, export_stmt, import_stmt;
+		          access_specifier, export_stmt, import_stmt;
 		
 		definition (decl_grammar const& self) {
 			using namespace boost::spirit;
             using namespace boost::wave;
             using boost::wave::util::pattern_p;
-			using boost::phoenix::var;
+			using namespace boost::phoenix;
 
             translation_unit
                 =	 *declaration
@@ -49,7 +51,7 @@ struct decl_grammar : public boost::spirit::grammar<decl_grammar> {
 			    =   +(   ~ch_p(T_IMPORT) & ~ch_p(T_EXPORT) 
 			           & ~ch_p(T_EOF)    & ~ch_p(T_RIGHTBRACE) 
 			           & ~ch_p(T_LEFTBRACE) ) 
-			         [ emit (var(self.m_del)) ]
+			         [ emit (self.m_del) ]
 			         
 			    |   ( ch_p(T_IMPORT) 
 			          >> import_stmt )
@@ -60,16 +62,18 @@ struct decl_grammar : public boost::spirit::grammar<decl_grammar> {
 			          
 			    |   ( ch_p(T_EXPORT) 
 			          >> ch_p(T_TEMPLATE) ) 
-			         [ emit (var(self.m_del)) ]
+			         [ emit (self.m_del) ]
 			         
-			    |   ( ch_p(T_LEFTBRACE) 
+			    |   ( ch_p(T_LEFTBRACE) [ emit (self.m_del) ]
 			          >> *inner_decl 
-			          >> ch_p(T_RIGHTBRACE) )
-			          [ emit (var(self.m_del)) ]
+			          >> ch_p(T_RIGHTBRACE) [ emit (self.m_del) ]
+			          )
+			          
 			    ;
 			    
 			inner_decl
-			   = +(declaration | ~ch_p(T_RIGHTBRACE))
+			   = declaration 
+			       | ( ~ch_p(T_RIGHTBRACE) && anychar_p) [ emit (self.m_del) ]
 			   ;
 
 			access_specifier
@@ -79,10 +83,10 @@ struct decl_grammar : public boost::spirit::grammar<decl_grammar> {
 			        >> ch_p(T_COLON)
 				;
 
-			skip_block
-			    = ch_p(T_LEFTBRACE) 
-			      >> *(access_specifier | inner_decl)
-			      >> ch_p(T_RIGHTBRACE)
+// 			skip_block
+// 			    = ch_p(T_LEFTBRACE) 
+// 			      >> *(access_specifier | inner_decl)
+// 			      >> ch_p(T_RIGHTBRACE)
 			    ;
 
 			export_stmt 
@@ -90,7 +94,7 @@ struct decl_grammar : public boost::spirit::grammar<decl_grammar> {
 			                         [ decl_module(self.m_del) ]
 			      >> ch_p(T_LEFTBRACE)
 			      >> *(access_specifier | inner_decl)
-			      >> ch_p(T_RIGHTBRACE) [ finish_decl(var(self.m_del)) ]
+			      >> ch_p(T_RIGHTBRACE) [ finish_decl(self.m_del) ]
 			    ; 
 
 			import_stmt
@@ -103,7 +107,7 @@ struct decl_grammar : public boost::spirit::grammar<decl_grammar> {
 			BOOST_SPIRIT_DEBUG_RULE(declaration);
 			BOOST_SPIRIT_DEBUG_RULE(inner_decl);
 			BOOST_SPIRIT_DEBUG_RULE(access_specifier);
-			BOOST_SPIRIT_DEBUG_RULE(skip_block);
+// 			BOOST_SPIRIT_DEBUG_RULE(skip_block);
 			BOOST_SPIRIT_DEBUG_RULE(export_stmt);
 			BOOST_SPIRIT_DEBUG_RULE(import_stmt);
 		}
