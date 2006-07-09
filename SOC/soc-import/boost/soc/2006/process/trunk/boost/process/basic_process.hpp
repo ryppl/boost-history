7bbbf8f3d6533b9780779f0625db90d936bb3090
Julio Merino <julio+github@meroh.net>
2006-07-09 16:58:26 +0000

    Unindent namespaces and move function bodies outside the class definition.
    SVN-Revision: 5259

--- SOC/soc-import/boost/soc/2006/process/trunk/boost/process/basic_process.hpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/boost/process/basic_process.hpp
@@ -15,36 +15,58 @@
 #include <boost/process/types.hpp>
 
 namespace boost {
-    namespace process {
+namespace process {
 
-        template< class Attributes >
-        class basic_process {
-            id_t m_id;
-            Attributes m_attributes;
+// ------------------------------------------------------------------------
 
-            basic_process(const id_t& id, const Attributes& a) :
-                m_id(id),
-                m_attributes(a)
-            {
-            };
+template< class Attributes >
+class basic_process {
+    id_t m_id;
+    Attributes m_attributes;
 
-        public:
-            const Attributes&
-            get_attributes(void)
-                const
-            {
-                return m_attributes;
-            }
+    basic_process(const id_t& id, const Attributes& a);
 
-            id_t
-            get_id(void)
-                const
-            {
-                return m_id;
-            };
-        };
+public:
+    const Attributes& get_attributes(void) const;
+    id_t get_id(void) const;
+};
 
-    } // namespace process
+// ------------------------------------------------------------------------
+
+template< class Attributes >
+inline
+basic_process< Attributes >::basic_process(const id_t& id,
+                                           const Attributes& a) :
+    m_id(id),
+    m_attributes(a)
+{
+}
+
+// ------------------------------------------------------------------------
+
+template< class Attributes >
+inline
+const Attributes&
+basic_process< Attributes >::get_attributes(void)
+    const
+{
+    return m_attributes;
+}
+
+// ------------------------------------------------------------------------
+
+template< class Attributes >
+inline
+id_t
+basic_process< Attributes >::get_id(void)
+    const
+{
+    return m_id;
+}
+
+// ------------------------------------------------------------------------
+
+} // namespace process
 } // namespace boost
 
 #endif // !defined(BOOST_PROCESS_BASIC_PROCESS_HPP)
