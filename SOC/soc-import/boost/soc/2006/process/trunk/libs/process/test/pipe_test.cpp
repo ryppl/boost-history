6eeaade382980124792c46164d1c2cbb761a0b9d
Julio Merino <julio+github@meroh.net>
2006-07-10 13:41:05 +0000

    No need to manually check for exceptions; the unit test framework does it automatically.
    SVN-Revision: 5265

--- SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/pipe_test.cpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/pipe_test.cpp
@@ -25,25 +25,20 @@ namespace but = ::boost::unit_test;
 static void
 test_read_and_write(void)
 {
-    try {
-        bpd::pipe p;
-        bpd::systembuf rbuf(p.get_read_end());
-        bpd::systembuf wbuf(p.get_write_end());
-        std::istream rend(&rbuf);
-        std::ostream wend(&wbuf);
+    bpd::pipe p;
+    bpd::systembuf rbuf(p.get_read_end());
+    bpd::systembuf wbuf(p.get_write_end());
+    std::istream rend(&rbuf);
+    std::ostream wend(&wbuf);
 
-        // XXX This assumes that the pipe's buffer is big enough to accept
-        // the data written without blocking!
-        wend << "Test message" << std::endl;
-        std::string tmp;
-        rend >> tmp;
-        BOOST_CHECK(tmp == "Test");
-        rend >> tmp;
-        BOOST_CHECK(tmp == "message");
-    } catch (const bp::system_error& e) {
-        // XXX Any better way to handle exceptions in tests?
-        BOOST_CHECK(false);
-    }
+    // XXX This assumes that the pipe's buffer is big enough to accept
+    // the data written without blocking!
+    wend << "Test message" << std::endl;
+    std::string tmp;
+    rend >> tmp;
+    BOOST_CHECK(tmp == "Test");
+    rend >> tmp;
+    BOOST_CHECK(tmp == "message");
 }
 
 // ------------------------------------------------------------------------
