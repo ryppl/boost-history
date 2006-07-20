4279f1b6ed6092830806aefe500a3fe5c9e65fb8
Julio Merino <julio+github@meroh.net>
2006-07-20 16:11:40 +0000

    Make the echo-one and echo-two helpers POSIX-specific to let this build under Win32.
    SVN-Revision: 5412

--- SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/helpers.cpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/helpers.cpp
@@ -28,13 +28,16 @@ namespace bpd = ::boost::process::detail;
 int h_echo_stdout(int, char*[]);
 int h_echo_stderr(int, char*[]);
 int h_echo_stdout_stderr(int, char*[]);
-int h_echo_one(int, char*[]);
-int h_echo_two(int, char*[]);
 int h_exit_failure(int, char*[]);
 int h_exit_success(int, char*[]);
 int h_pwd(int, char*[]);
 int h_query_env(int, char*[]);
 int h_stdin_to_stdout(int, char*[]);
+
+#if defined(BOOST_PROCESS_POSIX_API)
+int h_posix_echo_one(int, char*[]);
+int h_posix_echo_two(int, char*[]);
+#endif
 
 struct helper {
     const char* m_name;
@@ -45,13 +48,17 @@ struct helper {
     { "echo-stdout", h_echo_stdout, 2, "message" },
     { "echo-stderr", h_echo_stderr, 2, "message" },
     { "echo-stdout-stderr", h_echo_stdout_stderr, 2, "message" },
-    { "echo-one", h_echo_one, 3, "desc message" },
-    { "echo-two", h_echo_two, 4, "desc1 desc2 message" },
     { "exit-failure", h_exit_failure, 1, "" },
     { "exit-success", h_exit_success, 1, "" },
     { "pwd", h_pwd, 1, "" },
     { "query-env", h_query_env, 2, "variable" },
     { "stdin-to-stdout", h_stdin_to_stdout, 1, "" },
+
+#if defined(BOOST_PROCESS_POSIX_API)
+    { "posix-echo-one", h_posix_echo_one, 3, "desc message" },
+    { "posix-echo-two", h_posix_echo_two, 4, "desc1 desc2 message" },
+#endif
+
     { NULL, NULL }
 };
 
@@ -82,41 +89,6 @@ h_echo_stdout_stderr(int argc, char* argv[])
     std::cout.flush();
     std::cerr << "stderr " << argv[1] << std::endl;
     std::cerr.flush();
-    return EXIT_SUCCESS;
-}
-
-// ------------------------------------------------------------------------
-
-int
-h_echo_one(int argc, char* argv[])
-{
-    int desc = std::atoi(argv[1]);
-
-    bpd::systembuf buf(desc);
-    std::ostream os(&buf);
-    os << argv[2] << std::endl;
-
-    return EXIT_SUCCESS;
-}
-
-// ------------------------------------------------------------------------
-
-int
-h_echo_two(int argc, char* argv[])
-{
-    int desc1 = std::atoi(argv[1]);
-    int desc2 = std::atoi(argv[2]);
-
-    bpd::systembuf buf1(desc1);
-    std::ostream os1(&buf1);
-    os1 << argv[1] << " " << argv[3] << std::endl;
-    os1.flush();
-
-    bpd::systembuf buf2(desc2);
-    std::ostream os2(&buf2);
-    os2 << argv[2] << " " << argv[3] << std::endl;
-    os2.flush();
-
     return EXIT_SUCCESS;
 }
 
@@ -183,6 +155,45 @@ h_stdin_to_stdout(int argc, char* argv[])
 
     return EXIT_SUCCESS;
 }
+
+// ------------------------------------------------------------------------
+
+#if defined(BOOST_PROCESS_POSIX_API)
+int
+h_posix_echo_one(int argc, char* argv[])
+{
+    int desc = std::atoi(argv[1]);
+
+    bpd::systembuf buf(desc);
+    std::ostream os(&buf);
+    os << argv[2] << std::endl;
+
+    return EXIT_SUCCESS;
+}
+#endif
+
+// ------------------------------------------------------------------------
+
+#if defined(BOOST_PROCESS_POSIX_API)
+int
+h_posix_echo_two(int argc, char* argv[])
+{
+    int desc1 = std::atoi(argv[1]);
+    int desc2 = std::atoi(argv[2]);
+
+    bpd::systembuf buf1(desc1);
+    std::ostream os1(&buf1);
+    os1 << argv[1] << " " << argv[3] << std::endl;
+    os1.flush();
+
+    bpd::systembuf buf2(desc2);
+    std::ostream os2(&buf2);
+    os2 << argv[2] << " " << argv[3] << std::endl;
+    os2.flush();
+
+    return EXIT_SUCCESS;
+}
+#endif
 
 // ------------------------------------------------------------------------
 
