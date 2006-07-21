f68602b4cd3f38c855811f4eaa324c936d1f8b63
Xiaogang Zhang (bounce) <xiaogang.zhang@users.sf.net>
2006-07-21 09:01:30 +0000

    SVN-Revision: 5429

---
+++ SOC/soc-import/boost/soc/2006/tr1-math/trunk/libs/math/test/bessel_k0_test.cpp
@@ -0,0 +1,52 @@
+//  (C) Copyright Xiaogang Zhang 2006.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/test/included/test_exec_monitor.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include <boost/math/special_functions/bessel_k0.hpp>
+#include <boost/math/tools/testutils.hpp>
+#include <boost/array.hpp>
+
+template <typename T>
+void test_bessel_k0(T, const char* type_name)
+{
+   // TODO
+}
+
+template <typename T>
+void test_spots(T, const char* type_name)
+{
+    // function values calculated on http://functions.wolfram.com/
+    #define SC_(x) static_cast<T>(BOOST_JOIN(x, L))
+    static const boost::array<boost::array<T, 2>, 10> data = {
+        SC_(0), std::numeric_limits<T>::infinity(),
+        SC_(1), SC_(0.4210244382407083333356273792126090361362),
+        SC_(2), SC_(0.1138938727495334356527195749324818329983),
+        SC_(4), SC_(1.115967608585302426974519597983348922501e-02),
+        SC_(8), SC_(1.464707052228153870965844086986779219673e-04),
+        SC_(1e-05), SC_(1.162885698094436229341844471042334117576e+01),
+        SC_(1e-10), SC_(2.314178244559886928905098903433341338627e+01),
+        SC_(1e-20), SC_(4.616763337553932618401054912506305678534e+01),
+        SC_(5e+01), SC_(3.410167749789495513920675512352952231845e-23),
+        SC_(1e+02), SC_(4.656628229175902018939005289483886355808e-45),
+    };
+    #undef SC_
+
+    boost::math::tools::test_univariate
+        (data, type_name, "boost::math::bessel_k0", boost::math::bessel_k0);
+}
+
+int test_main(int, char* [])
+{
+    test_spots(0.0F, "float");
+    test_spots(0.0, "double");
+    test_spots(0.0L, "long double");
+
+    test_bessel_k0(0.0F, "float");
+    test_bessel_k0(0.0, "double");
+    test_bessel_k0(0.0L, "long double");
+
+    return 0;
+}
