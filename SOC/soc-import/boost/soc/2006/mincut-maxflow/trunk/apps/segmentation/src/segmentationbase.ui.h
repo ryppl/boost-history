ba305ab8098f84be33b1d88c56f0574976a25c78
Stephan Diederich <stephan.diederich@googlemail.com>
2006-07-23 13:34:32 +0000

    added dimacs-export option
    SVN-Revision: 5461

---
+++ SOC/soc-import/boost/soc/2006/mincut-maxflow/trunk/apps/segmentation/src/segmentationbase.ui.h
@@ -0,0 +1,118 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you want to add, delete, or rename functions or slots, use
+** Qt Designer to update this file, preserving your code.
+**
+** You should not define a constructor or destructor in this file.
+** Instead, write your code in functions called init() and destroy().
+** These will automatically be called by the form's constructor and
+** destructor.
+*****************************************************************************/
+
+
+void SegmentationBase::fileNew()
+{
+
+}
+
+
+void SegmentationBase::fileOpenImage()
+{
+
+}
+
+
+void SegmentationBase::fileOpenMask()
+{
+
+}
+
+
+void SegmentationBase::fileSave()
+{
+
+}
+
+
+void SegmentationBase::fileSaveAs()
+{
+
+}
+
+
+void SegmentationBase::filePrint()
+{
+
+}
+
+
+void SegmentationBase::fileExit()
+{
+
+}
+
+
+void SegmentationBase::editUndo()
+{
+
+}
+
+
+void SegmentationBase::editRedo()
+{
+
+}
+
+
+void SegmentationBase::editCut()
+{
+
+}
+
+
+void SegmentationBase::editCopy()
+{
+
+}
+
+
+void SegmentationBase::editPaste()
+{
+
+}
+
+
+void SegmentationBase::editFind()
+{
+
+}
+
+
+void SegmentationBase::helpIndex()
+{
+
+}
+
+
+void SegmentationBase::helpContents()
+{
+
+}
+
+
+void SegmentationBase::helpAbout()
+{
+
+}
+
+void SegmentationBase::fileNewMask()
+{
+
+}
+
+void SegmentationBase::storeProblemAsDimacsFile( bool )
+{
+
+}
+
