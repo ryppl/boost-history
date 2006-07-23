82fc61dc7b4de3538f3202be51228512fca4450c
Stephan Diederich <stephan.diederich@googlemail.com>
2006-07-23 12:15:11 +0000

    adapted
    SVN-Revision: 5452

--- SOC/soc-import/boost/soc/2006/mincut-maxflow/trunk/libs/graph/example/kolmogorov-eg.cpp
+++ SOC/soc-import/boost/soc/2006/mincut-maxflow/trunk/libs/graph/example/kolmogorov-eg.cpp
@@ -50,21 +50,21 @@ main()
   typedef adjacency_list_traits < vecS, vecS, directedS > Traits;
   typedef adjacency_list < vecS, vecS, directedS,
     property < vertex_name_t, std::string,
+    property < vertex_index_t, long,
     property < vertex_color_t, boost::default_color_type,
-    property < vertex_distance_t, long> > >,
+    property < vertex_distance_t, long,
+    property < vertex_predecessor_t, Traits::edge_descriptor > > > > >,
     
     property < edge_capacity_t, long,
     property < edge_residual_capacity_t, long,
     property < edge_reverse_t, Traits::edge_descriptor > > > > Graph;
  
   Graph g;
-
   property_map < Graph, edge_capacity_t >::type
-    capacity = get(edge_capacity, g);
-  property_map < Graph, edge_reverse_t >::type rev = get(edge_reverse, g);
+      capacity = get(edge_capacity, g);
   property_map < Graph, edge_residual_capacity_t >::type
-    residual_capacity = get(edge_residual_capacity, g);
-
+      residual_capacity = get(edge_residual_capacity, g);
+  property_map < Graph, edge_reverse_t >::type rev = get(edge_reverse, g);
   Traits::vertex_descriptor s, t;
   read_dimacs_max_flow(g, capacity, rev, s, t);
 
@@ -75,14 +75,14 @@ main()
   std::cout << "c  The total flow:" << std::endl;
   std::cout << "s " << flow << std::endl << std::endl;
 
-//   std::cout << "c flow values:" << std::endl;
-//   graph_traits < Graph >::vertex_iterator u_iter, u_end;
-//   graph_traits < Graph >::out_edge_iterator ei, e_end;
-//   for (tie(u_iter, u_end) = vertices(g); u_iter != u_end; ++u_iter)
-//     for (tie(ei, e_end) = out_edges(*u_iter, g); ei != e_end; ++ei)
-//       if (capacity[*ei] > 0)
-//         std::cout << "f " << *u_iter << " " << target(*ei, g) << " "
-//           << (capacity[*ei] - residual_capacity[*ei]) << std::endl;
+  std::cout << "c flow values:" << std::endl;
+  graph_traits < Graph >::vertex_iterator u_iter, u_end;
+  graph_traits < Graph >::out_edge_iterator ei, e_end;
+  for (tie(u_iter, u_end) = vertices(g); u_iter != u_end; ++u_iter)
+    for (tie(ei, e_end) = out_edges(*u_iter, g); ei != e_end; ++ei)
+      if (capacity[*ei] > 0)
+        std::cout << "f " << *u_iter << " " << target(*ei, g) << " "
+          << (capacity[*ei] - residual_capacity[*ei]) << std::endl;
 
   return EXIT_SUCCESS;
 }
