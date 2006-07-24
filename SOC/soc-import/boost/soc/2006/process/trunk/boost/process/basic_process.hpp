a2a5fb453d9c27866788e1d7ece4ccf087306e4f
Julio Merino <julio+github@meroh.net>
2006-07-24 11:15:15 +0000

    Drop trailing whitespace.  Uh, oh!  My 100th commit! ;-)
    SVN-Revision: 5478

--- SOC/soc-import/boost/soc/2006/process/trunk/boost/process/basic_process.hpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/boost/process/basic_process.hpp
@@ -8,7 +8,7 @@
 // License, Version 1.0.  (See accompanying file LICENSE_1_0.txt or copy
 // at http://www.boost.org/LICENSE_1_0.txt.)
 //
- 
+
 #if !defined(BOOST_PROCESS_BASIC_PROCESS_HPP)
 #define BOOST_PROCESS_BASIC_PROCESS_HPP
 
--- SOC/soc-import/boost/soc/2006/process/trunk/boost/process/detail/pipe.hpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/boost/process/detail/pipe.hpp
@@ -8,7 +8,7 @@
 // License, Version 1.0.  (See accompanying file LICENSE_1_0.txt or copy
 // at http://www.boost.org/LICENSE_1_0.txt.)
 //
- 
+
 #if !defined(BOOST_PROCESS_DETAIL_PIPE_HPP)
 #define BOOST_PROCESS_DETAIL_PIPE_HPP
 
