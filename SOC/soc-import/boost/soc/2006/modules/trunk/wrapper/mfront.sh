5f6364d47e0ea5a0ccdf3d37580c0d8cdd0f57ff
Lally Singh <lally.singh@gmail.com>
2006-08-05 18:23:10 +0000

    Added Mac OS X configuration.
    SVN-Revision: 5610

---
+++ SOC/soc-import/boost/soc/2006/modules/trunk/wrapper/mfront.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+###########################################################################
+# Simple Wrapper for mfront
+# Directions:
+#  - Choose which configuration file to use (or make your own)
+#  - Configure the path of your mfront.bin (exe) binary
+#  - Configure the path of your c++ compiler
+#  - Put this in your path
+###########################################################################
+BASE=`which $0`
+BASE=`dirname $BASE`
+
+#
+# Mac OS X Configuration
+CONFIGFILE="$BASE/params.macosx"
+MFRONTPATH="$BASE/../bin/darwin-3.2/debug/mfront_bin"
+CXXPATH=`which g++`
+
+echo "Configuration"
+echo "-------------"
+echo "BASE=$BASE"
+echo "CONFIGFILE=$CONFIGFILE"
+echo "MFRONTPATH=$MFRONTPATH"
+echo "CXXPATH=$CXXPATH"
+$MFRONTPATH @$CONFIGFILE -o - $* | $CXXPATH -x c++-cpp-output -
+
