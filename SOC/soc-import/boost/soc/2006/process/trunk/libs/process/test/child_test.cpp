6158e6f40774b9a5c95e1da6452d7f0b4a3dca45
Julio Merino <julio+github@meroh.net>
2006-08-14 16:14:43 +0000

    Generalize child tests (mmm, templates are great!) and use these new tests to verify the win32_child and posix_child classes.  The win32_child test suite also carries a new test to check the getters but it is untested (grr, I hate dual boot).
    SVN-Revision: 5742

--- SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/child_test.cpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/child_test.cpp
@@ -11,8 +11,9 @@
 
 #include <boost/process/child.hpp>
 #include <boost/process/detail/factories.hpp>
-#include <boost/process/detail/pipe.hpp>
 #include <boost/test/unit_test.hpp>
+
+#include "child_base_test.hpp"
 
 namespace bp = ::boost::process;
 namespace bpd = ::boost::process::detail;
@@ -20,80 +21,15 @@ namespace but = ::boost::unit_test;
 
 // ------------------------------------------------------------------------
 
-static
-void
-test_getters(void)
+struct factory
 {
-    bp::child::handle_type h = (bp::child::handle_type)0;
-    bpd::file_handle fhinvalid;
-    bp::child c = bp::detail::factories::create_child
-        (h, fhinvalid, fhinvalid, fhinvalid);
-
-    BOOST_CHECK_EQUAL(c.get_handle(), h);
-}
-
-// ------------------------------------------------------------------------
-
-static
-void
-test_stdin(void)
-{
-    bp::child::handle_type h = (bp::child::handle_type)0;
-    bpd::pipe p;
-    bpd::file_handle fhinvalid;
-    bp::child c = bp::detail::factories::create_child
-        (h, p.wend(), fhinvalid, fhinvalid);
-
-    bp::postream& os = c.get_stdin();
-    os << "test-stdin" << std::endl;
-
-    bp::pistream is(p.rend());
-    std::string word;
-    is >> word;
-    BOOST_CHECK_EQUAL(word, "test-stdin");
-}
-
-// ------------------------------------------------------------------------
-
-static
-void
-test_stdout(void)
-{
-    bp::child::handle_type h = (bp::child::handle_type)0;
-    bpd::pipe p;
-    bpd::file_handle fhinvalid;
-    bp::child c = bp::detail::factories::create_child
-        (h, fhinvalid, p.rend(), fhinvalid);
-
-    bp::postream os(p.wend());
-    os << "test-stdout" << std::endl;
-
-    bp::pistream& is = c.get_stdout();
-    std::string word;
-    is >> word;
-    BOOST_CHECK_EQUAL(word, "test-stdout");
-}
-
-// ------------------------------------------------------------------------
-
-static
-void
-test_stderr(void)
-{
-    bp::child::handle_type h = (bp::child::handle_type)0;
-    bpd::pipe p;
-    bpd::file_handle fhinvalid;
-    bp::child c = bp::detail::factories::create_child
-        (h, fhinvalid, fhinvalid, p.rend());
-
-    bp::postream os(p.wend());
-    os << "test-stderr" << std::endl;
-
-    bp::pistream& is = c.get_stderr();
-    std::string word;
-    is >> word;
-    BOOST_CHECK_EQUAL(word, "test-stderr");
-}
+    bp::child
+    operator()(bp::child::handle_type h, bpd::file_handle fhstdin,
+               bpd::file_handle fhstdout, bpd::file_handle fhstderr)
+    {
+        return bpd::factories::create_child(h, fhstdin, fhstdout, fhstderr);
+    }
+};
 
 // ------------------------------------------------------------------------
 
@@ -102,10 +38,7 @@ init_unit_test_suite(int argc, char* argv[])
 {
     but::test_suite* test = BOOST_TEST_SUITE("child test suite");
 
-    test->add(BOOST_TEST_CASE(&test_getters));
-    test->add(BOOST_TEST_CASE(&test_stdin));
-    test->add(BOOST_TEST_CASE(&test_stdout));
-    test->add(BOOST_TEST_CASE(&test_stderr));
+    add_tests_child_base< bp::child, factory >(test);
 
     return test;
 }
