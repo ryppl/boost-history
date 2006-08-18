ad600d1493dadddea92e090996e104bb643dbf57
Stephan Diederich <stephan.diederich@googlemail.com>
2006-08-18 21:19:09 +0000

    optimized version
    SVN-Revision: 5819

--- SOC/soc-import/boost/soc/2006/mincut-maxflow/trunk/libs/graph/src/max_flow_performance/do_perfomance_test.sh
+++ SOC/soc-import/boost/soc/2006/mincut-maxflow/trunk/libs/graph/src/max_flow_performance/do_perfomance_test.sh
@@ -7,8 +7,16 @@ BOOST_PUSH_EXE=$EXE_DIR/boost_push-relabel
 BOOST_KOLMO_EXE=$EXE_DIR/boost_kolmo
 H_PRF_EXE=external/h_prf/hi_pr
 H_PRFW_EXE=external/h_prf/hi_prw
+LEDA_EXE=$EXE_DIR/leda
+LEDA__STATIC_EXE=$EXE_DIR/optimization-off/leda_static
 
-DATADIR=data
+#number of repeats to get nice timestats
+NUMBER_OF_TEST_REPEATS=10
+
+#which executables to consider
+EXE_FILES="$KOLMO_EXE $BOOST_PUSH_EXE $BOOST_KOLMO_EXE $H_PRF_EXE $H_PRFW_EXE $LEDA_EXE $LEDA__STATIC_EXE"
+#description line 
+RESULT_DESCRIPTION="#filename vertices edges kolmogorov pushrelabel boost_kolmo hi_pr hi_prw leda leda_static"
 
 ###
 # check if executables are there, if not, build them
@@ -22,117 +30,52 @@ if [ ! -e $H_PRF_EXE ];then
 	cd ../..
 fi
 
-
-run_ak_test(){
-  AK_DATAFILES=`ls $DATADIR/*_ak_*.dat`
-  AK_LOG=results/ak.log
-  echo "Running AK tests"
-  echo "#filename vertices edges kolmogorov pushrelabel boost_kolmo hi_pr hi_prw" > $AK_LOG
-  for datafile in $AK_DATAFILES
-  do
-    echo "Running tests with $datafile" 
-    
-    NUMBER_OF_NODES=`grep "p max" $datafile | cut -d' ' -f3`
-    NUMBER_OF_EDGES=`grep "p max" $datafile | cut -d' ' -f4`
-    LOGFILE_LINE="$datafile $NUMBER_OF_NODES $NUMBER_OF_EDGES"
-    
-    echo "Kolmogorov..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$KOLMO_EXE < $datafile | grep time: |sed 's/time: //'` "
-    echo "Push-Relabel..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$BOOST_PUSH_EXE < $datafile | grep time: |sed 's/time: //'` "
-    echo "Boost::Kolmo..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$BOOST_KOLMO_EXE < $datafile | grep time: |sed 's/time: //'` "
-    echo "hi_pr..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$H_PRF_EXE < $datafile | grep time: |sed 's/time: //'` "
-    echo "hi_prw..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$H_PRFW_EXE < $datafile | grep time: |sed 's/time: //'` "
-    echo $LOGFILE_LINE >> $AK_LOG
-  done
-}
-
-run_ac_test(){
-  AC_DATAFILES=`ls $DATADIR/*_ac_*.dat`
-  AC_LOG=results/ac.log
-  echo "Running AC tests"
-  echo "#filename vertices edges kolmogorov pushrelabel boost_kolmo hi_pr hi_prw" > $AC_LOG
-  for datafile in $AC_DATAFILES
-  do
-    echo "Running tests with $datafile" 
-    NUMBER_OF_NODES=`grep "p max" $datafile | cut -d' ' -f3`
-    NUMBER_OF_EDGES=`grep "p max" $datafile | cut -d' ' -f4`
-    LOGFILE_LINE="$datafile $NUMBER_OF_NODES $NUMBER_OF_EDGES"
-    echo "Kolmogorov..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$KOLMO_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo "Push-Relabel..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$BOOST_PUSH_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo "Boost::Kolmo..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$BOOST_KOLMO_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo "hi_pr" 
-    LOGFILE_LINE="$LOGFILE_LINE `$H_PRF_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo "hi_prw" 
-    LOGFILE_LINE="$LOGFILE_LINE `$H_PRFW_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo $LOGFILE_LINE >> $AC_LOG
-  done
-}
-
-run_genrmf_test(){
-  GENRMF_DATAFILES=`ls $DATADIR/*_genrmf_*.dat`
-  GENRMF_LOG=results/genrmf.log
-  echo "Running genrmf tests"
-  echo "#filename vertices edges kolmogorov pushrelabel boost_kolmo hi_pr hi_prw" > $GENRMF_LOG
-  for datafile in $GENRMF_DATAFILES
-  do
-    echo "Running tests with $datafile" 
-    NUMBER_OF_NODES=`grep "p max" $datafile | cut -d' ' -f3`
-    NUMBER_OF_EDGES=`grep "p max" $datafile | cut -d' ' -f4`
-    LOGFILE_LINE="$datafile $NUMBER_OF_NODES $NUMBER_OF_EDGES"
-    echo "Kolmogorov..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$KOLMO_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo "Push-Relabel..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$BOOST_PUSH_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo "Boost::Kolmo..." 
-    LOGFILE_LINE="$LOGFILE_LINE `$BOOST_KOLMO_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo "hi_pr" 
-    LOGFILE_LINE="$LOGFILE_LINE `$H_PRF_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo "hi_prw" 
-    LOGFILE_LINE="$LOGFILE_LINE `$H_PRFW_EXE < $datafile |grep time: |sed 's/time: //'` "
-    echo $LOGFILE_LINE >> $GENRMF_LOG
-  done
-}
-
 case $1 in
-"AC") run_ac_test ;;
-
-"AK") run_ak_test ;;
-
-"GENRMF") run_genrmf_test;;
-
-"" ) echo "Usage: generate_test_cases {AC|AK|GENRMF|filename(s)} ";;
+"" ) echo "Usage: generate_test_cases filename(s) ";;
 
 *)
 	FILE_LOG=results/file.log
-	echo "#filename vertices edges kolmogorov pushrelabel boost_kolmo hi_pr hi_prw" > $FILE_LOG
+	echo $RESULT_DESCRIPTION > $FILE_LOG
 	for i; do
 		datafile="$i"
 		if [ -e "$datafile" ]; then
-			NUMBER_OF_NODES=`grep "p max" $datafile | cut -d' ' -f3`
-			NUMBER_OF_EDGES=`grep "p max" $datafile | cut -d' ' -f4`
-			LOGFILE_LINE="$datafile $NUMBER_OF_NODES $NUMBER_OF_EDGES"
-			echo "Running tests on file $datafile"
-			echo "Kolmogorov:" 
-			LOGFILE_LINE="$LOGFILE_LINE `$KOLMO_EXE < $datafile | grep time: |sed 's/time: //'` "
-			echo "Push-Relabel:" 
-			LOGFILE_LINE="$LOGFILE_LINE `$BOOST_PUSH_EXE < $datafile | grep time: |sed 's/time: //'` "
-			echo "Boost::Kolmo:" 
-			LOGFILE_LINE="$LOGFILE_LINE `$BOOST_KOLMO_EXE < $datafile | grep time: |sed 's/time: //'` "
-			echo "hi_pr" 
-			LOGFILE_LINE="$LOGFILE_LINE `$H_PRF_EXE < $datafile | grep time: |sed 's/time: //'` "
-			echo "hi_prw" 
-			LOGFILE_LINE="$LOGFILE_LINE `$H_PRFW_EXE < $datafile | grep time: |sed 's/time: //'` "
-			echo $LOGFILE_LINE >> $FILE_LOG
+        NUMBER_OF_NODES=`grep "p max" $datafile | cut -d' ' -f3`
+        NUMBER_OF_EDGES=`grep "p max" $datafile | cut -d' ' -f4`
+		  LOGFILE_LINE="$datafile $NUMBER_OF_NODES $NUMBER_OF_EDGES"
+        LAST_FLOW=""
+		  echo "Running tests on file $datafile"
+        for exe in $EXE_FILES
+        do
+          echo "$exe ... "
+          i=0
+          time_sum=0
+          while [ $i -lt $NUMBER_OF_TEST_REPEATS ]
+          do
+            echo "$exe < $datafile"
+            RESULT=`$exe < "$datafile"`
+            FLOW_VALUE=`echo $RESULT | cut -d' ' -f2`
+            TIME_VALUE=`echo $RESULT | cut -d' ' -f4`
+            if [ "$LAST_FLOW" = "" ] ; then
+              LAST_FLOW=$FLOW_VALUE
+            fi
+            if [ "$LAST_FLOW" != "$FLOW_VALUE" ] ; then
+              echo "--------------------------------------------------------------------------------------"
+              echo "WARNING! Flow values don't match! Check results which implementation caused the error!"
+              echo "--------------------------------------------------------------------------------------"
+            fi
+            echo "RUN $i; time / flow: $TIME_VALUE / $FLOW_VALUE"
+            time_sum=`echo "scale=3; $time_sum + $TIME_VALUE" | bc`
+            ((++i))
+          done
+          average_time=`echo "scale=3; $time_sum / $NUMBER_OF_TEST_REPEATS" | bc`
+          echo "average time: $average_time [s]"
+          echo "flow: $FLOW_VALUE"
+          LOGFILE_LINE="$LOGFILE_LINE $average_time"
+        done
+        echo $LOGFILE_LINE >> $FILE_LOG
 		else
-			echo "File not found $datafile"
-			exit 
+		  echo "File not found $datafile"
+		  exit 
 		fi
 	done
 esac
