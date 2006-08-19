49ac5d399bd3712eacdf7f9db92b29f9cdc68690
Xiaogang Zhang (bounce) <xiaogang.zhang@users.sf.net>
2006-08-19 19:24:21 +0000

    SVN-Revision: 5849

--- SOC/soc-import/boost/soc/2006/tr1-math/trunk/boost/math/tools/test.hpp
+++ SOC/soc-import/boost/soc/2006/tr1-math/trunk/boost/math/tools/test.hpp
@@ -1,4 +1,27 @@
 //  (C) Copyright John Maddock 2006.
+//
+//  This code may be used under either of the following two licences:
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE. OF SUCH DAMAGE.
+//
+//  Or:
+//
 //  Use, modification and distribution are subject to the
 //  Boost Software License, Version 1.0. (See accompanying file
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -122,6 +145,12 @@ test_result<typename calculate_result_type<A>::value_type> test(const A& a, F1 t
       value_type point = test_func(row);
       value_type expected = expect_func(row);
       value_type err = relative_error(point, expected);
+
+std::cout.flags(std::ios::scientific);
+std::cout << std::setw(34) << std::setprecision(24) << point;
+std::cout << std::setw(34) << std::setprecision(24) << expected;
+std::cout << std::setw(14) << std::setprecision(4) << (expected - point) / expected << std::endl;
+
 #ifdef BOOST_INSTRUMENT
       if(err != 0) 
       {
@@ -155,4 +184,3 @@ test_result<typename calculate_result_type<A>::value_type> test(const A& a, F1 t
 } } } // namespaces
 
 #endif
-
