da29088ecb0dd9d014bc3e141aba475f46f81acf
Matias Capeletto <matias.capeletto@gmail.com>
2006-08-20 19:50:56 +0000

    SoC final release
    SVN-Revision: 5876

--- SOC/soc-import/boost/soc/2006/misc/trunk/boost/bimap/unconstrained_set_of.hpp
+++ SOC/soc-import/boost/soc/2006/misc/trunk/boost/bimap/unconstrained_set_of.hpp
@@ -1,24 +1,42 @@
-// Boost.bimap
+// Boost.Bimap
 //
-// Copyright 2006 Matias Capeletto
+// Copyright (c) 2006 Matias Capeletto
+//
+// This code may be used under either of the following two licences:
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE. OF SUCH DAMAGE.
+//
+// Or:
+//
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
-//
-// See http://www.boost.org/libs/bimap for library home page.
 
-/// \file set_of.hpp
+/// \file unconstrained_set_of.hpp
 /// \brief Include support for set constrains for the bimap container
 
-#ifndef BOOST_BIMAP_SET_OF_HPP
-#define BOOST_BIMAP_SET_OF_HPP
+#ifndef BOOST_BIMAP_UNCONSTRAINED_SET_OF_HPP
+#define BOOST_BIMAP_UNCONSTRAINED_SET_OF_HPP
 
 #ifdef BOOST_BIMAP_DISABLE_SERIALIZATION
     #define BOOST_MULTI_INDEX_DISABLE_SERIALIZATION
 #endif
-
-#include <functional>
-#include <boost/mpl/placeholders.hpp>
 
 #include <boost/bimap/detail/concept_tags.hpp>
 
@@ -26,10 +44,10 @@
 #include <boost/bimap/detail/generate_view_binder.hpp>
 #include <boost/bimap/detail/generate_relation_binder.hpp>
 
-#include <boost/multi_index/ordered_index.hpp>
+#include <boost/bimap/views/unconstrained_map_view.hpp>
+#include <boost/bimap/views/unconstrained_set_view.hpp>
 
-#include <boost/bimap/views/map_view.hpp>
-#include <boost/bimap/views/set_view.hpp>
+#include <boost/bimap/detail/mpl/bool_result.hpp>
 
 namespace boost {
 namespace bimap {
@@ -40,143 +58,96 @@ This struct is used to specify a set specification.
 It is not a container, it is just a metaprogramming facility to
 express the type of a set. Generally, this specification will
 be used in other place to create a container.
-It has the same syntax that an std::set instantiation, except
-that the allocator can not be specified. The rationale behind
-this difference is that the allocator is not part of the set
-type specification, rather it is a container configuration
-parameter.
-The first parameter is the type of the objects in the set, and
-the second one is a Functor that compares them.
-Bimap binding metafunctions can be used with this class in
-the following way:
+The first parameter is the type of the objects in the set.
 
 \code
+
 using namespace support;
 
-BOOST_STATIC_ASSERT( is_set_type_of< set_of<Type> >::value );
-
-BOOST_STATIC_ASSERT
-(
-     is_same
-     <
-        set_of<Type,KeyCompare>::index_bind
-        <
-            KeyExtractor,
-            Tag
-
-        >::type,
-
-        ordered_unique< tag<Tag>, KeyExtractor, KeyCompare >
-
-    >::value
-);
-
-typedef bimap
-<
-    set_of<Type>, RightKeyType
-
-> bimap_with_left_type_as_set;
-
-BOOST_STATIC_ASSERT
-(
-    is_same
-    <
-        set_of<Type>::map_view_bind
-        <
-            member_at::left,
-            bimap_with_left_type_as_set
-
-        >::type,
-
-        map_view< member_at::left, bimap_with_left_type_as_set >
-
-    >::value
-);
+BOOST_STATIC_ASSERT( is_set_type_of< unconstrained_set_of<Type> >::value );
 
 \endcode
 
-See also set_of_relation.
+See also unconstrained_set_of_relation.
                                                                         **/
 
 template
 <
-    class KeyType,
-    class KeyCompare = std::less< KeyType >
+    class KeyType
 >
-struct set_of : public ::boost::bimap::detail::set_type_of_tag
+struct unconstrained_set_of : public ::boost::bimap::detail::set_type_of_tag
 {
     /// Type of the object that will be stored in the set
     typedef KeyType value_type;
 
-    /// Functor that compare two keys
-    typedef KeyCompare key_compare;
-
-
-    BOOST_BIMAP_GENERATE_INDEX_BINDER_1CP(
-
-        // binds to
-        multi_index::ordered_unique,
-
-        // with
-        key_compare
-    );
+    BOOST_BIMAP_GENERATE_INDEX_BINDER_FAKE
 
     BOOST_BIMAP_GENERATE_MAP_VIEW_BINDER(
 
         // binds to
-        views::map_view
+        views::unconstrained_map_view
     );
 
     BOOST_BIMAP_GENERATE_SET_VIEW_BINDER(
 
         // binds to
-        views::set_view
+        views::unconstrained_set_view
     );
 
 };
-
 
 /// \brief Set Of Relation Specification
 /**
-This struct is similar to set_of but it is bind logically to a
-relation. It is used in the bimap instantiation to specify the
-desired type of the main view. This struct implements internally
-a metafunction named bind_to that manages the quite complicated
-task of finding the right type of the set for the relation.
+This struct is similar to unconstrained_set_of but it is bind
+logically to a relation. It is used in the bimap instantiation to
+specify the desired type of the main view.
 
-\code
-template<class Relation>
-struct bind_to
-{
-    typedef -unspecified- type;
-};
-\endcode
-
-See also set_of, is_set_type_of_relation.
+See also unconstrained_set_of, is_set_type_of_relation.
                                                                 **/
 
-template< class KeyCompare = std::less< _relation > >
-struct set_of_relation : public ::boost::bimap::detail::set_type_of_relation_tag
+struct unconstrained_set_of_relation : public ::boost::bimap::detail::set_type_of_relation_tag
 {
-    /// Functor that compare two keys
-    typedef KeyCompare key_compare;
 
-
-    BOOST_BIMAP_GENERATE_RELATION_BINDER_1CP(
+    BOOST_BIMAP_GENERATE_RELATION_BINDER_0CP(
 
         // binds to
-        set_of,
-
-        // with
-        key_compare
+        unconstrained_set_of
     );
 
 };
 
+#ifndef BOOST_BIMAP_DOXYGEN_WILL_NOT_PROCESS_THE_FOLLOWING_LINES
+
+namespace detail {
+
+template<class T>
+struct is_unconstrained_set_of
+{
+    BOOST_BIMAP_MPL_BOOL_RESULT(false)
+};
+
+template<class T>
+struct is_unconstrained_set_of< unconstrained_set_of<T> >
+{
+    BOOST_BIMAP_MPL_BOOL_RESULT(true)
+};
+
+} // namespace detail
+
+#endif // BOOST_BIMAP_DOXYGEN_WILL_NOT_PROCESS_THE_FOLLOWING_LINES
 
 } // namespace bimap
 } // namespace boost
 
 
-#endif // BOOST_BIMAP_SET_OF_HPP
+/** \struct boost::bimap::detail::is_unconstrained_set_of
+\brief Trait to check if a type is unconstrained_set_of.
+\code
+template< class T >
+struct is_unconstrained_set_of;
+\endcode
+                                                                            **/
+
+
+#endif // BOOST_BIMAP_UNCONSTRAINED_SET_OF_HPP
 
