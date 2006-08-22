fe7d69a91df77060238a54307f41e1ac5716fbeb
Julio Merino <julio+github@meroh.net>
2006-08-22 21:10:48 +0000

    Avoid posix_status tests under Win32.
    SVN-Revision: 5939

--- SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/posix_status_test.cpp
+++ SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/posix_status_test.cpp
@@ -9,15 +9,32 @@
 // http://www.boost.org/LICENSE_1_0.txt.)
 //
 
+#include <boost/process/config.hpp>
+
+#if defined(BOOST_PROCESS_POSIX_API)
+
+#   include <boost/process/posix_status.hpp>
+
+#   include "status_base_test.hpp"
+
+namespace bp = ::boost::process;
+#endif
+
 #include <boost/filesystem/operations.hpp>
-#include <boost/process/posix_status.hpp>
 #include <boost/test/unit_test.hpp>
 
-#include "status_base_test.hpp"
-
 namespace bfs = ::boost::filesystem;
-namespace bp = ::boost::process;
 namespace but = ::boost::unit_test;
+
+// ------------------------------------------------------------------------
+
+#if !defined(BOOST_PROCESS_POSIX_API)
+static
+void
+test_dummy(void)
+{
+}
+#endif
 
 // ------------------------------------------------------------------------
 
@@ -28,7 +45,11 @@ init_unit_test_suite(int argc, char* argv[])
 
     but::test_suite* test = BOOST_TEST_SUITE("posix_status test suite");
 
+#if defined(BOOST_PROCESS_POSIX_API)
     add_tests_status_base< bp::posix_status >(test);
+#else
+    test->add(BOOST_TEST_CASE(&test_dummy));
+#endif
 
     return test;
 }
