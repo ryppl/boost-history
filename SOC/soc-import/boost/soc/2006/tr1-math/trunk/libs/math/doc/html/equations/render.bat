ae7484aa22b290e811f7911ed260aa57545feb94
Xiaogang Zhang (bounce) <xiaogang.zhang@users.sf.net>
2006-09-03 04:17:44 +0000

    SVN-Revision: 6016

---
+++ SOC/soc-import/boost/soc/2006/tr1-math/trunk/libs/math/doc/html/equations/render.bat
@@ -0,0 +1,107 @@
+latex bessel1.tex
+latex bessel2.tex
+latex bessel3.tex
+latex bessel4.tex
+latex bessel5.tex
+latex bessel6.tex
+latex bessel7.tex
+latex bessel8.tex
+latex bessel9.tex
+latex bessel10.tex
+latex bessel11.tex
+latex bessel12.tex
+latex bessel13.tex
+latex bessel14.tex
+latex bessel15.tex
+latex bessel16.tex
+latex ellint1.tex
+latex ellint2.tex
+latex ellint3.tex
+latex ellint4.tex
+latex ellint5.tex
+latex ellint6.tex
+latex ellint7.tex
+latex ellint8.tex
+latex ellint9.tex
+latex ellint10.tex
+latex ellint11.tex
+latex ellint12.tex
+latex ellint13.tex
+latex ellint14.tex
+latex ellint15.tex
+latex ellint16.tex
+latex mbessel1.tex
+latex mbessel2.tex
+latex mbessel3.tex
+latex mbessel4.tex
+latex mbessel5.tex
+latex mbessel6.tex
+latex mbessel7.tex
+latex mbessel8.tex
+latex mbessel9.tex
+latex mbessel10.tex
+latex mbessel11.tex
+latex mbessel12.tex
+latex mbessel13.tex
+latex mbessel14.tex
+latex mbessel15.tex
+latex mbessel16.tex
+latex sbessel1.tex
+latex sbessel2.tex
+latex sbessel3.tex
+latex sbessel4.tex
+dvipng -Q 8 -D 120 -T tight -o bessel1.png bessel1.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel2.png bessel2.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel3.png bessel3.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel4.png bessel4.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel5.png bessel5.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel6.png bessel6.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel7.png bessel7.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel8.png bessel8.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel9.png bessel9.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel10.png bessel10.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel11.png bessel11.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel12.png bessel12.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel13.png bessel13.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel14.png bessel14.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel15.png bessel15.dvi
+dvipng -Q 8 -D 120 -T tight -o bessel16.png bessel16.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint1.png ellint1.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint2.png ellint2.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint3.png ellint3.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint4.png ellint4.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint5.png ellint5.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint6.png ellint6.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint7.png ellint7.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint8.png ellint8.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint9.png ellint9.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint10.png ellint10.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint11.png ellint11.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint12.png ellint12.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint13.png ellint13.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint14.png ellint14.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint15.png ellint15.dvi
+dvipng -Q 8 -D 120 -T tight -o ellint16.png ellint16.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel1.png mbessel1.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel2.png mbessel2.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel3.png mbessel3.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel4.png mbessel4.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel5.png mbessel5.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel6.png mbessel6.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel7.png mbessel7.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel8.png mbessel8.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel9.png mbessel9.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel10.png mbessel10.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel11.png mbessel11.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel12.png mbessel12.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel13.png mbessel13.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel14.png mbessel14.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel15.png mbessel15.dvi
+dvipng -Q 8 -D 120 -T tight -o mbessel16.png mbessel16.dvi
+dvipng -Q 8 -D 120 -T tight -o sbessel1.png sbessel1.dvi
+dvipng -Q 8 -D 120 -T tight -o sbessel2.png sbessel2.dvi
+dvipng -Q 8 -D 120 -T tight -o sbessel3.png sbessel3.dvi
+dvipng -Q 8 -D 120 -T tight -o sbessel4.png sbessel4.dvi
+rm -f *.aux
+rm -f *.dvi
+rm -f *.log
