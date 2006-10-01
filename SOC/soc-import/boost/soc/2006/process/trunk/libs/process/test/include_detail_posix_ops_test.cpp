f4ae06a2a6be2453e6f6eee7ae19c126a455b753
Julio Merino <julio+github@meroh.net>
2006-10-01 17:25:09 +0000

    Add inclusion tests for detail/posix_ops.hpp and detail/win32_ops.hpp.
    SVN-Revision: 6126

---
+++ SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/include_detail_posix_ops_test.cpp
@@ -0,0 +1,18 @@
+//
+// Boost.Process
+// Checks that detail/posix_ops.hpp can be included without errors.
+//
+// Copyright (c) 2006 Julio M. Merino Vidal.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt.)
+//
+
+#include <boost/process/config.hpp>
+
+#if defined(BOOST_PROCESS_POSIX_API)
+
+#   include <boost/process/detail/posix_ops.hpp>
+
+#endif
---
+++ SOC/soc-import/boost/soc/2006/process/trunk/libs/process/test/include_detail_win32_ops_test.cpp
@@ -0,0 +1,18 @@
+//
+// Boost.Process
+// Checks that detail/win32_ops.hpp can be included without errors.
+//
+// Copyright (c) 2006 Julio M. Merino Vidal.
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt.)
+//
+
+#include <boost/process/config.hpp>
+
+#if defined(BOOST_PROCESS_WIN32_API)
+
+#   include <boost/process/detail/win32_ops.hpp>
+
+#endif
