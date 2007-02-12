0d0ff713a755ff7f1651eda991e6b8c6c6db4552
Matias Capeletto <matias.capeletto@gmail.com>
2007-02-12 04:57:28 +0000

    formal review version
    SVN-Revision: 6648

--- SOC/soc-import/boost/soc/2006/misc/trunk/libs/bimap/test/test_bimap_extra.cpp
+++ SOC/soc-import/boost/soc/2006/misc/trunk/libs/bimap/test/test_bimap_extra.cpp
@@ -9,17 +9,57 @@
 // Boost.Test
 #include <boost/test/minimal.hpp>
 
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_same.hpp>
+
 // Boost.Bimap
 #include <boost/bimap/support/lambda.hpp>
 #include <boost/bimap/bimap.hpp>
 #include <boost/bimap/list_of.hpp>
 
+// Support metafunctions
+#include <boost/bimap/support/data_type_by.hpp>
+#include <boost/bimap/support/key_type_by.hpp>
+#include <boost/bimap/support/map_type_by.hpp>
+#include <boost/bimap/support/value_type_by.hpp>
+#include <boost/bimap/support/iterator_type_by.hpp>
+
+using namespace boost::bimap;
+using namespace boost::bimap::support;
+
+typedef bimap<int, unconstrained_set_of<double> > bm;
+
+
+namespace support_metafunctions_test {
+
+    typedef boost::is_same
+    <
+        data_type_by< member_at::left , bm >::type,
+        key_type_by < member_at::right, bm >::type
+
+    >::type test_metafunction_1;
+    BOOST_STATIC_ASSERT(test_metafunction_1::value);
+
+    typedef boost::is_same
+    <
+        data_type_by< member_at::right, bm >::type,
+        key_type_by < member_at::left , bm >::type
+
+    >::type test_metafunction_2;
+    BOOST_STATIC_ASSERT(test_metafunction_2::value);
+
+    typedef boost::is_same
+    <
+        map_type_by  < member_at::left , bm >::type::value_type,
+        value_type_by< member_at::left , bm >::type
+
+    >::type test_metafunction_3;
+    BOOST_STATIC_ASSERT(test_metafunction_3::value);
+
+} // namespace support_metafunctions_test
+
 void test_bimap_extra()
 {
-    using namespace boost::bimap;
-
-    typedef bimap<int, unconstrained_set_of<double> > bm;
-
     bm b;
     b.left.insert( bm::left_value_type(1,0.1) );
 
@@ -27,7 +67,6 @@ void test_bimap_extra()
     iter->second = 0.2;
 
     b.begin()->right = 0.1;
-
 }
 
 
