8e3628af28401de03a6d82634d835986c674e0e1
Peter Simons <simons@cryp.to>
2007-05-09 22:32:36 +0000

    Cosmetic.
    SVN-Revision: 4027

--- boost/smart_enum.hpp
+++ boost/smart_enum.hpp
@@ -7,6 +7,7 @@
 // License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 //
+
 #ifndef BOOST_SMART_ENUM_HPP_INCLUDED
 #define BOOST_SMART_ENUM_HPP_INCLUDED
 
