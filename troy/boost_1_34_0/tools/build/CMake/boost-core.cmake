78df283ed11439a16a00f5753ab5cbf8088ec296
Troy D. Straszheim <troy@resophonic.com>
2007-05-15 22:02:56 +0000

    Basic working cmake setup:  Doug's tarball + a couple tiny tweaks
    SVN-Revision: 4066

---
+++ troy/boost_1_34_0/tools/build/CMake/boost-core.cmake
@@ -0,0 +1,132 @@
+##########################################################################
+# Boost core support                                                     #
+##########################################################################
+
+# Put the libs and bins that get built into directories at the top of
+# the build tree rather than in hard to find leaf directories
+SET(LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/lib)
+SET(EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/bin)
+
+# append to a variable
+macro(append varname)
+  set(${varname} ${${varname}} ${ARGN})
+endmacro(append varname)
+
+# Defines a Boost library subproject (e.g., for Boost.Python). Use as:
+#
+#   boost_library_subproject(libname, subdir1, subdir2, ...)
+#
+# where libname is the name of the library (e.g., Python, or
+# Filesystem) and subdir1, subdir2, etc. are the subdirectories that
+# contain CMakeLists files.
+#
+# This macro will define an option BUILD_BOOST_LIBNAME (which defaults
+# to ON). When the option is true, this macro will include the
+# subdirectories; otherwise, none of the subdirectories will be
+# included, so the library itself will not be built, installed, or
+# tested.
+macro(boost_library_subproject libname)
+  string(TOUPPER "BUILD_BOOST_${libname}" BOOST_LIB_OPTION_NAME)
+  option("${BOOST_LIB_OPTION_NAME}" "Build the Boost.${libname} library" ON)
+  if ("${BOOST_LIB_OPTION_NAME}")
+    foreach(SUBDIR ${ARGN})
+      add_subdirectory("${SUBDIR}")
+    endforeach(SUBDIR ${ARGN})
+  endif ("${BOOST_LIB_OPTION_NAME}")
+endmacro(boost_library_subproject)
+
+
+macro(boost_add_library libname)
+  # An internal flag used to keep track of the state of the keyword
+  # argument parser
+  set(BOOST_ADDLIB_PARSING_VAR "SOURCES")
+
+  # Whether we are parsing options for shared libraries or static
+  # libraries (an empty variable needs both)
+  set(BOOST_ADDLIB_SHARED_OR_STATIC) 
+
+  # Source files used to build this library
+  set(BOOST_ADDLIB_SOURCES)
+
+  # Libraries that this library depends on
+  set(BOOST_ADDLIB_LIBRARIES)
+
+  # Compile flags for the static library
+  set(BOOST_ADDLIB_STATIC_COMPILE_FLAGS "")
+
+  # Compile flags for the shared library
+  set(BOOST_ADDLIB_SHARED_COMPILE_FLAGS "")
+
+  # Whether we should build a static library
+  set(BOOST_ADDLIB_STATIC_REQ FALSE)
+
+  # Whether we should build a shared library
+  set(BOOST_ADDLIB_SHARED_REQ FALSE)
+
+  # Categorize each of the arguments
+  foreach(ARG ${ARGN})
+    # Handle keywords
+    if (${ARG} STREQUAL "SOURCES")
+      set(BOOST_ADDLIB_PARSING_VAR "SOURCES")
+    elseif (${ARG} STREQUAL "LIBRARIES")
+      set(BOOST_ADDLIB_PARSING_VAR "LIBRARIES")
+    elseif (${ARG} STREQUAL "STATIC")
+      set(BOOST_ADDLIB_STATIC_REQ TRUE)
+      set(BOOST_ADDLIB_SHARED_OR_STATIC "STATIC")
+    elseif (${ARG} STREQUAL "SHARED")
+      set(BOOST_ADDLIB_SHARED_REQ TRUE)
+      set(BOOST_ADDLIB_SHARED_OR_STATIC "SHARED")
+    elseif (${ARG} STREQUAL "COMPILE_FLAGS")
+      set(BOOST_ADDLIB_PARSING_VAR "COMPILE_FLAGS")
+
+    # Handle arguments based on the last keyword we saw
+    elseif(${BOOST_ADDLIB_PARSING_VAR} STREQUAL "SOURCES")
+      append(BOOST_ADDLIB_SOURCES ${ARG})
+    elseif (${BOOST_ADDLIB_PARSING_VAR} STREQUAL "LIBRARIES")
+      append(BOOST_ADDLIB_LIBRARIES ${ARG})
+    elseif (${BOOST_ADDLIB_PARSING_VAR} STREQUAL "COMPILE_FLAGS")
+      if (BOOST_ADDLIB_SHARED_OR_STATIC STREQUAL "STATIC")
+        append(BOOST_ADDLIB_STATIC_COMPILE_FLAGS ${ARG})
+      elseif (BOOST_ADDLIB_SHARED_OR_STATIC STREQUAL "SHARED")
+	append(BOOST_ADDLIB_SHARED_COMPILE_FLAGS ${ARG})
+      else (BOOST_ADDLIB_SHARED_OR_STATIC STREQUAL "STATIC")
+	append(BOOST_ADDLIB_STATIC_COMPILE_FLAGS ${ARG})
+	append(BOOST_ADDLIB_SHARED_COMPILE_FLAGS ${ARG})
+      endif (BOOST_ADDLIB_SHARED_OR_STATIC STREQUAL "STATIC")
+    endif (${ARG} STREQUAL "SOURCES")
+  endforeach(ARG ${ARGN})
+
+  # If neither STATIC nor SHARED was provided, we build both.
+  if (NOT BOOST_ADDLIB_STATIC_REQ)
+    if (NOT BOOST_ADDLIB_SHARED_REQ)
+      set(BOOST_ADDLIB_STATIC_REQ TRUE)
+      set(BOOST_ADDLIB_SHARED_REQ TRUE)
+    endif (NOT BOOST_ADDLIB_SHARED_REQ)
+  endif (NOT BOOST_ADDLIB_STATIC_REQ)
+
+  # Create the static library target
+  if (BOOST_ADDLIB_STATIC_REQ AND BUILD_STATIC_LIBS)
+    add_library("${libname}-static" STATIC ${BOOST_ADDLIB_SOURCES})
+    set_target_properties("${libname}-static" 
+      PROPERTIES OUTPUT_NAME "${libname}"
+                 CLEAN_DIRECT_OUTPUT 1
+                 COMPILE_FLAGS "${BOOST_ADDLIB_STATIC_COMPILE_FLAGS}")
+    if (BOOST_ADDLIB_LIBRARIES)
+      target_link_libraries ("${libname}-static" ${BOOST_ADDLIB_LIBRARIES})
+    endif (BOOST_ADDLIB_LIBRARIES)
+    install(TARGETS "${libname}-static" DESTINATION lib)
+  endif (BOOST_ADDLIB_STATIC_REQ AND BUILD_STATIC_LIBS)
+
+  # Create the shared library target
+  if (BOOST_ADDLIB_SHARED_REQ AND BUILD_SHARED_LIBS)
+    add_library("${libname}-shared" SHARED ${BOOST_ADDLIB_SOURCES})
+    set_target_properties("${libname}-shared" 
+      PROPERTIES OUTPUT_NAME "${libname}"
+                 CLEAN_DIRECT_OUTPUT 1
+                 COMPILE_FLAGS "${BOOST_ADDLIB_SHARED_COMPILE_FLAGS}")
+    if (BOOST_ADDLIB_LIBRARIES)
+      target_link_libraries ("${libname}-shared" ${BOOST_ADDLIB_LIBRARIES})
+    endif (BOOST_ADDLIB_LIBRARIES)
+    install(TARGETS "${libname}-shared" DESTINATION lib)
+  endif (BOOST_ADDLIB_SHARED_REQ AND BUILD_SHARED_LIBS)
+endmacro(boost_add_library)
---
+++ troy/boost_1_34_0/tools/build/CMake/boost-testing.cmake
@@ -0,0 +1,145 @@
+##########################################################################
+# Boost testing support                                                  #
+##########################################################################
+#
+# For each of the test macros below, the first argument, testname,
+# states the name of the test that will be created. If no other
+# arguments are provided, the source file testname.cpp will be used as
+# the source file; otherwise, all remaining arguments to the test
+# macro will be used as the source files.
+#
+# To link in additional libraries when building the executable, pass
+# LIBRARIES to the boost_*_test macro followed by followed by the list
+# of libraries to link, e.g.,
+#
+#   boost_test_run(signal_n_test LIBRARIES boost_test_exec_monitor)
+#
+# There are several macros that create tests:
+#
+#   boost_test_run: Builds an executable and runs it as a test. The test
+#                   succeeds if it builds and returns 0 when executed.
+#
+#   boost_test_run_fail: Builds an executable and runs it as a test. The
+#                        test succeeds if it builds but returns a non-zero
+#                        exit code when executed.
+#  
+#   boost_test_compile: Tests that the given source file compiles without 
+#                       any errors.
+#
+#   boost_test_compile_fail: Tests that the given source file produces 
+#                            errors when compiled.
+#
+
+option(BUILD_TESTING "Enable testing")
+include(CTest)
+
+macro(boost_test_parse_args testname)
+  set(BOOST_TEST_PARSING_VAR "SOURCES")
+  set(BOOST_TEST_SOURCES)
+  set(BOOST_TEST_LIBRARIES)
+  set(BOOST_TEST_OKAY TRUE)
+
+  # Categorize each of the arguments
+  foreach(ARG ${ARGN})
+    if (${ARG} STREQUAL "SOURCES")
+      set(BOOST_TEST_PARSING_VAR "SOURCES")
+    elseif (${ARG} STREQUAL "LIBRARIES")
+      set(BOOST_TEST_PARSING_VAR "LIBRARIES")
+    else (${ARG} STREQUAL "SOURCES")
+      if(${BOOST_TEST_PARSING_VAR} STREQUAL "SOURCES") 
+        # Add this argument to the list of sources
+        set(BOOST_TEST_SOURCES ${BOOST_TEST_SOURCES} ${ARG})
+      else(${BOOST_TEST_PARSING_VAR} STREQUAL "SOURCES")
+        # Add this argument to the list of libraries to link against
+        set(BOOST_TEST_LIBRARIES ${BOOST_TEST_LIBRARIES} ${ARG})
+
+        # If building static libraries is turned off..
+        if (NOT BUILD_STATIC_LIBS)
+          # And this is a -static library
+          if (ARG MATCHES ".*-static$")
+            # We cannot build this test
+            set(BOOST_TEST_OKAY FALSE)
+          endif (ARG MATCHES ".*-static$")
+        endif (NOT BUILD_STATIC_LIBS)
+
+        # If building shared libraries is turned off..
+        if (NOT BUILD_SHARED_LIBS)
+          # And this is a -shared library
+          if (ARG MATCHES ".*-shared$")
+            # We cannot build this test
+            set(BOOST_TEST_OKAY FALSE)
+          endif (ARG MATCHES ".*-shared$")
+        endif (NOT BUILD_SHARED_LIBS)
+      endif(${BOOST_TEST_PARSING_VAR} STREQUAL "SOURCES")
+    endif (${ARG} STREQUAL "SOURCES")
+  endforeach(ARG ${ARGN})
+
+  # Determine the list of sources
+  if (NOT BOOST_TEST_SOURCES)
+    set(BOOST_TEST_SOURCES "${testname}.cpp")
+  endif (NOT BOOST_TEST_SOURCES)
+
+  # If testing is turned off, this test is not okay
+  if (NOT BUILD_TESTING)
+    set(BOOST_TEST_OKAY FALSE)
+  endif(NOT BUILD_TESTING)
+endmacro(boost_test_parse_args)
+
+macro(boost_test_run testname)
+  boost_test_parse_args(${testname} ${ARGN})
+  if (BOOST_TEST_OKAY)
+    add_executable(${testname} ${BOOST_TEST_SOURCES})
+
+    if (BOOST_TEST_LIBRARIES)
+      target_link_libraries(${testname} ${BOOST_TEST_LIBRARIES})
+    endif(BOOST_TEST_LIBRARIES)
+
+    add_test(${testname} ${EXECUTABLE_OUTPUT_PATH}/${testname})
+  endif(BOOST_TEST_OKAY)
+endmacro(boost_test_run)
+
+macro(boost_test_run_fail testname)
+  boost_test_parse_args(${testname} ${ARGN})
+  if(BOOST_TEST_OKAY)
+    add_executable(${testname} ${BOOST_TEST_SOURCES})
+
+    if (BOOST_TEST_LIBRARIES)
+      target_link_libraries(${testname} ${BOOST_TEST_LIBRARIES})
+    endif(BOOST_TEST_LIBRARIES)
+
+    add_test(${testname} ${EXECUTABLE_OUTPUT_PATH}/${testname})
+    set_tests_properties(${testname} PROPERTIES WILL_FAIL TRUE)
+  endif(BOOST_TEST_OKAY)
+endmacro(boost_test_run_fail)
+
+macro(boost_test_compile testname)
+  boost_test_parse_args(${testname} ${ARGN})
+  if(BOOST_TEST_OKAY)
+    add_test(${testname}
+  	     ${CMAKE_CTEST_COMMAND}
+             --build-and-test
+             "${Boost_SOURCE_DIR}/tools/build/CMake/CompileTest"
+             "${Boost_BINARY_DIR}/tools/build/CMake/CompileTest"
+             --build-generator "${CMAKE_GENERATOR}"
+             --build-makeprogram "${MAKEPROGRAM}"
+	     --build-project CompileTest
+	     --build-options -DSOURCE=${CMAKE_CURRENT_SOURCE_DIR}/${BOOST_TEST_SOURCES} -DINCLUDES=${Boost_SOURCE_DIR})
+  endif(BOOST_TEST_OKAY)
+endmacro(boost_test_compile)
+
+macro(boost_test_compile_fail testname)
+  boost_test_parse_args(${testname} ${ARGN})
+    
+  if(BOOST_TEST_OKAY)
+    add_test(${testname}
+	     ${CMAKE_CTEST_COMMAND}
+             --build-and-test
+             "${Boost_SOURCE_DIR}/tools/build/CMake/CompileTest"
+             "${Boost_BINARY_DIR}/tools/build/CMake/CompileTest"
+             --build-generator "${CMAKE_GENERATOR}"
+             --build-makeprogram "${MAKEPROGRAM}"
+	     --build-project CompileTest
+	     --build-options -DSOURCE=${CMAKE_CURRENT_SOURCE_DIR}/${BOOST_TEST_SOURCES} -DINCLUDES=${Boost_SOURCE_DIR})
+    set_tests_properties(${testname} PROPERTIES WILL_FAIL ON)
+  endif(BOOST_TEST_OKAY)
+endmacro(boost_test_compile_fail)
