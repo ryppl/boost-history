0079a1927ac82b3d609602bcd682426056413a12
Troy D. Straszheim <troy@resophonic.com>
2007-05-16 23:37:15 +0000

    remove previous complications that turned out to be unnecessary.  disambiguate shared from static via OUTPUT_NAME and the two-pass SRC and TEST scheme is unnecessary.
    SVN-Revision: 4088

--- troy/boost_1_34_0/tools/build/CMake/boost-core.cmake
+++ troy/boost_1_34_0/tools/build/CMake/boost-core.cmake
@@ -33,18 +33,13 @@ macro(boost_library_subproject libname)
     )
   project(${libname})
   string(TOUPPER "BUILD_BOOST_${libname}" BOOST_LIB_OPTION_NAME)
-  option("${BOOST_LIB_OPTION_NAME}" "Build the Boost.${libname} library" ON)
-
-  if(${BOOST_CMAKE_READ_PASS} STREQUAL "SRC")
-    foreach(SUBDIR ${THIS_SUBPROJECT_SRCDIRS})
-      add_subdirectory(${SUBDIR})
-    endforeach(SUBDIR ${THIS_SUBPROJECT_SRCDIRS})
-  endif(${BOOST_CMAKE_READ_PASS} STREQUAL "SRC")
-  if(${BOOST_CMAKE_READ_PASS} STREQUAL "TEST")
-    foreach(SUBDIR ${THIS_SUBPROJECT_TESTDIRS})
-      add_subdirectory(${SUBDIR})
-    endforeach(SUBDIR ${THIS_SUBPROJECT_TESTDIRS})
-  endif(${BOOST_CMAKE_READ_PASS} STREQUAL "TEST")
+  # currently src and testdirs are irrelevant.  At one point it seemed
+  # that they would need to be kept separate and scanned in order
+  # CLEANUP:  put src/test dirs back together again, if no future
+  #           need for it comes up
+  foreach(SUBDIR ${THIS_SUBPROJECT_SRCDIRS} ${THIS_SUBPROJECT_TESTDIRS})
+    add_subdirectory(${SUBDIR})
+  endforeach(SUBDIR ${THIS_SUBPROJECT_SRCDIRS} ${THIS_SUBPROJECT_TESTDIRS})
 endmacro(boost_library_subproject)
 
 macro(boost_library)
@@ -59,6 +54,9 @@ macro(boost_library)
   IF(NOT "${THIS_LIB_NO_STATIC}" STREQUAL "TRUE")
     add_library("${libname}-static" STATIC ${sources})
     set_target_properties("${libname}-static" 
+      # notice that the static ones have -static added to the lib name.
+      # this is to accomodate those who insist on linking to the
+      # static varieties even when dynamics are available.
       PROPERTIES OUTPUT_NAME "${libname}-static"
       CLEAN_DIRECT_OUTPUT 1
       COMPILE_FLAGS "${THIS_LIB_COMPILE_FLAGS} ${THIS_LIB_STATIC_COMPILE_FLAGS}")
@@ -82,97 +80,3 @@ macro(boost_library)
   ENDIF(NOT "${THIS_LIB_NO_SHARED}" STREQUAL "TRUE")
 endmacro(boost_library)
 
-macro(boost_add_library libname)
-  # An internal flag used to keep track of the state of the keyword
-  # argument parser
-  set(BOOST_ADDLIB_PARSING_VAR "SOURCES")
-
-  # Whether we are parsing options for shared libraries or static
-  # libraries (an empty variable needs both)
-  set(BOOST_ADDLIB_SHARED_OR_STATIC) 
-
-  # Source files used to build this library
-  set(BOOST_ADDLIB_SOURCES)
-
-  # Libraries that this library depends on
-  set(BOOST_ADDLIB_LIBRARIES)
-
-  # Compile flags for the static library
-  set(BOOST_ADDLIB_STATIC_COMPILE_FLAGS "")
-
-  # Compile flags for the shared library
-  set(BOOST_ADDLIB_SHARED_COMPILE_FLAGS "")
-
-  # Whether we should build a static library
-  set(BOOST_ADDLIB_STATIC_REQ FALSE)
-
-  # Whether we should build a shared library
-  set(BOOST_ADDLIB_SHARED_REQ FALSE)
-
-  # Categorize each of the arguments
-  foreach(ARG ${ARGN})
-    # Handle keywords
-    if (${ARG} STREQUAL "SOURCES")
-      set(BOOST_ADDLIB_PARSING_VAR "SOURCES")
-    elseif (${ARG} STREQUAL "LIBRARIES")
-      set(BOOST_ADDLIB_PARSING_VAR "LIBRARIES")
-    elseif (${ARG} STREQUAL "STATIC")
-      set(BOOST_ADDLIB_STATIC_REQ TRUE)
-      set(BOOST_ADDLIB_SHARED_OR_STATIC "STATIC")
-    elseif (${ARG} STREQUAL "SHARED")
-      set(BOOST_ADDLIB_SHARED_REQ TRUE)
-      set(BOOST_ADDLIB_SHARED_OR_STATIC "SHARED")
-    elseif (${ARG} STREQUAL "COMPILE_FLAGS")
-      set(BOOST_ADDLIB_PARSING_VAR "COMPILE_FLAGS")
-
-    # Handle arguments based on the last keyword we saw
-    elseif(${BOOST_ADDLIB_PARSING_VAR} STREQUAL "SOURCES")
-      append(BOOST_ADDLIB_SOURCES ${ARG})
-    elseif (${BOOST_ADDLIB_PARSING_VAR} STREQUAL "LIBRARIES")
-      append(BOOST_ADDLIB_LIBRARIES ${ARG})
-    elseif (${BOOST_ADDLIB_PARSING_VAR} STREQUAL "COMPILE_FLAGS")
-      if (BOOST_ADDLIB_SHARED_OR_STATIC STREQUAL "STATIC")
-        append(BOOST_ADDLIB_STATIC_COMPILE_FLAGS ${ARG})
-      elseif (BOOST_ADDLIB_SHARED_OR_STATIC STREQUAL "SHARED")
-	append(BOOST_ADDLIB_SHARED_COMPILE_FLAGS ${ARG})
-      else (BOOST_ADDLIB_SHARED_OR_STATIC STREQUAL "STATIC")
-	append(BOOST_ADDLIB_STATIC_COMPILE_FLAGS ${ARG})
-	append(BOOST_ADDLIB_SHARED_COMPILE_FLAGS ${ARG})
-      endif (BOOST_ADDLIB_SHARED_OR_STATIC STREQUAL "STATIC")
-    endif (${ARG} STREQUAL "SOURCES")
-  endforeach(ARG ${ARGN})
-
-  # If neither STATIC nor SHARED was provided, we build both.
-  if (NOT BOOST_ADDLIB_STATIC_REQ)
-    if (NOT BOOST_ADDLIB_SHARED_REQ)
-      set(BOOST_ADDLIB_STATIC_REQ TRUE)
-      set(BOOST_ADDLIB_SHARED_REQ TRUE)
-    endif (NOT BOOST_ADDLIB_SHARED_REQ)
-  endif (NOT BOOST_ADDLIB_STATIC_REQ)
-
-  # Create the static library target
-  if (BOOST_ADDLIB_STATIC_REQ AND BUILD_STATIC_LIBS)
-    add_library("${libname}-static" STATIC ${BOOST_ADDLIB_SOURCES})
-    set_target_properties("${libname}-static" 
-      PROPERTIES OUTPUT_NAME "${libname}"
-                 CLEAN_DIRECT_OUTPUT 1
-                 COMPILE_FLAGS "${BOOST_ADDLIB_STATIC_COMPILE_FLAGS}")
-    if (BOOST_ADDLIB_LIBRARIES)
-      target_link_libraries ("${libname}-static" ${BOOST_ADDLIB_LIBRARIES}-static)
-    endif (BOOST_ADDLIB_LIBRARIES)
-    install(TARGETS "${libname}-static" DESTINATION lib)
-  endif (BOOST_ADDLIB_STATIC_REQ AND BUILD_STATIC_LIBS)
-
-  # Create the shared library target
-  if (BOOST_ADDLIB_SHARED_REQ AND BUILD_SHARED_LIBS)
-    add_library("${libname}-shared" SHARED ${BOOST_ADDLIB_SOURCES})
-    set_target_properties("${libname}-shared" 
-      PROPERTIES OUTPUT_NAME "${libname}"
-                 CLEAN_DIRECT_OUTPUT 1
-                 COMPILE_FLAGS "${BOOST_ADDLIB_SHARED_COMPILE_FLAGS}")
-    if (BOOST_ADDLIB_LIBRARIES)
-      target_link_libraries ("${libname}-shared" ${BOOST_ADDLIB_LIBRARIES}-shared)
-    endif (BOOST_ADDLIB_LIBRARIES)
-    install(TARGETS "${libname}-shared" DESTINATION lib)
-  endif (BOOST_ADDLIB_SHARED_REQ AND BUILD_SHARED_LIBS)
-endmacro(boost_add_library)
