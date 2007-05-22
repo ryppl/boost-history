05e3aca01b73353dfa3f194280a59375aba820d6
Troy D. Straszheim <troy@resophonic.com>
2007-05-22 22:41:51 +0000

    ugly duct-tape script that shows how to run continuous release/debug builds on unix
    SVN-Revision: 4196

---
+++ troy/unix_testing_node/continuous/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -x
+
+topdir=`pwd`
+
+
+cd $topdir/debug/1.34.0/src
+svn update
+cd $topdir/debug/1.34.0/build
+rm CMakeCache.txt
+cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
+
+cd $topdir/release/1.34.0/src
+svn update
+cd $topdir/release/1.34.0/build
+rm CMakeCache.txt
+cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
+
+# alternately build debug and release
+while true
+do
+  cd $topdir/debug/1.34.0/build
+  ctest -D Continuous
+  cd $topdir/release/1.34.0/build
+  ctest -D Continuous
+done
+
