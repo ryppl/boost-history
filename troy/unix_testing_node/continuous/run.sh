3f6b943d2c763ec87e2a30f1a30c6223d879d84c
Troy D. Straszheim <troy@resophonic.com>
2007-05-22 22:59:10 +0000

    split initialization and running
    SVN-Revision: 4198

--- troy/unix_testing_node/continuous/run.sh
+++ troy/unix_testing_node/continuous/run.sh
@@ -2,19 +2,6 @@
 
 topdir=`pwd`
 
-
-cd $topdir/debug/1.34.0/src
-svn update
-cd $topdir/debug/1.34.0/build
-rm CMakeCache.txt
-cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
-
-cd $topdir/release/1.34.0/src
-svn update
-cd $topdir/release/1.34.0/build
-rm CMakeCache.txt
-cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
-
 # alternately build debug and release
 while true
 do
@@ -22,5 +9,6 @@ do
   ctest -D Continuous
   cd $topdir/release/1.34.0/build
   ctest -D Continuous
+  sleep 180  # wait three minutes
 done
 
