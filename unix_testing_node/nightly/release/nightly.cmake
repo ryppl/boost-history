47fcc9fd5cb4b9e0b1afbfbecd8f1bd4c86c0dd5
Troy D. Straszheim <troy@resophonic.com>
2007-05-25 17:33:59 +0000

    ctest "scripts" to run nightly builds
    SVN-Revision: 4249

---
+++ unix_testing_node/nightly/release/nightly.cmake
@@ -0,0 +1,15 @@
+set (CTEST_BINARY_DIRECTORY "1.34.0/build")
+# this is relative to binary directory
+set (CTEST_SOURCE_DIRECTORY "../src") 
+
+set (CTEST_CMAKE_COMMAND "cmake")
+set (CTEST_COMMAND "ctest -D Nightly")
+
+set (CTEST_INITIAL_CACHE "
+
+CMAKE_BUILD_TYPE:STRING=Release
+BUILD_TESTING:BOOL=ON
+BUILDNAME:STRING=${BUILDNAME}
+
+"
+)
