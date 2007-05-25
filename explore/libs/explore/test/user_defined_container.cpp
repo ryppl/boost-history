3b2e77408ab77f887b3d468eba46310b6ab4b5fc
Jeffrey Faust <jeff@opticalres.com>
2007-05-25 01:00:46 +0000

    fix for the print case
    SVN-Revision: 4234

--- explore/libs/explore/test/user_defined_container.cpp
+++ explore/libs/explore/test/user_defined_container.cpp
@@ -9,131 +9,56 @@
 
 #define BOOST_TEST_MODULE PrintLib
 #include <boost/test/unit_test.hpp>
-#include <string>
 #include <sstream>
 #include <vector>
 #include "../../../boost/explore/explore.hpp"
 #include "../../../boost/explore/stream_container.hpp"
 
-BOOST_AUTO_TEST_CASE( basic_vector_print_test )
+class user_vector
+{
+public:
+    user_vector()
+    {
+        m_vec.push_back(1);
+        m_vec.push_back(2);
+        m_vec.push_back(3);
+    }
+
+    std::vector<int>::const_iterator start() const
+    {
+        return m_vec.begin();
+    }
+
+    std::vector<int>::const_iterator finish() const
+    {
+        return m_vec.end();
+    }
+
+private:
+    std::vector<int> m_vec;
+};
+
+std::ostream& operator<<(std::ostream& ostr, const user_vector& u)
+{
+    return boost::stream_container(ostr, u.start(), u.finish());
+}
+
+BOOST_AUTO_TEST_CASE( user_defined_print_test )
 {
     std::stringstream str_out;
 
-    std::vector<int> vi;
-    explore::print(vi, str_out);
-    BOOST_CHECK_EQUAL(str_out.str(), "[]");
-
-    str_out.str("");
-
-    vi.push_back(1);
-    explore::print(vi, str_out);
-    BOOST_CHECK_EQUAL(str_out.str(), "[1]");
-
-    str_out.str("");
-
-    vi.push_back(2);
-    vi.push_back(3);
-    explore::print(vi, str_out);
+    user_vector v;
+    explore::print(v, str_out);
     BOOST_CHECK_EQUAL(str_out.str(), "[1, 2, 3]");
 }
 
-BOOST_AUTO_TEST_CASE( basic_vector_stream_test )
+BOOST_AUTO_TEST_CASE( user_defined_stream_test )
 {
     using namespace boost;
     std::stringstream str_out;
 
-    std::vector<int> vi;
-    str_out << vi;
-    BOOST_CHECK_EQUAL(str_out.str(), "[]");
-
-    str_out.str("");
-
-    vi.push_back(1);
-    str_out << vi;
-    BOOST_CHECK_EQUAL(str_out.str(), "[1]");
-
-    str_out.str("");
-
-    vi.push_back(2);
-    vi.push_back(3);
-    str_out << vi;
+    user_vector v;
+    str_out << v;
     BOOST_CHECK_EQUAL(str_out.str(), "[1, 2, 3]");
 }
 
-BOOST_AUTO_TEST_CASE( vector_in_vector_print_test )
-{
-    std::stringstream str_out;
-
-    std::vector<int> vi;
-    vi.push_back(1);
-    vi.push_back(2);
-    vi.push_back(3);
-
-    std::vector<std::vector<int> > vvi;
-    explore::print(vvi, str_out);
-    BOOST_CHECK_EQUAL(str_out.str(), "[]");
-
-    str_out.str("");
-
-    vvi.push_back(vi);
-    explore::print(vvi, str_out);
-    BOOST_CHECK_EQUAL(str_out.str(), "[[1, 2, 3]]");
-
-    str_out.str("");
-
-    vvi.push_back(vi);
-    vvi.push_back(vi);
-    explore::print(vvi, str_out);
-    BOOST_CHECK_EQUAL(str_out.str(), "[[1, 2, 3], [1, 2, 3], [1, 2, 3]]");
-}
-
-BOOST_AUTO_TEST_CASE( vector_in_vector_stream_test )
-{
-    using namespace boost;
-    std::stringstream str_out;
-
-    std::vector<int> vi;
-    vi.push_back(1);
-    vi.push_back(2);
-    vi.push_back(3);
-
-    std::vector<std::vector<int> > vvi;
-    str_out << vvi;
-    BOOST_CHECK_EQUAL(str_out.str(), "[]");
-
-    str_out.str("");
-
-    vvi.push_back(vi);
-    str_out << vvi;
-    BOOST_CHECK_EQUAL(str_out.str(), "[[1, 2, 3]]");
-
-    str_out.str("");
-
-    vvi.push_back(vi);
-    vvi.push_back(vi);
-    str_out << vvi;
-    BOOST_CHECK_EQUAL(str_out.str(), "[[1, 2, 3], [1, 2, 3], [1, 2, 3]]");
-}
-
-// This test shows that you can use a string to cause the same outputs
-// as other print statements.
-BOOST_AUTO_TEST_CASE( vector_with_ugly_string_case_print_test )
-{
-    std::stringstream str_out;
-
-    std::vector<std::string> vs;
-    vs.push_back("[1, 2, 3], [1, 2, 3], [1, 2, 3]");
-    explore::print(vs, str_out);
-    BOOST_CHECK_EQUAL(str_out.str(), "[[1, 2, 3], [1, 2, 3], [1, 2, 3]]");
-}
-
-BOOST_AUTO_TEST_CASE( vector_with_ugly_string_case_stream_test )
-{
-    using namespace boost;
-    std::stringstream str_out;
-
-    std::vector<std::string> vs;
-    vs.push_back("[1, 2, 3], [1, 2, 3], [1, 2, 3]");
-    str_out << vs;
-    BOOST_CHECK_EQUAL(str_out.str(), "[[1, 2, 3], [1, 2, 3], [1, 2, 3]]");
-}
