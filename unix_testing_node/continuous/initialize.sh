aa177c98fa2f6c17c96295ee1ec10ae2304e4141
Troy D. Straszheim <troy@resophonic.com>
2007-05-26 01:03:45 +0000

    on fbsd that's gotta be g++
    SVN-Revision: 4261

--- unix_testing_node/continuous/initialize.sh
+++ unix_testing_node/continuous/initialize.sh
@@ -7,11 +7,11 @@ cd $topdir/debug/1.34.0/src
 svn update
 cd $topdir/debug/1.34.0/build
 rm CMakeCache.txt
-CXX="ccache gcc" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
+CXX="ccache g++" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
 
 cd $topdir/release/1.34.0/src
 svn update
 cd $topdir/release/1.34.0/build
 rm CMakeCache.txt
-CXX="ccache gcc" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
+CXX="ccache g++" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
 
