b2094c375d6ca5962030f3c8549b157b656cff19
Troy D. Straszheim <troy@resophonic.com>
2007-05-26 12:29:28 +0000

    Pipe between gcc subprocesses.  A little speedier.
    SVN-Revision: 4274

---
+++ boost_1_34_0/tools/build/CMake/boost.cmake
@@ -0,0 +1,27 @@
+# 
+#  organization point for includes 
+#
+include(list_contains)
+include(parse_arguments)
+
+if(CMAKE_COMPILER_IS_GNUCC)
+  add_definitions(-pipe)
+endif(CMAKE_COMPILER_IS_GNUCC)
+include(boost-core)
+include(boost-testing)
+
+OPTION(DEBUG "Print lots of gnasty trace output from makefile generation stage" OFF)
+macro(TRACE)
+  if (DEBUG)
+    MESSAGE("~~ " ${ARGN})
+  endif (DEBUG)
+endmacro(TRACE)
+
+macro(ASSERT value)
+  if (NOT ${value})
+    MESSAGE("Assertion failed: ${value} = ${${value}}")
+    MESSAGE(FATAL_ERROR ${ARGN})
+  endif (NOT ${value})
+endmacro(ASSERT value)
+    
+
