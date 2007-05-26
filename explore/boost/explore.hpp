53fe4419f3c88ac24a289dae0ef9fde4089a4980
Jeff Garland <jeff@crystalclearsoftware.com>
2007-05-26 23:17:43 +0000

    put in an overall header for explore library
    SVN-Revision: 4294

---
+++ explore/boost/explore.hpp
@@ -0,0 +1,12 @@
+//
+// explore.hpp -- conviencence header
+//
+// Copyright (C) 2007, Jeff Garland
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+
+#include <explore/explore.hpp>
+#include <explore/stream_container.hpp>
