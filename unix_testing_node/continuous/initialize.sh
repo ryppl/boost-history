bf60de2014684e42ca3a4702b6c3970023e4160b
Troy D. Straszheim <troy@resophonic.com>
2007-05-26 10:24:31 +0000

    print not portable, don't update
    SVN-Revision: 4271

--- unix_testing_node/continuous/initialize.sh
+++ unix_testing_node/continuous/initialize.sh
@@ -4,18 +4,14 @@ topdir=`pwd`
 COMPILER=$1
 if [[ -z "$COMPILER" ]]
 then
-    print "No compiler specified, using g++"
+    echo "No compiler specified, using g++"
     COMPILER=g++
 fi
 
-cd $topdir/debug/1.34.0/src
-svn update
 cd $topdir/debug/1.34.0/build
 rm CMakeCache.txt
 CXX="$COMPILER" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
 
-cd $topdir/release/1.34.0/src
-svn update
 cd $topdir/release/1.34.0/build
 rm CMakeCache.txt
 CXX="$COMPILER" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
