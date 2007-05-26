e8fae0e55c62fbba3ade3af633b344136af0af2c
Troy D. Straszheim <troy@resophonic.com>
2007-05-26 01:30:17 +0000

    specify compiler via script argument
    SVN-Revision: 4264

--- unix_testing_node/continuous/initialize.sh
+++ unix_testing_node/continuous/initialize.sh
@@ -1,17 +1,22 @@
 #!/bin/sh -x
 
 topdir=`pwd`
-
+COMPILER=$1
+if [[ -z "$COMPILER" ]]
+then
+    print "No compiler specified, using g++"
+    COMPILER=g++
+fi
 
 cd $topdir/debug/1.34.0/src
 svn update
 cd $topdir/debug/1.34.0/build
 rm CMakeCache.txt
-CXX="ccache g++" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
+CXX="ccache $COMPILER" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
 
 cd $topdir/release/1.34.0/src
 svn update
 cd $topdir/release/1.34.0/build
 rm CMakeCache.txt
-CXX="ccache g++" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
+CXX="ccache $COMPILER" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
 
