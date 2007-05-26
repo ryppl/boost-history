2f487c2b2b7e1c63e0dec90ab0b740fd749b5e6b
Troy D. Straszheim <troy@resophonic.com>
2007-05-26 02:08:20 +0000

    do ccache stuff outside the build.
    SVN-Revision: 4268

--- unix_testing_node/continuous/initialize.sh
+++ unix_testing_node/continuous/initialize.sh
@@ -12,11 +12,11 @@ cd $topdir/debug/1.34.0/src
 svn update
 cd $topdir/debug/1.34.0/build
 rm CMakeCache.txt
-CXX="ccache $COMPILER" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
+CXX="$COMPILER" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Debug ../src
 
 cd $topdir/release/1.34.0/src
 svn update
 cd $topdir/release/1.34.0/build
 rm CMakeCache.txt
-CXX="ccache $COMPILER" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
+CXX="$COMPILER" cmake -DBUILD_TESTING:BOOL=ON -DCMAKE_BUILD_TYPE:STRING=Release ../src
 
