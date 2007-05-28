3c496d4e9aa50efcb05b8437a4b22b7e05b29be6
Jeffrey Faust <jeff@opticalres.com>
2007-05-28 17:32:17 +0000

    Add is_assoc_iter test
    SVN-Revision: 4330

---
+++ explore/libs/explore/test/is_assoc_iter.cpp
@@ -0,0 +1,34 @@
+// Boost.Print library
+
+// Copyright Jeffrey Faust 2007. Use, modification and
+// distribution is subject to the Boost Software License, Version
+// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+// For more information, see http://www.boost.org
+
+#define BOOST_TEST_MODULE PrintLib
+#include <boost/test/unit_test.hpp>
+#include <sstream>
+#include <vector>
+#include "../../../boost/explore/explore.hpp"
+#include "../../../boost/explore/stream_container.hpp"
+#include "../../../boost/explore/is_assoc_iter.hpp"
+
+BOOST_AUTO_TEST_CASE( is_assoc_iter_test )
+{
+    bool mapHasAssocIter = explore::is_assoc_iter<std::map<int, double> >::type();
+    BOOST_CHECK(mapHasAssocIter);
+
+    bool multimapHasAssocIter = explore::is_assoc_iter<std::multimap<std::string, bool> >::type();
+    BOOST_CHECK(multimapHasAssocIter);
+
+    bool vectorHasAssocIter = explore::is_assoc_iter<std::vector<double> >::type();
+    BOOST_CHECK(!vectorHasAssocIter);
+
+    bool pointerHasAssocIter = explore::is_assoc_iter<int*>::type();
+    BOOST_CHECK(!pointerHasAssocIter);
+
+    bool pairHasAssocIter = explore::is_assoc_iter<std::pair<int, double> >::type();
+    BOOST_CHECK(!pairHasAssocIter);
+}
