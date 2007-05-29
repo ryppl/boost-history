6f664e888d4241752c928b7d562c3a28a3c9593c
Troy D. Straszheim <troy@resophonic.com>
2007-05-29 00:38:10 +0000

    have script clean out continuous build dirs every 5 builds
    SVN-Revision: 4343

--- unix_testing_node/continuous/run.sh
+++ unix_testing_node/continuous/run.sh
@@ -3,12 +3,38 @@
 topdir=`pwd`
 
 # alternately build debug and release
+clean_dirs () {
+    echo "cleaning dirs"
+    for builddir in debug release
+      do
+      dir = $topdir/$builddir/1.34.0/build
+      rm -rf $dir
+      mkdir -p $dir
+      cd $dir
+      cmake ../src
+    done
+}
+
+do_builds () {
+    echo "Doing builds..."
+    cd $topdir/debug/1.34.0/build
+    ctest -D Continuous
+    cd $topdir/release/1.34.0/build
+    ctest -D Continuous
+}
+
+COUNT=0
+
 while true
 do
-  cd $topdir/debug/1.34.0/build
-  ctest -D Continuous
-  cd $topdir/release/1.34.0/build
-  ctest -D Continuous
-  sleep 180  # wait three minutes
+#  sleep 180  # wait three minutes
+  COUNT=`expr $COUNT + 1`
+  echo "COUNT = $COUNT"
+  do_builds
+  if [ $COUNT -eq 5 ]
+      then
+      clean_dirs
+      COUNT=0
+  fi
 done
 
