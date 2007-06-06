7489c96ac76c26f30c641da8459a42b0dae61702
Troy D. Straszheim <troy@resophonic.com>
2007-06-06 10:40:21 +0000

    tweaks for fbsd
    SVN-Revision: 4470

--- unix_testing_node/nightly/nightly.sh
+++ unix_testing_node/nightly/nightly.sh
@@ -1,22 +1,23 @@
 #!/bin/sh -x
 
-if [[ -z "$UPDATE_DONE" ]] 
+topdir=`dirname $0`
+bin=`basename $0`
+cd $topdir
+topdir=`pwd`
+
+if [ -z "$UPDATE_DONE" ] 
 then
-    svn update $0
-    UPDATE_DONE=YES $0 $*
-    exit
+    export UPDATE_DONE=YES
+    svn update $bin
+    exec $0 $*
 fi
 
 COMPILER=$1
-if [[ -z "$COMPILER" ]]
+if [ -z "$COMPILER" ]
 then
     echo "Setting compiler to default of g++"
     COMPILER=g++
 fi
-
-topdir=`dirname $0`
-cd $topdir
-topdir=`pwd`
 
 cd $topdir/debug
 CXX="$COMPILER" ctest -S nightly.cmake -V
