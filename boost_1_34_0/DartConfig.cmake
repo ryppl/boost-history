fc98c01011d6bd97e960bdcb0fe881e937a681a6
Troy D. Straszheim <troy@resophonic.com>
2007-06-12 23:40:51 +0000

    Various cleanups.  Make things play nice with ctest scripts.
    SVN-Revision: 7019

--- boost_1_34_0/DartConfig.cmake
+++ boost_1_34_0/DartConfig.cmake
@@ -1,33 +1,69 @@
-# while developing, nice to start your nightly builds with the very latest 
-# code
-set(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+#
+#  DartConfig.cmake
+#
+#
 
+#
+# Where to send results to, and how
+#
 set(DROP_METHOD "xmlrpc")
-#set(DROP_SITE "http://public.kitware.com:8081")
 set(DROP_SITE "http://dart.resophonic.com:8081")
 set(DROP_LOCATION "boost_1_34_0")
 
-# no test may run longer than 3 minutes?  too loose?
+#
+# No test may run longer than 3 minutes.  Haven't seen any problems with this,
+# since no boost tests hang or run nearly this long.
+#
 set(DART_TESTING_TIMEOUT 180)
 
+#
+# Build Notes
+#
+# Put files in a special directory where they will be shipped to the dart server and 
+# make available with a click or two on the dashboard next to the build name.
+#
 set(NOTES_DIR ${CMAKE_BINARY_DIR}/Testing/Notes)
 file(MAKE_DIRECTORY ${NOTES_DIR})
+if (UNIX)
+  exec_program(uname ARGS -a > ${NOTES_DIR}/uname.txt OUTPUT_VARIABLE DEVNULL)
+  exec_program(${CMAKE_CXX_COMPILER} --version > ${NOTES_DIR}/compiler-version.txt OUTPUT_VARIABLE DEVNULL)
+else(UNIX)
+  # anything in the Windows world that ought to go here?
+endif(UNIX)
 
+#
+# Build Name 
+#
+# On unix we glom together bits of the uname, compiler version, e.g. 'Linux-2.6.1/i686/gcc-4.1.2/Release'
+# Windows version is quite primitive just yet.
+#
+if (UNIX)
+  exec_program(svn ARGS info ${CMAKE_SOURCE_DIR} | grep Revision | cut -c 11- OUTPUT_VARIABLE SVN_REVISION)
+  exec_program(${CMAKE_CXX_COMPILER} ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion OUTPUT_VARIABLE COMPILER_VERSION)
+  exec_program(uname ARGS -s OUTPUT_VARIABLE OSTYPE)
+  exec_program(uname ARGS -r OUTPUT_VARIABLE OSVERSION)
+  exec_program(uname ARGS -p OUTPUT_VARIABLE ARCH)
+  if(${ARCH} STREQUAL "unknown")
+    exec_program(uname ARGS -m OUTPUT_VARIABLE ARCH)
+  endif(${ARCH} STREQUAL "unknown")
+  set(BUILDNAME ${OSTYPE}-${OSVERSION}/${ARCH}/gcc-${COMPILER_VERSION})
+else(UNIX)
+  # improve me on Windows....   or is there nothing to improve.
+  set(BUILDNAME "${BOOST_TOOLSET}")
+endif(UNIX)
 
-# put various stuff into build notes
-exec_program(uname ARGS -a > ${NOTES_DIR}/uname.txt OUTPUT_VARIABLE NOWHERE)
-exec_program(${CMAKE_CXX_COMPILER} --version > ${NOTES_DIR}/compiler-version.txt OUTPUT_VARIABLE NOWHERE)
+#
+#  Append either Release or Debug (or both) to BUILDNAME.  This seems important, one can see
+#  the effect of optimization flags on test passing rates.
+#
+if (BUILD_RELEASE)
+  set (BUILDNAME "${BUILDNAME}/Release")
+endif(BUILD_RELEASE)
+if (BUILD_DEBUG)
+  set (BUILDNAME "${BUILDNAME}/Debug")
+endif(BUILD_DEBUG)
 
-exec_program(svn ARGS info ${CMAKE_SOURCE_DIR} | grep Revision | cut -c 11- OUTPUT_VARIABLE SVN_REVISION)
-
-exec_program(${CMAKE_CXX_COMPILER} ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion OUTPUT_VARIABLE COMPILER_VERSION)
-exec_program(uname ARGS -s OUTPUT_VARIABLE OSTYPE)
-exec_program(uname ARGS -r OUTPUT_VARIABLE OSVERSION)
-exec_program(uname ARGS -p OUTPUT_VARIABLE ARCH)
-if(${ARCH} STREQUAL "unknown")
-  exec_program(uname ARGS -m OUTPUT_VARIABLE ARCH)
-endif(${ARCH} STREQUAL "unknown")
-
-
-set(BUILDNAME ${OSTYPE}-${OSVERSION}/${ARCH}/gcc-${COMPILER_VERSION}/${CMAKE_BUILD_TYPE})
-message(STATUS "Testing is ON.  Build name = '${BUILDNAME}'")
+#
+#  Happy status message
+#
+message(STATUS "Testing is ON.  Dart build name = '${BUILDNAME}'")
