026e3bff2cc0c06195d7698506074424bb0fb1a0
Troy D. Straszheim <troy@resophonic.com>
2007-06-13 10:48:59 +0000

    Interim commit: removing externals from unix_testing_node.  Things broken atm.
    SVN-Revision: 7024

--- unix_testing_node/nightly.sh
+++ unix_testing_node/nightly.sh
@@ -1,23 +1,22 @@
 #!/bin/sh -x
 
-topdir=`dirname $0`
-bin=`basename $0`
-cd $topdir
-topdir=`pwd`
-
-if [ -z "$UPDATE_DONE" ] 
+if [[ -z "$UPDATE_DONE" ]] 
 then
-    export UPDATE_DONE=YES
-    svn update $bin
-    exec $0 $*
+    svn update $0
+    UPDATE_DONE=YES $0 $*
+    exit
 fi
 
 COMPILER=$1
-if [ -z "$COMPILER" ]
+if [[ -z "$COMPILER" ]]
 then
     echo "Setting compiler to default of g++"
     COMPILER=g++
 fi
+
+topdir=`dirname $0`
+cd $topdir
+topdir=`pwd`
 
 cd $topdir/debug
 CXX="$COMPILER" ctest -S nightly.cmake -V
