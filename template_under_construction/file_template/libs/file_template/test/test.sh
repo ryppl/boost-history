58948540d923ec76d28cf3a476dc0891b18a302f
Stjepan Rajko <stipe@asu.edu>
2007-06-15 05:12:07 +0000

    add sandbox template ignore and fignore tests
    SVN-Revision: 7056

--- template_under_construction/file_template/libs/file_template/test/test.sh
+++ template_under_construction/file_template/libs/file_template/test/test.sh
@@ -13,7 +13,7 @@ run_test ()
     echo $input \| make_template.py $* | cat >>test.results
     echo ---------------- | cat >>test.results
     echo $input | python2.3 ../../../../make_template.py destination=$result "$@"
-    diff -r $expected $result | cat >>test.results
+    diff -b -r $expected $result | cat >>test.results
 }
 
 rm test.results
@@ -21,6 +21,8 @@ rm -r results
 
 run_test simple_type_in simple_template substituted simple_template 
 run_test simple_command_line simple_template "" simple_template text=substituted
+run_test simple_ignore simple_template_ignored "" simple_template text=substituted ignore=\$template_text\$
+run_test simple_fignore simple_template_fignored "" simple_template text=substituted fignore=.txt
 run_test sandbox_command_line sandbox_test "" ../../../../sandbox "author=John Doe,Jane H. Dough" library=test
 
 echo
