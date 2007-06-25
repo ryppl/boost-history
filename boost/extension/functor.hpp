a35622d8980d3e4a9fdb43adfac37baa4951b04f
Mariano Gabriel Consoni <mariano.consoni@gmail.com>
2007-06-25 23:44:09 +0000

    - Added two or three lines to each file describing its contents (according Boost Library Requirements).
    SVN-Revision: 7151

--- boost/extension/functor.hpp
+++ boost/extension/functor.hpp
@@ -1,8 +1,13 @@
-/* (C) Copyright Jeremy Pack 2007
-* Distributed under the Boost Software License, Version 1.0. (See
-* accompanying file LICENSE_1_0.txt or copy at
-* http://www.boost.org/LICENSE_1_0.txt)
-*/
+/*
+ * Boost.Extension / functor:
+ * 	functor used as the exported function of the libraries (the one that 
+ *	registers the implementations at library loading time)
+ *
+ * (C) Copyright Jeremy Pack 2007
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ */
 
 #ifndef BOOST_EXTENSION_FUNCTOR_HPP
 #define BOOST_EXTENSION_FUNCTOR_HPP
--- boost/extension/impl/library_impl.hpp
+++ boost/extension/impl/library_impl.hpp
@@ -1,4 +1,8 @@
-/* (C) Copyright Jeremy Pack 2007
+/*
+ * Boost.Extension / libraries management:
+ * 	low-level platform specific dynamic library management
+ *
+ * (C) Copyright Jeremy Pack 2007
  * Distributed under the Boost Software License, Version 1.0. (See
  * accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
