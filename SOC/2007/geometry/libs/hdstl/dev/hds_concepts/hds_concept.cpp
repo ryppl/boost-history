90a493059053b919cd21fd079e423d1e08fc8797
Hervé Brönnimann <hervebronnimann@mac.com>
2007-06-26 12:04:28 +0000

    Redo the .cpp
    SVN-Revision: 7164

--- SOC/2007/geometry/libs/hdstl/dev/hds_concepts/hds_concept.cpp
+++ SOC/2007/geometry/libs/hdstl/dev/hds_concepts/hds_concept.cpp
@@ -1,8 +1,4 @@
 // hds_concept.cpp    -*- C++ -*-
 
-//#include <boost/hdstl/concepts/hds_concept.hpp>
-#include "hds_concept.hpp"
-
-int main()
-{}
+#include <boost/hdstl/concepts/hds_concept.hpp>
 
