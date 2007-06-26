3969cc57a38779d180cac19d29f4c042e6f2f85e
Mariano Gabriel Consoni <mariano.consoni@gmail.com>
2007-06-26 23:18:31 +0000

    - style for each of the files (license, "latest version" notice, 80 columns, identation and file summary). - fixed some warnings in the files because now it compiles on Linux (some modifictions on Jamfile where done).
    SVN-Revision: 7184

--- libs/extension/test/versions_test.cpp
+++ libs/extension/test/versions_test.cpp
@@ -1,7 +1,12 @@
-/* (C) Copyright Mariano G. Consoni 2007
- * Distributed under the Boost Software License, Version 1.0. (See
+/*
+ * Boost.Extension / implementations versions test
+ *
+ * (C) Copyright Mariano G. Consoni 2007
+ * Distributed under the Boost Software License, Version 1.0. (See             
  * accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * See http://www.boost.org/ for latest version.
  */
 
 #include <boost/extension/factory_map.hpp>
@@ -21,72 +26,81 @@
 
 BOOST_AUTO_TEST_CASE(versions_test)
 {
-	using namespace boost::extensions;
+  using namespace boost::extensions;
 
-	//  Create the factory_map object - it will hold all of the available
-	//  constructors. Multiple factory_maps can be constructed.
-	factory_map fm;
+  //  Create the factory_map object - it will hold all of the available
+  //  constructors. Multiple factory_maps can be constructed.
+  factory_map fm;
 
-	// load hello world first version
-	load_single_library(fm, "libHelloWorldLib.extension", "extension_export_word");
+  // load hello world first version
+  load_single_library(fm, "libHelloWorldLib.extension", 
+		      "extension_export_word");
 
-	// load hello world second version
-	load_single_library(fm, "libHelloWorldLibv2.extension", "extension_export_word");
+  // load hello world second version
+  load_single_library(fm, "libHelloWorldLibv2.extension", 
+		      "extension_export_word");
 
-	// load hello world second version again
-	load_single_library(fm, "libHelloWorldLibv2.extension", "extension_export_word");
+  // load hello world second version again
+  load_single_library(fm, "libHelloWorldLibv2.extension", 
+		      "extension_export_word");
 
-	// load salute library (with hello included)
-	load_single_library(fm, "libSaluteLib.extension", "extension_export_salute");
+  // load salute library (with hello included)
+  load_single_library(fm, "libSaluteLib.extension", "extension_export_salute");
 
-	//  Get a reference to the list of constructors for words.
-	std::list<factory<word, int> > & factory_list = fm.get<word, int>();  
+  //  Get a reference to the list of constructors for words.
+  std::list<factory<word, int> > & factory_list = fm.get<word, int>();  
 
-	// the point here is to check if six classes were loaded (two for each one of the first three libraries)
-	BOOST_CHECK_EQUAL( factory_list.size(), 6 );
+  // the point here is to check if six classes were loaded 
+  // (two for each one of the first three libraries)
+  BOOST_CHECK_EQUAL( factory_list.size(), 6U );
 
-	// these are the expected strings
-	std::vector<std::string> words;
-	words.push_back("hello");
-	words.push_back("world!");
-	words.push_back("| v2 hello");
-	words.push_back("world! v2");
-	words.push_back("| v2 hello");
-	words.push_back("world! v2");
+  // these are the expected strings
+  std::vector<std::string> words;
+  words.push_back("hello");
+  words.push_back("world!");
+  words.push_back("| v2 hello");
+  words.push_back("world! v2");
+  words.push_back("| v2 hello");
+  words.push_back("world! v2");
 
-	std::vector<std::string>::const_iterator expected_word = words.begin();
-	for (std::list<factory<word, int> >::iterator current_word = factory_list.begin();
-	     current_word != factory_list.end(); ++current_word)
-	{
-		/// check that the pointer is OK and the word is the one that we're expecting
-		std::auto_ptr<word> word_ptr(current_word->create());
-		BOOST_CHECK_EQUAL( !word_ptr.get(), 0 );
-		BOOST_CHECK_EQUAL( word_ptr->get_val(), *expected_word);
-		++expected_word;
-	}
+  std::vector<std::string>::const_iterator expected_word = words.begin();
+  for (std::list<factory<word, int> >::iterator current_word = 
+	 factory_list.begin(); current_word != factory_list.end(); 
+       ++current_word)
+    {
+      /// check that the pointer is OK and the word is the one that 
+      /// we're expecting
+      std::auto_ptr<word> word_ptr(current_word->create());
+      BOOST_CHECK_EQUAL( !word_ptr.get(), 0 );
+      BOOST_CHECK_EQUAL( word_ptr->get_val(), *expected_word);
+      ++expected_word;
+    }
 
-	//  Get a reference to the list of constructors for salutes.
-	std::list<factory<salute, int> > & salute_factory_list = fm.get<salute, int>();  
+  //  Get a reference to the list of constructors for salutes.
+  std::list<factory<salute, int> > & salute_factory_list = fm.get<salute, 
+    int>();  
 
-	// the point here is to check if only two classes were loaded
-	BOOST_CHECK_EQUAL( salute_factory_list.size(), 2 );
+  // the point here is to check if only two classes were loaded
+  BOOST_CHECK_EQUAL( salute_factory_list.size(), 2U );
 
-	// these are the expected strings
-	std::vector<std::string> salutes;
-	salutes.push_back("hello");
-	salutes.push_back("bye!");
+  // these are the expected strings
+  std::vector<std::string> salutes;
+  salutes.push_back("hello");
+  salutes.push_back("bye!");
 
-	std::vector<std::string>::const_iterator expected_salute = salutes.begin();
+  std::vector<std::string>::const_iterator expected_salute = salutes.begin();
 
-	for (std::list<factory<salute, int> >::iterator current_salute = salute_factory_list.begin();
-	     current_salute != salute_factory_list.end(); ++current_salute)
-	{
-		/// check that the pointer is OK and the salute is the one that we're expecting
-		std::auto_ptr<salute> salute_ptr(current_salute->create());
-		BOOST_CHECK_EQUAL( !salute_ptr.get(), 0 );
-		BOOST_CHECK_EQUAL( salute_ptr->say(), *expected_salute);
-		++expected_salute;
-	}
+  for (std::list<factory<salute, int> >::iterator current_salute = 
+	 salute_factory_list.begin();
+       current_salute != salute_factory_list.end(); ++current_salute)
+    {
+      /// check that the pointer is OK and the salute is the one that 
+      /// we're expecting
+      std::auto_ptr<salute> salute_ptr(current_salute->create());
+      BOOST_CHECK_EQUAL( !salute_ptr.get(), 0 );
+      BOOST_CHECK_EQUAL( salute_ptr->say(), *expected_salute);
+      ++expected_salute;
+    }
 
-	// all ok
+  // all ok
 }
