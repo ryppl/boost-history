2bb4df54f21e02465117616e30c338e5bfe1d56b
Mariano Gabriel Consoni <mariano.consoni@gmail.com>
2007-06-27 00:37:51 +0000

    - more style for the examples (file desc, 80 columns, indent, "latest version" notice, license + copyright)
    SVN-Revision: 7187

--- libs/extension/examples/multiple_inheritance/vehicle.hpp
+++ libs/extension/examples/multiple_inheritance/vehicle.hpp
@@ -1,3 +1,14 @@
+/*
+ * Boost.Extension / multiple inheritance example (vehicle)
+ *
+ * (C) Copyright Jeremy Pack 2007
+ * Distributed under the Boost Software License, Version 1.0. (See             
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * See http://www.boost.org/ for latest version.
+ */
+
 #ifndef BOOST_EXTENSION_VEHICLE_HPP
 #define BOOST_EXTENSION_VEHICLE_HPP
 #include <boost/extension/extension.hpp>
