e03a486d71dea06ce00563cd0dc3d186bcdb0cc3
John Moeller <fishcorn@gmail.com>
2007-06-30 01:25:08 +0000

    Got project-root.jam working for module build Added requirements to build.jam Had to disable auto-linking for now; module build doesn't annotate lib names
    SVN-Revision: 7318

--- langbinding/boost/langbinding/aux_/config.hpp
+++ langbinding/boost/langbinding/aux_/config.hpp
@@ -32,27 +32,27 @@
 //
 // Automatically link to the correct build variant where possible. 
 // 
-#if !defined(BOOST_ALL_NO_LIB) && !defined(BOOST_LANGBINDING_NO_LIB) && !defined(BOOST_WHATEVER_SOURCE)
+//#if !defined(BOOST_ALL_NO_LIB) && !defined(BOOST_LANGBINDING_NO_LIB) && !defined(BOOST_WHATEVER_SOURCE)
 //
 // Tell the autolink to link dynamically, this will get undef'ed by auto_link.hpp
 // once it's done with it: 
 //
-#if defined(BOOST_LANGBINDING_DYNAMIC_LIB)
-#   define BOOST_DYN_LINK
-#endif
+//#if defined(BOOST_LANGBINDING_DYNAMIC_LIB)
+//#   define BOOST_DYN_LINK
+//#endif
 //
 // Set the name of our library, this will get undef'ed by auto_link.hpp
 // once it's done with it:
 //
-#if defined(BOOST_LANGBINDING_LIB_NAME)
-#   define BOOST_LIB_NAME BOOST_LANGBINDING_LIB_NAME
-#else
-#   define BOOST_LIB_NAME boost_langbinding
-#endif
+//#if defined(BOOST_LANGBINDING_LIB_NAME)
+//#   define BOOST_LIB_NAME BOOST_LANGBINDING_LIB_NAME
+//#else
+//#   define BOOST_LIB_NAME boost_langbinding
+//#endif
 //
 // And include the header that does the work:
 //
-#include <boost/config/auto_link.hpp>
-#endif  // auto-linking disabled
+//#include <boost/config/auto_link.hpp>
+//#endif  // auto-linking disabled
 
 #endif // CONFIG_DWA2004918_HPP
