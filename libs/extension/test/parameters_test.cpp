7bd1770ae659cd7c1ec7291d69a868d22196e86c
Mariano Gabriel Consoni <mariano.consoni@gmail.com>
2007-07-02 17:00:15 +0000

    - changed the path to the lots_of_parameters_interface header file, now it's lots_of_parameters_iface.hpp (it exceeded 31 chars).
    SVN-Revision: 7337

--- libs/extension/test/parameters_test.cpp
+++ libs/extension/test/parameters_test.cpp
@@ -16,7 +16,7 @@
 #define BOOST_TEST_DYN_LINK 1
 #include <boost/test/unit_test.hpp>
 
-#include "../examples/parameters/lots_of_parameters_interface.hpp"
+#include "../examples/parameters/lots_of_parameters_iface.hpp"
 
 
 using namespace boost::extensions;
