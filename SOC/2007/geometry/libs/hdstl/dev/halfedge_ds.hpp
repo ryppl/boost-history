9c17924d0a34cdfeca3fd69cdec6703d590b98a5
Hervé Brönnimann <hervebronnimann@mac.com>
2007-07-09 04:17:00 +0000

    Added a skeleton.
    SVN-Revision: 7394

---
+++ SOC/2007/geometry/libs/hdstl/dev/halfedge_ds.hpp
@@ -0,0 +1,24 @@
+//halfedge_ds.hpp   -*- C++ -*-
+//
+//@PURPOSE: Provide a concrete bu parameterizable implementation of HDS.
+//
+//@CLASSES:
+//  'halfedge_ds':  swiss-army knife of HDS implementations
+//  
+//@SEE_ALSO: {hds_concepts.hpp}
+//
+//@DESCRIPTION: This file provide a single class template, 'halfedge_ds', which
+// accepts many template parameters that can be used to govern its
+// implementation.  Most of these parameters can be chosen as selectors (see
+// 'hds_selectors' component).
+
+#ifndef BOOST_HDSTL_HALFEDGE_DS_HPP
+#define BOOST_HDSTL_HALFEDGE_DS_HPP 1
+
+namespace boost{
+namespace hdstl {
+
+} // namespace hdstl
+} // namespace boost
+
+#endif
---
+++ SOC/2007/geometry/libs/hdstl/dev/hds_selectors.hpp
@@ -0,0 +1,24 @@
+//hds_selectors.hpp   -*- C++ -*-
+//
+//@PURPOSE: Provide selectors
+//
+//@CLASSES:
+//  
+//@SEE_ALSO: 
+//
+//@DESCRIPTION: This file provides many "tag" classes (i.e., classes which have
+// empty definitions, and are used solely for their distinct type in order to
+// compile-time dispatch some implementation using overloading or template
+// specialization) that can act as selectors for the 'halfedge_ds' class
+// template.
+
+#ifndef BOOST_HDSTL_HDS_SELECTORS_HPP
+#define BOOST_HDSTL_HDS_SELECTORS_HPP 1
+
+namespace boost{
+namespace hdstl {
+
+} // namespace hdstl
+} // namespace boost
+
+#endif
