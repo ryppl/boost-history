d4d2c0a5f10e488e33099dc4e5a283319e7e7b1e
Stjepan Rajko <stipe@asu.edu>
2007-07-29 22:57:36 +0000

    copy signal_network under dataflow
    SVN-Revision: 7582

---
+++ SOC/2007/signals/boost/dataflow/signal/connection.hpp
@@ -0,0 +1,15 @@
+// Copyright Stjepan Rajko 2007. Use, modification and
+// distribution is subject to the Boost Software License, Version
+// 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef SIGNAL_NETWORK_CONNECTION_HPP
+#define SIGNAL_NETWORK_CONNECTION_HPP
+
+#include <boost/dataflow/signal/connection/connect.hpp>
+#include <boost/dataflow/signal/connection/operators.hpp>
+#include <boost/dataflow/signal/connection/signal.hpp>
+#include <boost/dataflow/signal/connection/slot_selector.hpp>
+#include <boost/dataflow/signal/connection/slot_selector_map.hpp>
+
+#endif
