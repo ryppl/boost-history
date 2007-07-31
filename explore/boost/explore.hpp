2dc34d23aadb2adb0895d16b78f002b563d07916
Jeffrey Faust <jeff@opticalres.com>
2007-07-31 03:41:47 +0000

    include starting at boost directory
    SVN-Revision: 7602

---
+++ explore/boost/explore.hpp
@@ -0,0 +1,13 @@
+//
+// explore.hpp -- conviencence header
+//
+// Copyright (C) 2007, Jeff Garland
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+
+#include <boost/explore/explore.hpp>
+#include <boost/explore/stream_container.hpp>
+#include <boost/explore/iterator_range.hpp>
