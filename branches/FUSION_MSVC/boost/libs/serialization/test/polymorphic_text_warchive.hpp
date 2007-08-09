2af40984a5adfff8f8081f573baf829c8517c7e1
Daniel Wallin <daniel@boostpro.com>
2007-08-09 09:13:09 +0000

    Removed old branches.
    SVN-Revision: 38550

--- branches/FUSION_MSVC/boost/libs/serialization/test/polymorphic_text_warchive.hpp
+++
@@ -1,7 +0,0 @@
-#include <boost/archive/polymorphic_text_woarchive.hpp>
-typedef boost::archive::polymorphic_text_woarchive test_oarchive;
-typedef std::wofstream test_ostream;
-#include <boost/archive/polymorphic_text_wiarchive.hpp>
-typedef boost::archive::polymorphic_text_wiarchive test_iarchive;
-typedef std::wifstream test_istream;
-#define TEST_STREAM_FLAGS (std::ios_base::openmode)0
--- branches/FUSION_MSVC/boost/libs/serialization/test/polymorphic_xml_warchive.hpp
+++
@@ -1,7 +0,0 @@
-#include <boost/archive/polymorphic_xml_woarchive.hpp>
-typedef boost::archive::polymorphic_xml_woarchive test_oarchive;
-typedef std::wofstream test_ostream;
-#include <boost/archive/polymorphic_xml_wiarchive.hpp>
-typedef boost::archive::polymorphic_xml_wiarchive test_iarchive;
-typedef std::wifstream test_istream;
-#define TEST_STREAM_FLAGS (std::ios_base::openmode)0
