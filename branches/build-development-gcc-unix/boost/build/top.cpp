627375587fdfa1168adfac877423c00b12bd7d6b
Daniel Wallin <daniel@boostpro.com>
2007-08-09 09:13:09 +0000

    Removed old branches.
    SVN-Revision: 38550

--- branches/build-development-gcc-unix/boost/build/top.cpp
+++
@@ -1,7 +0,0 @@
-#include "top.hpp"
-#include <iostream>
-
-TOP_EXPIMP void top()
-{
-    std::cout << "hello from top" << std::endl ;
-}
--- branches/build-development-gcc-unix/boost/build/x/xl.cpp
+++
@@ -1,7 +0,0 @@
-#include "xl.hpp"
-#include <iostream>
-
-TOP_EXPIMP void xl()
-{
-    std::cout << "hello from xl" << std::endl ;
-}
