f3779ca79e08ae994e3affa626faed538e52341c
Mariano Gabriel Consoni <mariano.consoni@gmail.com>
2007-08-10 19:47:00 +0000

    - Made it work. We have our first unit test.
    SVN-Revision: 38586

--- libs/reflection/test/basic_test.cpp
+++ libs/reflection/test/basic_test.cpp
@@ -30,8 +30,7 @@ BOOST_AUTO_TEST_CASE(basic_example)
      template parameter declares that we will describe this reflection
      using a string. Any arbitrary type could be used here. */
   boost::extension::reflection<car, std::string> car_reflection("A Car!");
-  std::string &s = car_reflection.get_info();
-  BOOST_CHECK_EQUAL( i, std::string("A Car!") );
+  BOOST_CHECK_EQUAL( car_reflection.get_info(), "A Car!" );
 
   /* Here we add two methods to the reflection. The library must
      correctly parse and remember the parameters and return type of these
@@ -48,15 +47,21 @@ BOOST_AUTO_TEST_CASE(basic_example)
   car ferrari_f40("Ferrari F40");
 
   // call methods
-  car_reflection.call<int, bool>(&porsche_911, 3);
-  car_reflection.call<std::string, bool, 
-    float, std::string>(&porsche_911, "turn", .5f);
+  bool porsche_start = car_reflection.call<int, bool>(&porsche_911, 3);
+  BOOST_CHECK_EQUAL( porsche_start, true );
 
-  car_reflection.call<std::string, bool, 
+  bool porsche_turn = car_reflection.call<std::string, bool, 
+    float, std::string>(&porsche_911, "turn", .5f);
+  BOOST_CHECK_EQUAL( porsche_turn, true );
+
+  bool ferrari_turn = car_reflection.call<std::string, bool, 
     float, std::string>(&ferrari_f40, "turn", .10f);
+  BOOST_CHECK_EQUAL( ferrari_turn, false );
 
   // prepare the parameter map for a call
   boost::extension::parameter_map pm;
   pm.add<float, std::string>(.15f, "turn_angle");
   pm.add<int, std::string>(50, "aceleration");
+
+  // TODO: call and test result
 }
