1e5e8e5a6ee42f158f1f811fcf9d29d5f20e4a6c
Mariano Gabriel Consoni <mariano.consoni@gmail.com>
2007-08-13 21:38:44 +0000

    - Made shorter and more concrete the extension integration example. Now it's   just the creation of the reflection, loading the library and combining them.
    - Added the second tutorial, describing the Boost.Extension example.
    
    SVN-Revision: 38633

--- libs/reflection/examples/extension/car_lib.cpp
+++ libs/reflection/examples/extension/car_lib.cpp
@@ -2,7 +2,7 @@
  * Boost.Extension / hello world implementations
  *
  * (C) Copyright Jeremy Pack 2007
- * Distributed under the Boost Software License, Version 1.0. (See             
+ * Distributed under the Boost Software License, Version 1.0. (See
  * accompanying file LICENSE_1_0.txt or copy at
  * http://www.boost.org/LICENSE_1_0.txt)
  *
--- libs/reflection/examples/extension/extension.cpp
+++ libs/reflection/examples/extension/extension.cpp
@@ -26,50 +26,30 @@
 
 int main(void)
 {
-
-  /* Here we declare that we are reflecting out a Car. The second
-     template parameter declares that we will describe this reflection
-     using a string. Any arbitrary type could be used here. */
+  // Let's create the reflection and add the methods
   boost::extension::reflection<car, std::string> car_reflection("A Car!");
-
-  /* Here we add two methods to the reflection. The library must
-     correctly parse and remember the parameters and return type of these
-     methods. Here, we elect to describe the first method using a number
-     and the second using a string. For the second method, the library
-     knows now that it is a function of Car called turn that takes a float
-     named "turn_angle". */ 
   car_reflection.add<int, bool>(&car::start, 3);
   car_reflection.add<std::string, bool, float, 
     std::string>(&car::turn, "turn", "turn_angle");
 
-  car porsche_911("Porsche 911");
-  car ferrari_f40("Ferrari F40");
-
-  car_reflection.call<int, bool>(&porsche_911, 3);
-  car_reflection.call<std::string, bool, 
-    float, std::string>(&porsche_911, "turn", .5f);
-
-  car_reflection.call<std::string, bool, 
-    float, std::string>(&ferrari_f40, "turn", .10f);
-
-  // prepare the parameter map for a call
-  boost::extension::parameter_map pm;
-  pm.add<float, std::string>(.15f, "turn_angle");
-  pm.add<int, std::string>(50, "aceleration");
-
-
   // extension tests
   using namespace boost::extensions;
   factory_map fm;
+
+  // load the car library
   load_single_library(fm, "libcar_lib.extension", 
 		      "extension_export_car");
   std::list<factory<car, std::string, std::string> > & factory_list = 
 	  fm.get<car, std::string, std::string>();  
+
+  // check if the factories loaded fine
   if(factory_list.size() < 2) {
     std::cout << "Error - the classes were not found (" 
 	      << factory_list.size() << ").\n";
     return -1;
   }
+
+  // create some instances and call the method "3" (start)
   for (std::list<factory<car, std::string, std::string> >
 	 ::iterator current_car = factory_list.begin(); 
        current_car != factory_list.end(); 
