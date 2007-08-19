e1ae0509cd9341e47d56a593d6ab114ba0b37652
Jacob Voytko <JakeVoytko@gmail.com>
2007-08-19 21:06:26 +0000

    Added documentation dealing with stylesheets
    SVN-Revision: 38770

---
+++ SOC/2007/visualization/libs/svg_plot/example/1d_simple_style.cpp
@@ -0,0 +1,37 @@
+// 1d_simple_example.cpp
+// Copyright (C) Jacob Voytko 2007
+// Distributed under the Boost Software License, Version 1.0.
+// For more information, see http://www.boost.org
+// -----------------------------------------------------------------
+
+#include <boost/svg_plot/svg_1d_plot.hpp>
+#include <vector>
+
+using std::vector;
+using namespace boost::svg;
+
+int main()
+{
+	vector<double> dan_times;
+	vector<double> elaine_times;
+
+	dan_times.push_back(3.1);
+	dan_times.push_back(4.2);
+	elaine_times.push_back(2.1);
+	elaine_times.push_back(7.8);
+
+	svg_1d_plot my_plot;
+
+	my_plot.background_border_color(black)
+           .legend_on(true)
+	       .title("Race Times")
+	       .x_range(-1, 11);
+
+        my_plot.plot(dan_times, "Dan", blue);
+	my_plot.plot(elaine_times, "Elaine", orange);
+
+        my_plot.load_stylesheet("./style.css");
+
+        my_plot.write("./1d_simple_style.svg");
+	return 0;
+}
---
+++ SOC/2007/visualization/libs/svg_plot/example/boxplot_simple.cpp
@@ -0,0 +1,59 @@
+// boxplot_simple.cpp
+// Copyright (C) Jacob Voytko 2007
+// Distributed under the Boost Software License, Version 1.0.
+// For more information, see http://www.boost.org
+// -----------------------------------------------------------------
+
+#include <vector>
+#include <cmath>
+
+#include <boost/svg_plot/svg_boxplot.hpp>
+
+using std::vector;
+
+// Functions we are using to simulate distributions.
+double f(double x)
+{
+    return 50 / x;
+}
+
+double g(double x)
+{
+    return 40 + 25 * sin(x * 50);
+}
+
+int main()
+{
+    using namespace boost::svg;
+    std::vector<double> data1, data2;
+
+    // Fill our vectors;
+    for(double i = .1; i < 10; i+=.1)
+    {
+        data1.push_back(f(i));
+        data2.push_back(g(i));
+    }
+
+    // Initialize a new plot.
+    svg_boxplot my_plot;
+
+    // Color information.
+    my_plot.background_border_color(black);
+
+    // String information.
+    my_plot.title("Boxplots of Common Functions")
+           .x_label("Functions")
+           .y_label("Population Size");
+
+    // Axis information.
+    my_plot.y_range(0, 100)
+           .y_minor_tick_length(20)
+           .y_major_interval(20);
+
+    // Write data.
+    my_plot.plot(data1, "[50 / x]");
+    my_plot.plot(data2, "[40 + 25 * sin(x * 50)]");
+
+    my_plot.write("boxplot_simple.svg");
+    return 0;
+}
