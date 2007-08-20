bcf829386fd57c60c30702af6ede4a39d5187df1
Bernhard Reiter (bounce) <ockham@gmx.net>
2007-08-20 18:38:01 +0000

    Add ascending iterator
    SVN-Revision: 38802

---
+++ SOC/2006/tree/trunk/boost/tree/iterator.hpp
@@ -0,0 +1,24 @@
+//  Copyright (c) 2006, Bernhard Reiter
+//
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+/**
+ * @file iterators.hpp
+ * Iterator wrappers around cursors using the provided
+ * traversal methods for:
+ * In-, pre- and postorder; ascending traversal.
+ */
+
+
+#ifndef BOOST_TREE_ITERATORS_HPP
+#define BOOST_TREE_ITERATORS_HPP
+
+#include <boost/tree/ascending_iterator.hpp>
+
+#include <boost/tree/inorder_iterator.hpp>
+#include <boost/tree/preorder_iterator.hpp>
+#include <boost/tree/postorder_iterator.hpp>
+
+#endif // BOOST_TREE_ITERATORS_HPP
