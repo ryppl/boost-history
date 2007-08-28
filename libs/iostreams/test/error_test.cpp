6dff0888173ceeb5895ddfc3cc3caeaf4885b1a5
Frank Birbacher <bloodymir.crap@gmx.net>
2007-08-28 16:42:08 +0000

    added copyright notice to new source file
    SVN-Revision: 39042

--- libs/iostreams/test/error_test.cpp
+++ libs/iostreams/test/error_test.cpp
@@ -1,3 +1,9 @@
+// (C) Copyright Frank Birbacher 2007
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt.)
+
+// See http://www.boost.org/libs/iostreams for documentation.
+
 #include <boost/iostreams/categories.hpp>  // tags.
 #include <boost/iostreams/detail/ios.hpp>  // openmode, seekdir, int types.
 #include <boost/iostreams/detail/error.hpp>
