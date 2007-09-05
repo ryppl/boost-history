961522b1e4a101abe4edefff8c92fb6851c06839
Jeremy <sixtrillion@users.sf.net>
2007-09-05 03:56:48 +0000

    This includes the initial changes to the Boost.Extension interfaces.
    They all require Boost.Preprocessor now, though a non-Preprocessor version may be created in the future.
    
    Most of the unit tests have been disabled temporarily, as I work to change their usage of the API.
    
    The shared_library API will still be changed.
    
    SVN-Revision: 39124

---
+++ boost/extension/common.hpp
@@ -0,0 +1,25 @@
+/*
+ * Boost.Extension / common:
+ *         common include files
+ *
+ * (C) Copyright Jeremy Pack 2007
+ * Distributed under the Boost Software License, Version 1.0. (See
+ * accompanying file LICENSE_1_0.txt or copy at
+ * http://www.boost.org/LICENSE_1_0.txt)
+ *
+ * See http://www.boost.org/ for latest version.
+ */
+
+#ifndef BOOST_EXTENSION_COMMON_HPP
+#define BOOST_EXTENSION_COMMON_HPP
+
+#include <boost/preprocessor/arithmetic/inc.hpp>
+#include <boost/preprocessor/if.hpp>
+#include <boost/preprocessor/punctuation/comma_if.hpp>
+#include <boost/preprocessor/repetition.hpp>
+
+#ifndef BOOST_EXTENSION_MAX_FUNCTOR_PARAMS
+#define BOOST_EXTENSION_MAX_FUNCTOR_PARAMS 6
+#endif // BOOST_EXTENSION_MAX_FUNCTOR_PARAMS
+
+#endif // BOOST_EXTENSION_COMMON_HPP
