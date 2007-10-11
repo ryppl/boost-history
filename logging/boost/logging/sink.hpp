40a6f0ca41bc6cf8e1d99ec9c9065bbf8a5d459a
John Torjo <john.groups@torjo.com>
2007-10-11 16:50:19 +0000

    [logging] v0.5.2, 11 oct 2007 - changed license - added to boost
    SVN-Revision: 39931

--- logging/boost/logging/sink.hpp
+++ logging/boost/logging/sink.hpp
@@ -6,17 +6,13 @@
 //
 // Copyright (C) 2007 John Torjo (see www.torjo.com for email)
 //
-// Permission to copy, use, sell and distribute this software is granted
-// provided this copyright notice appears in all copies.
-// Permission to modify the code and to distribute modified code is granted
-// provided this copyright notice appears in all copies, and a notice
-// that the code was modified is included with the copyright notice.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 //
-// This software is provided "as is" without express or implied warranty,
-// and with no claim as to its suitability for any purpose.
- 
 // See http://www.boost.org for updates, documentation, and revision history.
 // See http://www.torjo.com/log2/ for more details
+
 
 #ifndef JT28092007_sink_HPP_DEFINED
 #define JT28092007_sink_HPP_DEFINED
