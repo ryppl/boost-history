680cc7290c42aacd9d1909292a9baca9a6f25e4e
John Torjo <john.groups@torjo.com>
2007-10-30 15:28:27 +0000

    v0.9.6, 30 oct 2007 - removed garbage from stdafx.h files - so that it can be compiled under Linux
    SVN-Revision: 40607

--- logging/lib/logging/samples/basic_usage/stdafx.h
+++ logging/lib/logging/samples/basic_usage/stdafx.h
@@ -3,14 +3,7 @@
 // are changed infrequently
 //
 
-#pragma once
-
-#ifndef _WIN32_WINNT		// Allow use of features specific to Windows XP or later.                   
-#define _WIN32_WINNT 0x0501	// Change this to the appropriate value to target other versions of Windows.
-#endif						
-
 #include <stdio.h>
-#include <tchar.h>
 
 
 
