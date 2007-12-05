256079a534925b1970b188e49881513b0ebbcf7a
Rene Rivera <grafikrobot@gmail.com>
2007-12-05 21:12:23 +0000

    Move the rest of the server only code to the various update scripts.
    SVN-Revision: 41760

--- public_html/beta/common/code/update-6x6.sh
+++ public_html/beta/common/code/update-6x6.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-/usr/bin/python24 /home/grafik/www.boost.org/testing/webcheck.py \
- "--yank=http://beta.boost.org/doc/libs" \
- "--yank=http://beta.boost.org/development/tests" \
- "--yank=http://beta.boost.org/development/webcheck" \
- "--base-only" \
- "--quiet" \
- "--output=/home/grafik/www.boost.org/testing/webcheck" \
- "--force" \
- "http://beta.boost.org/"
+cd ${HOME}/www.boost.org/testing
+for result in `find incoming -mmin +5 -name '*.zip'` ; do
+  f=`basename ${result}`
+  mv -f live/${f} old
+  mv -f ${result} live
+  rm -f old/${f}
+done
