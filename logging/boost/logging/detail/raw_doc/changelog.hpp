e753c620ffe8c55060156baa10edc69ab73b2e2c
John Torjo <john.groups@torjo.com>
2008-01-05 18:24:57 +0000

    [logging] v0.13.6, 6 jan 2008 - added Jamfiles for building examples/tests - not fully tested yet, since had problems buidling latest boost from SVN
    SVN-Revision: 42491

--- logging/boost/logging/detail/raw_doc/changelog.hpp
+++ logging/boost/logging/detail/raw_doc/changelog.hpp
@@ -1,7 +1,8 @@
 /** 
 @page page_changelog Changelog
 
-@section changelog_cur_ver Current Version: v0.13.5, 5 jan 2008
+@section changelog_cur_ver Current Version: v0.13.6, 6 jan 2008
+- added Jamfiles for building examples/tests - not fully tested yet, since had problems buidling latest boost from SVN
 - added docs about caching messages
 - can cache messages before initialization of logs
 - gather classes now have msg_type instead of param (msg_type contains the type of the returned msg - without const or &)
