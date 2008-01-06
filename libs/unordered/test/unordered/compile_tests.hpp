f2a107095e4d0bc3c77a8a4c0b26313d5f4dcb19
Daniel James <daniel@calamity.org.uk>
2008-01-06 18:00:01 +0000

    Merge changes from boost trunk.
    SVN-Revision: 42542

--- libs/unordered/test/unordered/compile_tests.hpp
+++ libs/unordered/test/unordered/compile_tests.hpp
@@ -1,10 +1,7 @@
 
-// Copyright 2006-2007 Daniel James.
+// Copyright 2005-2007 Daniel James.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-#include <boost/unordered_set.hpp>
-#include <boost/unordered_map.hpp>
 
 #if defined(BOOST_MSVC)
 #pragma warning(push)
@@ -18,14 +15,119 @@
 #endif
 
 #include <boost/mpl/assert.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/type_traits/is_convertible.hpp>
 #include <boost/iterator/iterator_traits.hpp>
+#include <boost/limits.hpp>
 #include "../helpers/check_return_type.hpp"
 
-#include <iostream>
-#include <boost/detail/lightweight_test.hpp>
-#include "../objects/minimal.hpp"
+typedef long double comparison_type;
 
 template <class T> void sink(T const&) {}
+
+template <class X, class T>
+void container_test(X& r, T&)
+{
+    typedef typename X::iterator iterator;
+    typedef typename X::const_iterator const_iterator;
+    typedef typename X::difference_type difference_type;
+    typedef typename X::size_type size_type;
+
+    typedef typename boost::iterator_value<iterator>::type iterator_value_type;
+    typedef typename boost::iterator_value<const_iterator>::type const_iterator_value_type;
+    typedef typename boost::iterator_difference<iterator>::type iterator_difference_type;
+    typedef typename boost::iterator_difference<const_iterator>::type const_iterator_difference_type;
+
+    typedef typename X::value_type value_type;
+    typedef typename X::reference reference;
+    typedef typename X::const_reference const_reference;
+
+    // value_type
+
+    BOOST_MPL_ASSERT((boost::is_same<T, value_type>));
+    boost::function_requires<boost::CopyConstructibleConcept<X> >();
+
+    // reference_type / const_reference_type
+
+    BOOST_MPL_ASSERT((boost::is_same<T&, reference>));
+    BOOST_MPL_ASSERT((boost::is_same<T const&, const_reference>));
+
+    // iterator
+
+    boost::function_requires<boost::InputIteratorConcept<iterator> >();
+    BOOST_MPL_ASSERT((boost::is_same<T, iterator_value_type>));
+    BOOST_MPL_ASSERT((boost::is_convertible<iterator, const_iterator>));
+
+    // const_iterator
+
+    boost::function_requires<boost::InputIteratorConcept<const_iterator> >();
+    BOOST_MPL_ASSERT((boost::is_same<T, const_iterator_value_type>));
+
+    // difference_type
+
+    BOOST_MPL_ASSERT((boost::mpl::bool_<
+                std::numeric_limits<difference_type>::is_signed>));
+    BOOST_MPL_ASSERT((boost::mpl::bool_<
+                std::numeric_limits<difference_type>::is_integer>));
+    BOOST_MPL_ASSERT((boost::is_same<difference_type,
+                iterator_difference_type>));
+    BOOST_MPL_ASSERT((boost::is_same<difference_type,
+                const_iterator_difference_type>));
+
+    // size_type
+
+    BOOST_MPL_ASSERT_NOT((boost::mpl::bool_<
+                std::numeric_limits<size_type>::is_signed>));
+    BOOST_MPL_ASSERT((boost::mpl::bool_<
+                std::numeric_limits<size_type>::is_integer>));
+
+    // size_type can represent any non-negative value type of difference_type
+    // I'm not sure about either of these tests...
+    size_type max_diff((std::numeric_limits<difference_type>::max)());
+    difference_type converted_diff(max_diff);
+    BOOST_TEST((std::numeric_limits<difference_type>::max)()
+            == converted_diff);
+
+    BOOST_TEST(
+        static_cast<comparison_type>(
+            (std::numeric_limits<size_type>::max)()) >
+        static_cast<comparison_type>(
+            (std::numeric_limits<difference_type>::max)()));
+
+    // I don't test the runtime post-conditions here.
+    X u;
+    BOOST_TEST(u.size() == 0);
+    BOOST_TEST(X().size() == 0);
+
+    X a,b;
+
+    sink(X(a));
+    X u2(a);
+    X u3 = a;
+
+    X* ptr = new X();
+    X& a1 = *ptr;
+    (&a1)->~X();
+
+    X const a_const;
+    test::check_return_type<iterator>::equals(a.begin());
+    test::check_return_type<const_iterator>::equals(a_const.begin());
+    test::check_return_type<const_iterator>::equals(a.cbegin());
+    test::check_return_type<const_iterator>::equals(a_const.cbegin());
+    test::check_return_type<iterator>::equals(a.end());
+    test::check_return_type<const_iterator>::equals(a_const.end());
+    test::check_return_type<const_iterator>::equals(a.cend());
+    test::check_return_type<const_iterator>::equals(a_const.cend());
+
+    // No tests for ==, != since they're not required for unordered containers.
+
+    a.swap(b);
+    test::check_return_type<X>::equals_ref(r = a);
+    test::check_return_type<size_type>::equals(a.size());
+    test::check_return_type<size_type>::equals(a.max_size());
+    test::check_return_type<bool>::convertible(a.empty());
+}
 
 template <class X, class Key>
 void unordered_set_test(X&, Key const&)
@@ -199,130 +301,4 @@ void unordered_test(X&, Key& k, T& t, Hash& hf, Pred& eq)
     test::check_return_type<float>::equals(b.max_load_factor());
     a.max_load_factor((float) 2.0);
     a.rehash(100);
-}
-
-void test1()
-{
-    boost::hash<int> hash;
-    std::equal_to<int> equal_to;
-    int value = 0;
-    std::pair<int const, int> map_value(0, 0);
-
-    std::cout<<"Test unordered_set.\n";
-
-    boost::unordered_set<int> set;
-    
-    unordered_unique_test(set, value);
-    unordered_set_test(set, value);
-    unordered_test(set, value, value, hash, equal_to);
-
-    std::cout<<"Test unordered_multiset.\n";
-
-    boost::unordered_multiset<int> multiset;
-    
-    unordered_equivalent_test(multiset, value);
-    unordered_set_test(multiset, value);
-    unordered_test(multiset, value, value, hash, equal_to);
-
-    std::cout<<"Test unordered_map.\n";
-
-    boost::unordered_map<int, int> map;
-
-    unordered_unique_test(map, map_value);
-    unordered_map_test(map, value, value);
-    unordered_test(map, value, map_value, hash, equal_to);
-    unordered_map_functions(map, value, value);
-
-    std::cout<<"Test unordered_multimap.\n";
-
-    boost::unordered_multimap<int, int> multimap;
-
-    unordered_equivalent_test(multimap, map_value);
-    unordered_map_test(multimap, value, value);
-    unordered_test(multimap, value, map_value, hash, equal_to);
-}
-
-void test2()
-{
-    test::minimal::assignable assignable
-        = test::minimal::assignable::create();
-    test::minimal::copy_constructible copy_constructible
-        = test::minimal::copy_constructible::create();
-    test::minimal::hash<test::minimal::assignable> hash
-        = test::minimal::hash<test::minimal::assignable>::create();
-    test::minimal::equal_to<test::minimal::assignable> equal_to
-        = test::minimal::equal_to<test::minimal::assignable>::create();
-
-    typedef std::pair<test::minimal::assignable const,
-            test::minimal::copy_constructible> map_value_type;
-    map_value_type map_value(assignable, copy_constructible);
-
-    std::cout<<"Test unordered_set.\n";
-
-    boost::unordered_set<
-        test::minimal::assignable,
-        test::minimal::hash<test::minimal::assignable>,
-        test::minimal::equal_to<test::minimal::assignable>,
-        test::minimal::allocator<test::minimal::assignable> > set;
-
-    unordered_unique_test(set, assignable);
-    unordered_set_test(set, assignable);
-    unordered_test(set, assignable, assignable, hash, equal_to);
-
-    std::cout<<"Test unordered_multiset.\n";
-
-    boost::unordered_multiset<
-        test::minimal::assignable,
-        test::minimal::hash<test::minimal::assignable>,
-        test::minimal::equal_to<test::minimal::assignable>,
-        test::minimal::allocator<test::minimal::assignable> > multiset;
-
-    unordered_equivalent_test(multiset, assignable);
-    unordered_set_test(multiset, assignable);
-    unordered_test(multiset, assignable, assignable, hash, equal_to);
-
-    std::cout<<"Test unordered_map.\n";
-
-    boost::unordered_map<
-        test::minimal::assignable,
-        test::minimal::copy_constructible,
-        test::minimal::hash<test::minimal::assignable>,
-        test::minimal::equal_to<test::minimal::assignable>,
-        test::minimal::allocator<map_value_type> > map;
-
-    unordered_unique_test(map, map_value);
-    unordered_map_test(map, assignable, copy_constructible);
-    unordered_test(map, assignable, map_value, hash, equal_to);
-
-
-    boost::unordered_map<
-        test::minimal::assignable,
-        test::minimal::default_copy_constructible,
-        test::minimal::hash<test::minimal::assignable>,
-        test::minimal::equal_to<test::minimal::assignable>,
-        test::minimal::allocator<map_value_type> > map2;
-
-    test::minimal::default_copy_constructible default_copy_constructible;
-
-    unordered_map_functions(map2, assignable, default_copy_constructible);
-
-    std::cout<<"Test unordered_multimap.\n";
-
-    boost::unordered_multimap<
-        test::minimal::assignable,
-        test::minimal::copy_constructible,
-        test::minimal::hash<test::minimal::assignable>,
-        test::minimal::equal_to<test::minimal::assignable>,
-        test::minimal::allocator<map_value_type> > multimap;
-
-    unordered_equivalent_test(multimap, map_value);
-    unordered_map_test(multimap, assignable, copy_constructible);
-    unordered_test(multimap, assignable, map_value, hash, equal_to);
-}
-
-int main() {
-    test1();
-    test2();
-
-    return boost::report_errors();
 }
