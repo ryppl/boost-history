5722813b648989aaf8c4294b8bc6b26707aeedd2
Daniel James <daniel@calamity.org.uk>
2008-01-12 12:56:47 +0000

    Pull the simple test framework wrapper out of memory.hpp
    SVN-Revision: 42695

--- libs/unordered/test/objects/memory.hpp
+++ libs/unordered/test/objects/memory.hpp
@@ -7,19 +7,10 @@
 #define BOOST_UNORDERED_TEST_MEMORY_HEADER
 
 #include <memory>
-#if defined(BOOST_UNORDERED_EXCEPTION_USE_TEST)
-#include <boost/test/test_tools.hpp>
-#define UNORDERED_CHECK(x) BOOST_CHECK(x)
-#define UNORDERED_REQUIRE(x) BOOST_REQUIRE(x)
-#else
-#include <boost/detail/lightweight_test.hpp>
-#define UNORDERED_CHECK(x) BOOST_TEST(x)
-#define UNORDERED_REQUIRE(x) if(!(x)) { BOOST_ERROR(BOOST_STRINGIZE(x)); throw test::lightweight::test_failure(); }
-#endif
-
 #include <map>
 #include <boost/mpl/apply.hpp>
 #include <boost/assert.hpp>
+#include "../helpers/test.hpp"
 
 namespace test
 {
