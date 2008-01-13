2662eb94dc3e16cdcdde23ed25abd36531b740a3
Daniel James <daniel@calamity.org.uk>
2008-01-13 14:36:45 +0000

    Fix a non-unique header guard.
    SVN-Revision: 42717

--- libs/unordered/test/objects/exception.hpp
+++ libs/unordered/test/objects/exception.hpp
@@ -3,8 +3,8 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#if !defined(BOOST_UNORDERED_TEST_OBJECTS_HEADER)
-#define BOOST_UNORDERED_TEST_OBJECTS_HEADER
+#if !defined(BOOST_UNORDERED_EXCEPTION_TEST_OBJECTS_HEADER)
+#define BOOST_UNORDERED_EXCEPTION_TEST_OBJECTS_HEADER
 
 #include "../helpers/test.hpp"
 
