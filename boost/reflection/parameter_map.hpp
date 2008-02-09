7da93a87f6f5e3a6c30f3c5bc2fe9ebf4d79ac98
Jeremy Pack <oryol@google.com>
2008-02-09 04:43:46 +0000

    Addition of new files for reflection headers
    SVN-Revision: 43181

--- boost/reflection/parameter_map.hpp
+++ boost/reflection/parameter_map.hpp
@@ -13,22 +13,62 @@
 #ifndef BOOST_REFLECTION_PARAMETER_MAP_HPP
 #define BOOST_REFLECTION_PARAMETER_MAP_HPP
 #include <boost/reflection/typeinfo.hpp>
+#include <exception>
 #include <multimap>
 #include <vector>
 namespace boost { namespace reflections {
-  
+class param_not_found_exception : public std::exception {
+public:
+  virtual const char* what() {
+    return "Cannot convert types";
+  }
+};
 typedef void (*FunctionPtr)();
 template <class TypeInfo>
 class generic_parameter {
 public:
   virtual ~basic_parameter() {}
   virtual TypeInfo type() = 0;
+  template <class T>
+  bool can_cast() {
+    type_info i = type_info_handler<TypeInfo, T>::get_class_type();
+    for (vector_type::const_iterator it = converter_.begin();
+         it != converters_.end(); ++it) {
+      if (it->first == i)
+        return true;
+    }
+  }
+  template <class T>
+  T* cast() {
+    type_info i = type_info_handler<TypeInfo, T>::get_class_type();
+    for (vector_type::const_iterator it = converter_.begin();
+         it != converters_.end(); ++it) {
+      if (it->first == i)
+        return true;
+    }
+  }
+  tempate <class T>
+  T cast() {
+    type_info i = type_info_handler<TypeInfo, T>::get_class_type();
+    for (vector_type::const_iterator it = converter_.begin();
+         it != converters_.end(); ++it) {
+      if (it->first == i) {
+        T ret;
+        cast_and_store(it, &ret);
+      } 
+    }
+    throw param_not_found_exception();
+  }
+protected
+  void cast_and_store(vector_type::iterator it, void* dest);
+private:
+  typedef std::vector<std::pair<TypeInfo, FunctionPtr> > vector_type;
+  vector_type converters_;
 };
 
 template <class S, class T>
-S convert(generic_parameter param) {
-  T val = 
-  return static_cast<S>(val); 
+S convert(T val) {
+  return static_cast<S>(T); 
 }
 
 
@@ -38,16 +78,26 @@ class parameter : public generic_parameter<TypeInfo> {
 public:
   template <class A, B, C>
   friend class parameter_map;
-  parameter(T value) : value_(value) {}
+  
+  parameter(T value) : value_(value) {
+    // Add converter for current type.
+    converters_.push_back
+      (make_pair(reflections::type_info_handler<TypeInfo, T>::get_class_type(),
+                 reinterpret_cast<FunctionPtr>(&convert<T, T&>)));
+  }
   template <class S>
   void converts_to(S (*convert_func)(T) = &convert<S, T>) {
     converters_.push_back
       (make_pair(reflections::type_info_handler<TypeInfo, S>::get_class_type(),
                  reinterpret_cast<FunctionPtr>(convert_func)));
   }
+  template <class S>
+  void ref_converts_to(S (*convert_func)(T&) = &convert<S, T>) {
+    converters_.push_back
+      (make_pair(reflections::type_info_handler<TypeInfo, S>::get_class_type(),
+                 reinterpret_cast<FunctionPtr>(convert_func)));
+  }
 private:
-  typedef std::vector<std::pair<TypeInfo, FunctionPtr> > vector_type;
-  vector_type converters_;
   T value_;
 };
 
@@ -59,7 +109,7 @@ public:
 };
 
 template <class Info = std::string,
-          class TypeInfo = reflections::default_type_info>
+          class TypeInfo = default_type_info>
 class basic_parameter_map 
   : public std::multimap<Info, basic_parameter*> {
 public:
@@ -89,6 +139,8 @@ private:
  
 };
 typedef basic_paramter_map<> parameter_map;
+
+
 }}
 
 #endif // BOOST_REFLECTION_PARAMETER_MAP_HPP
