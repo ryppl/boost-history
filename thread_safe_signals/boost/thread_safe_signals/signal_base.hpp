d6776ff9eed78fe429721d7b1fc23206f9672235
Frank Mori Hess <fmh6jj@gmail.com>
2008-03-10 19:46:29 +0000

    Added me as author.
    SVN-Revision: 43552

--- thread_safe_signals/boost/thread_safe_signals/signal_base.hpp
+++ thread_safe_signals/boost/thread_safe_signals/signal_base.hpp
@@ -1,6 +1,8 @@
 // Boost.Signals library
 
-// Copyright Douglas Gregor 2001-2004. Use, modification and
+// Copyright Douglas Gregor 2001-2004.
+// Copyright Frank Mori Hess 2007.
+// Use, modification and
 // distribution is subject to the Boost Software License, Version
 // 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -19,18 +21,16 @@
 namespace boost {
   namespace signalslib {
     class slot_base;
-    namespace detail {
 
-      class signal_base : public noncopyable
-      {
-      public:
-        friend class boost::signalslib::slot_base;
+    class signal_base : public noncopyable
+    {
+    public:
+      friend class boost::signalslib::slot_base;
 
-        virtual ~signal_base() {}
-      protected:
-        virtual shared_ptr<void> lock_pimpl() const = 0;
-      };
-    } // end namespace detail
+      virtual ~signal_base() {}
+    protected:
+      virtual shared_ptr<void> lock_pimpl() const = 0;
+    };
   } // end namespace BOOST_SIGNALS_NAMESPACE
 } // end namespace boost
 
