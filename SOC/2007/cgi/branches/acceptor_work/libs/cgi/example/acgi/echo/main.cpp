76a15fe4403041ae7af1c72a204fece1b284db1f
Darren Garvey <darren.garvey@gmail.com>
2008-03-21 18:17:06 +0000

    Adding basic example testing multipart forms.
    SVN-Revision: 43760

--- SOC/2007/cgi/branches/acceptor_work/libs/cgi/example/acgi/echo/main.cpp
+++ SOC/2007/cgi/branches/acceptor_work/libs/cgi/example/acgi/echo/main.cpp
@@ -14,6 +14,8 @@
 //
 
 #include <boost/cgi/acgi.hpp>
+#include <fstream>
+//#include <google/template.h>
 
 using namespace std;
 using namespace boost::acgi;
@@ -33,24 +35,72 @@ void show_map_contents(MapT& m, OStreamT& os, const std::string& title)
 
 int main()
 {
-  service s;
-  request req(s);
+  try{
+    //std::ofstream of("c:/cc/log/cgi.error");
+    //if (!of)
+    //  throw std::runtime_error("Couldn't open file for writing");
+    //std::cerr.rdbuf() = of.rdbuf();
+    service s;
+    request req(s);
 
-  req.load(true); // The 'true' means parse STDIN data too.
+    try {
 
-  response resp;
+      boost::system::error_code ec;
+      req.load(ec, true);
 
-  show_map_contents(req.env(), resp, "Environment Variables");
-  show_map_contents(req.GET(), resp, "GET Variables");
-  show_map_contents(req.POST(), resp, "POST Variables");
-  show_map_contents(req.cookie(), resp, "Cookie Variables");
+      if (ec)
+      {
+        response resp;
+        resp
+        << content_type("text/plain")
+        << "Error " << ec.value() << ": " << ec.message() << "<p />"
+           "--Original message follows--"
+           "<p />";
+        resp.send(req.client());
+      }
 
-  // Note that this (and any other) HTTP header can go either before or after
-  // the response contents.
-  resp<< content_type("text/html");
+      response resp;
+      resp<< content_type("text/html")
+          << "<form method=POST enctype='multipart/form-data'>"
+              "<input type=text name=name value='" << req.POST("name") << "' />"
+              "<br />"
+              "<input type=text name=hello value='" << req.POST("hello") << "' />"
+              "<br />"
+              "<input type=file name=user_file />"
+              "<input type=hidden name=cmd value=multipart_test />"
+              "<br />"
+              "<input type=submit value=submit />"
+             "</form><p />"
+             "boundary marker = " << req.boundary_marker() << "<p />";
 
-  // Send the response to the client associated with the request.
-  resp.send(req.client());
+      show_map_contents(req.GET(), resp, "GET Variables");
+      show_map_contents(req.POST(), resp, "POST Variables");
+      show_map_contents(req.cookie(), resp, "Cookie Variables");
+      show_map_contents(req.env(), resp, "Environment Variables");
 
-  return 0;
+      //resp<< content_type("text/html");
+      resp.send(req.client());
+
+    }catch(boost::system::error_code& ec){
+      response resp;
+      resp<< content_type("text/plain") << "Error " << ec.value() << ": "
+          << ec.message();
+    }catch(std::exception& e){
+      response resp;
+      resp<< content_type("text/plain") << "Error: " << e.what();
+      return_(resp, req, 2);
+    }
+
+  }catch(std::exception* e){
+    std::cout
+    << content_type("text/plain").content
+    << "Exception: " << e->what();
+    //resp.send(req.client());
+    return 3;
+  }catch(...){
+    std::cout<< content_type("text/plain").content
+             << "Unknown error.";
+    return 4;
+  }
+ return 0;
 }
