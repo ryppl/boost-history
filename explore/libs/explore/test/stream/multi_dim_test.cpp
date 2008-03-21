62691dc1447d4fef5cb204a6d31cbaabb431a190
Jared McIntyre <jmcintyre@dfsoftware.com>
2008-03-21 03:57:18 +0000

    print() tests were in the stream test set, which was causing test failures
    SVN-Revision: 43740

--- explore/libs/explore/test/stream/multi_dim_test.cpp
+++ explore/libs/explore/test/stream/multi_dim_test.cpp
@@ -14,28 +14,6 @@
 #include <vector>
 #include <boost/explore.hpp>
 
-BOOST_AUTO_TEST_CASE( two_dimension_print_test )
-{
-    std::stringstream str_out;
-
-    std::vector<int> vi;
-    std::vector<std::vector<int> > vvi;
-
-    vi.push_back(1);
-    vi.push_back(2);
-    vi.push_back(3);
-
-    vvi.push_back(vi);
-    vvi.push_back(vi);
-    vvi.push_back(vi);
-
-    explore::print(vvi, str_out/*, ??? */);
-    BOOST_CHECK_EQUAL(str_out.str(),
-        "|1 2 3|\n"
-        "|1 2 3|\n"
-        "|1 2 3|");
-}
-
 std::ostream& format_2d(std::ostream& ostr)
 {
     using namespace explore;
@@ -69,42 +47,6 @@ BOOST_AUTO_TEST_CASE( two_dimension_stream_test )
         "|1 2 3|\n"
         "|1 2 3|\n"
         "|1 2 3|");
-}
-
-BOOST_AUTO_TEST_CASE( three_dimension_print_test )
-{
-    std::stringstream str_out;
-
-    std::vector<int> vi;
-    std::vector<std::vector<int> > vvi;
-    std::vector<std::vector<std::vector<int> > > vvvi;
-
-    vi.push_back(1);
-    vi.push_back(2);
-    vi.push_back(3);
-
-    vvi.push_back(vi);
-    vvi.push_back(vi);
-    vvi.push_back(vi);
-
-    vvvi.push_back(vvi);
-    vvvi.push_back(vvi);
-    vvvi.push_back(vvi);
-
-    explore::print(vvvi, str_out/*, ??? */);
-    BOOST_CHECK_EQUAL(str_out.str(),
-        "|1 2 3|\n"
-        "|1 2 3|\n"
-        "|1 2 3|\n"
-        "\n"
-        "|1 2 3|\n"
-        "|1 2 3|\n"
-        "|1 2 3|\n"
-        "\n"
-        "|1 2 3|\n"
-        "|1 2 3|\n"
-        "|1 2 3|"
-    );
 }
 
 std::ostream& format_3d(std::ostream& ostr)
