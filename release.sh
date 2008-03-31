13bdf8d6725ec4c2798d18159568de9bf17db703
Daniel James <daniel@calamity.org.uk>
2008-03-31 17:11:43 +0000

    Branching unordered to create a version to use with boost 1.35
    SVN-Revision: 43960

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
-TARBALL_DIR=../../../../tarballs
+#!/bin/sh
+
+TARBALL_DIR=~/tarballs
 UNORDERED_DST=$TARBALL_DIR/unordered
 
 mkdir -p $TARBALL_DIR
@@ -11,10 +13,6 @@ svn export . $UNORDERED_DST
 cd $UNORDERED_DST/doc
 bjam
 cd -
-
-mkdir $UNORDERED_DST/doc/html/images/
-cp $BOOST_ROOT/doc/html/*.css $UNORDERED_DST/doc/html/
-cp $BOOST_ROOT/doc/html/images/*.png $UNORDERED_DST/doc/html/images/
 
 rm -r $UNORDERED_DST/libs/functional
 rm -r $UNORDERED_DST/bin.v2
