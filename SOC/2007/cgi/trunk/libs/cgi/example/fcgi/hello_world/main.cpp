05e051f0f4b379c702dddd6f393ae31c871620aa
Darren Garvey <darren.garvey@gmail.com>
2008-03-31 20:06:39 +0000

    Modifying jamfiles to make compiling examples easier.
    SVN-Revision: 43974

--- SOC/2007/cgi/trunk/libs/cgi/example/fcgi/hello_world/main.cpp
+++ SOC/2007/cgi/trunk/libs/cgi/example/fcgi/hello_world/main.cpp
@@ -1,107 +1,56 @@
+//                    -- main.hpp --
+//
+//           Copyright (c) Darren Garvey 2007.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+////////////////////////////////////////////////////////////////
+//
+//[fcgi_hello_world
+//
+// The simplest FastCGI program, outputs only "Hello there, universe."
+//
+
 #include <boost/cgi/fcgi.hpp>
-#include <boost/system/system_error.hpp>
-#include <fstream>
-#include <boost/date_time/posix_time/posix_time.hpp>
-#include <boost/program_options/environment_iterator.hpp>
 
-using namespace cgi::fcgi;
 using namespace std;
-
-#define LOG_FILE "/var/www/log/fcgi_hello_world.txt"
-
-template<typename OStream>
-void accept_handler(OStream& os, const boost::system::error_code ec)
-{
-  os<< "Accepted connection ok!" << endl;
-}
-
+using namespace boost::fcgi;
 
 int main()
 {
-  std::cerr<< "[hw] Started FCGI daemon (see " LOG_FILE " for more)..." << endl;
-try {
-  ofstream of(LOG_FILE);
-  if (!of) std::cerr<< "Couldn't open file: \"" LOG_FILE "\"." << endl;
-  of<< boost::posix_time::second_clock::local_time() << endl;
-  of<< "Going to start acceptor" << endl;
-  service s;
-  acceptor a(s);
+try
+{
+  service s;        // This becomes useful with async operations.
+  acceptor a(s);    // This is used to accept requests from the server.
+  request req(s);   // Our request.
 
-  a.assign(boost::asio::ip::tcp::v4(), 0);
-  of<< "[a] Assigned." << endl;
-  bool give_up = false;
-
-  do {
-
-  of<< "Writing environment variables..." << endl;
-  for(boost::environment_iterator i(environ), e; i != e; ++i)
+  for (int i(50); i != 0; --i) // Handle 50 requests then exit.
   {
-    of<< i->first << ", " << i->second << endl;
+    a.accept(req);
+    response resp;    // A response object to make our lives easier.
+
+    // This is a minimal response. The content_type(...) may go before or after
+    // the response text.
+    resp<< content_type("text/plain")
+        << "Hello there, universe.";
+
+    resp.send(req.client());  // Send the response.
+    req.close(resp.status()); // Close the request (we can reuse this object now).
   }
-  of<< "--------------------------------" << endl;
-  request req(s);
-  of<< "Constructed request" << endl;
-  boost::system::error_code ec;
-  a.accept(req, ec);
-
-  of<< "Called accept" << endl;
-  if (ec) { of<< "[hw] accept error: " << ec.message() << endl; return 13; }
-  of<< "request.is_open() := " << req.is_open() << endl;
-  of<< "request.client().is_open() := " << req.client().is_open() << endl;
-  //of<< "next_layer stuff := " << req.client().connection()->next_layer().is_open() << endl;
-  //a.async_accept(s, boost::bind(&accept_handler<std::ofstream>, boost::ref(of), boost::asio::placeholders::error));
-  of<< "Accept had result: " << ec.message() << endl;
-  boost::asio::streambuf buf;
-  of<< "Start log:" << endl;
-
-  req.load(ec, true);
-
-  string resp("Content-type: text/html\r\n\r\nHello there, universe!<p />");
-  //const char* resp2 = "Content-type: text/plain\r\n\r\nHello there, universe.";
-
-  write(req.client(), boost::asio::buffer(resp), boost::asio::transfer_all(), ec);
-
-  of<< "[a] Wrote some data: " << ec.message() << endl;
-
-  cgi::map::const_iterator iter = req.env().begin();
-  cgi::map::const_iterator end = req.env().end();
-
-  for (; iter != end; ++iter)
-  {
-    string bit;
-    bit += "<b>" + iter->first + "</b> := <i>" + iter->second + "</i><br />";
-    write(req.client(), boost::asio::buffer(bit), boost::asio::transfer_all(), ec);
-    if (ec)
-    {
-      of<< "Got error writing env: " << ec.message() << endl;
-      break;
-    }
-  }
-
-  //req.client().read_some(buf.prepare(64), ec);
-  //of<< "[a] Read some data: " << ec.message() << endl;
-
-  //for (;;)
-  //{
-  //  size_t br = req.client().read_some(buf.prepare(64), ec);
-  //  of<< "Client wrote " << br << " bytes (max: " << std::numeric_limits<size_t>::max() << ")." << endl
-  //    << string(boost::asio::buffer_cast<const char *>(buf.data()),
-  //              boost::asio::buffer_size(buf.data())) << endl;
-  //  if (ec) { of<< "read error: " << ec.message() << endl; return 5; }
-  //}
-  sleep(4);
-
-  }while(!give_up);
-  s.run();
   return 0;
-}catch(boost::system::system_error& se){
-  cerr<< "[hw] System error: " << se.what() << endl;
-  return 1313;
-}catch(exception& e){
-  cerr<< "[hw] Exception: " << e.what() << endl;
-  return 666;
-}catch(...){
-  cerr<< "[hw] Uncaught exception!" << endl;
-  return 667;
+}
+catch(boost::system::system_error& err)
+{
+  std::cerr<< "System error " << err.code() << ": "
+           << err.what() << std::endl;
+  return 1;
+}
+catch(...)
+{
+  std::cerr<< "Unknown error!" << std::endl;
+  return 2;
 }
 }
+//]
+
