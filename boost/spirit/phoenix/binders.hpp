35ff23552b322a2afed6770dbaf24885a98eaa3f
Joel de Guzman <djowel@gmail.com>
2008-04-09 01:51:47 +0000

    In preparation for spirit2: * flat includes * home directory * forwarding headers * classic spirit
    SVN-Revision: 44119

---
+++ boost/spirit/phoenix/binders.hpp
@@ -0,0 +1,20 @@
+/*=============================================================================
+    Copyright (c) 2002-2008 Joel de Guzman
+    http://spirit.sourceforge.net/
+
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+#ifndef BOOST_SPIRIT_DEPRECATED_INCLUDE_BINDERS
+#define BOOST_SPIRIT_DEPRECATED_INCLUDE_BINDERS
+
+#if defined(_MSC_VER) || defined(__BORLANDC__) || defined(__DMC__)
+#  pragma message (warning "This header is deprecated. Please use: boost/spirit/include/phx_binders.hpp")
+#elif defined(__GNUC__) || defined(__HP_aCC) || defined(__SUNPRO_CC) || defined(__IBMCPP__)
+#  warning "This header is deprecated. Please use: boost/spirit/include/phx_binders.hpp"
+#endif
+
+#include <boost/spirit/include/phx_binders.hpp>
+
+#endif
---
+++ boost/spirit/symbols/symbols_fwd.hpp
@@ -0,0 +1,20 @@
+/*=============================================================================
+    Copyright (c) 2002-2008 Joel de Guzman
+    http://spirit.sourceforge.net/
+
+    Use, modification and distribution is subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+#ifndef BOOST_SPIRIT_DEPRECATED_INCLUDE_SYMBOLS_FWD
+#define BOOST_SPIRIT_DEPRECATED_INCLUDE_SYMBOLS_FWD
+
+#if defined(_MSC_VER) || defined(__BORLANDC__) || defined(__DMC__)
+#  pragma message (warning "This header is deprecated. Please use: boost/spirit/include/symbols_fwd.hpp")
+#elif defined(__GNUC__) || defined(__HP_aCC) || defined(__SUNPRO_CC) || defined(__IBMCPP__)
+#  warning "This header is deprecated. Please use: boost/spirit/include/symbols_fwd.hpp"
+#endif
+
+#include <boost/spirit/include/symbols_fwd.hpp>
+
+#endif
