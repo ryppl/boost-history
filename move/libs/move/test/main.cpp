997019153fdd1178c4c4bb60281a4b5469825ac6
Daniel James <daniel@calamity.org.uk>
2008-04-20 18:48:35 +0000

    Change to boost license.
    SVN-Revision: 44648

--- move/libs/move/test/main.cpp
+++ move/libs/move/test/main.cpp
@@ -1,7 +1,10 @@
 /*
-	Copyright 2005-2007 Adobe Systems Incorporated
-	Distributed under the MIT License (see accompanying file LICENSE_1_0_0.txt
-	or a copy at http://stlab.adobe.com/licenses.html)
+    Copyright 2005-2007 Adobe Systems Incorporated
+
+    Use, modification and distribution are subject to the Boost Software
+    License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
+    http://www.boost.org/LICENSE_1_0.txt).
+
 */
 
 /*************************************************************************************************/
