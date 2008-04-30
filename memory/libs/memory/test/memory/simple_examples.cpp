1567562a7bffc85608a96d574abde5fc3a3b1bd1
xushiwei <xushiweizh@gmail.com>
2008-04-30 08:50:16 +0000

    a gc_alloc example.
    SVN-Revision: 44922

--- memory/libs/memory/test/memory/simple_examples.cpp
+++ memory/libs/memory/test/memory/simple_examples.cpp
@@ -60,22 +60,35 @@ void testTlsScopedAlloc()
 void testGCAlloc()
 {
 	boost::gc_alloc alloc;
-	// same as: boost::scoped_alloc(boost::tls_block_pool::instance());
+	// same as: boost::gc_alloc(boost::tls_block_pool::instance());
 	
 	int* intObj = BOOST_NEW(alloc, int);
 	int* intObjWithArg = BOOST_NEW(alloc, int)(10);
-	int* intArray = BOOST_NEW_ARRAY(alloc, int, 100);
+	int* intArray = BOOST_NEW_ARRAY(alloc, int, 10000); // > BlockSize
 	int* intBuf = BOOST_ALLOC(alloc, int);
 	int* intArrayBuf = BOOST_ALLOC_ARRAY(alloc, int, 100);
+
+	char* hugeBuf = BOOST_NEW_ARRAY(alloc, char, boost::policy::sys::AllocSizeHuge); // > HugeSize
 	
 	boost::gc_alloc* suballoc = BOOST_NEW(alloc, boost::gc_alloc);
 	int* e = BOOST_NEW(*suballoc, int);
+
+	boost::gc_alloc* allocArray = BOOST_NEW_ARRAY(alloc, boost::gc_alloc, 10);
+
+	alloc.destroyArray(intArray, 10000);
+	alloc.destroyArray(allocArray, 10);
+
+	for (int i = 0; i < 1000; ++i)
+		BOOST_NEW_ARRAY(alloc, int, 100);
+
+	boost::gc_alloc alloc2;
+	alloc.swap(alloc2);
 }
 
 void simpleExamples()
 {
 	boost::enableMemoryLeakCheck();
-//	_CrtSetBreakAlloc(55);
+//	_CrtSetBreakAlloc(51);
 //	testAutoAlloc();
 //	testScopedAlloc();
 //	testTlsScopedAlloc();
