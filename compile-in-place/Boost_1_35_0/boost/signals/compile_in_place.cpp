3dd34936fabd5f312621c528125bb48b5fbfee94
Ulrich Eckhardt <doomster@knuut.de>
2008-04-30 16:13:53 +0000

    - Add include file to compile the signals library in-place. - Add example for the in-place compiled signals library.
    SVN-Revision: 44935

---
+++ compile-in-place/Boost_1_35_0/boost/signals/compile_in_place.cpp
@@ -0,0 +1,23 @@
+/* compile in-place support for Boost.System
+
+Copyright 2008 Ulrich Eckhardt
+
+Distributed under the Boost Software License, Version 1.0. (See accompanying
+file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+*/
+// $Id$
+
+// This file must not be included more than once. Note that this 'once' is
+// not per translation unit but per binary! Never include this in a header!
+#ifdef BOOST_SIGNALS_COMPILE_IN_PLACE_CPP_INCLUDED
+#  error "this file should only be included once per binary"
+#endif
+
+#define BOOST_SIGNALS_COMPILE_IN_PLACE_CPP_INCLUDED
+
+
+#include <boost/../libs/signals/src/connection.cpp>
+#include <boost/../libs/signals/src/named_slot_map.cpp>
+#include <boost/../libs/signals/src/signal_base.cpp>
+#include <boost/../libs/signals/src/slot.cpp>
+#include <boost/../libs/signals/src/trackable.cpp>
---
+++ compile-in-place/test-signals.cpp
@@ -0,0 +1,37 @@
+/* example to demonstrate compile-in-place for the signals lib
+
+Compile with
+  $CXX -I path/to/boost_X_YY_Z test-signals.cpp
+
+Notes:
+
+$Id$
+*/
+
+#include <iostream>
+#include <ostream>
+#include <boost/signal.hpp>
+#include <boost/signals/compile_in_place.cpp>
+
+struct HelloWorld
+{
+    void operator()() const 
+    {
+        std::cout << "Hello, World!" << std::endl;
+    }
+};
+
+
+int main()
+{
+    // Signal with no arguments and a void return value
+    boost::signal<void ()> sig;
+
+    // Connect a HelloWorld slot
+    HelloWorld hello;
+    sig.connect(hello);
+
+    // Call all of the slots
+    sig();
+}
+
