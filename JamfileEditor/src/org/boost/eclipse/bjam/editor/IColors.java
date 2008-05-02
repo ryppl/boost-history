36af5f79a6f7f1426bb798bb67c31f16da724051
Frank Birbacher <bloodymir.crap@gmx.net>
2008-05-02 19:40:27 +0000

    changed colors for syntax highlighting
    SVN-Revision: 45038

--- JamfileEditor/src/org/boost/eclipse/bjam/editor/IColors.java
+++ JamfileEditor/src/org/boost/eclipse/bjam/editor/IColors.java
@@ -9,11 +9,11 @@ package org.boost.eclipse.bjam.editor;
 import org.eclipse.swt.graphics.RGB;
 
 public interface IColors {
-    RGB SINGLE_COMMENT = new RGB(127, 159, 191);
+    RGB SINGLE_COMMENT = new RGB(73, 115, 255);
     RGB COMMANDS = new RGB(63, 127, 95);
-    RGB PROPERTIES = new RGB(73, 115, 255);
-    RGB MAIN_TARGET_RULE = new RGB(150, 64, 150);
-    RGB PUNCTUATION = new RGB(127, 0, 85);
+    RGB PROPERTIES = new RGB(0, 0, 192);
+    RGB MAIN_TARGET_RULE = new RGB(127, 0, 85);
+    RGB PUNCTUATION = MAIN_TARGET_RULE;
     RGB JAM_BUILTIN = new RGB(191, 127, 95);
     RGB JAM_FLOW = PUNCTUATION;
     RGB JAM_PUNCTUATION = PUNCTUATION;
