c792bd04d6b0d1ff6ef4b21a6bf64efcb556a234
Thomas Klimpel <thomas.klimpel@gmx.de>
2008-05-22 09:09:12 +0000

    Add Georg Baum's override capability for Fortran naming.
    [email to ublas list at 2007-04-25 02:40:38]
    
    SVN-Revision: 45641

--- boost/numeric/bindings/traits/fortran.h
+++ boost/numeric/bindings/traits/fortran.h
@@ -9,6 +9,11 @@
 #ifndef BOOST_NUMERIC_BINDINGS_TRAITS_FORTRAN_H
 #define BOOST_NUMERIC_BINDINGS_TRAITS_FORTRAN_H
 
+#if defined(BIND_FORTRAN_LOWERCASE_UNDERSCORE) || defined(BIND_FORTRAN_LOWERCASE)
+// Allow manual override of the defaults, e.g. if you want to use a fortran
+// lib compiled with gcc from MSVC
+#else
+
 // First we need to know what the conventions for linking
 // C with Fortran is on this platform/toolset
 #if defined(__GNUC__) || defined(__ICC) || defined(__sgi) || defined(__COMO__) || defined(__KCC)
@@ -17,6 +22,8 @@
 #define BIND_FORTRAN_LOWERCASE
 #else
 #error do not know how to link with fortran for the given platform
+#endif
+
 #endif
 
 // Next we define macro's to convert our symbols to 
