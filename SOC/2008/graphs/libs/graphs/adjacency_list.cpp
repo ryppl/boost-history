f57cf429b84bdb903744cf9680c1f8b24e58d4bb
Andrew Sutton <andrew.n.sutton@gmail.com>
2008-05-29 14:09:29 +0000

    Moved the concepts header to the boost directory. Started rebuilding the adj. list impl to test lots of instantiations.
    SVN-Revision: 45905

--- SOC/2008/graphs/libs/graphs/adjacency_list.cpp
+++ SOC/2008/graphs/libs/graphs/adjacency_list.cpp
@@ -3,176 +3,119 @@
 #include <string>
 #include <vector>
 
-#include <typeinfo>
-#include <cxxabi.h>
-
 #include <boost/graphs/adjacency_list.hpp>
-#include "print_vertices.hpp"
+
+#include "add_vertices.hpp"
 
 using namespace std;
 using namespace boost::graphs;
 using namespace boost::graphs::adj_list;
 
-string demangle(string const& name)
-{
-    return string(abi::__cxa_demangle(name.c_str(), 0, 0, 0));
-}
+// The prurpose of this file is to instantiate every possible combination
+// of structural components for the adjacency list. There are a quite a few.
+// Testing functions are named for the storage components that define each
+// part of the adjacency list. They are:
+//
+// type_vertex_edge_incident
+//
+// Where type is the predominant structural type of the graph, vertex is the
+// vertex store, edge is the edge store and incidedent is the vertex edge store.
 
-static void test_graph();
-static void test_multigraph();
+// Anonymous vertices, multigraph, not reducible.
+void undirected_vector_vector_vector();
+void undirected_vector_vector_list();
+void undirected_vector_vector_set();
 
-static void vector_simple_graph();
-static void list_simple_graph();
-static void set_simple_graph();
-static void map_simple_graph();
+// Anonymous vertices, multigraph, fully mutable
+void undirected_vector_list_vector();
+void undirected_vector_list_list();
+void undirected_vector_list_set();
+
+// Anonymous vertices, simple graph, fully mutable
+void undirected_vector_set_vector();
+void undirected_vector_set_list();
+void undirected_vector_set_set();
+
+// Anonymous vertices, simple graph, fully mutable
+// Not implemented
+void undirected_vector_multiset_vector();
+void undirected_vector_multiset_list();
+void undirected_vector_multiset_set();
+
+static const int N = 100;
 
 int
 main(int argc, char *argv[])
 {
-    test_graph();
-    test_multigraph();
+    undirected_vector_vector_vector();
+    undirected_vector_vector_list();
+    undirected_vector_vector_set();
 
-    cout << "*** vector simple graph ***" << endl;
-    vector_simple_graph();
-
-    cout << "*** list simple graph ***" << endl;
-    list_simple_graph();
-
-    cout << "*** set simple graph ***" << endl;
-    set_simple_graph();
-
-    cout << "*** map simple graph ***" << endl;
-    map_simple_graph();
+    undirected_vector_list_vector();
+    undirected_vector_list_list();
+    undirected_vector_list_set();
 
     return 0;
 }
 
-void
-test_graph()
+void undirected_vector_vector_vector()
 {
-    graph<> g;
-}
-
-void
-test_multigraph()
-{
-    multigraph<> g;
-}
-
-void
-vector_simple_graph()
-{
-    static string V[] = {
-        "Houston", "Dallas", "Fort Worth", "Austin", "San Antonio", "El Paso",
-        "Austin", "Dallas"
-    };
-    int N = sizeof(V) / sizeof(string);
-
     typedef adjacency_list<
-            undirected,
-            string,
-            none,
-            vertex_vector,
-            edge_vector
+            undirected, int, int, vertex_vector, edge_vector, vertex_edge_vector
         > Graph;
     Graph g;
-
-    for(int i = 0; i < N; ++i) {
-        g.add_vertex(V[i]);
-    }
-
-    print_vertices(g);
+    add_vertices(g, N);
 }
 
-void
-list_simple_graph()
+void undirected_vector_vector_list()
 {
-    static string V[] = {
-        "Houston", "Dallas", "Fort Worth", "Austin", "San Antonio", "El Paso",
-        "Austin", "Dallas"
-    };
-    int N = sizeof(V) / sizeof(string);
-
     typedef adjacency_list<
-            undirected,
-            string,
-            none,
-            vertex_list
+            undirected, int, int, vertex_vector, edge_vector, vertex_edge_list
         > Graph;
     Graph g;
-
-    for(int i = 0; i < N; ++i) {
-        g.add_vertex(V[i]);
-    }
-
-    print_vertices(g);
+    add_vertices(g, N);
 }
 
-void
-set_simple_graph()
+void undirected_vector_vector_set()
 {
-    static string V[] = {
-        "Houston", "Dallas", "Fort Worth", "Austin", "San Antonio", "El Paso",
-        "Austin", "Dallas"
-    };
-    int N = sizeof(V) / sizeof(string);
-
     typedef adjacency_list<
-            undirected,
-            string,
-            none,
-            vertex_set
+            undirected, int, int, vertex_vector, edge_vector, vertex_edge_set
         > Graph;
     Graph g;
-
-    for(int i = 0; i < N; ++i) {
-       g.add_vertex(V[i]);
-    }
-
-    print_vertices(g);
+    add_vertices(g, N);
 }
 
-void
-map_simple_graph()
+void undirected_vector_list_vector()
 {
-    static string V[] = {
-        "Houston", "Dallas", "Fort Worth", "Austin", "San Antonio", "El Paso",
-        "Austin", "Dallas"
-    };
-    int N = sizeof(V) / sizeof(string);
-
     typedef adjacency_list<
-            undirected,
-            int,
-            none,
-            string_to_vertex_map,
-            edge_list,
-            vertex_edge_list,
-            no_loops_policy
+            undirected, int, int, vertex_vector, edge_list, vertex_edge_vector
         > Graph;
     Graph g;
-
-    for(int i = 0; i < N; ++i) {
-        g.add_vertex(V[i], i);
-    }
-
-    Graph::vertex_descriptor houston = g.find_vertex("Houston");
-    Graph::vertex_descriptor dallas = g.find_vertex("Dallas");
-    Graph::vertex_descriptor ftworth = g.find_vertex("Fort Worth");
-
-    std::cout << "Houston: " << houston.get() << endl;
-    std::cout << "Dallas: " << dallas.get() << endl;
-    std::cout << "Fort Worth: " << ftworth.get() << endl;
-
-    Graph::edge_descriptor hd = g.add_edge(houston, dallas);
-    Graph::edge_descriptor fw = g.add_edge(ftworth, houston);
-
-    cout << "edges: " << g.num_edges() << endl;
-    g.remove_edge(hd);
-    g.remove_edge(houston, ftworth);
-    cout << "edges: " << g.num_edges() << endl;
-
-    // print_vertices(g);
-
+    add_vertices(g, N);
 }
 
+void undirected_vector_list_list()
+{
+    typedef adjacency_list<
+            undirected, int, int, vertex_vector, edge_list, vertex_edge_vector
+        > Graph;
+    Graph g;
+    add_vertices(g, N);
+}
+
+void undirected_vector_list_set()
+{
+    typedef adjacency_list<
+            undirected, int, int, vertex_vector, edge_list, vertex_edge_vector
+        > Graph;
+    Graph g;
+    add_vertices(g, N);
+}
+
+void undirected_vector_set_vector();
+void undirected_vector_set_list();
+void undirected_vector_set_set();
+
+void undirected_vector_multiset_vector();
+void undirected_vector_multiset_list();
+void undirected_vector_multiset_set();
