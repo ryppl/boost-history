ed8b3f1da4afb3c7be2d8b41d0a37160e62876c1
Chintan Rao H <chintanraoh@gmail.com>
2008-06-02 19:14:37 +0000

    added find(element_type) to cursor method
    SVN-Revision: 46050

--- SOC/2008/digital_searching/dsearch/boost/dsearch/node_cursor.hpp
+++ SOC/2008/digital_searching/dsearch/boost/dsearch/node_cursor.hpp
@@ -4,7 +4,6 @@
 #include <boost/dsearch/from_tree/cursor_helpers.hpp> //namespace boost::tree
 #include<assert.h>
 
-
 namespace boost {
 namespace dsearch {
 	using boost::tree::cursor_facade;
@@ -13,126 +12,128 @@ namespace dsearch {
 				forward_traversal_tag,
 				bidirectional_traversal_tag>
 
-	{
-		private:
-		typedef trie_cursor<Key,Mapped,Node> cursor;
-		
-		friend class boost::iterator_core_access;
-		friend class boost::tree::cursor_core_access;
-		unsigned char only_root;
-		bool top;
-
-		Node *cur;
-		typedef typename Node::iterator node_iterator;
-		node_iterator pos;
-
-		public:
-		trie_cursor()//	:only_root(0),cur(0),top(0) (gave warnings!?!)
-		{
-			only_root=0;cur=0;top=0;
-		}
-
-		trie_cursor(Node * const n_ptr): only_root(1) , top(false)
-		{
-			cur=n_ptr;
-		}
-
-		trie_cursor(const Node * n_ptr,const node_iterator it): only_root(0), top(false)
-		{
-			cur=const_cast<Node *>(n_ptr);
-			pos=it;
-		}
-
-		private:
-		bool equal(cursor const& other) const
-		{
-			if(top==other.top)
-			{
-				if(top) return true;
-			}
-			else
-				return false;
-
-			if(only_root==other.only_root) 
-			{
-				if (only_root==1 || only_root==2)
-					return	true;
-				assert(only_root==0);
-			}
-			else 
-				return false;
-
-			if(cur==other.cur && pos==other.pos) return true;
-			return false;
-		}
-
-		cursor left() const
-		{
-			if(top)
-			{
-				return cursor(cur);
-			}
-			if(only_root)
-				return cursor(cur,cur->begin());
-			return cursor(*pos,(*pos)->begin());
-		}
-
-		cursor right() const
-		{
-			if(top)
-				return (++cursor(cur));
-			if(only_root)
-				return cursor(cur,cur->end());
-			return cursor(*pos,(*pos)->end());
-		}
-
-		void decrement()
-		{
-			if(only_root)
-				only_root--;
-			else
-				pos--;
-		}
-		
-		bool empty_() const
-		{
-			if(only_root)
-				return cur->empty();
-			return (*pos)->empty();
-		}
-
-		Node &dereference() const
-		{
-			return *get_node();
-		}
-
-		Node *get_node() const
-		{
-			if(only_root)
-				return cur;
-			return *pos;
-		}
-
-		void increment()
-		{
-			if(only_root)
-				only_root++;
-			else
-				pos++;
-		}
-		public:
+{
+	private:
+	typedef trie_cursor<Key,Mapped,Node> cursor;
+	typedef typename Node::element_type element_type;
 	
+	friend class boost::iterator_core_access;
+	friend class boost::tree::cursor_core_access;
+	unsigned char only_root;
+	bool top;
 
-		bool has_value() const
-		{
-			return get_node()->has_value();
-		}
+	Node *cur;
+	typedef typename Node::iterator node_iterator;
+	node_iterator pos;
 
-		Mapped &get_value() const
+	public:
+	trie_cursor()//	:only_root(0),cur(0),top(0) (gave warnings!?!)
+	{
+		only_root=0;cur=0;top=0;
+	}
+
+	trie_cursor(Node * const n_ptr): only_root(1) , top(false)
+	{
+		cur=n_ptr;
+	}
+
+	trie_cursor(const Node * n_ptr,const node_iterator it): only_root(0), top(false)
+	{
+		cur=const_cast<Node *>(n_ptr);
+		pos=it;
+	}
+
+	private:
+	bool equal(cursor const& other) const
+	{
+		if(top==other.top)
 		{
-			return 	get_node()->get_value_ref();
+			if(top) return true;
 		}
-	};
+		else
+			return false;
+		if(only_root==other.only_root) 
+		{
+			if (only_root==1 || only_root==2)
+				return	true;
+			assert(only_root==0);
+		}
+		else 
+			return false;
+
+		if(cur==other.cur && pos==other.pos) return true;
+			return false;
+	}
+
+	cursor left() const
+	{
+		if(top)
+		{
+			return cursor(cur);
+		}
+		if(only_root)
+			return cursor(cur,cur->begin());
+		return cursor(*pos,(*pos)->begin());
+	}
+
+	cursor right() const
+	{
+		if(top)
+			return (++cursor(cur));
+		if(only_root)
+			return cursor(cur,cur->end());
+		return cursor(*pos,(*pos)->end());
+	}
+
+	void decrement()
+	{
+		if(only_root)
+			only_root--;
+		else
+			pos--;
+	}
+		
+	bool empty_() const
+	{
+		return get_node()->empty();
+	}
+
+	Node &dereference() const
+	{
+		return *get_node();
+	}
+
+	Node *get_node() const
+	{
+		if(only_root)
+			return cur;
+		return *pos;
+	}
+
+	void increment()
+	{
+		if(only_root)
+			only_root++;
+		else
+			pos++;
+	}
+
+	public:
+	bool has_value() const
+	{
+		return get_node()->has_value();
+	}
+
+	Mapped &get_value() const
+	{
+		return 	get_node()->get_value_ref();
+	}
+		
+	cursor find(const element_type &e)
+	{
+		return cursor(get_node(),get_node()->find(e));
+	}
+};
 }
 }
 
