41583c7898044dec794517313e8fa4f138ba0afa
Niels Dekker <nielsdekker@xs4all.nl>
2008-07-02 19:58:08 +0000

    Added array support to boost/sandbox/swap, according to ticket #2056. Was discussed at "[boost] [utility/swap] Okay to add array support to Boost.Swap(sandbox/swap)?" <http://lists.boost.org/Archives/boost/2008/06/138753.php>
    SVN-Revision: 47003

--- swap/boost/utility/swap.hpp
+++ swap/boost/utility/swap.hpp
@@ -1,15 +1,19 @@
-// Copyright (C) 2007 Steven Watanabe, Joseph Gauterin
+// Copyright (C) 2007, 2008 Steven Watanabe, Joseph Gauterin
 //
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 // For more information, see http://www.boost.org
+//
+// Update:
+// 29 June 2008 (Added support for built-in arrays.) Niels Dekker  
 
 
 #ifndef BOOST_UTILITY_SWAP_HPP
 #define BOOST_UTILITY_SWAP_HPP
 
 #include <algorithm> //for std::swap
+#include <cstddef> //for std::size_t
 
 namespace boost_swap_impl
 {
@@ -18,6 +22,15 @@ namespace boost_swap_impl
   {
     using std::swap;//use std::swap if argument dependent lookup fails
     swap(left,right);
+  }
+
+  template<class T, std::size_t N>
+  void swap_impl(T (& left)[N], T (& right)[N])
+  {
+    for (std::size_t i = 0; i < N; ++i)
+    {
+      ::boost_swap_impl::swap_impl(left[i], right[i]);
+    }
   }
 }
 
