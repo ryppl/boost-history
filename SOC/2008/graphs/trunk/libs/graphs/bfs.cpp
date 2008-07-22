6d6614e1ce70d210a9ae3cc0c877f2e52ab31d83
Andrew Sutton <andrew.n.sutton@gmail.com>
2008-07-22 15:47:21 +0000

    Started implementing iterable BFS's. These apparently shake out int two distinct types of iteration sequences: vertices and edges. Of course the edge iteration provides its own difficulties (i.e., with undirected edges) so I had to introduce a new edge "concept" called a rooted edge, which implements source and target functions and an adapter type for undirected edges.
    Started adding some basic test data.
    
    SVN-Revision: 47691

---
+++ SOC/2008/graphs/trunk/libs/graphs/bfs.cpp
@@ -0,0 +1,69 @@
+
+#include <iostream>
+#include <queue>
+
+#include <boost/graphs/undirected_graph.hpp>
+#include <boost/graphs/algorithms/breadth_first.hpp>
+
+#include "typestr.hpp"
+#include "read.hpp"
+
+using namespace std;
+
+template <typename Graph, typename Walk>
+void iterate(Graph& g, Walk& walk)
+{
+    typedef typename Walk::vertex_descriptor Vertex;
+
+    typedef typename Walk::iterator Iterator;
+    typedef typename Iterator::value_type Edge;
+    typedef pair<Iterator, Iterator> Range;
+
+    Range rng(walk.begin(), walk.end());
+    for( ; rng.first != rng.second; ++rng.first) {
+        Vertex u = rng.first->source();
+        Vertex v = rng.first->target();
+        cout << "edge: " << g[u] << g[v] << endl;
+    }
+}
+
+template <typename Graph>
+void edge_walk(Graph& g)
+{
+    typedef typename Graph::vertex_descriptor Vertex;
+    typedef typename Graph::edge_descriptor Edge;
+    typedef exterior_vertex_property<Graph, color> ColorProp;
+    typedef exterior_property_map<ColorProp> ColorMap;
+    typedef queue<Vertex> Queue;
+
+    typedef breadth_first_edge_walk<Graph, ColorMap, Queue> BreadthFirstWalk;
+    typedef breadth_first_edge_traversal<Graph, ColorMap, Queue> BreadthFirstTraversal;
+
+    {
+        cout << "--- walk ---" << endl;
+        ColorProp color_prop(g, white_color);
+        ColorMap color(color_prop);
+        BreadthFirstWalk walk(g, *g.begin_vertices(), color);
+        iterate(g, walk);
+    }
+
+    {
+        cout << "--- traversal ---" << endl;
+        ColorProp color_prop(g, white_color);
+        ColorMap color(color_prop);
+        BreadthFirstTraversal trav(g, color);
+        iterate(g, trav);
+    }
+}
+
+int main()
+{
+    typedef undirected_graph<string, string, vertex_set<>, edge_set<>> Graph;
+
+    Graph g;
+    read(cin, g);
+
+    edge_walk(g);
+
+    return 0;
+}
