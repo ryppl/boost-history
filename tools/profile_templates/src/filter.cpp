9d4bf8c168e2179742526e77b6968050a0ea445d
Steven Watanabe <steven@providere-consulting.com>
2008-08-13 21:41:22 +0000

    Add primitive call graph generation--only msvc works
    SVN-Revision: 48132

--- tools/profile_templates/src/filter.cpp
+++ tools/profile_templates/src/filter.cpp
@@ -1,9 +1,19 @@
+// filter.cpp
+//
+// Copyright (c) 2008
+// Steven Watanabe
+// 
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <string>
 #include <cstdio>
 
 const char* search("template_profiler");
+const char* back_trace_search("see reference to");
 
-int main() {
+void copy_flat_only() {
     std::string buffer;
     int ch;
     int pos = 0;
@@ -37,5 +47,83 @@ int main() {
         } else {
             pos = 0;
         }
+    }
+}
+
+void copy_call_graph() {
+#ifdef _MSC_VER
+    std::string buffer;
+    int ch;
+    int pos = 0;
+    bool matched = false;
+    int counter = 0;
+    while((ch = std::getchar()) != EOF) {
+        buffer.push_back(static_cast<char>(ch));
+        if(ch == '\n') {
+            if(matched) {
+                for(std::size_t i = 0; i < buffer.size(); ++i) {
+                    std::putchar(buffer[i]);
+                }
+                if(++counter % 100 == 0) {
+                    std::fprintf(stderr, "On Instantiation %d\n", counter);
+                }
+                buffer.clear();
+                matched = false;
+                // process instantiation back-trace
+                pos = 0;
+                while((ch = std::getchar()) != EOF) {
+                    if(ch == ' ') {
+                        buffer.push_back(static_cast<char>(ch));
+                        while((ch = std::getchar()) != EOF) {
+                            buffer.push_back(static_cast<char>(ch));
+                            if(ch == '\n') {
+                                if(matched) {
+                                    for(std::size_t i = 0; i < buffer.size(); ++i) {
+                                        std::putchar(buffer[i]);
+                                    }
+                                }
+                                buffer.clear();
+                                matched = false;
+                                pos = 0;
+                                break;
+                            }
+                            if(ch == back_trace_search[pos]) {
+                                ++pos;
+                                if(back_trace_search[pos] == '\0') {
+                                    matched = true;
+                                }
+                            } else {
+                                pos = 0;
+                            }
+                        }
+                    } else {
+                        std::ungetc(ch, stdin);
+                        break;
+                    }
+                }
+            }
+            buffer.clear();
+            matched = false;
+            pos = 0;
+        }
+        if(ch == search[pos]) {
+            ++pos;
+            if(search[pos] == '\0') {
+                matched = true;
+            }
+        } else {
+            pos = 0;
+        }
+    }
+#else
+    #error gcc is not supported yet.
+#endif
+}
+
+int main(int argc, char** argv) {
+    if(argc == 2 && std::strcmp(argv[1], "--call-graph") == 0) {
+        copy_call_graph();
+    } else {
+        copy_flat_only();
     }
 }
--- tools/profile_templates/src/postprocess.cpp
+++ tools/profile_templates/src/postprocess.cpp
@@ -1,8 +1,20 @@
+// postprocess.cpp
+//
+// Copyright (c) 2008
+// Steven Watanabe
+// 
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/regex.hpp>
+#include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
 #include <string>
 #include <iostream>
 #include <fstream>
 #include <map>
+#include <set>
 #include <exception>
 #include <iterator>
 #include <algorithm>
@@ -11,10 +23,13 @@
 #ifdef _MSC_VER
 
 boost::regex warning_message("(.*) : warning C4150: deletion of pointer to incomplete type 'template_profiler::incomplete'; no destructor called");
+boost::regex call_graph_line("        (.*)\\((\\d+)\\) : see reference to .*");
+boost::regex split_file_and_line("(.*)\\((\\d+)\\)");
 
 #elif defined(__GNUC__)
 
 boost::regex warning_message("(.*): warning: division by zero in .template_profiler::value / 0.");
+#error gcc is not supported yet
 
 #else
 
@@ -40,17 +55,55 @@ struct compare {
     }
 };
 
+typedef std::pair<std::string, int> line_id;
+
+struct node_info {
+    node_info() : count(0), total_with_children(0) {}
+    std::map<const line_id*, int> children;
+    std::map<const line_id*, int> parents;
+    int count;
+    int total_with_children;
+};
+
+struct call_graph_less {
+    template<class T>
+    bool operator()(const T& lhs, const T& rhs) const {
+        return(lhs.second.total_with_children > rhs.second.total_with_children);
+    }
+};
+
+struct print_line_id {
+    print_line_id(const line_id* x) : line(x) {}
+    const line_id* line;
+};
+
+std::ostream& operator<<(std::ostream& os, const print_line_id& arg) {
+    os << arg.line->first << '(' << arg.line->second << ')';
+    return(os);
+}
+
 int main(int argc, char** argv) {
-    if(argc != 2) {
+    const char* input_file_name = 0;
+    bool use_call_graph = false;
+    for(int i = 1; i < argc; ++i) {
+        if(std::strcmp(argv[i], "--call-graph") == 0) {
+            use_call_graph = true;
+        } else {
+            input_file_name = argv[i];
+        }
+    }
+    if(input_file_name == 0) {
         std::cerr << "Usage: " << argv[0] << " <input file>\n";
         return(EXIT_FAILURE);
     }
+
+
     std::map<std::string, int> messages;
     std::string line;
     int total_matches = 0;
     std::ptrdiff_t max_match_length = 0;
     {
-        std::ifstream input(argv[1]);
+        std::ifstream input(input_file_name);
         while(std::getline(input, line)) {
             boost::smatch match;
             if(boost::regex_match(line, match, warning_message)) {
@@ -68,4 +121,65 @@ int main(int argc, char** argv) {
     std::cout << std::setfill('-') << std::setw(max_match_length + 20) << '-' << std::setfill(' ') << std::endl;
     print p = { &cummulative, max_match_length };
     std::for_each(copy.begin(), copy.end(), p);
+
+    if(use_call_graph) {
+        std::map<const line_id*, node_info> graph;
+        std::set<line_id> lines;
+        typedef std::pair<std::string, int> raw_line;
+        BOOST_FOREACH(const raw_line& l, messages) {
+            boost::smatch match;
+            boost::regex_match(l.first, match, split_file_and_line);
+            lines.insert(line_id(match[1], boost::lexical_cast<int>(match[2].str())));
+        }
+        const line_id* current_instantiation = 0;
+        std::ifstream input(input_file_name);
+        while(std::getline(input, line)) {
+            boost::smatch match;
+            if(boost::regex_match(line, match, warning_message)) {
+                std::string file_and_line(match[1].str());
+                boost::regex_match(file_and_line, match, split_file_and_line);
+                std::string file = match[1];
+                int line = boost::lexical_cast<int>(match[2].str());
+                current_instantiation = &*lines.find(line_id(file, line));
+                ++graph[current_instantiation].total_with_children;
+                ++graph[current_instantiation].count;
+            } else if(boost::regex_match(line, match, call_graph_line)) {
+                std::string file = match[1];
+                int line = boost::lexical_cast<int>(match[2].str());
+                std::set<line_id>::const_iterator pos = lines.lower_bound(line_id(file, line));
+                if(pos != lines.end()) {
+                    if(*pos != line_id(file, line)) {
+                        if(pos != lines.begin()) {
+                            --pos;
+                        }
+                    }
+                } else if(pos != lines.begin()) {
+                    --pos;
+                } else {
+                    break;
+                }
+                const line_id* parent = &*pos;
+                ++graph[current_instantiation].parents[parent];
+                ++graph[parent].children[current_instantiation];
+                ++graph[parent].total_with_children;
+            }
+        }
+        typedef std::pair<const line_id*, node_info> call_graph_node_t;
+        std::vector<call_graph_node_t> call_graph;
+        std::copy(graph.begin(), graph.end(), std::back_inserter(call_graph));
+        std::sort(call_graph.begin(), call_graph.end(), call_graph_less());
+        std::cout << "\nCall Graph\n\n";
+        BOOST_FOREACH(const call_graph_node_t& node, call_graph) {
+            std::cout << print_line_id(node.first) << " (" << node.second.count << ")\n";
+            typedef std::map<const line_id*, int>::const_reference node_t;
+            std::cout << "  Parents:\n";
+            BOOST_FOREACH(node_t n, node.second.parents) {
+                std::cout << "    " << print_line_id(n.first) << " (" << n.second << ")\n";
+            }
+            std::cout << "  Children:\n";
+            BOOST_FOREACH(node_t n, node.second.children) {
+                std::cout << "    " << print_line_id(n.first) << " (" << n.second << "/" << graph[n.first].count << ")\n";
+            }
+        }
+    }
 }
