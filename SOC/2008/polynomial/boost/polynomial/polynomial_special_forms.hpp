222b19de7eab25c63b7475cda2e80a644d056103
Pawel Kieliszczyk <lord.zerom1nd@gmail.com>
2008-08-29 14:37:37 +0000

    poly
    SVN-Revision: 48450

--- SOC/2008/polynomial/boost/polynomial/polynomial_special_forms.hpp
+++ SOC/2008/polynomial/boost/polynomial/polynomial_special_forms.hpp
@@ -1,3 +1,8 @@
+//  (C) Copyright Pawel Kieliszczyk 2008.
+//  Use, modification and distribution are subject to the
+//  Boost Software License, Version 1.0. (See accompanying file
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef BOOST_MATH_TOOLS_POLYNOMIAL_SPECIAL_FORMS_HPP
 #define BOOST_MATH_TOOLS_POLYNOMIAL_SPECIAL_FORMS_HPP
 
