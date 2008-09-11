a59679423922d70f35dee1a39747eafcd2baabde
Daniel James <daniel@calamity.org.uk>
2008-09-11 07:10:31 +0000

    Move only example.
    SVN-Revision: 48723

--- move/libs/move/doc/listing1.cpp
+++ move/libs/move/doc/listing1.cpp
@@ -58,11 +58,56 @@ class movable : public boost::equality_comparable<movable>
     implementation* member;
 };
 
-int main()
+void example1()
 {
     movable x(10);
     movable y = x;
-
-    return 0;
 }
 //]
+
+//[move_only
+class move_only : public boost::equality_comparable<move_only>
+{
+    move_only(move_only&);
+
+ public:
+
+    explicit move_only(int x = 0) : member(new implementation(x)) { }
+    ~move_only() { delete member; }
+
+    // Move Constructor
+    move_only(boost::move_from<move_only> rhs) : member(rhs.source.member)
+    { rhs.source.member = 0; }
+ 
+    // operator=() on a move_only type takes parameter by value and consumes it
+    // note that this type can't be copied, so operator= can only be called
+    // for a moved value.
+    move_only& operator=(move_only x)
+    { swap(*this, x); return *this; }
+
+    operator boost::move_from<move_only>()
+    { return boost::move_from<move_only>(*this); }
+    
+    friend bool operator==(const move_only& x, const move_only &y)
+    { return *x.member == *y.member; }
+    
+    friend void swap(move_only& x, move_only& y)
+    { swap(x.member, y.member); }
+    
+ private:
+    implementation* member;
+};
+
+void example2()
+{
+    move_only x(20);
+    move_only y = boost::move(x);
+}
+//]
+
+int main() {
+    std::cout<<"Example 1:\n\n";
+    example1();
+    std::cout<<"\nExample 2:\n\n";
+    example2();
+}
