569ff9d7da1a1f2fb7158326c2cbee94e93c4cf9
Christopher Kohlhoff <chris@kohlhoff.com>
2008-10-09 20:43:26 +0000

    Merge from trunk.
    ........
      r49155 | nmusatti | 2008-10-07 08:46:14 +1100 (Tue, 07 Oct 2008) | 1 line
    
      Patch from  Ticket #2372
    ........
      r49195 | chris_kohlhoff | 2008-10-09 17:22:58 +1100 (Thu, 09 Oct 2008) | 2 lines
    
      Add missing bounds checks as specified in TR2 proposal.
    ........
      r49197 | chris_kohlhoff | 2008-10-09 17:28:39 +1100 (Thu, 09 Oct 2008) | 2 lines
    
      Merge codegear changes from non-boost version of asio.
    ........
      r49198 | chris_kohlhoff | 2008-10-09 17:30:16 +1100 (Thu, 09 Oct 2008) | 4 lines
    
      Ensure the streambuf's egptr() is kept in sync the pptr(). Use std::memmove
      rather than std::rotate to minimise data copying. Avoid unnecessary resizes
      of the underlying vector.
    ........
      r49199 | chris_kohlhoff | 2008-10-09 17:31:01 +1100 (Thu, 09 Oct 2008) | 3 lines
    
      Fix basic_socket_streambuf to work with Protocol objects that don't
      provide a resolver.
    ........
      r49200 | chris_kohlhoff | 2008-10-09 17:32:00 +1100 (Thu, 09 Oct 2008) | 2 lines
    
      Add example showing use of local::stream_protocol::iostream.
    ........
      r49201 | chris_kohlhoff | 2008-10-09 17:33:34 +1100 (Thu, 09 Oct 2008) | 4 lines
    
      Only use TerminateThread when explicitly requested by the user by calling
      asio::detail::thread::set_terminate_threads(true). This fixes a memory leak
      that may occur with internally created threads.
    ........
      r49202 | chris_kohlhoff | 2008-10-09 17:34:48 +1100 (Thu, 09 Oct 2008) | 3 lines
    
      Make the service_registry's usage of typeid work when the default gcc
      linker visibility is set to hidden.
    ........
      r49203 | chris_kohlhoff | 2008-10-09 17:39:05 +1100 (Thu, 09 Oct 2008) | 2 lines
    
      Reduce memory usage by doing lazy initialisation of the io_service's reactor.
    ........
    
    SVN-Revision: 49221

--- boost/asio/detail/reactive_descriptor_service.hpp
+++ boost/asio/detail/reactive_descriptor_service.hpp
@@ -21,6 +21,7 @@
 #include <boost/asio/error.hpp>
 #include <boost/asio/io_service.hpp>
 #include <boost/asio/detail/bind_handler.hpp>
+#include <boost/asio/detail/handler_base_from_member.hpp>
 #include <boost/asio/detail/noncopyable.hpp>
 #include <boost/asio/detail/service_base.hpp>
 #include <boost/asio/detail/descriptor_ops.hpp>
@@ -67,6 +68,9 @@ public:
 
     // Flags indicating the current state of the descriptor.
     unsigned char flags_;
+
+    // Per-descriptor data used by the reactor.
+    typename Reactor::per_descriptor_data reactor_data_;
   };
 
   // The maximum number of buffers to support in a single operation.
@@ -78,6 +82,7 @@ public:
         reactive_descriptor_service<Reactor> >(io_service),
       reactor_(boost::asio::use_service<Reactor>(io_service))
   {
+    reactor_.init_task();
   }
 
   // Destroy all user-defined handler objects owned by the service.
@@ -97,7 +102,7 @@ public:
   {
     if (impl.descriptor_ != -1)
     {
-      reactor_.close_descriptor(impl.descriptor_);
+      reactor_.close_descriptor(impl.descriptor_, impl.reactor_data_);
 
       if (impl.flags_ & implementation_type::internal_non_blocking)
       {
@@ -125,7 +130,8 @@ public:
       return ec;
     }
 
-    if (int err = reactor_.register_descriptor(native_descriptor))
+    if (int err = reactor_.register_descriptor(
+          native_descriptor, impl.reactor_data_))
     {
       ec = boost::system::error_code(err,
           boost::asio::error::get_system_category());
@@ -150,7 +156,7 @@ public:
   {
     if (is_open(impl))
     {
-      reactor_.close_descriptor(impl.descriptor_);
+      reactor_.close_descriptor(impl.descriptor_, impl.reactor_data_);
 
       if (impl.flags_ & implementation_type::internal_non_blocking)
       {
@@ -172,7 +178,7 @@ public:
   }
 
   // Get the native descriptor representation.
-  native_type native(implementation_type& impl)
+  native_type native(const implementation_type& impl) const
   {
     return impl.descriptor_;
   }
@@ -187,7 +193,7 @@ public:
       return ec;
     }
 
-    reactor_.cancel_ops(impl.descriptor_);
+    reactor_.cancel_ops(impl.descriptor_, impl.reactor_data_);
     ec = boost::system::error_code();
     return ec;
   }
@@ -269,7 +275,8 @@ public:
     for (;;)
     {
       // Try to complete the operation without blocking.
-      int bytes_sent = descriptor_ops::writev(impl.descriptor_, bufs, i, ec);
+      int bytes_sent = descriptor_ops::gather_write(
+          impl.descriptor_, bufs, i, ec);
 
       // Check if operation succeeded.
       if (bytes_sent >= 0)
@@ -304,25 +311,27 @@ public:
   }
 
   template <typename ConstBufferSequence, typename Handler>
-  class write_handler
+  class write_operation :
+    public handler_base_from_member<Handler>
   {
   public:
-    write_handler(int descriptor, boost::asio::io_service& io_service,
+    write_operation(int descriptor, boost::asio::io_service& io_service,
         const ConstBufferSequence& buffers, Handler handler)
-      : descriptor_(descriptor),
+      : handler_base_from_member<Handler>(handler),
+        descriptor_(descriptor),
         io_service_(io_service),
         work_(io_service),
-        buffers_(buffers),
-        handler_(handler)
+        buffers_(buffers)
     {
     }
 
-    bool operator()(const boost::system::error_code& result)
+    bool perform(boost::system::error_code& ec,
+        std::size_t& bytes_transferred)
     {
       // Check whether the operation was successful.
-      if (result)
+      if (ec)
       {
-        io_service_.post(bind_handler(handler_, result, 0));
+        bytes_transferred = 0;
         return true;
       }
 
@@ -340,16 +349,21 @@ public:
       }
 
       // Write the data.
-      boost::system::error_code ec;
-      int bytes = descriptor_ops::writev(descriptor_, bufs, i, ec);
+      int bytes = descriptor_ops::gather_write(descriptor_, bufs, i, ec);
 
       // Check if we need to run the operation again.
       if (ec == boost::asio::error::would_block
           || ec == boost::asio::error::try_again)
         return false;
 
-      io_service_.post(bind_handler(handler_, ec, bytes < 0 ? 0 : bytes));
+      bytes_transferred = (bytes < 0 ? 0 : bytes);
       return true;
+    }
+
+    void complete(const boost::system::error_code& ec,
+        std::size_t bytes_transferred)
+    {
+      io_service_.post(bind_handler(this->handler_, ec, bytes_transferred));
     }
 
   private:
@@ -357,7 +371,6 @@ public:
     boost::asio::io_service& io_service_;
     boost::asio::io_service::work work_;
     ConstBufferSequence buffers_;
-    Handler handler_;
   };
 
   // Start an asynchronous write. The data being sent must be valid for the
@@ -405,31 +418,39 @@ public:
         impl.flags_ |= implementation_type::internal_non_blocking;
       }
 
-      reactor_.start_write_op(impl.descriptor_,
-          write_handler<ConstBufferSequence, Handler>(
+      reactor_.start_write_op(impl.descriptor_, impl.reactor_data_,
+          write_operation<ConstBufferSequence, Handler>(
             impl.descriptor_, this->get_io_service(), buffers, handler));
     }
   }
 
   template <typename Handler>
-  class null_buffers_handler
+  class null_buffers_operation :
+    public handler_base_from_member<Handler>
   {
   public:
-    null_buffers_handler(boost::asio::io_service& io_service, Handler handler)
-      : work_(io_service),
-        handler_(handler)
+    null_buffers_operation(boost::asio::io_service& io_service, Handler handler)
+      : handler_base_from_member<Handler>(handler),
+        work_(io_service)
     {
     }
 
-    bool operator()(const boost::system::error_code& result)
+    bool perform(boost::system::error_code&,
+        std::size_t& bytes_transferred)
     {
-      work_.get_io_service().post(bind_handler(handler_, result, 0));
+      bytes_transferred = 0;
       return true;
+    }
+
+    void complete(const boost::system::error_code& ec,
+        std::size_t bytes_transferred)
+    {
+      work_.get_io_service().post(bind_handler(
+            this->handler_, ec, bytes_transferred));
     }
 
   private:
     boost::asio::io_service::work work_;
-    Handler handler_;
   };
 
   // Start an asynchronous wait until data can be written without blocking.
@@ -444,8 +465,8 @@ public:
     }
     else
     {
-      reactor_.start_write_op(impl.descriptor_,
-          null_buffers_handler<Handler>(this->get_io_service(), handler),
+      reactor_.start_write_op(impl.descriptor_, impl.reactor_data_,
+          null_buffers_operation<Handler>(this->get_io_service(), handler),
           false);
     }
   }
@@ -499,7 +520,8 @@ public:
     for (;;)
     {
       // Try to complete the operation without blocking.
-      int bytes_read = descriptor_ops::readv(impl.descriptor_, bufs, i, ec);
+      int bytes_read = descriptor_ops::scatter_read(
+          impl.descriptor_, bufs, i, ec);
 
       // Check if operation succeeded.
       if (bytes_read > 0)
@@ -526,7 +548,7 @@ public:
 
   // Wait until data can be read without blocking.
   size_t read_some(implementation_type& impl,
-      const null_buffers& buffers, boost::system::error_code& ec)
+      const null_buffers&, boost::system::error_code& ec)
   {
     if (!is_open(impl))
     {
@@ -541,25 +563,27 @@ public:
   }
 
   template <typename MutableBufferSequence, typename Handler>
-  class read_handler
+  class read_operation :
+    public handler_base_from_member<Handler>
   {
   public:
-    read_handler(int descriptor, boost::asio::io_service& io_service,
+    read_operation(int descriptor, boost::asio::io_service& io_service,
         const MutableBufferSequence& buffers, Handler handler)
-      : descriptor_(descriptor),
+      : handler_base_from_member<Handler>(handler),
+        descriptor_(descriptor),
         io_service_(io_service),
         work_(io_service),
-        buffers_(buffers),
-        handler_(handler)
+        buffers_(buffers)
     {
     }
 
-    bool operator()(const boost::system::error_code& result)
+    bool perform(boost::system::error_code& ec,
+        std::size_t& bytes_transferred)
     {
       // Check whether the operation was successful.
-      if (result)
+      if (ec)
       {
-        io_service_.post(bind_handler(handler_, result, 0));
+        bytes_transferred = 0;
         return true;
       }
 
@@ -577,8 +601,7 @@ public:
       }
 
       // Read some data.
-      boost::system::error_code ec;
-      int bytes = descriptor_ops::readv(descriptor_, bufs, i, ec);
+      int bytes = descriptor_ops::scatter_read(descriptor_, bufs, i, ec);
       if (bytes == 0)
         ec = boost::asio::error::eof;
 
@@ -587,8 +610,14 @@ public:
           || ec == boost::asio::error::try_again)
         return false;
 
-      io_service_.post(bind_handler(handler_, ec, bytes < 0 ? 0 : bytes));
+      bytes_transferred = (bytes < 0 ? 0 : bytes);
       return true;
+    }
+
+    void complete(const boost::system::error_code& ec,
+        std::size_t bytes_transferred)
+    {
+      io_service_.post(bind_handler(this->handler_, ec, bytes_transferred));
     }
 
   private:
@@ -596,7 +625,6 @@ public:
     boost::asio::io_service& io_service_;
     boost::asio::io_service::work work_;
     MutableBufferSequence buffers_;
-    Handler handler_;
   };
 
   // Start an asynchronous read. The buffer for the data being read must be
@@ -644,8 +672,8 @@ public:
         impl.flags_ |= implementation_type::internal_non_blocking;
       }
 
-      reactor_.start_read_op(impl.descriptor_,
-          read_handler<MutableBufferSequence, Handler>(
+      reactor_.start_read_op(impl.descriptor_, impl.reactor_data_,
+          read_operation<MutableBufferSequence, Handler>(
             impl.descriptor_, this->get_io_service(), buffers, handler));
     }
   }
@@ -662,8 +690,8 @@ public:
     }
     else
     {
-      reactor_.start_read_op(impl.descriptor_,
-          null_buffers_handler<Handler>(this->get_io_service(), handler),
+      reactor_.start_read_op(impl.descriptor_, impl.reactor_data_,
+          null_buffers_operation<Handler>(this->get_io_service(), handler),
           false);
     }
   }
