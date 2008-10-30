d5223d23c97eea62a5726ead46fee85b62ffa803
Kevin Sopp <baraclese@googlemail.com>
2008-10-30 16:57:02 +0000

    * added license statement to mp_int_serialization.hpp
    SVN-Revision: 49492

--- mp_math/boost/mp_math/mp_int_serialization.hpp
+++ mp_math/boost/mp_math/mp_int_serialization.hpp
@@ -1,3 +1,8 @@
+// Copyright Kevin Sopp 2008.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
 #include <boost/mp_math/mp_int/mp_int_fwd.hpp>
 #include <boost/serialization/split_free.hpp>
 #include <boost/serialization/string.hpp>
