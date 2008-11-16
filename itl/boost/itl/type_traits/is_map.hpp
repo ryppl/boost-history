f031aa3fd9b5b593940884315488b246406ebbdd
Joachim Faulhaber <afojgo@gmail.com>
2008-11-16 17:07:57 +0000

    Added trait is_map.hpp. Stable {msvc-9.0}
    SVN-Revision: 49797

---
+++ itl/boost/itl/type_traits/is_map.hpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------+
+Copyright (c) 2008-2008: Joachim Faulhaber
++-----------------------------------------------------------------------------+
+   Distributed under the Boost Software License, Version 1.0.
+      (See accompanying file LICENCE.txt or copy at
+           http://www.boost.org/LICENSE_1_0.txt)
++----------------------------------------------------------------------------*/
+#ifndef __itl_type_traits_is_map_JOFA_081107_H__
+#define __itl_type_traits_is_map_JOFA_081107_H__
+
+namespace boost{ namespace itl
+{
+    template <class Type> struct is_map;
+
+    template <class Type> struct is_map{ enum {value = false}; };
+
+}} // namespace boost itl
+
+#endif
+
+
