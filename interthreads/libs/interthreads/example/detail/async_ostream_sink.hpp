8d1ad80522a3ac8b18d09cd9a8e658031176b968
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2008-11-19 15:52:01 +0000

    interthreads version 0.1
    SVN-Revision: 49844

---
+++ interthreads/libs/interthreads/example/detail/async_ostream_sink.hpp
@@ -0,0 +1,46 @@
+#ifndef BOOST_INTERTHREADS_DETAIL_ASYNC_OSTREAM_SINK_HPP
+#define BOOST_INTERTHREADS_DETAIL_ASYNC_OSTREAM_SINK_HPP
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2008. 
+// Distributed under the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt or 
+//  copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/interthreads for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <boost/iostreams/categories.hpp>   // iostreams::sink_tag
+
+#include <iosfwd>                           // std::streamsize
+#include <iostream>                         // std::ostream
+#include <boost/shared_ptr.hpp>             // shared_ptr
+
+namespace boost {
+namespace interthreads {
+    class async_ostream;
+    namespace detail {
+        
+        struct async_ostream_sink {
+            typedef char      char_type;
+            typedef  boost::iostreams::sink_tag  category;
+            
+            async_ostream_sink(std::ostream& os);
+            
+            std::streamsize write(const char* s, std::streamsize n);
+            void flush();
+            
+        private:
+            friend class async_ostream_concentrator;
+            friend class async_ostream;
+            struct impl;
+            boost::shared_ptr<impl> impl_;                
+        };
+    }
+}
+}
+
+#endif
+
