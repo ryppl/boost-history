aec05e61cfd3e7d8d70fa2349914cf3510e18dcb
Ulrich Eckhardt <doomster@knuut.de>
2008-11-30 18:22:40 +0000

    - Rename module to test_python_extend and document that the name is actually important. - Some more documentation updates.
    SVN-Revision: 50037

--- compile-in-place/test-python-extend.cpp
+++ compile-in-place/test-python-extend.cpp
@@ -1,17 +1,21 @@
 /* example to demonstrate compile-in-place for extending Python
 
 Compile with
-  $CXX -I path/to/boost_X_YY_Z -I path/to/python-includes test-python-extend.cpp -shared -o hello.so
+  $CXX -I path/to/boost_X_YY_Z -I path/to/python-includes test-python-extend.cpp -shared -o test_python_extend.so
 
 Notes:
 
-- This must be compiled into a shared object (hello.so) or DLL (hello.dll) so
-that you can load it from Python. The exact syntax for that probably differs
-from above commandline.
+- This must be compiled into a shared object (*.so) or DLL (*.dll) so that you
+can load it from Python. The exact syntax for that probably differs from above
+commandline. Note: Python 2.5 deprecates the use of *.dll for modules in favor
+of *.pyd and Python 2.6 removes support for *.dll entirely. I'm not sure if a
+similar problem exists under POSIX platforms, too.
 
 - To load it, simply open an interactive interpreter in the same dir as the
-compiled module and "import hello". You can then call the exported function
-using "hello.greet()".
+compiled module and "import test_python_extend". You can then call the exported
+function using "test_python_extend.greet()". Note that the name used below in
+the macro BOOST_PYTHON_MODULE must be the same as the filename of the module
+without the extension, otherwise Python can't load it!
 
 - Compiling with g++ -Wall will give you lots of warnings that a string
 literal is converted to a non-const pointer because the Python functions are
@@ -32,7 +36,7 @@ char const* greet()
     return "hello, world";
 }
 
-BOOST_PYTHON_MODULE(hello)
+BOOST_PYTHON_MODULE(test_python_extend)
 {
     def("greet", greet);
 }
