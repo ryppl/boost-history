fabc172c543d0f48f370720077a6dbcf98112de4
Arno Schoedl <aschoedl@think-cell.com>
2009-01-06 10:17:17 +0000

    added fall-back for BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
    SVN-Revision: 50493

--- partition_point/boost/partition_point/detail/lazy_typedef.hpp
+++ partition_point/boost/partition_point/detail/lazy_typedef.hpp
@@ -7,9 +7,7 @@
 
 #include <boost/config.hpp>
 
-#ifdef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
-#error lazy_typedef.hpp requires partial template specialization!
-#endif
+#ifndef BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION // required for lazy_typedef to work as intended
 
 namespace boost {
 	namespace detail {
@@ -17,8 +15,8 @@ namespace boost {
 	}
 }
 
-// Defines extract_some_typedef<T> which exposes T::some_typedef as extract_some_typedef<T>::type
-// if T::some_typedef exists. Otherwise extract_some_typedef<T> is empty.
+// Defines lazy_typedef_[some_typedef]<T> which exposes T::[some_typedef] as lazy_typedef_[some_typedef]<T>::type
+// if T::[some_typedef] exists. Otherwise lazy_typedef_[some_typedef]<T> is empty.
 #define BOOST_DECLARE_LAZY_TYPEDEF( some_typedef )                                                     \
 	template< typename C, typename Enable=void >                                                       \
 	struct lazy_typedef_ ## some_typedef                                                               \
@@ -29,5 +27,16 @@ namespace boost {
 	> {                                                                                                \
 		typedef BOOST_DEDUCED_TYPENAME C::some_typedef type;                                           \
 	};
+
+#else // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
+
+#define BOOST_DECLARE_LAZY_TYPEDEF( some_typedef )                                                     \
+	template< typename C >                                                                             \
+	struct lazy_typedef_ ## some_typedef                                                               \
+	{                                                                                                  \
+		typedef BOOST_DEDUCED_TYPENAME C::some_typedef type;                                           \
+	};
+
+#endif // BOOST_NO_TEMPLATE_PARTIAL_SPECIALIZATION
 
 #endif // BOOST_PARTITION_POINT_LAZY_TYPEDEF_HPP
