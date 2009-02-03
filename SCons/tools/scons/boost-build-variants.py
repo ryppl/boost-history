f12a37cc9eb2b3d1ca855b37eaabb0a0bf069a30
Sergey Popov <loonycyborg@gmail.com>
2009-02-03 22:09:08 +0000

    Added support for debug/release and static/dynamic build variants.
    SVN-Revision: 51000

---
+++ SCons/tools/scons/boost-build-variants.py
@@ -0,0 +1,12 @@
+# vi: syntax=python:et:ts=4
+from SCons.Script import Split
+
+def exists():
+    return True
+
+def generate(env):
+    env["CXXFLAGS"] = Split("-ftemplate-depth-128 -Wall")
+    env["THREADING_MULTI_CCFLAGS"] = "-pthread"
+    env["THREADING_MULTI_LIBFLAGS"] = "-pthread"
+    env["DEBUG_CCFLAGS"] = Split("-O0 -g -fno-inline")
+    env["RELEASE_CCFLAGS"] = Split("-O3 -DNDEBUG -finline-functions -Wno-inline")
--- SCons/tools/scons/boost-libs.py
+++ SCons/tools/scons/boost-libs.py
@@ -1,7 +1,10 @@
 # vi: syntax=python:et:ts=4
 
 def BoostLibrary(env, lib, sources):
-    env.StaticLibrary("boost_" + lib, sources)
+    if env["LINK_DYNAMIC"]:
+        env.SharedLibrary("boost_" + lib, sources)
+    else:
+        env.StaticLibrary("boost_" + lib, sources)
 
 def exists(env):
     return True
