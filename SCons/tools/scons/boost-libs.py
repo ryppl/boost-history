fb16105c1203136de4a3efc50f93eaacd379bbac
Sergey Popov <loonycyborg@gmail.com>
2009-02-04 12:39:39 +0000

    Added aliases to request building of only particular libs, e.g. "scons iostreams"
    SVN-Revision: 51008

--- SCons/tools/scons/boost-libs.py
+++ SCons/tools/scons/boost-libs.py
@@ -2,9 +2,12 @@
 
 def BoostLibrary(env, lib, sources):
     if env["LINK_DYNAMIC"]:
-        env.SharedLibrary("boost_" + lib, sources)
+        lib_node = env.SharedLibrary("boost_" + lib, sources)
     else:
-        env.StaticLibrary("boost_" + lib, sources)
+        lib_node = env.StaticLibrary("boost_" + lib, sources)
+
+    env.Alias(lib, lib_node)
+    return lib_node
 
 def exists(env):
     return True
