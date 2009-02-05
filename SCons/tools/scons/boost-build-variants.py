27a440efcefda7ec131ce53e635f00c3c376e243
Sergey Popov <loonycyborg@gmail.com>
2009-02-05 11:37:00 +0000

    Added support for threading aspect of build variants.
    SVN-Revision: 51027

--- SCons/tools/scons/boost-build-variants.py
+++ SCons/tools/scons/boost-build-variants.py
@@ -7,6 +7,6 @@ def exists():
 def generate(env):
     env["CXXFLAGS"] = Split("-ftemplate-depth-128 -Wall")
     env["THREADING_MULTI_CCFLAGS"] = "-pthread"
-    env["THREADING_MULTI_LIBFLAGS"] = "-pthread"
+    env["THREADING_MULTI_LINKFLAGS"] = "-pthread"
     env["DEBUG_CCFLAGS"] = Split("-O0 -g -fno-inline")
     env["RELEASE_CCFLAGS"] = Split("-O3 -DNDEBUG -finline-functions -Wno-inline")
