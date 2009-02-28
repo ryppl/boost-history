b715b2f6f473efb69963e01c6305861e99b76a76
Peter Dimov <pdimov@pdimov.com>
2009-02-28 21:40:27 +0000

    Make bind.hpp and mem_fn.hpp forward to bind/bind.hpp and bind/mem_fn.hpp. (Ref #2238)
    SVN-Revision: 51489

--- boost/mem_fn.hpp
+++ boost/mem_fn.hpp
@@ -1,5 +1,5 @@
-#ifndef BOOST_BIND_HPP_INCLUDED
-#define BOOST_BIND_HPP_INCLUDED
+#ifndef BOOST_MEM_FN_HPP_INCLUDED
+#define BOOST_MEM_FN_HPP_INCLUDED
 
 // MS compatible compilers support #pragma once
 
@@ -8,7 +8,7 @@
 #endif
 
 //
-//  bind.hpp - binds function objects to arguments
+//  mem_fn.hpp - a generalization of std::mem_fun[_ref]
 //
 //  Copyright (c) 2009 Peter Dimov
 //
@@ -16,9 +16,9 @@
 //  See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt
 //
-//  See http://www.boost.org/libs/bind/bind.html for documentation.
+//  See http://www.boost.org/libs/bind/mem_fn.html for documentation.
 //
 
-#include <boost/bind/bind.hpp>
+#include <boost/bind/mem_fn.hpp>
 
-#endif // #ifndef BOOST_BIND_HPP_INCLUDED
+#endif // #ifndef BOOST_MEM_FN_HPP_INCLUDED
