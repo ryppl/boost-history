551e0c0d0de46f0c3c918332c5e0902df426f78a
Dave Abrahams <dave@boostpro.com>
2009-03-04 02:08:07 +0000

    Fix benchmark.sh so it works
    SVN-Revision: 51590

--- reference_closure/benchmark.sh
+++ reference_closure/benchmark.sh
@@ -1,12 +1,13 @@
-CC=/home/build/buildtools/crosstool/v10/gcc-4.2.1-glibc-2.3.5/x86_64-unknown-linux-gnu/bin/x86_64-unknown-linux-gnu-g++
+#!/bin/sh
+CC=g++
 
 echo compile trivial_nested
 time $CC -O2 trivial_nested.cc -o trivial_nested
 echo execute trivial_nested
-time trivial_nested
+time ./trivial_nested
 
 echo compile trivial_function
 time $CC -O2 trivial_function.cc -o trivial_function
 echo execute trivial_function
-time trivial_function
+time ./trivial_function
 
