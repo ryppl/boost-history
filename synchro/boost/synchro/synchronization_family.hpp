15a1a49667ba34e70389698e07d2f84b6671a7ae
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2009-03-22 22:19:00 +0000

    0.3.0 : Adding lock_until/for try_lock_until/for free functions + usage of Boost/Chrono
    SVN-Revision: 51915

---
+++ synchro/boost/synchro/synchronization_family.hpp
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2008-2009. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/synchro for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_SYNCHRO_SYNCHROHRONIZATION_FAMILY__HPP
+#define BOOST_SYNCHRO_SYNCHROHRONIZATION_FAMILY__HPP
+
+namespace boost {
+namespace synchro {
+
+//!\file
+//!Describes thread synchronization family to use with Sync framework
+//!based on boost synchronization objects.
+//[synchronization_family
+template <typename ScopeTag>
+struct synchronization_family;
+//]
+
+
+}
+}
+
+#endif
