fdca9a787c710f1570f30e13ad001863b9c3e033
Stjepan Rajko <stipe@asu.edu>
2009-04-01 00:53:39 +0000

    making alpha channel for color_type optional
    SVN-Revision: 52093

--- guigl/boost/guigl/types.hpp
+++ guigl/boost/guigl/types.hpp
@@ -17,7 +17,18 @@ namespace boost { namespace guigl {
 
 typedef gil::point2<double> size_type;
 typedef gil::point2<double> position_type;
-typedef gil::rgba32f_pixel_t color_type;
+struct color_type : public gil::rgba32f_pixel_t
+{
+    typedef gil::rgba32f_pixel_t base_type;
+    typedef gil::bits32f channel_t;
+    
+    explicit color_type(channel_t v)
+        : base_type(v)
+    {}
+    color_type(channel_t v0, channel_t v1, channel_t v2, channel_t v3 = static_cast<channel_t>(1))
+        : base_type(v0,v1,v2,v3)
+    {}
+};
 
 }}
 
