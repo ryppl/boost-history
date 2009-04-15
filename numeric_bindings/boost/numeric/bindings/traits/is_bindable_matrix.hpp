ac069d3d68d76c9a7a755de61bd6e273636d3a20
Rutger ter Borg <rutger@terborg.net>
2009-04-15 10:25:39 +0000

    Initial working version of is_bindable_vector and is_bindable_matrix metafunctions
    SVN-Revision: 52399

--- numeric_bindings/boost/numeric/bindings/traits/is_bindable_matrix.hpp
+++ numeric_bindings/boost/numeric/bindings/traits/is_bindable_matrix.hpp
@@ -6,10 +6,11 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef BOOST_NUMERIC_BINDINGS_TRAITS_IS_MATRIX_HPP
-#define BOOST_NUMERIC_BINDINGS_TRAITS_IS_MATRIX_HPP
+#ifndef BOOST_NUMERIC_BINDINGS_TRAITS_IS_BINDABLE_MATRIX_HPP
+#define BOOST_NUMERIC_BINDINGS_TRAITS_IS_BINDABLE_MATRIX_HPP
 
 #include <boost/mpl/bool.hpp>
+#include <boost/numeric/bindings/traits/matrix_traits.hpp>
 
 namespace boost {
 namespace numeric {
@@ -17,7 +18,21 @@ namespace bindings {
 namespace traits {
 
 template< typename T >
-struct is_matrix: boost::mpl::bool_<false> {};
+struct is_bindable_matrix {
+
+    typedef char yes;
+    typedef char (&no)[2];
+
+    template< typename M >
+    static yes tester( typename matrix_traits<M>::pointer );
+
+    template< typename M >
+    static no tester( ... );
+
+    static bool const value = sizeof(tester<T>(0)) == sizeof(yes);
+
+    typedef mpl::bool_<value> type;
+};
 
 }}}}
 
