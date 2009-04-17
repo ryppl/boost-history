1409931b3151f0e971c97f8913bf4a3e3c3b5985
Oliver Kowalke <oliver.kowalke@gmail.com>
2009-04-17 18:01:50 +0000

    * updated license declaration
    SVN-Revision: 52451

--- task/boost/task/detail/bind_processor.hpp
+++ task/boost/task/detail/bind_processor.hpp
@@ -1,6 +1,8 @@
-//  Copyright (c) 2008 Oliver Kowalke. Distributed under the Boost
-//  Software License, Version 1.0. (See accompanying file
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//          Copyright Oliver Kowalke 2009.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
 
 #ifndef BOOST_TASK_DETAIL_BIND_PROCESSOR_H
 #define BOOST_TASK_DETAIL_BIND_PROCESSOR_H
