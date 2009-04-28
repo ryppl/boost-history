68e6d262dcd2873008043c2a745377196b868789
Jesse Williamson <jesse@mind.net>
2009-04-28 19:57:54 +0000

    Documentation updates and new Doxyfile; new unit test and minor fix; moved minmax_macro; new example.
    SVN-Revision: 52655

---
+++ boost/algorithm.hpp
@@ -0,0 +1,49 @@
+#ifndef BOOST_ALGORITHM_HPP
+ #define BOOST_ALGORITHM_HPP
+
+#include <algorithm/all.hpp>
+#include <algorithm/apply.hpp>
+#include <algorithm/copy.hpp>
+#include <algorithm/find_if_not.hpp>
+#include <algorithm/for_each_if.hpp>
+#include <algorithm/iota.hpp>
+#include <algorithm/select.hpp>
+#include <algorithm/transform_if.hpp>
+
+/*! \mainpage Boost.Algorithm Documentation
+
+\section intro_sec Introduction
+
+<p>The Boost Algorithms library compliments the C++ standard library by providing implementations of commonly used and commonly useful STL-style algorithms.  
+
+\section credits_sec Credits
+
+\author Marshall Clow
+\author Jeff Garland
+\author Jesse Williamson
+
+<p>Awknowledgements:<br>
+
+Matt Austern<br>
+Some of the algorithms are based on <a href="http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2008/n2666.pdf">suggestions</a> from Matt Austern for C++0x.<br>
+
+Jeremy Siek<br>
+Some of the algorithms are based on <a href="http://www.crystalclearsoftware.com/cgi-bin/boost_wiki/wiki.pl?STLAlgorithmExtensions">suggestions</a> from Jeremy Siek.<br>
+
+Rob Stewart<br>
+Reviewed code and helped determine naming conventions.<br>
+
+
+
+<p>&copy; Copyright 2008-2009 Boost.Algorithm authors.<br>
+Distributed under the Boost Software License, Version 1.0.<br>
+(See accompanying file <a href="../../../LICENSE_1_0.txt">
+LICENSE_1_0.txt</a> or copy at <a href="http://www.boost.org/LICENSE_1_0.txt">http://www.boost.org/LICENSE_1_0.txt</a>)
+</p>
+
+\namespace boost::algorithm
+\namespace boost::algorithm::sequence 
+
+*/
+
+#endif
