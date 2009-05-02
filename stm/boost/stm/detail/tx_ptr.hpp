2c9134be29e27f0bf5232a1aa3f1d8be28cf106d
Justin Gottschlich <justin@nodeka.com>
2009-05-02 03:41:02 +0000

    changed read and write transaction pointer implementations. at least temporarily removed upgradable pointer until Vicente and I can discuss it in more detail.
    SVN-Revision: 52714

--- stm/boost/stm/detail/tx_ptr.hpp
+++ stm/boost/stm/detail/tx_ptr.hpp
@@ -23,11 +23,11 @@
 namespace boost { namespace stm {
 
 template <typename T>
-class tx_rd_ptr
+class read_ptr
 {
 public:
  
-   tx_rd_ptr(transaction &t, T const &tx_obj) : 
+   read_ptr(transaction &t, T const &tx_obj) : 
       t_(t), tx_ptr_(&const_cast<T&>(t_.read(tx_obj))), written_(false)
    {}
 
@@ -57,6 +57,36 @@ public:
    T const & operator*() const { return *get(); }
    T const * operator->() const { return get(); }
 
+   transaction &trans() { return t_; }
+
+   T* write_ptr()
+   {
+      if (t_.forced_to_abort()) 
+      {
+         t_.lock_and_abort();
+         throw aborted_transaction_exception("aborting transaction");
+      }
+
+      // we are already holding the written object
+      if (written_) return tx_ptr_;
+
+      T* temp = t_.get_written(*tx_ptr_);
+ 
+      // if we found something, store this as the tx_ptr_
+      if (0 != temp)
+      {
+         tx_ptr_ = temp;
+         written_ = true;
+      }
+      else
+      {
+         tx_ptr_ = &t_.write(*tx_ptr_);
+         written_ = true;
+      }
+
+      return tx_ptr_;
+   }
+
 private:
     
    mutable transaction &t_;
@@ -64,73 +94,15 @@ private:
    mutable bool written_;
 };
 
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
 template <typename T>
-class tx_wr_ptr;
-
-template <typename T>
-class tx_upgrd_ptr
+class write_ptr
 {
 public:
  
-   tx_upgrd_ptr(transaction &t, T & tx_obj) : 
-      t_(t), tx_obj_(const_cast<T&>(t_.read(tx_obj))), upgraded_(false)
-   {}
-
-   const T* get()
-   {
-      if (t_.forced_to_abort()) 
-      {
-         t_.lock_and_abort();
-         throw aborted_transaction_exception("aborting transaction");
-      }
-      if (!upgraded_) {
-          T &new_tx_obj_=const_cast<T&>(t_.read(tx_obj_));
-          if (&new_tx_obj_!=&tx_obj_) {
-              tx_obj_=new_tx_obj_;
-              upgraded_=true;
-          }
-      }
-      return &tx_obj_;
-   }
-
-   const T& operator*()
-   {
-      return *get();
-   }
-
-   const T* operator->()
-   {
-      return get();
-   }
-
-
-   T& upgrade() {
-       tx_obj_=t_.write(tx_obj_);
-       upgraded_=true;
-       return tx_obj_;
-   }
-
-private:
-   //template <typename TT> 
-   friend class tx_wr_ptr;
-
-   mutable transaction &t_;
-   mutable T &tx_obj_;
-   bool upgraded_;
-};
-
-
-template <typename T>
-class tx_wr_ptr
-{
-public:
- 
-   tx_wr_ptr(transaction &t, T & tx_obj) : 
+   write_ptr(transaction &t, T & tx_obj) : 
       t_(t), tx_obj_(t_.write(tx_obj))
-   {}
-
-   tx_wr_ptr(tx_upgrd_ptr<T> &upgrd) : 
-      t_(upgrd.t_), tx_obj_(upgrd.upgrade())
    {}
 
    T& operator*()
