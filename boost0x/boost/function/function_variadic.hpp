e1218ed0a87c04066567eda799ee61b8ebd2c69d
Sebastian Redl <sebastian.redl@getdesigned.at>
2009-05-05 22:06:14 +0000

    Update variadic function
    SVN-Revision: 52777

--- boost0x/boost/function/function_variadic.hpp
+++ boost0x/boost/function/function_variadic.hpp
@@ -28,7 +28,7 @@ namespace boost
             };
             template <typename R, typename Arg1, typename Arg2>
             struct which_std_function<R, Arg1, Arg2> {
-                typedef std::unary_function<Arg1, Arg2, R> type;
+                typedef std::binary_function<Arg1, Arg2, R> type;
             };
         }
     }
@@ -37,5 +37,62 @@ namespace boost
     class function<R (Args...)> : public function_base,
         public detail::function::which_std_function<R, Args...>::type
     {
+        typedef function_base base_type;
+        typedef function<R (Args...)> self_type;
+        struct clear_type {};
+
+    public:
+
+        function() : base_type() {}
+
+        template<typename Functor>
+        function(Functor f
+               ,typename enable_if_c<
+                                (boost::type_traits::ice_not<
+                              (is_integral<Functor>::value)>::value),
+                           int>::type = 0
+               ) :
+        base_type(f)
+        {
+        }
+        template<typename Functor, typename Allocator>
+        function(Functor f, Allocator a
+               ,typename enable_if_c<
+                                (boost::type_traits::ice_not<
+                              (is_integral<Functor>::value)>::value),
+                           int>::type = 0
+               ) :
+        base_type(f,a)
+        {
+        }
+
+        function(clear_type*) : base_type() {}
+
+        function(const self_type& f)
+            : base_type(static_cast<const base_type&>(f))
+        {}
+
+        self_type& operator=(const self_type& f)
+        {
+            self_type(f).swap(*this);
+            return *this;
+        }
+
+        template<typename Functor>
+        typename enable_if_c<
+                                (boost::type_traits::ice_not<
+                             (is_integral<Functor>::value)>::value),
+                          self_type&>::type
+        operator=(Functor f)
+        {
+            self_type(f).swap(*this);
+            return *this;
+        }
+
+        self_type& operator=(clear_type*)
+        {
+            this->clear();
+            return *this;
+        }
     };
 }
