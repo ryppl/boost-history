5b43ef05e99c54cd3f5368ead181e33c2daa2f7f
Timothy Zachary Laine <whatwasthataddress@gmail.com>
2009-05-08 15:05:10 +0000

    Implemented vector push_front() and O(N) vector at().
    SVN-Revision: 52853

--- ftmpl/vector.hpp
+++ ftmpl/vector.hpp
@@ -21,6 +21,9 @@ type_t< vector_t< T..., Back > >& push_back( type_t< vector_t< T... > >&, Back& 
 template< typename ... T, typename Last >
 type_t< vector_t< T... > >& pop_back( type_t< vector_t< T..., Last > >& );
 
+template< typename ... T, typename Front >
+type_t< vector_t< Front, T...> >& push_front( type_t< vector_t< T... > >&, Front& );
+
 template< typename First, typename ... Tail >
 type_t< vector_t< Tail... > >& pop_front( type_t< vector_t< First, Tail... > >& );
 
@@ -29,6 +32,14 @@ Tail& back( type_t< vector_t< T..., Tail > >& );
 
 template< typename Head, typename ... T >
 Head& front( type_t< vector_t< Head, T... > >& );
+
+template< typename Head, typename ... T>
+auto at( type_t< vector_t< Head, T... > >&, type_t< value_t< std::size_t, 0u > >& )
+-> decltype( type< Head >() );
+
+template< typename Head, typename ... T, std::size_t I>
+auto at( type_t< vector_t< Head, T... > >&, type_t< value_t< std::size_t, I > >& )
+-> decltype( at( vector<T ...>(), value<std::size_t, I - 1>() ) );
 
 template< typename ... T >
 type_t< value_t< ::std::size_t, sizeof...( T ) > >& size( type_t< vector_t< T... > >& );
