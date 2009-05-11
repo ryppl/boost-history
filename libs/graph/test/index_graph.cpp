661b44a1a846a4faf2c47b7dcab6072b10eb2e28
Andrew Sutton <andrew.n.sutton@gmail.com>
2009-05-11 17:43:24 +0000

    Added a legitimate graph testing framework.
    SVN-Revision: 52915

--- libs/graph/test/index_graph.cpp
+++ libs/graph/test/index_graph.cpp
@@ -1,63 +1,94 @@
-
-// (C) Copyright Andrew Sutton 2007
+// (C) Copyright 2007-2009 Andrew Sutton
 //
 // Use, modification and distribution are subject to the
 // Boost Software License, Version 1.0 (See accompanying file
 // LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
 
 #include <iostream>
-#include <boost/test/minimal.hpp>
 #include <boost/graph/undirected_graph.hpp>
+#include <boost/graph/directed_graph.hpp>
 
 using namespace std;
 using namespace boost;
 
-typedef undirected_graph<> Graph;
-typedef Graph::vertex_descriptor Vertex;
-typedef property_map<Graph, vertex_index_t>::type IndexMap;
-
-int test_main(int, char*[])
+template <typename Graph>
+void test()
 {
-    static const size_t N = 5;
-    Graph g;
-    Vertex v[N];
+    typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef typename property_map<Graph, vertex_index_t>::type IndexMap;
 
+    static const size_t N = 5;
+
+    Graph g;
     IndexMap x = get(vertex_index, g);
 
     // build up the graph
+    Vertex v[N];
     for(size_t i = 0; i < N; ++i) {
         v[i] = add_vertex(g);
     }
 
     // after the first build, we should have these conditions
-    BOOST_CHECK(max_vertex_index(g) == N);
+    BOOST_ASSERT(max_vertex_index(g) == N);
     for(size_t i = 0; i < N; ++i) {
-        BOOST_CHECK(get_vertex_index(v[i], g) == i);
+        BOOST_ASSERT(get_vertex_index(v[i], g) == i);
     }
 
-    // remove some vertices and re-add them...
+    // Remove all vertices and then re-add them.
     for(size_t i = 0; i < N; ++i) remove_vertex(v[i], g);
-    BOOST_CHECK(num_vertices(g) == 0);
-
+    BOOST_ASSERT(num_vertices(g) == 0);
     for(size_t i = 0; i < N; ++i) {
-	v[i] = add_vertex(g);
+        v[i] = add_vertex(g);
+    }
+    BOOST_ASSERT(num_vertices(g) == N);
+
+    // Before renumbering, our vertices should be off by N. In other words,
+    // we're not in a good state.
+    BOOST_ASSERT(max_vertex_index(g) == 10);
+    for(size_t i = 0; i < N; ++i) {
+        BOOST_ASSERT(get_vertex_index(v[i], g) == N + i);
     }
 
-    // before renumbering, our vertices should be off by
-    // about N...
-    BOOST_CHECK(max_vertex_index(g) == 10);
-    for(size_t i = 0; i < N; ++i) {
-	BOOST_CHECK(get_vertex_index(v[i], g) == N + i);
-    }
-
-    // renumber vertices
+    // Renumber vertices
     renumber_vertex_indices(g);
 
-    // and we should be back to the initial condition
-    BOOST_CHECK(max_vertex_index(g) == N);
+    // And we should be back to the initial condition
+    BOOST_ASSERT(max_vertex_index(g) == N);
     for(size_t i = 0; i < N; ++i) {
-	BOOST_CHECK(get_vertex_index(v[i], g) == i);
+        BOOST_ASSERT(get_vertex_index(v[i], g) == i);
     }
+}
+
+// Make sure that graphs constructed over n vertices will actually number
+// their vertices correctly.
+template <typename Graph>
+void build()
+{
+    typedef typename graph_traits<Graph>::vertex_descriptor Vertex;
+    typedef typename graph_traits<Graph>::vertex_iterator Iterator;
+    typedef typename property_map<Graph, vertex_index_t>::type IndexMap;
+
+    static const size_t N = 5;
+
+    Graph g(N);
+    BOOST_ASSERT(max_vertex_index(g) == N);
+
+    IndexMap x = get(vertex_index, g);
+
+    // Each vertex should be numbered correctly.
+    Iterator i, end;
+    tie(i, end) = vertices(g);
+    for(size_t x = 0; i != end; ++i, ++x) {
+        BOOST_ASSERT(get_vertex_index(*i, g) == x);
+    }
+}
+
+int main(int, char*[])
+{
+    test< undirected_graph<> >();
+    test< directed_graph<> >();
+
+    build< undirected_graph<> >();
 
     return 0;
 }
