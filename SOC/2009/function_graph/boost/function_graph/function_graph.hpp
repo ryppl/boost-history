495774798b5e742e5a9b01ea9414ea1f65db2378
Andrew Sutton <andrew.n.sutton@gmail.com>
2009-05-28 17:13:05 +0000

    Adding proper specializations.
    SVN-Revision: 53347

--- SOC/2009/function_graph/boost/function_graph/function_graph.hpp
+++ SOC/2009/function_graph/boost/function_graph/function_graph.hpp
@@ -1,60 +1,82 @@
-#ifndef FUNCTION_GRAPH_FUNCTOR_HPP_
-#define FUNCTION_GRAPH_FUNCTOR_HPP_
+/**
+ * Copyright (C) 2009
+ * Michael Lopez
+ *
+ * Issued under Boost Software License - Version 1.0
+ * http://www.boost.org/LICENSE_1_0.txt
+ *
+ */
+
+
+
+#ifndef FUNCTION_GRAPH_HPP_
+#define FUNCTION_GRAPH_HPP_
 
 #include <boost/function.hpp>
+#include <boost/function_types/function_arity.hpp>
+#include <boost/mpl/if.hpp>
 
 namespace boost { namespace graph {
 
-/**
- * beta of function_graph using functor
- * 
- * function_graph is a data structure that creates an implicit graph based on a
- * function or functor that is either user defined or from the stl or boost.
- * 
- * @internal
- * as of current, this data structure acts over a container. The type of the
- * container is immaterial, so long as it uses T least an input iterator.
- * 
- * @todo
- * implement a pointer that will set itself to null if the object it points to
- *     is destroyed
- * have type deduced from container
- * implement domains and ranges (given a type and function, but no container)
- * implement an edge iterator
- * implement a function_graph that acts over two sets that may be different
- *     types
- */
- 
- /**
-  * function_graph - functor version
-  */
-template<typename container, template <typename> class functor>
-class function_graph
-{
-    typedef typename container::value_type value_type;
-    // functor type is deduced from the container
-    // could use better method of deduction
-    typedef functor<value_type> functor_type;
-    
-public:
+// [asutton] You're going about this in slightly the wrong way...
 
-    /** Constructor - blank */
-    function_graph()
-        : container_(0) { }
-    
-    /** Constructor - take the container */
-    function_graph(container const& x)
-        : container_(&x) { }
-    
-    /** edge functor - allows it to act as edge(...) */
-public:
-    functor_type edge;
-    
-private:
-    const container* container_;
+/**
+ * function_graph_base handles the edge function. A user can define the function
+ * through a boost::function or a functor, but the user must only have access to
+ * this function through function graph via the function edge.
+ *
+ * @note [asutton] Don't try to deduce what might be passed to this function.
+ * Just require that F be of the form boost::function<R,T,T>. Use the derived
+ * classes to put the type in the right form.
+ */
+template <typename Func>
+struct function_graph_base {
+    // If the type F has an arity of 2, then it is the boost function syntax.
+    // Otherwise, this function is either a functor or a boost function that is
+    // already defined.
+//     typedef typename mpl::if_c<function_types::function_arity<F>::value == 2,
+//                                function<F>,
+//                                F>::type func_type;
+
+    typedef Func function_type;
+    typedef typename Func::result_type edge_type;
+    typedef typename Func::argument_type vertex_type;
+
+    /** Edge function that defines the implicit graph */
+    function_type func;
 };
+
+/**
+ * @note [asutton] I'm defining an empty function graph so that we can create
+ * specializations that force the user to provide T in certain forms. This
+ * variant will be instantiated for meaningless types such as graph<int>.
+ * Obviously, that should fail.
+ */
+template <typename T> struct functon_graph { };
+
+/**
+ * @note [asutton] This specialization will be instantiated whenever the
+ * user tries to make something like graph< function<bool(int,int)> >. Note
+ * the subtelty. If you write: graph<function<bool(int, double)> >, then
+ * the compiler will match the version above, causing compiler errors.
+ */
+template <typename Edge, typename Vertex>
+struct function_graph< function<Edge(Vertex, Vertex)> >
+    : function_graph_base< function<Edge(Vertex, Vertex)> >
+{ };
+
+/**
+ * @note This specialization will match any function of the form E(V,V) and
+ * generates the graph over an adapted boost function. Note that functions of
+ * the form E(U,V) will match the empty class causing compiler errors.
+ *
+template <typename Edge, typename Vertex>
+struct fucntion_graph<Edge(Vertex, Vertex)>
+    : function_graph_base< function<Edge(Vertex, Vertex)> >
+{ };
 
 }   // graph namespace
 }   // boost namespace
 
-#endif /*FUNCTION_GRAPH_FUNCTOR_HPP_*/
+#endif /*FUNCTION_GRAPH_HPP_*/
+
