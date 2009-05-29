291a372f04de52d39f38ffd46f168a2ae8dff969
Michael Lopez <mlopez7@kent.edu>
2009-05-29 16:22:01 +0000

    commented small test file
    SVN-Revision: 53395

--- SOC/2009/function_graph/libs/test/test1.cpp
+++ SOC/2009/function_graph/libs/test/test1.cpp
@@ -1,3 +1,8 @@
+/**
+ * This file is intended test compilation only.
+ */
+
+
 #include <iostream>
 #include <boost/function.hpp>
 #include <functional>
@@ -5,16 +10,24 @@
 #include <vector>
 #include "function_graph.hpp"
 
+// test boost function
 struct int_less_than {
     bool operator()(int a, int b) { return a < b; }
 }; 
 
 int main()
 {
+    // create two boost functions
     boost::function2<bool,int,int> h = int_less_than();
     boost::function2<bool,int,int> g = int_less_than();
+
+    // test constructor
     boost::graph::function_graph<bool, int> implicitG1(h);
+
+    // set the edge function
     implicitG1.set_edge_function(g);
+
+    // test the edge function
     implicitG1.edge(1,2);
     
     
