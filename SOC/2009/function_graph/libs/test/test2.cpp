e86187d8c627e81f144c1c8f453581c30a3bd3ef
Michael Lopez <mlopez7@kent.edu>
2009-06-03 17:21:11 +0000

    Adapting edge(u,v,g) into function_graph
    SVN-Revision: 53604

--- SOC/2009/function_graph/libs/test/test2.cpp
+++ SOC/2009/function_graph/libs/test/test2.cpp
@@ -1,27 +1,21 @@
-/**
- * Testing a function that returns a boolean
- */
-
 #include <iostream>
 #include <boost/function.hpp>
 #include <functional>
 #include <algorithm>
 #include <vector>
-#include <utility>
 #include "function_graph.hpp"
 
 template <typename T>
 struct less_than {
     bool operator()(T a, T b) { return a < b; }
-};
+}; 
 
 int main()
 {
     ////////
     // Create a boost function and function graph.
     typedef boost::function<bool(int,int)> function_type;
-    typedef boost::function_graph<function_type> graph;
-    typedef graph::edge_descriptor edge_descriptor;
+    typedef boost::graph::function_graph<function_type> graph;
     function_type f = less_than<int>();
     function_type g = less_than<int>();
     graph funcGraph(f);
@@ -33,15 +27,8 @@ int main()
     ////////
     // Check the edge output.
     std::cout << "2 < 1 check ";
-    if(funcGraph.edge(2,1)) std::cout << "fails." << "\n";
-    else std::cout << "passes." << "\n";
-
-    ////////
-    // Check the adjacency matrix edge
-    std::pair<edge_descriptor, bool> edge_pair = boost::edge(1, 2, funcGraph);
-    std::cout << edge_pair.first << "\n";
-
-    
+    if(funcGraph.edge(2,1)) std::cout << "passes." << "\n";
+    else std::cout << "fails." << "\n";
     
     return 0;
 }
