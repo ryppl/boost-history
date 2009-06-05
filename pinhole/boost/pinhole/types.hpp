c2135010d04ca041337f33bdae4d1f0d5b6b981c
Jared McIntyre <jmcintyre@dfsoftware.com>
2009-06-05 00:51:55 +0000

    pinhole - commit missing file
    SVN-Revision: 53647

---
+++ pinhole/boost/pinhole/types.hpp
@@ -0,0 +1,32 @@
+// Pinhole category_iterator.hpp file
+//
+// Copyright Jared McIntyre 2009.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_PINHOLE_TYPES
+#define BOOST_PINHOLE_TYPES
+
+#include <set>
+#include <string>
+#include <list>
+#include <map>
+
+namespace boost { namespace pinhole
+{
+    class property_group;
+
+    namespace detail
+    {
+        struct property_info_base;
+        struct action_info;
+    } 
+
+    typedef std::set<std::string> category_collection;
+    typedef std::list<property_group*> children_collection;
+    typedef std::map<std::string, detail::property_info_base*> property_collection;
+    typedef std::map<std::string, detail::action_info*> action_collection;
+}}
+
+#endif // include guard
