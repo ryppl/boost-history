7068f0d500ab1ce016272f8981a48527ab582739
Dean Michael Berris <mikhailberis@gmail.com>
2009-06-10 08:03:38 +0000

    Changes: * Making the collection of hash function types a template parameter instead of a number; The requirement on the collection is that it should be a valid Fusion sequence. * Changing the signature and tests to support the changes to the static_bloom_filter and bloom_filter interfaces. * Refactoring the insert and contains implementation to a common internals base. * All static_bloom_filters are now fully equally comparable since the types will contain the hash functions in the signature too. * The default hash implementation has been changed to use Boost.Hash internally.
    SVN-Revision: 53784

--- bloom_filter/trunk/libs/bloom_filter/test/static_bloom_filter_equality_test.cpp
+++ bloom_filter/trunk/libs/bloom_filter/test/static_bloom_filter_equality_test.cpp
@@ -6,10 +6,10 @@
 #include <boost/bloom_filter.hpp>
 #include <boost/detail/lightweight_test.hpp>
 
-using boost::bloom_filter;
+using boost::static_bloom_filter;
 
 int main(int argc, char * arg[]) {
-    bloom_filter<int, 32, 3> filter1, filter2;
+    static_bloom_filter<int, 32> filter1, filter2;
     assert(filter1 == filter2);
     filter1.insert(1);
     assert(filter1 != filter2);
