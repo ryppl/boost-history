6035670c91b8056d0910d21ccd3b5201e315f76f
Christian Schladetsch <christian.schladetsch@gmail.com>
2009-06-12 08:14:00 +0000

    now using std::limits for max of size_t
    SVN-Revision: 53812

--- monotonic/boost/monotonic/storage_base.h
+++ monotonic/boost/monotonic/storage_base.h
@@ -5,7 +5,7 @@
 
 #pragma once
 
-#include <boost/cstdint.hpp>	
+#include <limits>
 #include <boost/monotonic/storage.h>
 
 namespace boost
@@ -29,7 +29,7 @@ namespace boost
 
 			size_t max_size() const
 			{
-				return INT_MAX;
+				return std::numeric_limits<std::size_t>::max();
 			}
 		};
 	}
