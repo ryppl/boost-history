2b5faac9eb3ddc3165494188596cb9ff56f23f8a
Christian Schladetsch <christian.schladetsch@gmail.com>
2009-07-04 09:37:15 +0000

    udpated
    SVN-Revision: 54637

---
+++ cloneable/boost/cloneable/cloneable.hpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2009 Christian Schladetsch
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying 
+//  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_CLONEABLE_CLONEABLE_HPP
+#define BOOST_CLONEABLE_CLONEABLE_HPP
+
+#include <boost/cloneable/base.hpp>
+
+#endif // BOOST_CLONEABLE_CLONEABLE_HPP
+
+//EOF
