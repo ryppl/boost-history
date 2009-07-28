bafcff3cf45f4a3ab7f22a5ce8b72c72d91d96fc
Frank Mori Hess <fmh6jj@gmail.com>
2009-07-28 13:50:41 +0000

    Replaced tabs with spaces.
    SVN-Revision: 55230

--- fmhess/boost/generic_ptr/monitor.hpp
+++ fmhess/boost/generic_ptr/monitor.hpp
@@ -47,98 +47,98 @@ namespace boost
       typedef monitor_unique_lock<monitor> moveable_monitor_lock;
 #endif // BOOST_NO_RVALUE_REFERENCES
 
-		public:
-			typedef typename pointer_traits<T>::value_type value_type;
-			typedef T pointer;
-			typedef typename pointer_traits<T>::reference reference;
-			typedef Mutex mutex_type;
+    public:
+      typedef typename pointer_traits<T>::value_type value_type;
+      typedef T pointer;
+      typedef typename pointer_traits<T>::reference reference;
+      typedef Mutex mutex_type;
 
-			template<typename ValueType>
-			struct rebind
-			{
-				typedef monitor<typename generic_ptr::rebind<pointer, ValueType>::other, Mutex > other;
-			};
+      template<typename ValueType>
+      struct rebind
+      {
+        typedef monitor<typename generic_ptr::rebind<pointer, ValueType>::other, Mutex > other;
+      };
 
-			monitor(): px(), _mutex_p()
-			{}
-			template<typename U>
-			monitor( U p, const shared<Mutex*> & mutex_p = shared<Mutex*>(new Mutex()) ): px( p ), _mutex_p(mutex_p)
-			{}
-			template<typename U>
-			monitor(const monitor<U, Mutex> & other): px(other.px), _mutex_p(other._mutex_p)
-			{}
+      monitor(): px(), _mutex_p()
+      {}
+      template<typename U>
+      monitor( U p, const shared<Mutex*> & mutex_p = shared<Mutex*>(new Mutex()) ): px( p ), _mutex_p(mutex_p)
+      {}
+      template<typename U>
+      monitor(const monitor<U, Mutex> & other): px(other.px), _mutex_p(other._mutex_p)
+      {}
 #ifndef BOOST_NO_RVALUE_REFERENCES
-			monitor(monitor && other): px(std::move(other.px)), _mutex_p(std::move(other._mutex_p)
-			{}
-			template<typename U>
-			monitor(monitor<U> && other): px(std::move(other.px)), _mutex_p(std::move(other._mutex_p)
-			{}
+      monitor(monitor && other): px(std::move(other.px)), _mutex_p(std::move(other._mutex_p)
+      {}
+      template<typename U>
+      monitor(monitor<U> && other): px(std::move(other.px)), _mutex_p(std::move(other._mutex_p)
+      {}
 #endif
 
-			void swap(monitor & other)
-			{
-				boost::swap(px, other.px);
-				boost::swap(_mutex_p, other._mutex_p);
-			}
+      void swap(monitor & other)
+      {
+        boost::swap(px, other.px);
+        boost::swap(_mutex_p, other._mutex_p);
+      }
 
-			monitor & operator=(const monitor & other)
-			{
-				monitor(other).swap(*this);
-				return *this;
-			}
+      monitor & operator=(const monitor & other)
+      {
+        monitor(other).swap(*this);
+        return *this;
+      }
 
-			template<typename U>
-			monitor & operator=(const monitor<U, mutex_type> & other)
-			{
-				monitor(other).swap(*this);
-				return *this;
-			}
+      template<typename U>
+      monitor & operator=(const monitor<U, mutex_type> & other)
+      {
+        monitor(other).swap(*this);
+        return *this;
+      }
 #ifndef BOOST_NO_RVALUE_REFERENCES
-			monitor & operator=(monitor && other)
-			{
-				monitor(std::move(other)).swap(*this);
-				return *this;
-			}
-			template<typename U>
-			monitor & operator=(monitor<U, mutex_type> && other)
-			{
-				monitor(std::move(other)).swap(*this);
-				return *this;
-			}
+      monitor & operator=(monitor && other)
+      {
+        monitor(std::move(other)).swap(*this);
+        return *this;
+      }
+      template<typename U>
+      monitor & operator=(monitor<U, mutex_type> && other)
+      {
+        monitor(std::move(other)).swap(*this);
+        return *this;
+      }
 #endif
-			void reset()
-			{
-				monitor().swap(*this);
-			}
-			template<typename U> void reset(U object_p, const shared<mutex_type*> &mutex_p)
-			{
-				monitor(object_p, mutex_p).swap(*this);
-			}
+      void reset()
+      {
+        monitor().swap(*this);
+      }
+      template<typename U> void reset(U object_p, const shared<mutex_type*> &mutex_p)
+      {
+        monitor(object_p, mutex_p).swap(*this);
+      }
 
-			pointer get() const {return px;}
-			shared<mutex_type*> get_shared_mutex() const {return _mutex_p;}
+      pointer get() const {return px;}
+      shared<mutex_type*> get_shared_mutex() const {return _mutex_p;}
       mutex_type& get_mutex_ref() const {return *_mutex_p;}
 
 // implicit conversion to "bool"
 #include <boost/generic_ptr/detail/operator_bool.hpp>
 
-			moveable_monitor_lock operator->() const
-			{
-				return moveable_monitor_lock(px, *_mutex_p);
-			}
+      moveable_monitor_lock operator->() const
+      {
+        return moveable_monitor_lock(px, *_mutex_p);
+      }
 
-		private:
-			pointer px;
-			shared<mutex_type*> _mutex_p;
-		};
+    private:
+      pointer px;
+      shared<mutex_type*> _mutex_p;
+    };
 
-		template<typename T, typename Mutex>
-		T get_pointer(const monitor<T, Mutex> &mp)
-		{
-			return mp.get();
-		}
+    template<typename T, typename Mutex>
+    T get_pointer(const monitor<T, Mutex> &mp)
+    {
+      return mp.get();
+    }
 //TODO: casts, comparisons
-	} // namespace generic_ptr
+  } // namespace generic_ptr
 } // namespace boost
 
 #endif  // #ifndef BOOST_GENERIC_PTR_MONITOR_HPP_INCLUDED
--- fmhess/boost/generic_ptr/pointer_traits.hpp
+++ fmhess/boost/generic_ptr/pointer_traits.hpp
@@ -18,96 +18,96 @@
 
 namespace boost
 {
-	namespace generic_ptr
-	{
-		template<typename T> struct pointer_traits
-		{
-			typedef typename T::value_type value_type;
-			typedef typename T::pointer pointer;
-			typedef typename T::reference reference;
-		};
+  namespace generic_ptr
+  {
+    template<typename T> struct pointer_traits
+    {
+      typedef typename T::value_type value_type;
+      typedef typename T::pointer pointer;
+      typedef typename T::reference reference;
+    };
 
-		template<typename T> struct pointer_traits<T*>
-		{
-			typedef T value_type;
-			typedef T * pointer;
-			typedef T & reference;
-		};
+    template<typename T> struct pointer_traits<T*>
+    {
+      typedef T value_type;
+      typedef T * pointer;
+      typedef T & reference;
+    };
 
-		template<> struct pointer_traits<void*>
-		{
-			typedef void value_type;
-			typedef void * pointer;
-			typedef void reference;
-		};
+    template<> struct pointer_traits<void*>
+    {
+      typedef void value_type;
+      typedef void * pointer;
+      typedef void reference;
+    };
 
-		template<> struct pointer_traits<const void*>
-		{
-			typedef void value_type;
-			typedef const void * pointer;
-			typedef void reference;
-		};
+    template<> struct pointer_traits<const void*>
+    {
+      typedef void value_type;
+      typedef const void * pointer;
+      typedef void reference;
+    };
 
-		template<> struct pointer_traits<volatile void*>
-		{
-			typedef void value_type;
-			typedef volatile void * pointer;
-			typedef void reference;
-		};
+    template<> struct pointer_traits<volatile void*>
+    {
+      typedef void value_type;
+      typedef volatile void * pointer;
+      typedef void reference;
+    };
 
-		template<> struct pointer_traits<const volatile void*>
-		{
-			typedef void value_type;
-			typedef const volatile void * pointer;
-			typedef void reference;
-		};
+    template<> struct pointer_traits<const volatile void*>
+    {
+      typedef void value_type;
+      typedef const volatile void * pointer;
+      typedef void reference;
+    };
 
-		template<typename T>
-		T * get_plain_old_pointer(T * p)
-		{
-			return p;
-		}
+    template<typename T>
+    T * get_plain_old_pointer(T * p)
+    {
+      return p;
+    }
 
-		template<typename GenericPtr>
-		typename pointer_traits<GenericPtr>::value_type *
-				get_plain_old_pointer(GenericPtr gp)
-		{
-			using boost::get_pointer;
-			return get_plain_old_pointer(get_pointer(gp));
-		}
+    template<typename GenericPtr>
+    typename pointer_traits<GenericPtr>::value_type *
+        get_plain_old_pointer(GenericPtr gp)
+    {
+      using boost::get_pointer;
+      return get_plain_old_pointer(get_pointer(gp));
+    }
 
-		// two-argument cast overloads for raw pointers (really belongs in boost/pointer_cast.hpp)
-		template<typename T, typename U>
-		T* static_pointer_cast(U *r, boost::mpl::identity<T>)
-		{
-			return static_cast<T*>(r);
-		}
+    // two-argument cast overloads for raw pointers (really belongs in boost/pointer_cast.hpp)
+    template<typename T, typename U>
+    T* static_pointer_cast(U *r, boost::mpl::identity<T>)
+    {
+      return static_cast<T*>(r);
+    }
 
-		template<typename T, typename U>
-		T* const_pointer_cast(U *r, boost::mpl::identity<T>)
-		{
-			return const_cast<T*>(r);
-		}
+    template<typename T, typename U>
+    T* const_pointer_cast(U *r, boost::mpl::identity<T>)
+    {
+      return const_cast<T*>(r);
+    }
 
-		template<typename T, typename U>
-		T* dynamic_pointer_cast(U *r, boost::mpl::identity<T>)
-		{
-			return dynamic_cast<T*>(r);
-		}
+    template<typename T, typename U>
+    T* dynamic_pointer_cast(U *r, boost::mpl::identity<T>)
+    {
+      return dynamic_cast<T*>(r);
+    }
 
-		template<typename GenericPtr, typename ValueType>
-		struct rebind
-		{
-			typedef typename GenericPtr::template rebind<ValueType>::other other;
-		};
+    template<typename GenericPtr, typename ValueType>
+    struct rebind
+    {
+      typedef typename GenericPtr::template rebind<ValueType>::other other;
+    };
 
-		template<typename T, typename ValueType>
-		struct rebind<T*, ValueType>
-		{
-			typedef ValueType * other;
-		};
+    template<typename T, typename ValueType>
+    struct rebind<T*, ValueType>
+    {
+      typedef ValueType * other;
+    };
 
-	} // namespace generic_ptr
+  } // namespace generic_ptr
 } // namespace boost
 
 #endif  // #ifndef BOOST_GENERIC_PTR_POINTER_TRAITS_HPP_INCLUDED
