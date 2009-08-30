6bff2db81aed8c66c074c22be4ca02413d988309
Erwann Rogard <erwann.rogard@gmail.com>
2009-08-30 21:58:32 +0000

    m
    SVN-Revision: 55889

--- statistics/tree/libs/tree/example/tree.h
+++ statistics/tree/libs/tree/example/tree.h
@@ -5,8 +5,8 @@
 //  Software License, Version 1.0. (See accompanying file                    //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)         //
 ///////////////////////////////////////////////////////////////////////////////
-#ifndef LIBS_TREE_EXAMPLE_TREE_HPP_ER_2009
-#define LIBS_TREE_EXAMPLE_TREE_HPP_ER_2009
+#ifndef LIBS_TREE_VIEW_EXAMPLE_TREE_HPP_ER_2009
+#define LIBS_TREE_VIEW_EXAMPLE_TREE_HPP_ER_2009
 #include <ostream>
 
 void example_tree(std::ostream&);
