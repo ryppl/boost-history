4f7944f7899346b4c7b3bbc38dcc0d829492634e
Erwann Rogard <erwann.rogard@gmail.com>
2009-09-26 22:46:46 +0000

    m
    SVN-Revision: 56393

--- statistics/distribution_toolkit/boost/statistics/detail/distribution_toolkit/include.hpp
+++ statistics/distribution_toolkit/boost/statistics/detail/distribution_toolkit/include.hpp
@@ -9,11 +9,15 @@
 #ifndef BOOST_STATISTICS_DETAIL_DISTRIBUTION_TOOLKIT_INCLUDE_HPP_ER_2009
 #define BOOST_STATISTICS_DETAIL_DISTRIBUTION_TOOLKIT_INCLUDE_HPP_ER_2009
 
-#include <boost/statistics/detail/distribution_toolkit/unscope/include.hpp>
-#include <boost/statistics/detail/distribution_toolkit/map_pdf/include.hpp>
+#include <boost/statistics/detail/distribution_toolkit/concept/include.hpp>
+#include <boost/statistics/detail/distribution_toolkit/data/include.hpp>
 #include <boost/statistics/detail/distribution_toolkit/distributions/include.hpp>
 #include <boost/statistics/detail/distribution_toolkit/iterator/include.hpp>
 #include <boost/statistics/detail/distribution_toolkit/fun_wrap/include.hpp>
+#include <boost/statistics/detail/distribution_toolkit/fwd_math/include.hpp>
+#include <boost/statistics/detail/distribution_toolkit/map_pdf/include.hpp>
 #include <boost/statistics/detail/distribution_toolkit/meta/include.hpp>
+#include <boost/statistics/detail/distribution_toolkit/random/include.hpp>
+#include <boost/statistics/detail/distribution_toolkit/unscope/include.hpp>
 
 #endif
