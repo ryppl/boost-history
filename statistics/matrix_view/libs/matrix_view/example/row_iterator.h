4b6c97d32b0223dae6824f4e9ecb63673ce5c79f
Erwann Rogard <erwann.rogard@gmail.com>
2009-09-27 00:10:16 +0000

    m
    SVN-Revision: 56406

--- statistics/matrix_view/libs/matrix_view/example/row_iterator.h
+++ statistics/matrix_view/libs/matrix_view/example/row_iterator.h
@@ -1,12 +1,12 @@
 ///////////////////////////////////////////////////////////////////////////////
-// matrix_view::example::row_iterator.h                                      //
+// view::example::row_iterator.h                                      //
 //                                                                           //
 //  Copyright 2009 Erwann Rogard. Distributed under the Boost                //
 //  Software License, Version 1.0. (See accompanying file                    //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)         //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef LIBS_MATRIX_VIEW_EXAMPLE_ROW_ITERATOR_H_ER_2009
-#define LIBS_MATRIX_VIEW_EXAMPLE_ROW_ITERATOR_H_ER_2009
+#ifndef LIBS_VIEW_EXAMPLE_ROW_ITERATOR_H_ER_2009
+#define LIBS_VIEW_EXAMPLE_ROW_ITERATOR_H_ER_2009
 #include <ostream>
 
 void example_row_iterator(std::ostream&);
