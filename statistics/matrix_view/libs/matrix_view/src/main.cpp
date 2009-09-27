07f3bbad6b3209d948d261d2e226bd363845860b
Erwann Rogard <erwann.rogard@gmail.com>
2009-09-27 00:14:59 +0000

    m
    SVN-Revision: 56410

--- statistics/matrix_view/libs/matrix_view/src/main.cpp
+++ statistics/matrix_view/libs/matrix_view/src/main.cpp
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// matrix_view::src::main.cpp                                                //  
+// view::src::main.cpp                                                //  
 //                                                                           //
 //  (C) Copyright 2009 Erwann Rogard                                         //
 //  Use, modification and distribution are subject to the                    //
@@ -7,13 +7,19 @@
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)         //
 ///////////////////////////////////////////////////////////////////////////////
 #include <iostream>
-#include <libs/matrix_view/example/row_iterator.h>
-#include <libs/matrix_view/example/transform_column.h>
+//#include <libs/matrix_view/example/row_iterator.h>
+//#include <libs/matrix_view/example/transform_column.h>
+#include <libs/matrix_view/example/step_iterator.h>
+#include <libs/matrix_view/example/columns_iterator.h>
+#include <libs/matrix_view/example/rows_iterator.h>
 
 int main(){
 
-    example_row_iterator(std::cout);
-    example_transform_column(std::cout);
+    //example_row_iterator(std::cout);
+    //example_transform_column(std::cout);
+    //example_step_iterator(std::cout);
+    //example_columns_iterator(std::cout);
+    example_rows_iterator(std::cout);
 
     return 0;
 }
