2a0a9857a31bc6d508dca1908b18727a8df76abc
Erwann Rogard <erwann.rogard@gmail.com>
2009-09-27 02:21:49 +0000

    a
    SVN-Revision: 56431

---
+++ statistics/distribution_toolkit/boost/statistics/detail/distribution_toolkit/data/include.hpp
@@ -0,0 +1,16 @@
+//////////////////////////////////////////////////////////////////////////////
+// distribution_toolkit::data::include.hpp                                  //
+//                                                                          //
+//  (C) Copyright 2009 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_STATISTICS_DETAIL_DISTRIBUTION_TOOLKIT_DATA_INCLUDE_HPP_ER_2009
+#define BOOST_STATISTICS_DETAIL_DISTRIBUTION_TOOLKIT_DATA_INCLUDE_HPP_ER_2009
+
+#include <boost/statistics/detail/distribution_toolkit/data/sample_cdf.hpp>
+#include <boost/statistics/detail/distribution_toolkit/data/generate_n_sample_cdf.hpp>
+#include <boost/statistics/detail/distribution_toolkit/data/generate2_n_sample_cdf.hpp>
+
+#endif
