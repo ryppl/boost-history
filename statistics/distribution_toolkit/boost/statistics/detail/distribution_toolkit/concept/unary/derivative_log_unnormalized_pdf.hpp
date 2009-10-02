1355c1d25740fb32be9495793eb4b800da71ac7f
Erwann Rogard <erwann.rogard@gmail.com>
2009-10-02 17:50:24 +0000

    m
    SVN-Revision: 56515

--- statistics/distribution_toolkit/boost/statistics/detail/distribution_toolkit/concept/unary/derivative_log_unnormalized_pdf.hpp
+++ statistics/distribution_toolkit/boost/statistics/detail/distribution_toolkit/concept/unary/derivative_log_unnormalized_pdf.hpp
@@ -1,16 +1,16 @@
 //////////////////////////////////////////////////////////////////////////////////////////////
-// distribution_toolkit::distributions::concept::unary::derivative_log_unnormalized_pdf.hpp //
+// distribution_toolkit::distributions::distribution::common::concept::unary::derivative_log_unnormalized_pdf.hpp //
 //                                                                                          //
 //  (C) Copyright 2009 Erwann Rogard                                                        //
 //  Use, modification and distribution are subject to the                                   //
 //  Boost Software License, Version 1.0. (See accompanying file                             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)                        //
 //////////////////////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_DISTRIBUTION_TOOLKIT_DISTRIBUTIONS_CONCEPT_UNARY_DERIVATIVE_LOG_UNNORMALIZED_PDF_HPP_ER_2009
-#define BOOST_DISTRIBUTION_TOOLKIT_DISTRIBUTIONS_CONCEPT_UNARY_DERIVATIVE_LOG_UNNORMALIZED_PDF_HPP_ER_2009
+#ifndef BOOST_COMMON_DISTRIBUTION_CONCEPT_UNARY_DERIVATIVE_LOG_UNNORMALIZED_PDF_HPP_ER_2009
+#define BOOST_COMMON_DISTRIBUTION_CONCEPT_UNARY_DERIVATIVE_LOG_UNNORMALIZED_PDF_HPP_ER_2009
 #include <boost/concept_check.hpp>
-#include <boost/statistics/detail/distribution_toolkit/meta/value.hpp>
-
+#include <boost/statistics/detail/distribution_common/meta/value.hpp>
+/*
 namespace boost{
 namespace statistics{
 namespace detail{
@@ -26,7 +26,7 @@ namespace concept{
         }
 
         private:
-        typedef typename meta::value<D>::type value_;
+        typedef typename distribution::common::meta::value<D>::type value_;
         D dist;
         value_ val;
         value_ x;
@@ -38,5 +38,5 @@ namespace concept{
 }// statistics
 }// detail
 }// boost
-
+*/
 #endif
