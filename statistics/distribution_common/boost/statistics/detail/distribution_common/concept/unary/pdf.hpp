d58b6e742021f0ce60898f260aba64c2d9b34d20
Erwann Rogard <erwann.rogard@gmail.com>
2009-10-02 17:46:16 +0000

    a
    SVN-Revision: 56513

---
+++ statistics/distribution_common/boost/statistics/detail/distribution_common/concept/unary/pdf.hpp
@@ -0,0 +1,48 @@
+//////////////////////////////////////////////////////////////////////////////////
+// distribution::common::concept::concept::unary::pdf.hpp                       //
+//                                                                              //
+//  (C) Copyright 2009 Erwann Rogard                                            //
+//  Use, modification and distribution are subject to the                       //
+//  Boost Software License, Version 1.0. (See accompanying file                 //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)            //
+//////////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_STATISTICS_DETAIL_DISTRIBUTION_COMMON_CONCEPT_UNARY_PDF_HPP_ER_2009
+#define BOOST_STATISTICS_DETAIL_DISTRIBUTION_COMMON_CONCEPT_UNARY_PDF_HPP_ER_2009
+#include <boost/concept_check.hpp>
+
+namespace boost{
+namespace statistics{
+namespace detail{
+namespace distribution{
+namespace common{
+namespace concept{
+
+    template<typename B>
+    class Pdf : public B{
+        typedef typename B::distribution_type   d_;
+        typedef typename B::variable_type       x_;
+        typedef typename B::probability_type    p_;
+
+        public:
+    
+        BOOST_CONCEPT_USAGE(Pdf){
+            p =  boost::statistics::detail::pdf(d,x);
+        }
+
+        private:
+        typedef typename meta::value<d_>::type value_;
+        d_ d;
+        p_ p;
+        x_ x;
+        
+    };
+
+}// concept
+}// common
+}// distribution
+
+}// statistics
+}// detail
+}// boost
+
+#endif
