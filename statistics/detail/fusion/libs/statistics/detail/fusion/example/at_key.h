ef384cd4684f993c47f63a0dc6d3666ca50ce9ce
Erwann Rogard <erwann.rogard@gmail.com>
2009-10-16 04:13:45 +0000

    a
    SVN-Revision: 56886

---
+++ statistics/detail/fusion/libs/statistics/detail/fusion/example/at_key.h
@@ -0,0 +1,14 @@
+///////////////////////////////////////////////////////////////////////////////
+// statistics::detail::fusion::example::at_key_iterator.h                    //
+//                                                                           //
+//  Copyright 2009 Erwann Rogard. Distributed under the Boost                //
+//  Software License, Version 1.0. (See accompanying file                    //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)         //
+/////////////////////////////////////////////////////////////////////////////// 
+#ifndef LIBS_STATISTICS_DETAIL_FUSION_EXAMPLE_AT_KEY_ITERATOR_HPP_ER_2009
+#define LIBS_STATISTICS_DETAIL_FUSION_EXAMPLE_AT_KEY_ITERATOR_HPP_ER_2009
+#include <iostream>
+
+void example_at_key_iterator(std::ostream&);
+
+#endif
