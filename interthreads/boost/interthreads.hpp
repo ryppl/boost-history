d642f3872fb02f798173da848dc19d59764b1fdb
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2009-10-23 18:17:30 +0000

    TBoost.Interthreads: Add boost/interthreads.hpp file
    SVN-Revision: 57113

---
+++ interthreads/boost/interthreads.hpp
@@ -0,0 +1,33 @@
+#ifndef BOOST_INTERTHREADS__HPP
+#define BOOST_INTERTHREADS__HPP
+
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Roland Schwarz 2006.
+// (C) Copyright Vicente J. Botet Escriba 2008-2009.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// Extension of the init class of the threadalert library of Roland Schwarz
+//
+// See http://www.boost.org/libs/interthreads for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <boost/interthreads/basic_thread_decorator.hpp>
+#include <boost/interthreads/set_once.hpp>
+//#include <boost/interthreads/thread_algo.hpp>
+#include <boost/interthreads/thread_and_join.hpp>
+#include <boost/interthreads/thread_decoration.hpp>
+#include <boost/interthreads/thread_decorator.hpp>
+//#include <boost/interthreads/thread_group_once.hpp>
+#include <boost/interthreads/thread_keep_alive.hpp>
+#include <boost/interthreads/thread_specific_shared_ptr.hpp>
+#include <boost/interthreads/thread_tuple.hpp>
+#include <boost/interthreads/thread_tuple_once.hpp>
+#include <boost/interthreads/threader_decorator.hpp>
+#include <boost/interthreads/wait_thread_decorator.hpp>
+
+#endif
+
