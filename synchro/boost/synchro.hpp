b6c0e143f29bc87c7297b6703ceae76133695f2a
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2009-10-23 18:18:34 +0000

    TBoost.Synchro: Add boost/synchro.hpp file
    SVN-Revision: 57114

---
+++ synchro/boost/synchro.hpp
@@ -0,0 +1,50 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2008-2009. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/synchro for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_SYNCHRO__HPP
+#define BOOST_SYNCHRO__HPP
+
+#include <boost/synchro/condition_backdoor.hpp>
+#include <boost/synchro/condition_lockable.hpp>
+#include <boost/synchro/condition_safe.hpp>
+#include <boost/synchro/is_lockable.hpp>
+#include <boost/synchro/lockable_adapter.hpp>
+#include <boost/synchro/lockable_concepts.hpp>
+#include <boost/synchro/lockable_scope_traits.hpp>
+#include <boost/synchro/lockable_traits.hpp>
+#include <boost/synchro/locker_concepts.hpp>
+#include <boost/synchro/locker_options.hpp>
+#include <boost/synchro/monitor.hpp>
+#include <boost/synchro/null_condition.hpp>
+#include <boost/synchro/null_mutex.hpp>
+#include <boost/synchro/null_synchronization_family.hpp>
+#include <boost/synchro/process_synchronization_family.hpp>
+#include <boost/synchro/semaphore.hpp>
+#include <boost/synchro/synchronization_family.hpp>
+#include <boost/synchro/synchronized.hpp>
+#include <boost/synchro/syntactic_lock_traits.hpp>
+#include <boost/synchro/thread_synchronization_family.hpp>
+#include <boost/synchro/timeout_exception.hpp>
+
+//#include <boost/synchro/dsl.hpp>
+//#include <boost/synchro/iccl.hpp>
+//#include <boost/synchro/poly.hpp>
+//#include <boost/synchro/lockable.hpp>
+//#include <boost/synchro/lockables.hpp>
+//#include <boost/synchro/lockers.hpp>
+//#include <boost/synchro/poly.hpp>
+//#include <boost/synchro/process.hpp>
+//#include <boost/synchro/queues.hpp>
+//#include <boost/synchro/thread.hpp>
+
+
+}
+}
+#endif
