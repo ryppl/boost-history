468221c06fdd7669cc250af9f772faa240ec74a3
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2009-11-03 15:56:44 +0000

    TBoost.STM vbe: Added boost/synchro.hpp file
    SVN-Revision: 57316

---
+++ stm/branches/vbe/boost/synchro.hpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Justin E. Gottchlich 2009.
+// (C) Copyright Vicente J. Botet Escriba 2009.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/stm for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_SYNCHO__HPP
+#define BOOST_SYNCHO__HPP
+
+//-----------------------------------------------------------------------------
+#include <boost/synchro/lockable.hpp>
+#include <boost/synchro/locks.hpp>
+#include <boost/synchro/pthread/mutex.hpp>
+#include <boost/synchro/thread/mutex.hpp>
+#include <boost/synchro/tss.hpp>
+//-----------------------------------------------------------------------------
+
+#endif // BOOST_SYNCHO__HPP
