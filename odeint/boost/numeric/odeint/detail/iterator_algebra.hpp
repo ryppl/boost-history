4e6848d44aa313c9b714f8aaab706ca0053daafa
Karsten Ahnert <karsten.ahnert@gmx.de>
2009-11-05 16:56:06 +0000

    cosmetics in rk4
    SVN-Revision: 57410

--- odeint/boost/numeric/odeint/detail/iterator_algebra.hpp
+++ odeint/boost/numeric/odeint/detail/iterator_algebra.hpp
@@ -4,7 +4,7 @@
  Copyright 2009 Mario Mulansky
  Copyright 2009 Andre Bergner
  
- Some accumulators for odeint
+ Some algebraic operations for iterators
 
  Distributed under the Boost Software License, Version 1.0.
  (See accompanying file LICENSE_1_0.txt or
@@ -20,31 +20,85 @@ namespace boost {
 namespace numeric {
 namespace odeint {
 namespace detail {
+namespace it_algebra { // iterator algebra
 
-    template< class InputIterator ,
-	      class OutputIterator ,
+    /* computes x_n += alpha * y_n for all n
+       where x and y are given by the iterators x_start, x_end, y_start
+       make sure that x and y have the same range
+     */
+    template< class Iterator1 ,
+	      class Iterator2 ,
 	      class T >
-    void multiply_and_add( InputIterator first1 ,
-			   InputIterator last1 ,
-			   OutputIterator first2 ,
-			   T dt )
+    void scale_and_add( Iterator1 x_start ,
+			Iterator1 x_end ,
+			Iterator2 y_start ,
+			T alpha )
     {
-	while( first1 != last1 )
-	    (*first1++) += dt * (*first2++);
+	while( x_start != x_end )
+	    (*x_start++) += alpha * (*y_start++);
     }
 
+
+    /* computes out_n = x_n - y_n for all n
+       where x,y, and out are given as iterators
+       make sure that x, y and out have the same range
+    */
     template< class InputIterator1 ,
 	      class InputIterator2 ,
 	      class OutputIterator >
-    void substract_and_assign( InputIterator1 first1 ,
-			       InputIterator1 last1 ,
-			       InputIterator2 first2 ,
-			       OutputIterator first3 )
+    void substract_and_assign( InputIterator1 x_start ,
+			       InputIterator1 x_end ,
+			       InputIterator2 y_start ,
+			       OutputIterator out_start )
     {
-	while( first1 != last1 )
-	    ( *first3++ ) = ( *first1++ ) - ( *first2++ );
+	while( x_start != x_end )
+	    ( *out_start++ ) = ( *x_start++ ) - ( *y_start++ );
     }
 
+
+    /* computes out_n = x_n + alpha * y_n for all n
+       where x,y and out are given as iterators.
+       make sure, that x,y and out have the same range
+    */
+    template< class InputIterator1 ,
+	      class InputIterator2 ,
+	      class OutputIterator ,
+	      class T >
+    void scale_and_add_and_assign( InputIterator1 x_start ,
+				   InputIterator1 x_end ,
+				   InputIterator2 y_start ,
+				   OutputIterator out_start ,
+				   T alpha )
+    {
+	while( x_start != x_end )
+	    ( *out_start++) = ( *x_start++ ) + alpha * ( *y_start++ );
+    }
+
+
+
+
+    /* computes out_n = alpha2* ( x_n + y_n + alpha1*z_n )for all n
+       where x,y,z and out are given as iterators.
+       make sure, that x,y,z and out have the same range
+    */
+    template< class InputIterator1 ,
+	      class InputIterator2 ,
+	      class InputIterator3 ,
+	      class OutputIterator ,
+	      class T >
+    void scale_and_add_and_add_and_assign( InputIterator1 x_start ,
+					   InputIterator1 x_end ,
+					   InputIterator2 y_start ,
+					   InputIterator3 z_start ,
+					   OutputIterator out_start ,
+					   T alpha1 ,
+					   T alpha2 )
+    {
+	while( x_start != x_end )
+	    (*out_start++) += alpha2 * ( (*x_start++) + (*y_start++) + alpha1*(*z_start++) );
+    }
+
+}
 }
 }
 }
