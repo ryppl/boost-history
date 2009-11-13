3edd202c5c83dbacecc2af6cfde3aaea237f7ffa
Erwann Rogard <erwann.rogard@gmail.com>
2009-11-13 02:53:28 +0000

    m
    SVN-Revision: 57623

--- statistics/detail/fusion/boost/statistics/detail/fusion/map/serialization/saver.hpp
+++ statistics/detail/fusion/boost/statistics/detail/fusion/map/serialization/saver.hpp
@@ -1,10 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////
 // detail::fusion::map::serialization::saver.hpp                            //
 //                                                                          //
-//  (C) Copyright 2009 Erwann Rogard                                        //
-//  Use, modification and distribution are subject to the                   //
-//  Boost Software License, Version 1.0. (See accompanying file             //
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
 #ifndef BOOST_STATISTICS_DETAIL_FUSION_MAP_SERIALIZATION_SAVER_HPP_ER_2009
 #define BOOST_STATISTICS_DETAIL_FUSION_MAP_SERIALIZATION_SAVER_HPP_ER_2009
