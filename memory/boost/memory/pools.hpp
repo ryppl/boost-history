a3717ed51765c14177b9a51c65bc8805a41ff1f0
xushiwei <xushiweizh@gmail.com>
2009-11-15 07:43:14 +0000

    gcc log2
    SVN-Revision: 57669

--- memory/boost/memory/pools.hpp
+++ memory/boost/memory/pools.hpp
@@ -47,7 +47,14 @@ __forceinline unsigned int log2(IN unsigned int val) {
 #pragma warning(pop)
 #endif
 
-#else // if defined(__GNUG__) || defined(__GNUC__) // g++/gcc
+#else if defined(__GNUG__) || defined(__GNUC__) // g++/gcc
+
+__forceinline unsigned int log2(IN unsigned int val) {
+	BOOST_MEMORY_ASSERT(val != 0);
+	asm(bsr $val, %eax);
+}
+
+#else
 
 #error "Error: to do - unsupport compiler!"
 
