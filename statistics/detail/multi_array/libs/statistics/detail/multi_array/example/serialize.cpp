42c8ae85388fbd0ab0adfab769bcc5f0457a79a9
Erwann Rogard <erwann.rogard@gmail.com>
2009-11-28 23:05:34 +0000

    m
    SVN-Revision: 58023

--- statistics/detail/multi_array/libs/statistics/detail/multi_array/example/serialize.cpp
+++ statistics/detail/multi_array/libs/statistics/detail/multi_array/example/serialize.cpp
@@ -9,8 +9,12 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <boost/mpl/int.hpp>
+#include <boost/fusion/include/make_map.hpp>
+#include <boost/fusion/include/at_key.hpp>
 #include <boost/multi_array.hpp>
 #include <boost/statistics/detail/multi_array/serialization/serialize.hpp>
+#include <boost/statistics/detail/fusion/serialization/include.hpp>
 #include <boost/serialization/serialization.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
@@ -29,13 +33,16 @@ void example_multi_array_serialize(std::ostream& os)
     typedef std::ifstream ifs_;
 	typedef boost::archive::text_oarchive oa_;
 	typedef boost::archive::text_iarchive ia_;
-    typedef double val_;
+    typedef unsigned val_;
     typedef std::vector<val_> vec_;
+    typedef boost::mpl::int_<0> tag_;
+    typedef boost::fusion::result_of::make_map<tag_,val_>::type map_;
 	typedef boost::multi_array<val_, 2> array_;
 	typedef boost::multi_array<vec_,2> array_vec_;
+    typedef boost::multi_array<map_,2> array_map_;
     typedef boost::array<ma::multi_array_base::index,2> extents_;
-    unsigned n0 = 2;
-    unsigned n1 = 2;
+    const unsigned n0 = 2;
+    const unsigned n1 = 2;
 	{
     	const str_ path = "./multi_array_serialization";
     	{
@@ -107,6 +114,44 @@ void example_multi_array_serialize(std::ostream& os)
 					BOOST_ASSERT(a[i][j][0] == v0); 
 					BOOST_ASSERT(a[i][j][1] == v1); 
 					BOOST_ASSERT(a[i][j][2] == v2); 
+        		}
+    		}
+    	}
+	}
+	{
+    	const str_ path = "./multi_array_serialization3";
+    	{
+
+        
+    		ofs_ ofs(path.c_str());
+	    	oa_ oa(ofs);
+    		extents_ extents = {{ n0, n1 }};
+			array_map_ a(extents);
+			for(unsigned i = 0; i<n0; i++)
+    		{
+    			for(unsigned j = 0; j<n1; j++)
+        		{
+					map_ map = boost::fusion::make_map<tag_>(i * n1 + j);
+					a[i][j] = map; 
+        		}
+    		}
+        	oa << a;
+    	}
+    	{
+    		ifs_ ifs(path.c_str());
+    		ia_ ia(ifs);
+    		array_map_ a;
+        	ia >> a;
+			for(unsigned i = 0; i<n0; i++)
+    		{
+    			for(unsigned j = 0; j<n1; j++)
+        		{
+                	map_ map =  a[i][j]; 
+                	val_ val = boost::fusion::at_key<tag_>(map);
+					BOOST_ASSERT(
+                    	val == i * n1 + j
+                    ); 
+                	os  << val << ' ';
         		}
     		}
     	}
