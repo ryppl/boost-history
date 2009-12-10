b55bd79a5e10eef08782888cee1f496aaeb64c3d
Daniel James <daniel@calamity.org.uk>
2009-12-10 22:49:33 +0000

    An example of how a proposed boost library might be structured.
    SVN-Revision: 58278

---
+++ example/boost/example/example.hpp
@@ -0,0 +1,22 @@
+
+// Copyright (C) 2008-2009 John Doe
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+//  See http://www.boost.org/libs/unordered for documentation
+
+namespace boost {
+    /**
+     * Example class.
+     */
+
+    template <class T>
+    class example {
+    public:
+        void foo();
+    };
+
+    template <class T>
+    void example<T>::foo() {
+    }
+}
