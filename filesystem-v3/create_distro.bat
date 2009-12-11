a4ee35c48cec066f2be6a3c2b9b987025b03cf1e
Beman Dawes <bdawes@acm.org>
2009-12-11 21:53:38 +0000

    Initial commit
    SVN-Revision: 58303

---
+++ filesystem-v3/create_distro.bat
@@ -0,0 +1,31 @@
+rem  Create Boost Filesystem Version 3 distribution for Windows
+
+rem  Copyright 2009 Beman Dawes
+rem  Distributed under the Boost Software License, Version 1.0.
+rem  See http://www.boost.org/LICENSE_1_0.txt
+
+@echo off
+echo Create Boost Filesystem Version 3 distribution for Windows
+if {%1}=={} goto usage
+
+echo Preparing temporary directory %TEMP%\%1...
+pushd %TEMP%
+rmdir /S /Q %1 2>nul
+del %1.zip 2>nul
+
+echo Exporting https://svn.boost.org/svn/boost/sandbox/filesystem-v3 to %TEMP%\%1...
+svn export --non-interactive --trust-server-cert --native-eol CRLF ^
+ https://svn.boost.org/svn/boost/sandbox/filesystem-v3 %1
+
+echo Creating %TEMP%\%1.zip...
+zip -r -q %1.zip %1
+
+echo Finishing up...
+popd
+dir %TEMP%\%1.zip
+goto done
+
+:usage
+echo Usage: create-distro distro-name
+echo Will create %TEMP%\distro-name.zip
+:done
