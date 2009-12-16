789495d214e8ef9223ddef7fb0c8ef24bc06b6ab
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2009-12-16 08:31:13 +0000

    Boost.Chrono: Version 0.2.7, Bug fixes * Add test to check, min/max macros on windows
    SVN-Revision: 58411

---
+++ chrono/libs/chrono/test/test_minmax.cpp
@@ -0,0 +1,19 @@
+//  test_duration.cpp  ----------------------------------------------------------//
+
+//  Copyright 2009 Vicente J. Botet Escriba
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+
+#define min(A,B) ((A)<(B)?(A):(B))
+#define max(A,B) ((A)>(B)?(A):(B))
+
+#include <boost/chrono/chrono.hpp>
+#include <boost/chrono/process_times.hpp>
+
+ int main()
+ {
+    boost::chrono::run_timer t;
+ }
+
