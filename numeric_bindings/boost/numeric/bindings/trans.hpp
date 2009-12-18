693f844b5c2d0d0f96b896fcbd6010141602fd69
Rutger ter Borg <rutger@terborg.net>
2009-12-18 14:21:49 +0000

    Sync of work on numeric_bindings traits functions
    SVN-Revision: 58455

--- numeric_bindings/boost/numeric/bindings/trans.hpp
+++ numeric_bindings/boost/numeric/bindings/trans.hpp
@@ -6,11 +6,12 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef BOOST_NUMERIC_BINDINGS_TRANSPOSE_HPP
-#define BOOST_NUMERIC_BINDINGS_TRANSPOSE_HPP
+#ifndef BOOST_NUMERIC_BINDINGS_TRANSP_HPP
+#define BOOST_NUMERIC_BINDINGS_TRANSP_HPP
 
 #include <boost/numeric/bindings/value.hpp>
-#include <boost/numeric/bindings/entity.hpp>
+#include <boost/numeric/bindings/size.hpp>
+#include <boost/numeric/bindings/begin.hpp>
 #include <boost/mpl/max.hpp>
 
 namespace boost {
@@ -19,22 +20,34 @@ namespace bindings {
 namespace detail {
 
 template< typename T >
-struct transpose_wrapper: reference_wrapper<T> {}
+struct trans_wrapper: reference_wrapper<T> {};
 
 template< typename T, typename Id, typename Enable >
-struct adaptor< transpose_wrapper<T>, Id, Enable > {
+struct adaptor< trans_wrapper<T>, Id, Enable > {
 
     typedef typename value<T>::type value_type;
     typedef typename boost::mpl::max<
             boost::mpl::int_<2>,
             typename entity<T>::type >::type entity;
 
+    // Flip the data order
+    typedef typename flip_data_order< key_at::data_order >::type data_order;
+
+    // Flip size1/size2
     static std::ptrdiff_t size1( const Id& t ) {
-        return bindings::tensor_size2( t.get() );
+        return bindings::size2( t.get() );
     }
 
     static std::ptrdiff_t size2( const Id& t ) {
-        return bindings::tensor_size1( t.get() );
+        return bindings::size1( t.get() );
+    }
+
+    static typename result_of::begin_value< T >::type begin_value( Id& id ) {
+        return bindings::begin_value( id.get() );
+    }
+
+    static typename result_of::end_value< T >::type end_value( Id& id ) {
+        return bindings::end_value( id.get() );
     }
 
 };
@@ -43,13 +56,13 @@ struct adaptor< transpose_wrapper<T>, Id, Enable > {
 
 
 template< typename T >
-detail::transpose_wrapper<T> trans( T& underlying ) {
-    return detail::transpose_wrapper<T>( underlying );
+detail::trans_wrapper<T> trans( T& underlying ) {
+    return detail::trans_wrapper<T>( underlying );
 }
 
 template< typename T >
-detail::transpose_wrapper<const T> trans( const T& underlying ) {
-    return detail::transpose_wrapper<const T>( underlying );
+detail::trans_wrapper<const T> trans( const T& underlying ) {
+    return detail::trans_wrapper<const T>( underlying );
 }
 
 } // namespace bindings
