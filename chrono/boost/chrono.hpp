1209150108cf3b42f2bb93ff8ca474885f5ecfa5
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2010-01-19 22:50:45 +0000

    Boost.Chrono: Version 0.3.1, * added chrono.hpp and stopwatches.hpp at the boost level
    SVN-Revision: 59147

---
+++ chrono/boost/chrono.hpp
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2010.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/stm for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_CHRONO__HPP
+#define BOOST_CHRONO__HPP
+
+//-----------------------------------------------------------------------------
+#include <boost/chrono/chrono.hpp>
+//-----------------------------------------------------------------------------
+
+#endif // BOOST_CHRONO__HPP
---
+++ chrono/boost/stopwatches.hpp
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2010.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/stm for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_STOPWATCHES__HPP
+#define BOOST_STOPWATCHES__HPP
+
+//-----------------------------------------------------------------------------
+#include <boost/chrono/digital_time.hpp>
+#include <boost/chrono/digital_time_formatter.hpp>
+#include <boost/chrono/function_stopwatch.hpp>
+#include <boost/chrono/process_cpu_clocks.hpp>
+#include <boost/chrono/stopclock.hpp>
+#include <boost/chrono/stopclock_accumulator.hpp>
+#include <boost/chrono/stopwatch.hpp>
+#include <boost/chrono/stopwatch_accumulator.hpp>
+#include <boost/chrono/stopwatch_accumulator_formatter.hpp>
+#include <boost/chrono/stopwatch_formatter.hpp>
+#include <boost/chrono/stopwatch_reporter.hpp>
+#include <boost/chrono/stopwatch_scoped.hpp>
+#include <boost/chrono/time_formatter.hpp>
+//-----------------------------------------------------------------------------
+
+#endif // BOOST_STOPWATCHES__HPP
