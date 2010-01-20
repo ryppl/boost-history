721fe17cee471d62074d77d194414d862c1f2424
Erwann Rogard <erwann.rogard@gmail.com>
2010-01-20 23:39:18 +0000

    mv
    SVN-Revision: 59177

---
+++ statistics/non_parametric/boost/statistics/detail/non_parametric/kolmogorov_smirnov/tag/reference_distribution.hpp
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+// kolmogorov_smirnov::tag::reference_distribution.hpp 					  	  //
+//                                                                            //
+//  Copyright 2009 Erwann Rogard. Distributed under the Boost                 //
+//  Software License, Version 1.0. (See accompanying file                     //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)          //
+////////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_STATISTICS_DETAIL_NON_PARAMETRIC_KOLMOGOROV_SMIRNOV_TAG_REFERENCE_DISTRIBUTION_HPP_2010
+#define BOOST_STATISTICS_DETAIL_NON_PARAMETRIC_KOLMOGOROV_SMIRNOV_TAG_REFERENCE_DISTRIBUTION_HPP_2010
+#include <boost/parameter/name.hpp>
+#include <boost/parameter/keyword.hpp>
+
+namespace boost { 
+namespace statistics{
+namespace detail{
+namespace kolmogorov_smirnov{
+namespace tag{
+    struct reference_distribution{};
+}// tag
+}// kolmogorov_smirnov
+}// detail
+}// statistics
+}// boost
+
+#endif
