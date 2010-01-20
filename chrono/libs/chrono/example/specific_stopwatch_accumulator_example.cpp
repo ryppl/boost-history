778fcb1157d9c3356fb32655c41a2a400cdacdba
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2010-01-20 23:09:10 +0000

    Boost.Chrono: Version 0.3.2, * Added example/specific_stopwatch_accumulator_example.cpp
    SVN-Revision: 59169

---
+++ chrono/libs/chrono/example/specific_stopwatch_accumulator_example.cpp
@@ -0,0 +1,46 @@
+//  stopwatch_accumulator_example.cpp  ---------------------------------------------------//
+
+//  Copyright 2009 Vicente J. Botet Escriba
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+//  See http://www.boost.org/libs/chrono for documentation.
+
+#include <boost/chrono/stopwatches.hpp>
+#include <cmath>
+
+
+using namespace boost::chrono;
+using namespace boost::accumulators;
+typedef stopwatch_accumulator<process_real_cpu_clock, 
+            accumulator_set<process_real_cpu_clock::rep,
+                features<
+                        tag::count,
+                        tag::sum,
+                        tag::min,
+                        tag::max,
+                        tag::mean 
+                >
+            >
+  >::reporter my_stopwatch_accumulator_reporter;
+
+int f1(long j)
+{
+  static my_stopwatch_accumulator_reporter acc(BOOST_CHRONO_ACCUMULATOR_FUNCTION_FORMAT);
+  my_stopwatch_accumulator_reporter::scoped_run _(acc);
+
+  for ( long i = 0; i < j; ++i )
+    std::sqrt( 123.456L );  // burn some time
+
+  return 0;
+}
+int main()
+{
+
+  f1(100000);
+  f1(200000);
+  f1(300000);
+  return 0;
+}
+
