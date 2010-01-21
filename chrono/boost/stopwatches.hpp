16c3ee903b01929f52eb883c4e22c8d0b3867f71
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2010-01-21 20:49:53 +0000

    Boost.Chrono: Version 0.3.2, * change to #include <boost/chrono/stopwatches.hpp>
    SVN-Revision: 59205

--- chrono/boost/stopwatches.hpp
+++ chrono/boost/stopwatches.hpp
@@ -14,19 +14,7 @@
 #define BOOST_STOPWATCHES__HPP
 
 //-----------------------------------------------------------------------------
-#include <boost/chrono/digital_time.hpp>
-#include <boost/chrono/digital_time_formatter.hpp>
-#include <boost/chrono/function_stopclock.hpp>
-#include <boost/chrono/process_cpu_clocks.hpp>
-#include <boost/chrono/stopclock.hpp>
-#include <boost/chrono/stopclock_accumulator.hpp>
-#include <boost/chrono/stopwatch.hpp>
-#include <boost/chrono/stopwatch_accumulator.hpp>
-#include <boost/chrono/stopwatch_accumulator_formatter.hpp>
-#include <boost/chrono/stopwatch_formatter.hpp>
-#include <boost/chrono/stopwatch_reporter.hpp>
-#include <boost/chrono/stopwatch_scoped.hpp>
-#include <boost/chrono/time_formatter.hpp>
+#include <boost/chrono/stopwatches.hpp>
 //-----------------------------------------------------------------------------
 
 #endif // BOOST_STOPWATCHES__HPP
