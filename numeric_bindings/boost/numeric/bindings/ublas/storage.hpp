c03ef9a1f3f9b16668074b3ec522ef7983079c01
Rutger ter Borg <rutger@terborg.net>
2010-01-22 13:43:47 +0000

    Added ublas traits for matrix_vector_slice and -range, bounded matrices and vectors, bounded array. Fixed trans_tag for vector cases.
    SVN-Revision: 59221

--- numeric_bindings/boost/numeric/bindings/ublas/storage.hpp
+++ numeric_bindings/boost/numeric/bindings/ublas/storage.hpp
@@ -6,8 +6,8 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#ifndef BOOST_NUMERIC_BINDINGS_UBLAS_UNBOUNDED_ARRAY_HPP
-#define BOOST_NUMERIC_BINDINGS_UBLAS_UNBOUNDED_ARRAY_HPP
+#ifndef BOOST_NUMERIC_BINDINGS_UBLAS_STORAGE_HPP
+#define BOOST_NUMERIC_BINDINGS_UBLAS_STORAGE_HPP
 
 #include <boost/numeric/bindings/detail/adaptor.hpp>
 #include <boost/numeric/ublas/storage.hpp>
@@ -16,6 +16,32 @@ namespace boost {
 namespace numeric {
 namespace bindings {
 namespace detail {
+
+template< typename T, std::size_t N, typename Alloc, typename Id, typename Enable >
+struct adaptor< ::boost::numeric::ublas::bounded_array< T, N, Alloc >, Id, Enable > {
+
+    typedef typename copy_const< Id, T >::type value_type;
+    typedef mpl::map<
+        mpl::pair< tag::value_type, value_type >,
+        mpl::pair< tag::entity, tag::vector >,
+        mpl::pair< tag::size_type<1>, std::ptrdiff_t >,
+        mpl::pair< tag::data_structure, tag::linear_array >,
+        mpl::pair< tag::stride_type<1>, tag::contiguous >
+    > property_map;
+
+    static std::ptrdiff_t size1( const Id& id ) {
+        return id.size();
+    }
+
+    static value_type* begin_value( Id& id ) {
+        return id.begin();
+    }
+
+    static value_type* end_value( Id& id ) {
+        return id.end();
+    }
+
+};
 
 template< typename T, typename Alloc, typename Id, typename Enable >
 struct adaptor< ublas::unbounded_array< T, Alloc >, Id, Enable > {
@@ -29,16 +55,16 @@ struct adaptor< ublas::unbounded_array< T, Alloc >, Id, Enable > {
         mpl::pair< tag::stride_type<1>, tag::contiguous >
     > property_map;
 
-    static std::ptrdiff_t size1( const Id& t ) {
-        return t.size();
+    static std::ptrdiff_t size1( const Id& id ) {
+        return id.size();
     }
 
-    static value_type* begin_value( Id& t ) {
-        return t.begin();
+    static value_type* begin_value( Id& id ) {
+        return id.begin();
     }
 
-    static value_type* end_value( Id& t ) {
-        return t.end();
+    static value_type* end_value( Id& id ) {
+        return id.end();
     }
 
 };
