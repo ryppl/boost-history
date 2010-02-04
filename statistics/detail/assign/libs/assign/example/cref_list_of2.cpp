bf1fd3b967202aa374ed9151b82987acc279b6b8
Erwann Rogard <erwann.rogard@gmail.com>
2010-02-04 03:23:18 +0000

    m
    SVN-Revision: 59462

--- statistics/detail/assign/libs/assign/example/cref_list_of2.cpp
+++ statistics/detail/assign/libs/assign/example/cref_list_of2.cpp
@@ -6,6 +6,7 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
+#include <boost/timer.hpp>
 #include <iostream>
 #include <vector>
 #include <boost/assign/list_of.hpp>
@@ -18,21 +19,48 @@ void example_cref_list_of2(std::ostream& os)
 	using namespace boost::assign;
     
   	typedef std::vector<int> ints_;
+
+	{    
+    	int a=1,b=2;
+    	ints_ ints;
     
-    int a=1,b=2;
-    
-    ints_ ints;
-    
-    ints = cref_list_of<3>(a)(b)(3);
-	BOOST_ASSERT(ints[0] == a);    
-	BOOST_ASSERT(ints[1] == b);    
-	BOOST_ASSERT(ints[2] == 3);    
-    ints.clear();
-	ints = cref_list_of2(a)(b)(3);     
-	BOOST_ASSERT(ints[0] == a);    
-	BOOST_ASSERT(ints[1] == b);    
-	BOOST_ASSERT(ints[2] == 3);    
-    
+    	ints = cref_list_of<3>(a)(b)(3);
+		BOOST_ASSERT(ints[0] == a);    
+		BOOST_ASSERT(ints[1] == b);    
+		BOOST_ASSERT(ints[2] == 3);    
+    	ints.clear();
+		ints = cref_list_of2(a)(b)(3);     
+		BOOST_ASSERT(ints[0] == a);    
+		BOOST_ASSERT(ints[1] == b);    
+		BOOST_ASSERT(ints[2] == 3);    
+    }
+    {
+		const int n = 100 * 1000;
+        typedef std::vector<ints_> vec_ints_;
+        ints_ a( n );
+    	ints_ b( n );
+		boost::timer t;
+
+		{
+			t.restart();    
+			vec_ints_ vec_ints = cref_list_of<3>( a )( b )( ints_( n ) );
+            double t_val = t.elapsed();
+			os << "cref_list_of : t = " << t_val << std::endl;
+            BOOST_ASSERT(vec_ints[0] == a);    
+            BOOST_ASSERT(vec_ints[1] == b);    
+            BOOST_ASSERT(vec_ints[2] == ints_( n ));    
+        }
+		{
+			t.restart();    
+			vec_ints_ vec_ints = cref_list_of2( a )( b )( ints_( n ) );
+            double t_val = t.elapsed();
+			os << "cref_list_of2 : t = " << t_val << std::endl;
+            BOOST_ASSERT(vec_ints[0] == a);    
+            BOOST_ASSERT(vec_ints[1] == b);    
+            BOOST_ASSERT(vec_ints[2] == ints_( n ));    
+        }
+	}        
+
 	os << "<- " << std::endl;
     
 };
