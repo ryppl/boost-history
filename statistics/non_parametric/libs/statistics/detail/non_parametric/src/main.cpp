8c6a113f796b35b82852aa26f46ac171095371dd
Erwann Rogard <erwann.rogard@gmail.com>
2010-02-05 23:37:20 +0000

    m
    SVN-Revision: 59516

--- statistics/non_parametric/libs/statistics/detail/non_parametric/src/main.cpp
+++ statistics/non_parametric/libs/statistics/detail/non_parametric/src/main.cpp
@@ -6,9 +6,9 @@
 int main()
 {
 	example_frequency_int(std::cout);
-	//example_kolmogorov_smirnov(std::cout,0.1,6,10,10);
+	example_kolmogorov_smirnov(std::cout,0.1,6,10,10);
 	example_kolmogorov_smirnov(std::cout,10.0,6,10,10);
-	//example_kolmogorov_smirnov(std::cout,100.0,6,10,10);
+	example_kolmogorov_smirnov(std::cout,100.0,6,10,10);
 
     return 0;
 }
