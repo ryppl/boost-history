87d56efcb9bf78c9a7b2c8baef08299033d48bf7
Slawomir Lisznianski (bounce) <sl@paramay.com>
2010-02-21 15:30:08 +0000

    initial checkin
    SVN-Revision: 59809

---
+++ tokenmap/boost/tokenmap.hpp
@@ -0,0 +1,19 @@
+// Boost.Tokenmap
+//
+// Copyright (c) 2009 Slawomir Lisznianski
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+//  See www.boost.org/libs/bimap for documentation.
+
+// Convenience header
+
+#include <boost/tokenmap/tokenmap.hpp>
+
+namespace boost
+{
+  using ::boost::tokenmaps::tokenmap;
+}
+
