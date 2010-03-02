97b5f675ba2b428c26ea4737e2472dc4eaf5f773
Erwann Rogard <erwann.rogard@gmail.com>
2010-03-02 00:24:19 +0000

    m
    SVN-Revision: 60064

--- statistics/detail/assign/libs/assign/src/main.cpp
+++ statistics/detail/assign/libs/assign/src/main.cpp
@@ -2,14 +2,14 @@
 #include <libs/assign/example/ref_list_of.h>
 #include <libs/assign/test/speed.h>
 #include <libs/assign/test/speed2.h>
-//#include <libs/assign/test/static_list_of_auto_size.h>
+#include <libs/assign/test/static_list_of_auto_size.h>
 
 int main (int argc, char * const argv[]) {
 
 	//example_ref_list_of(std::cout);
 	//test_speed(std::cout);
-    test_speed2(std::cout);
-//	check_static_list_of_auto_size();
+    //test_speed2(std::cout);
+	check_static_list_of_auto_size();
 
 	return 0;
 
