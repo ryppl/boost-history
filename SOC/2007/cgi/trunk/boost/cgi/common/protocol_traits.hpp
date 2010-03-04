b56db86233051f48c1e078f8c1873471bfd61bb5
Darren Garvey <darren.garvey@gmail.com>
2010-03-04 23:36:33 +0000

    More work on experimental session support. See the cgi/sessions example. Tested on ubuntu with g++ 4.3.3.
    SVN-Revision: 60158

--- SOC/2007/cgi/trunk/boost/cgi/common/protocol_traits.hpp
+++ SOC/2007/cgi/trunk/boost/cgi/common/protocol_traits.hpp
@@ -1,20 +1,13 @@
 //                -- protocol_traits.hpp --
 //
-//            Copyright (c) Darren Garvey 2007.
+//          Copyright (c) Darren Garvey 2007-2009.
 // Distributed under the Boost Software License, Version 1.0.
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 //
 ////////////////////////////////////////////////////////////////
-//
-// Wow this is messy. It's not expected to last though; it's
-// merely here to make things more separated than they need to
-// be, so everything can be meshed together slowly.
-// (or something like that)
-//
-/////////////////////////////////////////////////////////////////
-#ifndef CGI_REQUEST_TRAITS_HPP_INCLUDED__
-#define CGI_REQUEST_TRAITS_HPP_INCLUDED__
+#ifndef BOOST_CGI_PROTOCOL_TRAITS_HPP_INCLUDED_
+#define BOOST_CGI_PROTOCOL_TRAITS_HPP_INCLUDED_
 
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/none.hpp>
@@ -29,8 +22,14 @@
 #include "boost/cgi/fwd/basic_protocol_service_fwd.hpp"
 #include "boost/cgi/fwd/basic_request_fwd.hpp"
 #include "boost/cgi/fwd/form_parser_fwd.hpp"
+#ifdef BOOST_CGI_ENABLE_SESSIONS
+#  include "boost/cgi/utility/sessions.hpp"
+#  include <boost/uuid/uuid_generators.hpp>
+#endif // BOOST_CGI_ENABLE_SESSIONS
 
 BOOST_CGI_NAMESPACE_BEGIN
+
+  struct empty;
 
   namespace cgi  {}
   namespace fcgi
@@ -56,15 +55,14 @@ BOOST_CGI_NAMESPACE_BEGIN
   class cgi_request_service;
   class fcgi_request_service;
 
- namespace detail {
+ namespace common {
 
-   namespace tags = ::BOOST_CGI_NAMESPACE::common::tags;
-   
    template<typename Protocol>
     struct protocol_traits
     {
     };
 
+    /// Common Traits for CGI.
     template<>
     struct protocol_traits<tags::cgi>
     {
@@ -82,7 +80,6 @@ BOOST_CGI_NAMESPACE_BEGIN
                   tags::async_stdio
               >                                      connection_type;
       typedef common::basic_client<
-                  connection_type,
                   tags::cgi
               >                                      client_type;
       typedef common::form_parser                    form_parser_type;
@@ -90,14 +87,24 @@ BOOST_CGI_NAMESPACE_BEGIN
       typedef char                                   char_type;
       typedef std::basic_string<char_type>           string_type;
       typedef string_type                            buffer_type;
+      typedef boost::array<unsigned char, 8>         header_buffer_type;
       typedef boost::asio::const_buffers_1           const_buffers_type;
       typedef boost::asio::mutable_buffers_1         mutable_buffers_type;
       typedef common::role_type                      role_type;
       typedef boost::shared_ptr<request_type>        pointer;
-      
+#ifdef BOOST_CGI_ENABLE_SESSIONS
+      typedef basic_session<
+                std::map<string_type, string_type> 
+              >                                      session_type;
+      typedef session_manager                        session_manager_type;
+      static const bool auto_start_session = true;
+
+      typedef boost::uuids::random_generator          uuid_generator_type;
+#endif // BOOST_CGI_ENABLE_SESSIONS
       static const common::parse_options parse_opts = common::parse_all;
     };
 
+    /// Common Traits for FastCGI.
     template<>
     struct protocol_traits<tags::fcgi>
     {
@@ -125,10 +132,7 @@ BOOST_CGI_NAMESPACE_BEGIN
       typedef acceptor_service_type::native_type     native_type;
       typedef unsigned short                         port_number_type;
       typedef boost::asio::ip::tcp::endpoint         endpoint_type;
-      typedef common::basic_client<
-                  connection_type,
-                  tags::fcgi
-              >                                      client_type;
+      typedef common::basic_client<tags::fcgi>       client_type;
       typedef common::form_parser                    form_parser_type;
       typedef fcgi::spec::header                     header_type;
       typedef fcgi::spec_detail::role_types          role_type;
@@ -136,42 +140,22 @@ BOOST_CGI_NAMESPACE_BEGIN
       typedef char                                   char_type;
       typedef std::basic_string<char_type>           string_type;
       typedef string_type                            buffer_type;
+      typedef boost::array<unsigned char, 8>         header_buffer_type;
       typedef boost::asio::const_buffers_1           const_buffers_type;
       typedef boost::asio::mutable_buffers_1         mutable_buffers_type;
       typedef boost::shared_ptr<request_type>        pointer;
+#ifdef BOOST_CGI_ENABLE_SESSIONS
+      typedef basic_session<
+                std::map<string_type, string_type> 
+              >                                      session_type;
+      typedef session_manager                        session_manager_type;
+      static const bool auto_start_session = true;
+#endif // BOOST_CGI_ENABLE_SESSIONS
 
       static const common::parse_options parse_opts = common::parse_none;
     };
 
-    template<>
-    struct protocol_traits<tags::scgi>
-    {
-      typedef protocol_traits<tags::scgi>            type;
-      typedef scgi::scgi_request_service             request_service_impl;
-      typedef scgi::scgi_request_service             service_type;
-      typedef common::basic_protocol_service<tags::scgi>     protocol_service_type;
-      typedef common::basic_request<
-                  tags::scgi
-      >                                              request_type; 
-      typedef scgi::scgi_service_impl                service_impl_type;
-      typedef scgi::scgi_acceptor_service            acceptor_service_impl;
-      typedef common::basic_connection<tags::tcp_socket>     connection_type;
-      typedef common::basic_client<
-                  connection_type,
-                  tags::scgi
-              >                                      client_type;
-      typedef common::form_parser                    form_parser_type;
-      typedef char                                   char_type;
-      typedef std::basic_string<char_type>           string_type;
-      typedef string_type                            buffer_type;
-      typedef boost::asio::const_buffers_1           const_buffers_type;
-      typedef boost::asio::mutable_buffers_1         mutable_buffers_type;
-      typedef boost::shared_ptr<request_type>        pointer;
-
-      static const common::parse_options parse_opts = common::parse_none;
-    };
-
- } // namespace detail
+ } // namespace common
 BOOST_CGI_NAMESPACE_END
 
-#endif // CGI_REQUEST_TRAITS_HPP_INCLUDED__
+#endif // BOOST_CGI_PROTOCOL_TRAITS_HPP_INCLUDED_
