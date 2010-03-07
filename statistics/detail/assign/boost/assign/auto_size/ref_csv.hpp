565196b96b5b2ef0940f19425b97fe59e8022de8
Erwann Rogard <erwann.rogard@gmail.com>
2010-03-07 17:46:20 +0000

    m
    SVN-Revision: 60319

--- statistics/detail/assign/boost/assign/auto_size/ref_csv.hpp
+++ statistics/detail/assign/boost/assign/auto_size/ref_csv.hpp
@@ -6,8 +6,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_AUTO_SIZE_REF_LIST_OF_CSV_ER_2010_HPP
-#define BOOST_ASSIGN_AUTO_SIZE_REF_LIST_OF_CSV_ER_2010_HPP
+#ifndef BOOST_ASSIGN_AUTO_SIZE_REF_CSV_ER_2010_HPP
+#define BOOST_ASSIGN_AUTO_SIZE_REF_CSV_ER_2010_HPP
 
 // Usage:
 // ref_csv(a,b,c);
