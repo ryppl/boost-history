4679acd0db0dcbb593310dc442f48070a0b66256
Erwann Rogard <erwann.rogard@gmail.com>
2010-03-13 02:06:43 +0000

    m
    SVN-Revision: 60535

--- statistics/detail/assign/boost/assign/auto_size/detail/expr.hpp
+++ statistics/detail/assign/boost/assign/auto_size/detail/expr.hpp
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////
 // assign::detail::expr.hpp                                                 //
 //                                                                          //
-//  (C) Copyright 2010 Fwann Rogard                                        //
+//  (C) Copyright 2010 Erwann Rogard                                        //
 //  Use, modification and distribution are subject to the                   //
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
