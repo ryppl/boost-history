918a4c7aad855cad71ffe00c8707d594b83cf1fe
Erwann Rogard <erwann.rogard@gmail.com>
2010-03-15 00:09:35 +0000

    m
    SVN-Revision: 60601

---
+++ statistics/detail/assign/boost/assign/chain_convert.hpp
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////
+// assign::auto_size.hpp                                                    //
+//                                                                          //
+//  (C) Copyright 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_AUTO_SIZE_ER_2010_HPP
+#define BOOST_ASSIGN_AUTO_SIZE_ER_2010_HPP
+
+#include <boost/assign/auto_size/chain_convert.hpp> 
+
+#endif
