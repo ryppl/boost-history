a39d3f60e9ec60daecbcb404370ca71020a9628b
Erwann Rogard <erwann.rogard@gmail.com>
2010-03-19 04:22:27 +0000

    m
    SVN-Revision: 60702

---
+++ statistics/detail/assign/boost/assign/auto_size/check/constants.hpp
@@ -0,0 +1,35 @@
+//////////////////////////////////////////////////////////////////////////////
+// assign::detail::auto_size::check_constants.hpp                           //
+//                                                                          //
+//  (C) Copyright 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_DETAIL_AUTO_SIZE_CHECK_CONSTANTS_ER_2010_HPP
+#define BOOST_ASSIGN_DETAIL_AUTO_SIZE_CHECK_CONSTANTS_ER_2010_HPP
+
+namespace boost{                                                               
+namespace assign{                                                              
+namespace detail{                                                              
+namespace auto_size{                                                           
+
+     namespace check_constants{
+     
+        const int a = 1;
+        const int b = 5;
+        const int c = 3;
+        const int d = 4;
+        const int e = 2;
+        const int f = 9;
+        const int g = 0;
+        const int h = 7;
+
+    }
+    
+}// auto_size
+}// detail
+}// assign
+}// boost    
+    
+#endif
