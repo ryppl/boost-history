8861af5e2492a37ee36b710e39c73a12aca95ff7
Darren Garvey <darren.garvey@gmail.com>
2010-03-24 01:06:49 +0000

    * Changes to the way sessions work (should be in a working state now). * Changed the default session implementation to std::map<string,string>, when BOOST_CGI_ENABLE_SESSIONS is defined. * Add put(char) to data_map_proxy. * Cleanup sessions example. * Add custom_sessions example. * Other cleanups
    SVN-Revision: 60795

--- SOC/2007/cgi/trunk/libs/cgi/example/cgi/sessions/main.cpp
+++ SOC/2007/cgi/trunk/libs/cgi/example/cgi/sessions/main.cpp
@@ -3,83 +3,34 @@
 #include <iostream>
 
 using namespace std;
-
-class context
-{
-public:
-
-  context() {}
-  
-  map<string,int> data;
-
-  template<typename Archive>
-  void serialize(Archive& ar, const unsigned int version)
-  {
-    ar & data;
-  }
-  
-};
-
-/*<
- * Now we can override the default session type to use the `context` class.
- *
- * To do this we need to create a `Tag`. A `Tag` is a plain struct used as
- * an identifier. If we specialise the `protocol_traits<>` template for this
- * tag we can change the behaviour of the library at compile-time.
- *
- * In this example we are just going to override the actual `session_type`
- * and otherwise inherit the properties for CGI.
->*/
-
-/// The `Tag`
-struct session_enabled_cgi {};
-
-/// Specialise the `protocol_traits` template in the `boost::cgi::common`
-/// namespace.
-namespace boost { namespace cgi { namespace common {
-
-template<>
-struct protocol_traits<session_enabled_cgi>
-  : protocol_traits<boost::cgi::tags::cgi>
-{
-  typedef basic_session<context> session_type; /*<
-The session_type for the request holds all session-related data, which in this
-example is everything stored in the `context` class defined above.
->*/
-  static const bool auto_start_session = false; /*<
-`auto_start_session` is `true` by default. Here we override it to be `false`,
-which means we must explicitly call `basic_request<>::start_session()` for
-any sessions to be created / loaded.
->*/
-};
-
-} } } // namespace boost::cgi::common
-
 using namespace boost::cgi;
-
-/// Define a request type which uses the traits for our
-/// `session_enabled_cgi` Tag.
-typedef basic_request<session_enabled_cgi> my_request;
 
 int main(int, char**)
 {
   try
   {
-    my_request req;
+    request req;
     response resp;
+    
+    if (req.get.pick("clear", "") == "1") {
+      req.stop_session();
+      resp<< "Cleared session";
+      return commit(req, resp);
+    }
 
     // Start the session. It is safe to call `start_session()` if a session
     // is already open, but you shouldn't have to do that...
     req.start_session();
 
-    resp<< "one = " << req.session.data["one"]
-        << ", two = " << req.session.data["two"]
-        << ", ten = " << req.session.data["ten"];
-    
+    resp<< content_type("text/plain")
+        << "one = " << req.session["one"]
+        << ", two = " << req.session["two"]
+        << ", ten = " << req.session["ten"];
+
     // Modify the request session.
-    req.session.data["one"] = 1;
-    req.session.data["two"] = 2;
-    req.session.data["ten"] = 10;
+    req.session["one"] = "1";
+    req.session["two"] = "2";
+    req.session["ten"] = "10";
 
     // The session is saved by `commit()`.
     return commit(req, resp);
