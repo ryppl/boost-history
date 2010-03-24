65f778ee120aa172c9ea9af1070a0ede25e5995f
Erwann Rogard <erwann.rogard@gmail.com>
2010-03-24 11:54:16 +0000

    m
    SVN-Revision: 60808

--- statistics/detail/assign/boost/assign/chain_convert.hpp
+++ statistics/detail/assign/boost/assign/chain_convert.hpp
@@ -9,8 +9,7 @@
 #ifndef BOOST_ASSIGN_CHAIN_CONVERT_ER_2010_HPP
 #define BOOST_ASSIGN_CHAIN_CONVERT_ER_2010_HPP
 
-#include <boost/assign/auto_size/range/chain_l.hpp> 
-#include <boost/assign/auto_size/range/chain_r.hpp> 
+#include <boost/assign/auto_size/range/chain.hpp> 
 #include <boost/assign/auto_size/range/chain_convert_l.hpp> 
 
 #endif
