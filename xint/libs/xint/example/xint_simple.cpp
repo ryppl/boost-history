e81f1b6de18fbf535df2c17927e6e4c9173ab884
Paul A. Bristow <pbristow@hetp.u-net.com>
2010-03-30 13:54:39 +0000

    Ultra simple example
    SVN-Revision: 60944

---
+++ xint/libs/xint/example/xint_simple.cpp
@@ -0,0 +1,55 @@
+// xint_simple.cpp
+
+// Copyright Paul A. Bristow 2010
+//
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+/** 
+\file     
+\brief Simple demo of xint including stream output.
+\details
+Tests a few operators for output to a std:: stream.
+**/
+
+#include <boost/xint/xint.hpp>
+
+#include <iostream>
+using std::cout;
+using std::endl;
+#include <iomanip>
+using std::setprecision;
+using std::setw;
+#include <limits>
+
+int main()
+{    
+  // TODO I think you should get this into the boost namespace now or there will be zillions of places to change.
+  //using namespace boost::xint
+
+  using namespace xint;
+  using xint::integer;
+
+//[xint_simple_snippet_1
+
+  integer a(2);
+  integer b = 3;
+
+  cout << a << ", b = " << b << ", a + b = " << a + b << endl;
+
+//] [xint_simple_snippet_1]
+
+
+
+      return 0;
+} // int(main)
+
+/*
+
+Output
+
+*/
+
+
+
