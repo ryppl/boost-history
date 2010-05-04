81bec95b4ce807ac9dd013073f2e876b42faf5fb
Christopher Schmidt <mr.chr.schmidt@online.de>
2010-05-04 16:23:01 +0000

    added /fusion/view/single_view.hpp and /fusion/view/nview.hpp to /fusion/view.hpp
    SVN-Revision: 61761

--- boost/fusion/view.hpp
+++ boost/fusion/view.hpp
@@ -7,11 +7,13 @@
 #if !defined(FUSION_SEQUENCE_VIEW_10022005_0620)
 #define FUSION_SEQUENCE_VIEW_10022005_0620
 
-#include <boost/fusion/sequence/view/filter_view.hpp>
-#include <boost/fusion/sequence/view/iterator_range.hpp>
-#include <boost/fusion/sequence/view/joint_view.hpp>
-#include <boost/fusion/sequence/view/transform_view.hpp>
-#include <boost/fusion/sequence/view/reverse_view.hpp>
-#include <boost/fusion/sequence/view/zip_view.hpp>
+#include <boost/fusion/view/filter_view.hpp>
+#include <boost/fusion/view/iterator_range.hpp>
+#include <boost/fusion/view/joint_view.hpp>
+#include <boost/fusion/view/nview.hpp>
+#include <boost/fusion/view/single_view.hpp>
+#include <boost/fusion/view/reverse_view.hpp>
+#include <boost/fusion/view/transform_view.hpp>
+#include <boost/fusion/view/zip_view.hpp>
 
 #endif
