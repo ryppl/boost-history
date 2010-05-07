ef9c99761e1af3581d5f8461dac4584b459edb56
Brian Bartman <bbartmanboost@gmail.com>
2010-05-07 21:55:54 +0000

    Stubbing out ideas about how to effectivly structure bit_mask implementation
    SVN-Revision: 61849

--- SOC/2010/bit_masks/boost/integer/bit_mask.hpp
+++ SOC/2010/bit_masks/boost/integer/bit_mask.hpp
@@ -10,11 +10,35 @@
 #define BOOST_INTEGER_BIT_MASK_HPP
 
 #include <boost/mpl/int.hpp>
+#include <boost/mpl/bitwise.hpp>
+
+#include <boost/integer/details/bit_mask_impl.hpp>
 
 namespace boost {
 
-// template <typename TypeWidth, typename BitOffset, typename MaskWidth>
-// struct 
+// forward declaration of the bit mask structure which will all 
+// a recursive definition of how
+template <typename Value, typename SecondValue = mpl::int_<0> > struct bit_mask;
+
+
+// termination specialization this will cause the psudo-recursive definition 
+// being evalueated to finish.
+template <int NewMaskValue, int CurrentMaskValue>
+struct bit_mask< mpl::int_< NewMaskValue >, mpl::int_< CurrentMaskValue > >
+    :mpl::bitor_<
+        mpl::int_<NewMaskValue>,
+        mpl::int_< CurrentMaskValue >
+    >::type
+{ };
+
+// this is the recursive step for the bit_mask.
+template <int MaskValue, int CurrentMaskValue>
+struct bit_mask < bit_mask< mpl::int_<MaskValue> >, mpl::int_<CurrentMaskValue> >
+    :mpl::bitor_<
+        mpl::int_< CurrentMaskValue >,
+        bit_mask< mpl::int_< MaskValue > >
+    >::type
+{};
 
 } // namespace boost
 
---
+++ SOC/2010/bit_masks/boost/integer/details/bit_mask_impl.hpp
@@ -0,0 +1,10 @@
+#ifndef BOOST_BIT_MASK_IMPL_HPP
+#define BOOST_BIT_MASK_IMPL_HPP
+
+
+namespace boost { namespace details {
+
+} // end of details namespace.
+} // end of boost namespace.
+
+#endif
