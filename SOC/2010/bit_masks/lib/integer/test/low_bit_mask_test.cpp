e7aba4d3b81f6f357bc941e4936a5e895490a282
Brian Bartman <bbartmanboost@gmail.com>
2010-05-13 14:44:55 +0000

    adding a testing file for bit_width meta function
    SVN-Revision: 61948

--- SOC/2010/bit_masks/lib/integer/test/low_bit_mask_test.cpp
+++ SOC/2010/bit_masks/lib/integer/test/low_bit_mask_test.cpp
@@ -3,29 +3,34 @@
 #include <boost/assert.hpp>
 #include <boost/type_traits.hpp>
 #include <boost/integer/high_low_bits.hpp>
-
-// #include <iostream>
-// #include <fstream>
 // Testing of low_bits function.
-
-
-
-using namespace std;
 using namespace boost;
 
-
+// basic typedef
 typedef low_bits<unsigned int, 3> lbits;
 
 int main() {
-    // ofstream log("log.cpp");
-    // log << lbits::value << endl;
 
+    // making sure that the value type is transfered correctly.
+    BOOST_ASSERT(( is_same< lbits::value_type, unsigned int >::value ));
 
-    // throw(lbits::value);
-    // std::cerr << lbits::value << std::endl;
-    BOOST_ASSERT(( is_same< lbits::value_type, unsigned int>::value ));
+    // assert that mask is correct.
     BOOST_ASSERT(( lbits::value == 7 ));
-    // using namespace std;
+
+    // assert that type returns the correct typedef.
+    BOOST_ASSERT(( is_same<lbits::type, lbits>::value ));
+
+    // testing additional integral types.
+    // char
+    // unsigned char
+    // short
+    // unsigned short
+    // long
+    // unsigned long
+    // int
+    // unsigned int
+    // long long
+    // unsigned long long    
     
     return 0;
 }
