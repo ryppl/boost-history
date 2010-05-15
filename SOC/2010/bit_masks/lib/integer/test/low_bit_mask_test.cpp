8f7f2ea86ad1feeec184c653baee82456ab56a05
Brian Bartman <bbartmanboost@gmail.com>
2010-05-15 13:13:38 +0000

    working on devising the best way to test everything at once and that seems to be to use for_each and mpl::list
    SVN-Revision: 61992

--- SOC/2010/bit_masks/lib/integer/test/low_bit_mask_test.cpp
+++ SOC/2010/bit_masks/lib/integer/test/low_bit_mask_test.cpp
@@ -3,7 +3,7 @@
 #include <boost/mpl/for_each.hpp>
 
 
-
+#include <boost/detail/lightweight_test.hpp>  // for main, BOOST_TEST
 #include <boost/assert.hpp>
 #include <boost/type_traits.hpp>
 #include <boost/integer/high_low_bits.hpp>
@@ -70,6 +70,7 @@ typedef boost::mpl::list<
                             int,
                             unsigned int
 #if 0
+                            ,
                             long long,
                             unsigned long long
 #endif
@@ -80,14 +81,14 @@ template <typename T>
 void test_function() {
 
     // making sure that the value type is transfered correctly.
-    BOOST_ASSERT((is_same< typename low_bits<T, 3>::value_type, T >::value));
+    BOOST_TEST((is_same< typename low_bits<T, 3>::value_type, T >::value));
 
     // assert that mask is correct.
     // T r = typename ;
-    BOOST_ASSERT(( boost::low_bits<T,3>::value ));
+    BOOST_TEST(( boost::low_bits<T,3>::value ));
 
     // assert that type returns the correct typedef.
-    BOOST_ASSERT(( is_same< typename low_bits<T, 3>::type, low_bits<T, 3> >::value ));
+    BOOST_TEST(( is_same< typename low_bits<T, 3>::type, low_bits<T, 3> >::value ));
 
 }
 
