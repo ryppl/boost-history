7dbd0dfc9509df40f742350f36d4a2d0fdeead91
Brian Bartman <bbartmanboost@gmail.com>
2010-05-16 19:12:26 +0000

    working on overloads for bitwise operators
    SVN-Revision: 62045

--- SOC/2010/bit_masks/lib/integer/test/bitwise_operator_test.cpp
+++ SOC/2010/bit_masks/lib/integer/test/bitwise_operator_test.cpp
@@ -6,32 +6,19 @@
 
 
 #include "test_type_list.hpp"
+#include <boost/integer/high_low_bits.hpp>
+#include <boost/integer/bit_mask.hpp>
 #include <boost/integer/details/bit_mask_function_support.hpp>
+
+void ignore_unused(...) { }
 
 // Testing for operator overloads.
 using namespace boost;
 
 template <typename T>
 void test_function() {
-
-    // making sure that the value type is transfered correctly.
-    BOOST_ASSERT((is_same< typename high_bits<T, 3>::value_type, T >::value));
-
-    
-    BOOST_ASSERT(( boost::high_bits<T,1>::value == ~(~(T(0)) >> 1) ));
-    BOOST_ASSERT(( boost::high_bits<T,2>::value == ~(~(T(0)) >> 2) ));
-    BOOST_ASSERT(( boost::high_bits<T,3>::value == ~(~(T(0)) >> 3) ));
-    BOOST_ASSERT(( boost::high_bits<T,4>::value == ~(~(T(0)) >> 4) ));
-    BOOST_ASSERT(( boost::high_bits<T,5>::value == ~(~(T(0)) >> 5) ));
-    BOOST_ASSERT(( boost::high_bits<T,6>::value == ~(~(T(0)) >> 6) ));
-    BOOST_ASSERT(( boost::high_bits<T,7>::value == ~(~(T(0)) >> 7) ));
-    BOOST_ASSERT(( boost::high_bits<T,8>::value == ~(~(T(0)) >> 8) ));
-
-    // assert that type returns the correct typedef.
-    BOOST_ASSERT(( is_same< 
-                    typename high_bits<T, 3>::type,
-                    high_bits<T, 3> >::value
-              ));
+    T bitwise_and = T(0) & bit_mask<T, 1>()();
+    ignore_unused( bitwise_and );
 }
 
 
@@ -39,14 +26,15 @@ struct type_tester {
     template< typename U >
     void operator()(U) {
         test_function<U>();
-
     }
 };
 
 
 int main() {
     mpl::for_each< test_types   >( type_tester() ); 
-    mpl::for_each< test_types_2 >( type_tester() ); 
-    mpl::for_each< test_types_3 >( type_tester() ); 
+    // mpl::for_each< test_types_2 >( type_tester() ); 
+    // mpl::for_each< test_types_3 >( type_tester() ); 
+    char bitwise_and = char(0) & bit_mask<char, 1>();
+    ignore_unused( bitwise_and );
     return 0;
 }
