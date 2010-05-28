6d22fc9846226f22885f78ce6dd34fd49e213715
Brian Bartman <bbartmanboost@gmail.com>
2010-05-28 15:32:44 +0000

    working on creating header file and testing enviornment for compound masks
    SVN-Revision: 62300

---
+++ SOC/2010/bit_masks/lib/integer/test/compound_mask_test.cpp
@@ -0,0 +1,10 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/integer/compound_mask.hpp>
+
+int main() {
+    return 0;
+}
