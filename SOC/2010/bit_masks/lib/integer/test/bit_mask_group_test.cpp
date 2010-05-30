e5c632f1526f887fa6abe8e67af0f9579c345e23
Brian Bartman <bbartmanboost@gmail.com>
2010-05-30 15:17:57 +0000

    adding bit mask group testing file
    SVN-Revision: 62312

---
+++ SOC/2010/bit_masks/lib/integer/test/bit_mask_group_test.cpp
@@ -0,0 +1,14 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/integer/bit_mask_group.hpp>
+#include "test_type_list.hpp"
+
+
+
+int main() {
+    
+    return 0;
+}
