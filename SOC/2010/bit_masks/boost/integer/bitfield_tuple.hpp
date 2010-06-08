b4b34d77a04a6f00cf6b3419f4744e1274c69787
Brian Bartman <bbartmanboost@gmail.com>
2010-06-08 13:55:20 +0000

    Setting up testing facility and include file structure for bitfield_tuple
    SVN-Revision: 62568

---
+++ SOC/2010/bit_masks/boost/integer/bitfield_tuple.hpp
@@ -0,0 +1,14 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef BOOST_BITFIELD_TUPLE_HPP
+#define BOOST_BITFIELD_TUPLE_HPP
+
+namespace boost {
+
+} // end boost
+
+#endif
