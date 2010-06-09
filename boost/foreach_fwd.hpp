c7e21960d1ddded81d1aad093abc71963ccf6d13
Eric Niebler <eric@boostpro.com>
2010-06-09 14:52:52 +0000

    add foreach_fwd.hpp for forward declarations of foreach's customization points, fixes #3998
    SVN-Revision: 62661

--- boost/foreach_fwd.hpp
+++ boost/foreach_fwd.hpp
@@ -81,7 +81,6 @@
 #include <boost/type_traits/is_base_and_derived.hpp>
 #include <boost/iterator/iterator_traits.hpp>
 #include <boost/utility/addressof.hpp>
-#include <boost/foreach_fwd.hpp>
 
 #ifdef BOOST_FOREACH_RUN_TIME_CONST_RVALUE_DETECTION
 # include <new>
@@ -89,6 +88,12 @@
 # include <boost/utility/enable_if.hpp>
 # include <boost/type_traits/remove_const.hpp>
 #endif
+
+// This must be at global scope, hence the uglified name
+enum boost_foreach_argument_dependent_lookup_hack
+{
+    boost_foreach_argument_dependent_lookup_hack_value
+};
 
 namespace boost
 {
@@ -111,6 +116,11 @@ namespace foreach
     {
         return std::make_pair(begin, end);
     }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // boost::foreach::tag
+    //
+    typedef boost_foreach_argument_dependent_lookup_hack tag;
 
     ///////////////////////////////////////////////////////////////////////////////
     // boost::foreach::is_lightweight_proxy
