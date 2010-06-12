2905463d23899fd0f306cb1d11ad7ffe3f586ada
Brian Bartman <bbartmanboost@gmail.com>
2010-06-12 17:54:40 +0000

    adding vicentes work to my work to make the managment of the bitfields within the bitfield_tuple
    SVN-Revision: 62866

---
+++ SOC/2010/bit_masks/boost/bitfield.hpp
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2009. Distributed under the Boost
+// Software License, Version 1.0. (See accompanying file
+// LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/integer for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_BITFIELD__HPP
+#define BOOST_BITFIELD__HPP
+
+#include <boost/bitfield/bitfield.hpp>
+#include <boost/bitfield/bitfield_dcl.hpp>
+//#include <boost/bitfield/endian_bitfield_value_type.hpp>
+//#include <boost/bitfield/bitfield_group.hpp>
+//#include <boost/bitfield/pointer_plus_bits.hpp>
+#endif
+
