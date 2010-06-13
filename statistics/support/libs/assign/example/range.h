fe538176e97a409555dc7a10f7744c72c637e2e0
Erwann Rogard <erwann.rogard@gmail.com>
2010-06-13 14:41:18 +0000

    m
    SVN-Revision: 62908

--- statistics/support/libs/assign/example/range.h
+++ statistics/support/libs/assign/example/range.h
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////
-// test::range.h                                                            //
+// :range.h                                                                 //
 //                                                                          //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef LIBS_ASSIGN_TEST_RANGE_ER_2010_H
-#define LIBS_ASSIGN_TEST_RANGE_ER_2010_H
+#ifndef LIBS_ASSIGN_EXAMPLE_RANGE_ER_2010_H
+#define LIBS_ASSIGN_EXAMPLE_RANGE_ER_2010_H
 #include <ostream>
 
 void example_range(std::ostream&);
