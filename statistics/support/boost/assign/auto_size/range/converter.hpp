f703d9b382ac8d7dcb167665f3795fd7efc6dfa2
Erwann Rogard <erwann.rogard@gmail.com>
2010-06-14 13:27:04 +0000

    misc
    SVN-Revision: 62930

--- statistics/support/boost/assign/auto_size/range/converter.hpp
+++ statistics/support/boost/assign/auto_size/range/converter.hpp
@@ -1,6 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////
 // assign::detail::range::converter.hpp                                     //
 //                                                                          //
+//  Copyright Thorsten Ottosen 2003-2004.                                   //
+//  (C) Copyright 2010 Erwann Rogard                                        //
 //  Use, modification and distribution are subject to the                   //
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
