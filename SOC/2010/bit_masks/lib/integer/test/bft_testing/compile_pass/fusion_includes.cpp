11dd0fa383f6211434991457dcf746195b338104
Brian Bartman <bbartmanboost@gmail.com>
2010-06-15 15:41:39 +0000

    reorganizing header files and directories to be more correctly named and in the correct location
    SVN-Revision: 62977

---
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/compile_pass/fusion_includes.cpp
@@ -0,0 +1,14 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+#include <iostream>
+#include <boost/integer/bitfield_tuple.hpp>
+
+using namespace boost;
+
+// Testing to see if the include paths work correctly.
+#include <boost/integer/details/bft/ext/bitfield_tuple_fusion_includes.hpp>
+
+int main() { return 0; }
--- SOC/2010/bit_masks/lib/integer/test/bft_testing/fusion_integraltion_testing.cpp
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/fusion_integraltion_testing.cpp
@@ -25,7 +25,25 @@ typedef bitfield_tuple<
 
 
 int main() {
+    /*
+    bmg_t bmg;
 
+    BOOST_ASSERT(( (*begin(bmg)) == bmg.get<0>() ));
+    BOOST_ASSERT(( *next(begin(bmg)) == bmg.get<1>() ));
+    BOOST_ASSERT(( *advance_c<2>(begin(bmg)) == bmg.get<2>() ));
+    BOOST_ASSERT(( prior(next(begin(bmg))) == begin(bmg) ));
+    BOOST_ASSERT(( *prior(end(bmg)) == bmg.get<5>() ));
+    BOOST_ASSERT(( at_c<2>(bmg) == bmg.get<2>() ));
+    BOOST_ASSERT(( size(bmg) == 6));
+    BOOST_ASSERT(( distance(begin(bmg), end(bmg)) == 6 ));
+
+    BOOST_MPL_ASSERT((
+        is_same<
+            traits::category_of<bmg_t>::type,
+            random_access_traversal_tag
+        >
+    ));
+*/
 }
 
 
