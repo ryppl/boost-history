81b53c18139b0f6e983e365f99d6d4af45a0ec8f
Brian Bartman <bbartmanboost@gmail.com>
2010-06-21 22:41:48 +0000

    completed testing for flag class
    SVN-Revision: 63212

---
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/flag_test.cpp
@@ -0,0 +1,36 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/integer/bitfield_tuple.hpp>
+#include <boost/assert.hpp>
+
+
+using namespace boost;
+
+struct red;
+struct green;
+struct pink;
+struct blue;
+struct salmon;
+
+typedef bitfield_tuple<
+    member<char,red,4>,
+    member<unsigned char, green,5>,
+    storage<std::size_t>,
+    member<int, salmon, 16>,
+    flag<blue>
+>                                       test_tuple;
+
+
+int main() {
+    test_tuple temp;
+
+    temp.get<blue>() = false;
+    BOOST_ASSERT(( temp.get<blue>() == false));
+    return 0;
+}
+
+
