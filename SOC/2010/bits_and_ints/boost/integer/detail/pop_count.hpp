08e2fe6de0140c035876f7f4d1c8fab899ef5071
Murilo Adriano Vasconcelos <muriloufg@gmail.com>
2010-07-09 04:52:48 +0000

    Added missed file
    SVN-Revision: 63774

---
+++ SOC/2010/bits_and_ints/boost/integer/detail/pop_count.hpp
@@ -0,0 +1,26 @@
+//  Boost integer/detail/pop_count.hpp header file  ------------------------------//
+
+//  (C) Copyright Murilo Adriano Vasconcelos 2010.
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt
+
+//  See http://www.boost.org for updates, documentation, and revision history. 
+
+#ifndef BOOST_DETAIL_POPCOUNT_INCLUDED
+#define BOOST_DETAIL_POPCOUNT_INCLUDED
+
+namespace boost { namespace integer_detail {
+
+static const uintmax_t pop_count_mask[6] = {
+#ifndef BOOST_NO_INT64_T
+	0x5555555555555555, 0x3333333333333333, 0x0F0F0F0F0F0F0F0F, 
+	0x00FF00FF00FF00FF, 0x0000FFFF0000FFFF, 0x00000000FFFFFFFF
+#else
+	0x55555555, 0x33333333, 0x0F0F0F0F, 0x00FF00FF, 0x0000FFFF, 0x0
+#endif
+};
+	
+} }
+
+#endif
