9988b0d5a3ee8b0d19fc573255e60f7f82839865
Brian Bartman <bbartmanboost@gmail.com>
2010-07-11 14:33:59 +0000

    forgot to remove function from my test file which caused errors
    SVN-Revision: 63869

--- SOC/2010/bit_masks/lib/integer/test/bft_testing/make_bft_testing.cpp
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/make_bft_testing.cpp
@@ -27,8 +27,6 @@ typedef bitfield_tuple<
 >                                       test_tuple;
 #ifdef BOOST_MSVC
 
-template <typename BitfieldTuple>
-BitfieldTuple make_bitfield_tuple(...);
 
 #endif
 
