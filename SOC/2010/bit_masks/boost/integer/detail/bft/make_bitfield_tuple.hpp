e2af54fa408d90dc27aad2a2507effff77b941d2
Brian Bartman <bbartmanboost@gmail.com>
2010-07-11 12:34:10 +0000

    adding directory for any MSVC fixes I may need to include in my project
    SVN-Revision: 63852

--- SOC/2010/bit_masks/boost/integer/detail/bft/make_bitfield_tuple.hpp
+++ SOC/2010/bit_masks/boost/integer/detail/bft/make_bitfield_tuple.hpp
@@ -47,29 +47,6 @@ struct get_create_parameter {
     typedef typename bft_create_param_fix_return_type<element_type>::type type;
 };
 
-typedef ::boost::bitfield_tuple< bitfield::storage<int> > fixer_tuple;
-
-/** Used for fixing an inadequacy in MSVC9 which causes my make_bitfield_tuple
- *  function template parameters not to be instantiated unless they are
- *  typedef'ed peior to the function declaration.
- */
-#define BOOST_MAKE_BFT_HELPER_TYPEDEF(Z, N, DATA) \
-    typedef get_create_parameter<fixer_tuple,N>::is_valid_index fixer_type ## N;
-
-/** Defines typedefs which prevent the MSVC9 compiler from failing where it 
- *  shouldn't
- *  Creates the following pattern
- *      typedef 
- */
-#define BOOST_MAKE_BFT_COMPENSATE_FOR_MSVC() \
-        BOOST_PP_REPEAT_FROM_TO( \
-            0,\
-            BOOST_BFT_PARAM_COUNT, \
-            BOOST_MAKE_BFT_HELPER_TYPEDEF,\
-            BOOST_BFT_NOTHING )
-
-BOOST_MAKE_BFT_COMPENSATE_FOR_MSVC()
-
 // get_create_parameter<test_tuple,0>::is_valid_index
 /*
 BOOST_MPL_ASSERT(( get_create_parameter<test_tuple,1>::is_valid_index ));
