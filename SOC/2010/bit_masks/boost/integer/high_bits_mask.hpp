2d30dd5612200875240aa900f1148364f9822ed7
Brian Bartman <bbartmanboost@gmail.com>
2010-07-13 18:33:53 +0000

    attempting a different type of fix for msvc. introduced additional warnings in the process fixing them, again
    SVN-Revision: 63981

--- SOC/2010/bit_masks/boost/integer/high_bits_mask.hpp
+++ SOC/2010/bit_masks/boost/integer/high_bits_mask.hpp
@@ -37,10 +37,12 @@ namespace detail {
     template<typename T, unsigned int Width>
     struct evaluate_for_msvc_08 {
         typedef typename make_unsigned<T>::type unsigned_type;
-        BOOST_STATIC_CONSTANT(unsigned_type, val = (~(mpl::shift_right<
-            mpl::size_t<~( T(0) )>,
-            mpl::size_t<Width>
-        >::type::value)
+        BOOST_STATIC_CONSTANT(unsigned_type, val = static_cast<unsigned_type>(
+            ~(mpl::shift_right<
+                mpl::size_t<~( T(0) )>,
+                mpl::size_t<Width>
+            >::type::value)
+            )
         ));
         typedef integral_constant<T, val>       type;
     };
