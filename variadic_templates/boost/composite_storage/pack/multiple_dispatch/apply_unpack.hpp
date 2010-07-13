a6fbff62bb91896045436040fb0464b9e58a8020
Larry Joe Evans <cppljevans@suddenlink.net>
2010-07-13 08:24:53 +0000

    added functor_bad_args as alternative to using can_be_called
    SVN-Revision: 63943

--- variadic_templates/boost/composite_storage/pack/multiple_dispatch/apply_unpack.hpp
+++ variadic_templates/boost/composite_storage/pack/multiple_dispatch/apply_unpack.hpp
@@ -11,8 +11,10 @@
 
 #include <boost/mpl/package_range_c.hpp>
 #include <boost/type_traits/remove_cv.hpp>
-#include <boost/function_types/can_be_called.hpp>
 #include <exception>
+#ifndef APPLY_UNPACK_USER_CHECKED_ARGS
+  #include <boost/function_types/can_be_called.hpp>
+#endif  
 
 namespace boost
 {
@@ -29,14 +31,6 @@ namespace multiple_dispatch
       typedef typename functor_type::result_type type;
   };  
   
-  template <typename Functor, typename... Args>
-    typename functor_result_type<Functor>::type
-  apply_ftor_callable_args( mpl::bool_<true>, Functor& ftor
-    , Args const&... args)
-  {
-      return ftor(args...);
-  };
-  
   template<typename FunctorArgs>
   struct bad_functor_args
     : public std::exception
@@ -49,6 +43,32 @@ namespace multiple_dispatch
       }
   };
       
+#ifdef APPLY_UNPACK_USER_CHECKED_ARGS
+    template
+    < typename Functor
+    , typename ResultType
+    >
+  struct functor_bad_args
+  {
+      typedef ResultType result_type;
+      
+      template<typename... Args>
+      result_type operator()(Args&... args)FUNCTOR_CONSTANCY throw()
+      {
+          throw bad_functor_args<Functor(Args&...)>();
+          return result_type();
+      }
+  }; 
+
+#else
+  template <typename Functor, typename... Args>
+    typename functor_result_type<Functor>::type
+  apply_ftor_callable_args( mpl::bool_<true>, Functor& ftor
+    , Args const&... args)
+  {
+      return ftor(args...);
+  };
+  
   template <typename Functor, typename... Args>
     typename functor_result_type<Functor>::type
   apply_ftor_callable_args( mpl::bool_<false>, Functor& ftor
@@ -77,6 +97,7 @@ namespace multiple_dispatch
       return apply_ftor_callable_args( is_ftor_args_callable()
         , ftor, args...);
   };
+#endif  
   
   template <typename Indices>
   struct apply_unpack;
@@ -89,7 +110,7 @@ namespace multiple_dispatch
       operator()( Functor& a_functor, ArgsPacked const& a_args)
       {
           return 
-          #ifndef APPLY_UNPACK_DEMO_UNCHECK_ARGS
+          #ifndef APPLY_UNPACK_USER_CHECKED_ARGS
             apply_ftor_check_args( a_functor
               , a_args.template project<Indices>()...);
           #else
