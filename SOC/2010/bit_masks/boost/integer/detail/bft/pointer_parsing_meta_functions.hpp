3dce42eede4e2c4082f6e039f4d04335d45c2faf
Brian Bartman <bbartmanboost@gmail.com>
2010-07-17 16:25:34 +0000

    completed initial work of creating metafunctions which will allow the arguments being parsed for bitfield_tuple to be able to handle operations specific to the new pointer member
    SVN-Revision: 64105

--- SOC/2010/bit_masks/boost/integer/detail/bft/pointer_parsing_meta_functions.hpp
+++ SOC/2010/bit_masks/boost/integer/detail/bft/pointer_parsing_meta_functions.hpp
@@ -7,12 +7,125 @@
 #ifndef BOOST_BITFIELD_TUPLE_POINTER_PARSING_HELPERS_HPP
 #define BOOST_BITFIELD_TUPLE_POINTER_PARSING_HELPERS_HPP
 
+#include <boost/integer/bits_mask.hpp>
+#include <cstddef>
+#include <boost/mpl/size_t.hpp>
+#include <boost/type_traits/make_unsigned.hpp>
+
 /** This file contains metafunctions which are used to do complex operations
  *  on the Mask provided by the user.
  */
 namespace boost { namespace detail { namespace pointer_member {
 
 
+
+// this is ugly but I would like it for organizational purposes.
+namespace ptr_detail {
+
+// Count leading zeros helper
+// basically recurse until the true false condition is evaluated as false.
+template <
+    typename Mask,
+    typename IndexingMask = bits_mask<
+        typename make_unsigned<
+            typename Mask::value_type
+        >::type,
+        bit_width<
+            typename Mask::value_type
+        >::value - 1
+    >,
+    std::size_t ZeroCount = 0,
+    bool = true
+>
+struct clz_helper;
+
+/** Continued recursive case. */
+template <
+    typename Mask,
+    typename IndexingMask,
+    std::size_t ZeroCount
+>
+struct clz_helper<Mask,IndexingMask,ZeroCount,true>
+    :clz_helper<
+        Mask,
+        bits_mask<
+            typename IndexingMask::value_type,
+            IndexingMask::offset - 1
+        >,
+        ZeroCount + 1,
+        ((IndexingMask::value & Mask::value) == 0)
+          &&
+        (IndexingMask::offset >= 0)
+    >
+{ };
+
+/** Recursive Termination Case. */ 
+template <
+    typename Mask,
+    typename IndexingMask,
+    std::size_t ZeroCount
+>
+struct clz_helper<Mask,IndexingMask,ZeroCount,false> {
+    typedef mpl::size_t<ZeroCount - 1> type;
+};
+
+
+template <
+    typename Mask,
+    typename IndexingMask = bits_mask<
+        typename make_unsigned< typename Mask::value_type >::type,
+        0
+    >,
+    std::size_t ZeroCount = 0,
+    bool = true
+>
+struct ctz_helper;
+
+/** Recursive loop. */
+
+template <
+    typename Mask,
+    typename IndexingMask,
+    std::size_t ZeroCount
+>
+struct ctz_helper<Mask,IndexingMask,ZeroCount,true>
+    :ctz_helper<
+        Mask,
+        bits_mask<
+            typename IndexingMask::value_type,
+            IndexingMask::offset + 1
+        >,
+        ZeroCount + 1,
+        ((IndexingMask::value & Mask::value) == 0)
+          &&
+        (IndexingMask::offset < bit_width< typename Mask::value_type >::value)
+    >
+{ };
+
+/** Recursive tremination. */
+template <
+    typename Mask,
+    typename IndexingMask,
+    std::size_t ZeroCount
+>
+struct ctz_helper<Mask,IndexingMask,ZeroCount,false> {
+    typedef mpl::size_t<ZeroCount - 1 >  type;
+};
+
+
+
+} // end ptr_detail
+
+template <typename Mask>
+struct count_leading_zeros {
+    typedef typename ptr_detail::clz_helper<Mask>::type type;
+};
+
+
+template <typename Mask>
+struct count_trailing_zeros {
+    typedef typename ptr_detail::ctz_helper<Mask>::type type;
+};
 }}} // end boost::detail::pointer_member
 
 #endif
