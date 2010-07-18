4f0a1efe15bce239f43101d9420b393f1ea5f4af
Brian Bartman <bbartmanboost@gmail.com>
2010-07-18 15:03:17 +0000

    working on creating the pointer member type for bitfield_tuple the first working draft is completed.
    SVN-Revision: 64125

--- SOC/2010/bit_masks/lib/integer/test/bft_testing/pointer_member_test.cpp
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/pointer_member_test.cpp
@@ -24,16 +24,9 @@ int main() {
     t1.get<rd>() = &i;
     BOOST_TEST(*t1.get<rd>() == 0);
     }
+
     // testing member<int*,rd, width of pointer-2>
     {
-
-    // int i = 0;
-    // test_type_2 t2;
-    // main issue with this is that the pointer type may not be retrieved
-    // correctly using bitfield however this shouldn't be an issue if I provide
-    // a specialization for bitfield to operate on pointer types.
-    // t2.get<rd>() = &i;
-    // BOOST_TEST( t2.get<rd>() == &i );
     }
     return boost::report_errors();
 }
