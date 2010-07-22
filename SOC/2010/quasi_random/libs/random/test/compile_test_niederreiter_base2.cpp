b4db865ddde5daa90e26c6201ea34fbe9753ee89
Justinas Vygintas Daugmaudis <jvd@tja.lt>
2010-07-22 11:10:56 +0000

    Added trivial compile tests. A compilation unit has to be well formed if a header is #included on its own.
    SVN-Revision: 64256

---
+++ SOC/2010/quasi_random/libs/random/test/compile_test_niederreiter_base2.cpp
@@ -0,0 +1,15 @@
+// Copyright Justinas Vygintas Daugmaudis, 2010.
+// Use, modification and distribution is subject to the
+// Boost Software License, Version 1.0. (See accompanying
+// file LICENSE-1.0 or http://www.boost.org/LICENSE-1.0)
+
+#include <boost/random/niederreiter_base2.hpp>
+
+namespace {
+
+void instantiate()
+{
+  boost::niederreiter_base2_1d t;
+}
+
+}
---
+++ SOC/2010/quasi_random/libs/random/test/compile_test_sobol.cpp
@@ -0,0 +1,15 @@
+// Copyright Justinas Vygintas Daugmaudis, 2010.
+// Use, modification and distribution is subject to the
+// Boost Software License, Version 1.0. (See accompanying
+// file LICENSE-1.0 or http://www.boost.org/LICENSE-1.0)
+
+#include <boost/random/sobol.hpp>
+
+namespace {
+
+void instantiate()
+{
+  boost::sobol_1d t;
+}
+
+}
