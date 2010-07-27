1adb75308a062cc230e6e5c6612bf768fbd3ae3e
Brian Bartman <bbartmanboost@gmail.com>
2010-07-27 16:21:22 +0000

    fixing misspelled file name
    SVN-Revision: 64389

---
+++ SOC/2010/bit_masks/boost/integer/bitfield_tuple/element.hpp
@@ -0,0 +1,28 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+
+#ifndef BOOST_BITFIELD_TUPLE_ELEMENT_HPP
+#define BOOST_BITFIELD_TUPLE_ELEMENT_HPP
+#include <boost/integer/bitfield_tuple/interface_metafunctions.hpp>
+#include <cstddef>
+
+namespace boost {
+namespace bitfields {
+
+
+template<std::size_t Index, typename BitfieldTuple>
+struct element {
+    typedef typename get_proxy_reference_type_by_index< BitfieldTuple, Index >::type type;
+};
+
+template<typename Name, typename BitfieldTuple>
+struct element_n {
+    typedef typename get_proxy_reference_type_by_name< BitfieldTuple, Name >::type type;
+};
+
+}} // end boost::bitfields
+
+#endif
