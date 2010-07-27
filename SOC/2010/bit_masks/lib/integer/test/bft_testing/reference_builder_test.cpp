33ce495672cf6b5b6241ac767cf694672b6e3028
Brian Bartman <bbartmanboost@gmail.com>
2010-07-27 19:58:29 +0000

    updating test suites for reference creation helper types
    SVN-Revision: 64399

--- SOC/2010/bit_masks/lib/integer/test/bft_testing/reference_builder_test.cpp
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/reference_builder_test.cpp
@@ -5,7 +5,7 @@
 
 
 #include <boost/integer/bitfield_tuple.hpp>
-#include <boost/assert.hpp>
+#include <boost/detail/lightweight_test.hpp>
 
 
 using namespace boost;
@@ -38,13 +38,29 @@ int main() {
             test_tuple_1,
             salmon
         >::type                 ref_t_2;
+
+        typedef bitfields::detail::enable_if_proxy_reference_type_by_index<
+            test_tuple_1,
+            0
+        >::type                 ref_t_3;
+        typedef bitfields::detail::enable_if_proxy_reference_type_by_index<
+            const test_tuple_1,
+            0
+        >::type                 ref_t_4;
+        //  test_tuple_1
+        ref_t_3 non_const_ref_3(store_house);
+
         ref_t_2 non_const_ref(store_house);
         non_const_ref = 3;
         ref_t_1 const_ref(store_house);
-        BOOST_ASSERT(( const_ref == 3));
+        BOOST_TEST( const_ref == 3);
+
+        non_const_ref_3 = 4;
+        ref_t_3 const_ref_t_4(store_house);
+        BOOST_TEST( const_ref_t_4 == 4 );
     }
     
 
 
-    return 0;
+    return boost::report_errors();
 }
