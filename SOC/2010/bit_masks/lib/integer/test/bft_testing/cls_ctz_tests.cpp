4b1e4ea882d8f5b7cba3d43be3df1f98050a2f1b
Brian Bartman <bbartmanboost@gmail.com>
2010-07-29 14:16:27 +0000

    need to rename a test so i'm commiting first
    SVN-Revision: 64441

---
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/cls_ctz_tests.cpp
@@ -0,0 +1,37 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/integer/bitfield_tuple.hpp>
+#include <boost/detail/lightweight_test.hpp>
+
+
+using namespace boost;
+using namespace boost::bitfields;
+using namespace boost::bitfields::detail::pointer_member;
+
+typedef bits_mask< int, 2, bit_width<int>::value - 4> mask_1;
+typedef bits_mask< int, 4, bit_width<int>::value - 4> mask_2;
+typedef bits_mask< int, 0, bit_width<int>::value - 4> mask_2;
+int main() {
+    // count leading zeros' test
+    {
+        // count_leading_zeros
+        BOOST_TEST( count_leading_zeros<mask_1>::value == 2);
+        BOOST_TEST( count_leading_zeros<mask_2>::value == 0);
+        BOOST_TEST( count_leading_zeros<mask_3>::value == 4);
+    }
+
+    // count trailing zeros' test
+    {
+        // count_trailing_zeros
+        BOOST_TEST( count_trailing_zeros<mask_1>::value == 2);
+        BOOST_TEST( count_trailing_zeros<mask_2>::value == 4);
+        BOOST_TEST( count_trailing_zeros<mask_3>::value == 0);
+
+    }
+    return boost::report_errors();
+}
+
