bc2efd4b5c6a4d5bade5c4c50950e4b5e0de718d
Brian Bartman <bbartmanboost@gmail.com>
2010-07-29 18:25:10 +0000

    added additional compile failure tests for more of the interface meta-functions
    SVN-Revision: 64452

---
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/compile_fail/find_by_index_with_indvalid_index.cpp
@@ -0,0 +1,25 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/integer/bitfield_tuple.hpp>
+
+using namespace boost;
+using namespace boost::bitfields;
+
+struct red { };
+
+// fails because 3 isn't a valid index.
+typedef bitfield_tuple<
+    storage<int>,
+    member<int,red,2>
+> bft;
+
+
+
+int main() {
+    typedef find_by_element_index<bft,3>::type t;
+    return 0;
+}
---
+++ SOC/2010/bit_masks/lib/integer/test/bft_testing/compile_fail/get_proxy_reference_type_by_name_name_does_not_exist.cpp
@@ -0,0 +1,27 @@
+//  Copyright 2010 Brian Bartman.
+//  Distributed under the Boost Software License, Version 1.0.
+//  (See accompanying file LICENSE_1_0.txt or copy at 
+//  http://www.boost.org/LICENSE_1_0.txt)
+
+
+#include <boost/integer/bitfield_tuple.hpp>
+
+using namespace boost;
+using namespace boost::bitfields;
+
+struct red { };
+struct pink;
+
+// fails because pink isn't a valid name
+typedef bitfield_tuple<
+    storage<int>,
+    member<int,red,2>
+> bft;
+
+
+
+int main() {
+    typedef get_proxy_reference_type_by_name<bft,pink>::type t;
+    t();
+    return 0;
+}
