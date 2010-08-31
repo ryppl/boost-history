aa049229f93afb2620ab3ba11b68dc7cb1a22a5f
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2010-08-31 19:42:49 +0000

    Refactoring of lihtweight_stopwatch
    SVN-Revision: 65155

--- chrono/libs/chrono/example/specific_stopwatch_accumulator_example.cpp
+++ chrono/libs/chrono/example/specific_stopwatch_accumulator_example.cpp
@@ -29,9 +29,9 @@ using namespace boost::accumulators;
                                 int places, ostream_type & os, boost::system::error_code & ec)
         {
             if (&ec != &boost::system::throws) ec.clear();
-            typedef typename Stopwatch::accumulator accumulator_t;
+            typedef typename Stopwatch::storage_type accumulator_t;
             typedef typename Stopwatch::duration duration_t;
-            accumulator_t& acc = stopwatch_.accumulated();
+            accumulator_t& acc = stopwatch_.get_storage();
 
             boost::io::ios_flags_saver ifs( os );
             os.setf( std::ios_base::fixed, std::ios_base::floatfield );
