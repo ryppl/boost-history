a5582de057fe51d7226959cdd0ba6d9f7f686d9b
Joachim Faulhaber <afojgo@gmail.com>
2010-09-01 22:09:37 +0000

    Refactoring. Unifying interval_sets. Extracted global functions. Stable{msvc-9.0, gcc-3.4.4}
    SVN-Revision: 65183

---
+++ itl/boost/itl/type_traits/is_interval_joiner.hpp
@@ -0,0 +1,23 @@
+/*-----------------------------------------------------------------------------+    
+Copyright (c) 2008-2009: Joachim Faulhaber
++------------------------------------------------------------------------------+
+   Distributed under the Boost Software License, Version 1.0.
+      (See accompanying file LICENCE.txt or copy at
+           http://www.boost.org/LICENSE_1_0.txt)
++-----------------------------------------------------------------------------*/
+#ifndef BOOST_ITL_TYPE_TRAITS_IS_INTERVAL_JOINER_HPP_JOFA_100901
+#define BOOST_ITL_TYPE_TRAITS_IS_INTERVAL_JOINER_HPP_JOFA_100901
+
+namespace boost{ namespace itl
+{
+    template <class Type> struct is_interval_joiner
+    { 
+        typedef is_interval_joiner<Type> type;
+        BOOST_STATIC_CONSTANT(bool, value = false); 
+    };
+
+}} // namespace boost itl
+
+#endif
+
+
