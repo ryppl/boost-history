a98b1737a17a890cb1d902c1083980d6176891f5
Joachim Faulhaber <afojgo@gmail.com>
2010-09-03 10:56:53 +0000

    Refactoring. Unifying interval_sets: Extraction of major functions complete. Stable{msvc-9.0, gcc-3.4.4}
    SVN-Revision: 65216

---
+++ itl/boost/itl/type_traits/domain_type_of.hpp
@@ -0,0 +1,53 @@
+/*-----------------------------------------------------------------------------+    
+Copyright (c) 2008-2009: Joachim Faulhaber
++------------------------------------------------------------------------------+
+   Distributed under the Boost Software License, Version 1.0.
+      (See accompanying file LICENCE.txt or copy at
+           http://www.boost.org/LICENSE_1_0.txt)
++-----------------------------------------------------------------------------*/
+#ifndef BOOST_ITL_TYPE_TRAITS_DOMAIN_TYPE_OF_HPP_JOFA_100902
+#define BOOST_ITL_TYPE_TRAITS_DOMAIN_TYPE_OF_HPP_JOFA_100902
+
+#include <boost/mpl/has_xxx.hpp>
+#include <boost/mpl/bool.hpp>
+#include <boost/itl/type_traits/no_type.hpp>
+
+namespace boost{ namespace itl
+{
+    namespace detail
+    {
+        BOOST_MPL_HAS_XXX_TRAIT_DEF(domain_type)
+    }
+
+    template <class Type>
+    struct has_domain_type 
+      : mpl::bool_<detail::has_domain_type<Type>::value>
+    {};
+
+    template <class Type, bool has_domain_type> 
+	struct get_domain_type;
+
+    template <class Type>
+	struct get_domain_type<Type, false>
+    {
+		typedef no_type type;
+    };
+
+    template <class Type>
+	struct get_domain_type<Type, true>
+    {
+		typedef typename Type::domain_type type;
+    };
+
+    template <class Type>
+	struct domain_type_of
+    {
+		typedef typename 
+			get_domain_type<Type, has_domain_type<Type>::value>::type type;
+    };
+
+}} // namespace boost itl
+
+#endif
+
+
