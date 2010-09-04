67d05067494979c30e789aa838704bf631e359e0
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2010-09-04 14:40:47 +0000

    Continue the library split with Boost.TypeTraits.ExtContinue the library split with Boost.Ratio and Boost.TypeTraits.Ext
    SVN-Revision: 65253

---
+++ chrono/boost/type_traits/add_rvalue_reference.hpp
@@ -0,0 +1,15 @@
+//  common_type.hpp  ---------------------------------------------------------//
+
+//  Copyright 2008 Howard Hinnant
+//  Copyright 2008 Beman Dawes
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_TYPE_TRAITS_ADD_RVALUE_REFERENCE__HPP
+#define BOOST_TYPE_TRAITS_ADD_RVALUE_REFERENCE__HPP
+
+#include <boost/type_traits/ext/add_rvalue_reference.hpp>
+
+
+#endif  // BOOST_TYPE_TRAITS_ADD_RVALUE_REFERENCE__HPP
---
+++ chrono/boost/type_traits/declval.hpp
@@ -0,0 +1,14 @@
+//  common_type.hpp  ---------------------------------------------------------//
+
+//  Copyright 2008 Howard Hinnant
+//  Copyright 2008 Beman Dawes
+
+//  Distributed under the Boost Software License, Version 1.0.
+//  See http://www.boost.org/LICENSE_1_0.txt
+
+#ifndef BOOST_TYPE_TRAITS_DECLVAL__HPP
+#define BOOST_TYPE_TRAITS_DECLVAL__HPP
+
+#include <boost/type_traits/ext/declval.hpp>
+
+#endif  // BOOST_TYPE_TRAITS_DECLVAL__HPP
