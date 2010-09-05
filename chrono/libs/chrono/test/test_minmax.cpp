059d85a7b1c7dd2453e2157114b6b801704a94af
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2010-09-05 10:46:54 +0000

    Fix test minmax to be run only if GCC is not defined
    SVN-Revision: 65288

--- chrono/libs/chrono/test/test_minmax.cpp
+++ chrono/libs/chrono/test/test_minmax.cpp
@@ -5,15 +5,13 @@
 //  Distributed under the Boost Software License, Version 1.0.
 //  See http://www.boost.org/LICENSE_1_0.txt
 
+#if !defined(__GNUC__)
 
 #define min(A,B) ((A)<(B)?(A):(B))
 #define max(A,B) ((A)>(B)?(A):(B))
 
 #include <boost/chrono/chrono.hpp>
-#include <boost/chrono/stopclock.hpp>
 
- int main()
- {
-    boost::chrono::stopclock<> _;
- }
+#endif
+
 
