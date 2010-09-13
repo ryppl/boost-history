10e57cb1c27f95078bc748dc6b34b98ab76f33ce
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2010-09-13 05:27:07 +0000

    Added is_lockable_type
    SVN-Revision: 65408

---
+++ sync/boost/sync/lockable_traits/is_lockable_type.hpp
@@ -0,0 +1,45 @@
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+// (C) Copyright 2007 Anthony Williams
+// (C) Copyright 2010 Vicente J. Botet Escriba
+
+#ifndef BOOST_SYNC_IS_LOCKABLE_TYPE_HPP
+#define BOOST_SYNC_IS_LOCKABLE_TYPE_HPP
+
+#include <boost/config.hpp>
+#include <boost/sync/lockable_traits/detail/has_members.hpp>
+
+namespace boost
+{
+
+#ifndef BOOST_SYNC_NO_AUTO_DETECT_TYPES   
+
+    template<typename T>
+    struct is_mutex_type
+    {
+        BOOST_STATIC_CONSTANT(bool, value = 
+            detail::has_member_lock<T>::value &&
+            detail::has_member_unlock<T>::value &&
+            detail::has_member_try_lock<T>::value);
+        
+    };
+#else
+    template<typename T>
+    struct is_mutex_type
+    {
+        BOOST_STATIC_CONSTANT(bool, value = false);
+    };
+#endif    
+
+    template<typename T>
+    struct is_lockable_type
+    {
+        BOOST_STATIC_CONSTANT(bool, value=is_mutex_type<T>::value);
+    };
+    
+    
+}
+
+
+#endif
