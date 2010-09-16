68e2a69c54c01d05c8dac834911f9b8131507bbf
xushiwei <xushiweizh@gmail.com>
2010-09-16 07:30:33 +0000

    1. windows 64bits -- to do 2. class MemoryState;
    SVN-Revision: 65435

--- memory/libs/memory/examples/pool/simple_examples.cpp
+++ memory/libs/memory/examples/pool/simple_examples.cpp
@@ -10,7 +10,6 @@
 //  See http://www.boost.org/libs/memory/index.htm for documentation.
 //
 #include <boost/memory.hpp>
-#include <boost/memory/object_pool.hpp>
 
 // -------------------------------------------------------------------------
 
@@ -21,8 +20,8 @@
 void testPool()
 {
 	int i;
-	boost::memory::pool alloc(sizeof(int));
-	boost::memory::pool alloc2(sizeof(double));
+	NS_BOOST_MEMORY::pool alloc(sizeof(int));
+	NS_BOOST_MEMORY::pool alloc2(sizeof(double));
 
 	int* p[3000];
 	for (i = 0; i < countof(p); ++i)
@@ -53,9 +52,9 @@ void testPool()
 void testScopedPool()
 {
 	int i;
-	boost::memory::block_pool recycle;
-	boost::memory::scoped_pool alloc(recycle, sizeof(int));
-	boost::memory::scoped_pool alloc2(alloc.get_alloc(), sizeof(double));
+	NS_BOOST_MEMORY::block_pool recycle;
+	NS_BOOST_MEMORY::scoped_pool alloc(recycle, sizeof(int));
+	NS_BOOST_MEMORY::scoped_pool alloc2(alloc.get_alloc(), sizeof(double));
 
 	void* p[3000];
 	for (i = 0; i < countof(p); ++i)
@@ -78,63 +77,21 @@ void testScopedPool()
 		alloc.allocate();
 }
 
-int g_nConstruct = 0;
-int g_nDestruct = 0;
-
-struct Obj
+void testPools()
 {
-	char m_val[100];
+	NS_BOOST_MEMORY::pools alloc;
 
-	Obj()		 { ++g_nConstruct; m_val[1] = 0x23; }
-	Obj(int val) { ++g_nConstruct; }
-	~Obj()		 { ++g_nDestruct; }
-};
-
-void testObjectPool()
-{
-	boost::memory::object_pool<int> alloc;
-	int* p1 = alloc.construct();
-	int* p2 = alloc.construct();
-	int* p3 = BOOST_NEW(alloc, int)(30);
-	BOOST_MEMORY_ASSERT(*p3 == 30);
-
-	{
-		boost::memory::object_pool<Obj> alloc2;
-		for (int i = 0; i < 3000; ++i)
-		{
-			Obj* o1 = alloc2.construct();
-			Obj* o2 = alloc2.construct();
-			Obj* o3 = BOOST_NEW(alloc2, Obj)(20);
-			Obj* o4 = BOOST_NEW(alloc2, Obj)(25);
-			alloc2.destroy(o2);
-			alloc2.destroy(o3);
-		}
-	}
-	BOOST_MEMORY_ASSERT(g_nConstruct == g_nDestruct);
-	if (g_nConstruct != g_nDestruct)
-		printf("ERROR: memory leaks!\n");
-}
-
-void testScopedPools()
-{
-	size_t i;
-	boost::memory::block_pool recycle;
-	boost::memory::scoped_pools alloc(recycle);
-
-	void* p[22];
-	for (i = 0; i < countof(p); ++i)
-		p[i] = alloc.get_pool(i * 7).allocate();
-	for (i = 3; i < countof(p) - 2; ++i)
-		alloc.get_pool(i * 7).deallocate(p[i]);
+	void* p = alloc.allocate(134);
+	alloc.deallocate(p, 134);
+	//alloc.deallocate(p, 134);
 }
 
 int main()
 {
 	NS_BOOST_MEMORY::enableMemoryLeakCheck();
 
-	testScopedPools();
 	testPool();
 	testScopedPool();
-	testObjectPool();
+	testPools();
 	return 0;
 }
