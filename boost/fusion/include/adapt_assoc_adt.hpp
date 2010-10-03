a591c7397c88246398c3ff7372d0febfa65f30f0
Christopher Schmidt <mr.chr.schmidt@online.de>
2010-10-03 09:46:00 +0000

    Fusion: merge from trunk
    SVN-Revision: 65735

--- boost/fusion/include/adapt_assoc_adt.hpp
+++ boost/fusion/include/adapt_assoc_adt.hpp
@@ -1,12 +1,13 @@
 /*=============================================================================
-    Copyright (c) 2001-2009 Joel de Guzman
+    Copyright (c) 2010 Christopher Schmidt
 
     Distributed under the Boost Software License, Version 1.0. (See accompanying
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 ==============================================================================*/
-#if !defined(FUSION_INCLUDE_ADAPT_CLASS)
-#define FUSION_INCLUDE_ADAPT_CLASS
 
-#include <boost/fusion/adapted/class/adapt_class.hpp>
+#ifndef BOOST_FUSION_INCLUDE_ADAPT_ASSOC_ADT_HPP
+#define BOOST_FUSION_INCLUDE_ADAPT_ASSOC_ADR_HPP
+
+#include <boost/fusion/adapted/adt/adapt_assoc_adt.hpp>
 
 #endif
