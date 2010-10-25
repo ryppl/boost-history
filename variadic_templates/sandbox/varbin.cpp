bdb3e25991b7ca45bf1f7c67d1d39fef15c0aa97
Larry Joe Evans <cppljevans@suddenlink.net>
2010-10-25 18:09:49 +0000

    Hmmm, this compiles:
    -*- mode: compilation; default-directory: "~/prog_dev/boost-svn/ro/sandbox/rw/variadic_templates/sandbox/" -*-
    Compilation started at Mon Oct 25 13:09:13
    
    make MAIN=varbin o
    install -d `dirname /home/evansl/prog_dev/boost-svn/ro/trunk/sandbox-local/build/gcc4_5_1v/boost-svn/ro/sandbox/rw/variadic_templates/sandbox/varbin.o`
    /home/evansl/download/gcc/4.5.1-release/install/bin/g++ -c -Wall -ftemplate-depth-300 -Os -std=gnu++0x -DCXX0X_VARIADIC_TEMPLATES -DBOOST_USE_MPL_VARIADIC_TEMPLATES -I/home/evansl/prog_dev/boost-svn/ro/sandbox/rw/variadic_templates -I/home/evansl/prog_dev/boost-svn/ro/sandbox/ro/switch -I/home/evansl/prog_dev/boost-svn/ro/trunk  -DTEMPLATE_DEPTH=300  varbin.cpp -MMD -o /home/evansl/prog_dev/boost-svn/ro/trunk/sandbox-local/build/gcc4_5_1v/boost-svn/ro/sandbox/rw/variadic_templates/sandbox/varbin.o
    sed -e 's#^varbin.o:#/home/evansl/prog_dev/boost-svn/ro/trunk/sandbox-local/build/gcc4_5_1v/boost-svn/ro/sandbox/rw/variadic_templates/sandbox/varbin.o:#' /home/evansl/prog_dev/boost-svn/ro/trunk/sandbox-local/build/gcc4_5_1v/boost-svn/ro/sandbox/rw/variadic_templates/sandbox/varbin.d> /home/evansl/prog_dev/boost-svn/ro/trunk/sandbox-local/build/gcc4_5_1v/boost-svn/ro/sandbox/rw/variadic_templates/sandbox/varbin.o.dep
    
    Compilation finished at Mon Oct 25 13:09:14
    
    SVN-Revision: 66186

--- variadic_templates/sandbox/varbin.cpp
+++ variadic_templates/sandbox/varbin.cpp
@@ -2,19 +2,21 @@
 //  see if variadic reduce is possible using variadic templates.
 //
 //Result:
-//  No, apparently b... is not an arg pack.
-//
-int add(int a,int b){return a+b;}
+// //  No, apparently b... is not an arg pack.
+// //
+// int add(int a,int b){return a+b;}
 
-template<typename T>
-T fold(T (*op)(T,T), T z, T a...)
+template<typename T, typename... U>
+T fold(T (*op)(T,T), T z, U... a)
 ;
+
 template<typename T>
 T fold(T (*op)(T,T), T z, T a)
 { return op(z,a)
 ;}
-template<typename T>
-T fold(T (*op)(T,T), T z, T a, T b...)
+
+template<typename T, typename... U>
+T fold(T (*op)(T,T), T z, T a, U... b)
 { return op(z,fold(op,a,b...))
 ;}
 
