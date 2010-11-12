991d713e278fda4dc68dd60cac6dff6a3437a187
Erwann Rogard <erwann.rogard@gmail.com>
2010-11-12 16:45:36 +0000

    adding boost/assign/v2/ref/anon/unary
    SVN-Revision: 66542

---
+++ statistics/support/boost/assign/v2/ref/anon/unary/assign_rebind.hpp
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_REF_ANON_UNARY_ASSIGN_REBIND_ER_2010_HPP
+#define BOOST_ASSIGN_V2_REF_ANON_UNARY_ASSIGN_REBIND_ER_2010_HPP
+#include <boost/assign/v2/ref/wrapper/rebind.hpp>
+#include <boost/assign/v2/ref/anon/unary/assign_xxx.hpp>
+
+BOOST_ASSIGN_V2_REF_ANON_UNARY_XXX( assign_rebind, assign_tag::rebind )
+
+#endif
---
+++ statistics/support/boost/assign/v2/ref/anon/unary/assign_xxx.hpp
@@ -0,0 +1,64 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_ASSIGN_V2_REF_ANON_UNARY_XXX
+#include <boost/assign/v2/detail/keyword/nil.hpp>
+#include <boost/assign/v2/ref/anon/unary/make.hpp>
+#define BOOST_ASSIGN_V2_REF_ANON_UNARY_XXX(NS, Tag1) \
+ \
+namespace boost{ \
+namespace assign{ \
+namespace v2{ \
+namespace ref{ \
+namespace NS{ \
+namespace nth_result_of{ \
+    template<std::size_t N,typename T> \
+	struct anon : ref::nth_result_of::anon<N,Tag1,T>{}; \
+}\
+namespace result_of{ \
+ \
+	template<typename T> \
+    struct anon_nil : ref::result_of::anon_nil< \
+        Tag1, T \
+    >{}; \
+ \
+ 	template<typename T> \
+    struct anon :  ref::result_of::anon< \
+    	Tag1, T \
+    >{}; \
+ \
+} \
+	template<typename T> \
+    typename NS::result_of::anon_nil<T>::type \
+	anon( keyword_aux::nil const& ) \
+    { \
+    	return ref::anon<Tag1,T>( v2::_nil ); \
+    } \
+    template<typename T> \
+	typename NS::result_of::anon<T>::type \
+    anon(T& t) \
+    { \
+    	return ref::anon<Tag1>( t ); \
+    } \
+    template<typename T> \
+	typename NS::result_of::anon<T const>::type \
+    anon(T const& t) \
+    { \
+    	return ref::anon<Tag1>( t ); \
+    } \
+\
+} \
+} \
+} \
+} \
+} \
+/**/
+
+#endif
