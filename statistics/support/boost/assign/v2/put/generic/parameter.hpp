b5aeb98aeca8230202b36314cbc560f105484df1
Erwann Rogard <erwann.rogard@gmail.com>
2010-11-29 18:40:12 +0000

    small reorganization to boost/assign/v2
    SVN-Revision: 66846

--- statistics/support/boost/assign/v2/put/generic/parameter.hpp
+++ statistics/support/boost/assign/v2/put/generic/parameter.hpp
@@ -7,13 +7,24 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_V2_PUT_ER_2010_HPP
-#define BOOST_ASSIGN_V2_PUT_ER_2010_HPP
+#ifndef BOOST_ASSIGN_V2_PUT_GENERIC_PARAMETER_ER_2010_HPP
+#define BOOST_ASSIGN_V2_PUT_GENERIC_PARAMETER_ER_2010_HPP
 
-#include <boost/assign/v2/put/compose.hpp>
-#include <boost/assign/v2/put/generic.hpp>
-#include <boost/assign/v2/put/modifier.hpp>
-#include <boost/assign/v2/put/range.hpp>
-#include <boost/assign/v2/put/sub.hpp>
+namespace boost{
+namespace assign{
+namespace v2{
+namespace put_parameter{
+
+	template<typename F,typename Tag>
+	struct helper{
+        typedef F fun_type;
+        typedef Tag modifier_tag;
+		typedef helper<F, Tag> type;
+	};
+
+}// put_parameter
+}// v2
+}// assign
+}// boost
 
 #endif
