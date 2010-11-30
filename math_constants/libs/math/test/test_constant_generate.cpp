36e089a468e77e828f62828edc2aa375f7061bc3
John Maddock <john@johnmaddock.co.uk>
2010-11-30 17:59:24 +0000

    Added generator to make adding new constants easier.
    SVN-Revision: 66905

---
+++ math_constants/libs/math/test/test_constant_generate.cpp
@@ -0,0 +1,15 @@
+// Copyright John Maddock 2010.
+
+// Use, modification and distribution are subject to the
+// Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt
+// or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <boost/math/constants/generate.hpp>
+
+int main()
+{
+   BOOST_CONSTANTS_GENERATE(pi);
+   return 0;
+}
+
