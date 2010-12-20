7c395d4f965d3960d1fb9c3a3040d3055e062dbc
Barend Gehrels <barend@xs4all.nl>
2010-12-20 21:25:21 +0000

    Added Reverse to copy_segment_point.hpp, and therefore, to enrich_intersection_points.hpp, handle_tangencies.hpp Protected point_order with MPL_ASSERT
    SVN-Revision: 67374

--- geometry/boost/geometry/multi/algorithms/detail/overlay/copy_segment_point.hpp
+++ geometry/boost/geometry/multi/algorithms/detail/overlay/copy_segment_point.hpp
@@ -5,14 +5,14 @@
 // Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
 
-#ifndef BOOST_GEOMETRY_MULTI_ALGORITHMS_OVERLAY_COPY_SEGMENT_POINT_HPP
-#define BOOST_GEOMETRY_MULTI_ALGORITHMS_OVERLAY_COPY_SEGMENT_POINT_HPP
+#ifndef BOOST_GEOMETRY_MULTI_ALGORITHMS_DETAIL_OVERLAY_COPY_SEGMENT_POINT_HPP
+#define BOOST_GEOMETRY_MULTI_ALGORITHMS_DETAIL_OVERLAY_COPY_SEGMENT_POINT_HPP
 
 
 #include <boost/range.hpp>
 
 #include <boost/geometry/multi/core/tags.hpp>
-#include <boost/geometry/algorithms/overlay/copy_segment_point.hpp>
+#include <boost/geometry/algorithms/detail/overlay/copy_segment_point.hpp>
 
 
 namespace boost { namespace geometry
@@ -62,6 +62,7 @@ namespace dispatch
 template
 <
     typename MultiGeometry,
+    bool Reverse,
     typename SegmentIdentifier,
     typename PointOut
 >
@@ -69,6 +70,7 @@ struct copy_segment_point
     <
         multi_polygon_tag,
         MultiGeometry,
+        Reverse,
         SegmentIdentifier,
         PointOut
     >
@@ -80,6 +82,7 @@ struct copy_segment_point
             detail::copy_segments::copy_segment_point_polygon
                 <
                     typename boost::range_value<MultiGeometry>::type,
+                    Reverse,
                     SegmentIdentifier,
                     PointOut
                 >
@@ -94,4 +97,4 @@ struct copy_segment_point
 }} // namespace boost::geometry
 
 
-#endif // BOOST_GEOMETRY_MULTI_ALGORITHMS_OVERLAY_COPY_SEGMENT_POINT_HPP
+#endif // BOOST_GEOMETRY_MULTI_ALGORITHMS_DETAIL_OVERLAY_COPY_SEGMENT_POINT_HPP
