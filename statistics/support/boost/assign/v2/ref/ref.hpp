c18ec94d7c016dd95befabadc0d8b2f79e6e37a7
Erwann Rogard <erwann.rogard@gmail.com>
2011-01-09 02:04:31 +0000

    upd boost/assign/v2/ref
    SVN-Revision: 67839

--- statistics/support/boost/assign/v2/ref/ref.hpp
+++ statistics/support/boost/assign/v2/ref/ref.hpp
@@ -1,18 +1,1 @@
-//////////////////////////////////////////////////////////////////////////////
-//  Boost.Assign v2                                                         //
-//                                                                          //
-//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
-//  Copyright (C) 2010 Erwann Rogard                                        //
-//  Use, modification and distribution are subject to the                   //
-//  Boost Software License, Version 1.0. (See accompanying file             //
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
-//////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_V2_REF_REF_ER_2010_HPP
-#define BOOST_ASSIGN_V2_REF_REF_ER_2010_HPP
-
-#include <boost/assign/v2/ref/anon.hpp>
-#include <boost/assign/v2/ref/fusion.hpp>
-#include <boost/assign/v2/ref/fusion_matrix.hpp>
-#include <boost/assign/v2/ref/wrapper.hpp>
-
-#endif
+// TODO remove file
