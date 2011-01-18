8496841ed7da5f38d99995ea7b99502438108a76
Erwann Rogard <erwann.rogard@gmail.com>
2011-01-18 19:57:07 +0000

    rm boost/assign/v2
    SVN-Revision: 68251

--- statistics/support/boost/assign/v2/detail/config/borland_workaround.hpp
+++
@@ -1,9 +0,0 @@
-
-// Maybe needed for unit testing
-
-#include <boost/detail/workaround.hpp>
-
-#if BOOST_WORKAROUND(__BORLANDC__, BOOST_TESTED_AT(0x564))
-#  pragma warn -8091 // supress warning in Boost.Test
-#  pragma warn -8057 // unused argument argc/argv in Boost.Test
-#endif
