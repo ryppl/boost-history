21441c4d0d0944dad9d630f53c3280c0b536b9f0
Erwann Rogard <erwann.rogard@gmail.com>
2011-01-21 01:39:11 +0000

    upd assign_v2
    SVN-Revision: 68336

--- assign_v2/libs/assign/v2/test/utility/chain.cpp
+++ assign_v2/libs/assign/v2/test/utility/chain.cpp
@@ -7,8 +7,7 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#include <boost/assign/v2/utility/chain/checking/twin_values.hpp>
-#include <boost/assign/v2/utility/chain/checking/distinct_values.hpp>
+#include <boost/assign/v2/utility/chain/check.hpp>
 #include <libs/assign/v2/test/utility/chain.h>
 
 namespace test_assign_v2{
@@ -19,11 +18,8 @@ namespace xxx_chain{
     {
     	namespace as2 = boost::assign::v2;
         namespace ns = as2::checking::chain;
-        ns::twin_values::do_check<int>();
-      	//ns::distinct_values::do_check<short>();
-      	//ns::distinct_values::do_check<int>();
-      	//ns::distinct_values::do_check<float>();
-      	//ns::distinct_values::do_check<double>();
+        ns::do_check<int>();
+        ns::do_check<double>();
     }
 
 }// xxx_chain
