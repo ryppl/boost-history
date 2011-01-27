1d0ed7f730553a59d5e4be7e382207e928ab1d51
Erwann Rogard <erwann.rogard@gmail.com>
2011-01-27 23:41:18 +0000

    upd assign_v2
    SVN-Revision: 68505

---
+++ assign_v2/boost/assign/v2/ref/array/size_type.hpp
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_REF_ARRAY_SIZE_TYPE_ER_2010_HPP
+#define BOOST_ASSIGN_V2_REF_ARRAY_SIZE_TYPE_ER_2010_HPP
+#include <cstddef>
+
+namespace boost{
+namespace assign{
+namespace v2{
+namespace ref{
+namespace array_aux{
+
+	typedef std::size_t size_type;
+
+}// array_aux
+}// ref
+}// v2
+}// assign
+}// boost
+
+#endif BOOST_ASSIGN_V2_REF_ARRAY_SIZE_TYPE_ER_2010_HPP
---
+++ assign_v2/boost/assign/v2/ref/list.hpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_REF_LIST_ER_2010_HPP
+#define BOOST_ASSIGN_V2_REF_LIST_ER_2010_HPP
+
+#include <boost/assign/v2/ref/list/holder.hpp>
+#include <boost/assign/v2/ref/list/array.hpp>
+#include <boost/assign/v2/ref/list/policy.hpp>
+#include <boost/assign/v2/ref/list/at.hpp>
+#include <boost/assign/v2/ref/list/container.hpp>
+#include <boost/assign/v2/ref/list/nth_result_of.hpp>
+#include <boost/assign/v2/ref/list/size_type.hpp>
+#include <boost/assign/v2/ref/list/make.hpp>
+
+// Must remain commented out
+// #include <boost/assign/v2/ref/list/converter.hpp>
+
+#endif
