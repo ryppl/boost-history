72eb8c10c682b367eac999721eadcac7831201b8
Edward Diener <eldiener@tropicsoft.com>
2011-02-12 02:26:27 +0000

    Change to boost::tti and BOOST_TTI
    SVN-Revision: 68796

--- tti/libs/tti/test/test_member_type.hpp
+++ tti/libs/tti/test/test_member_type.hpp
@@ -1,16 +1,16 @@
 #if !defined(TEST_MEMBER_TYPE_HPP)
 #define TEST_MEMBER_TYPE_HPP
 
-#include "TestStructs.hpp"
-#include <boost/tti/TTIntrospection.hpp>
+#include "test_structs.hpp"
+#include <boost/tti/tti.hpp>
 
-TTI_MEMBER_TYPE(AnIntType)
-TTI_TRAIT_MEMBER_TYPE(NameStruct,AStructType)
-TTI_MEMBER_TYPE(AnIntTypeReference)
-TTI_MEMBER_TYPE(BType)
-TTI_TRAIT_MEMBER_TYPE(TheInteger,AnIntegerType)
-TTI_MEMBER_TYPE(CType)
-TTI_MEMBER_TYPE(AnotherIntegerType)
-TTI_TRAIT_MEMBER_TYPE(SomethingElse,someOtherType)
+BOOST_TTI_MEMBER_TYPE(AnIntType)
+BOOST_TTI_TRAIT_MEMBER_TYPE(NameStruct,AStructType)
+BOOST_TTI_MEMBER_TYPE(AnIntTypeReference)
+BOOST_TTI_MEMBER_TYPE(BType)
+BOOST_TTI_TRAIT_MEMBER_TYPE(TheInteger,AnIntegerType)
+BOOST_TTI_MEMBER_TYPE(CType)
+BOOST_TTI_MEMBER_TYPE(AnotherIntegerType)
+BOOST_TTI_TRAIT_MEMBER_TYPE(SomethingElse,someOtherType)
 
 #endif // TEST_MEMBER_TYPE_HPP
--- tti/libs/tti/test/test_mf_has_static_data.hpp
+++ tti/libs/tti/test/test_mf_has_static_data.hpp
@@ -1,13 +1,13 @@
 #if !defined(TEST_MF_HAS_STATIC_DATA_HPP)
 #define TEST_MF_HAS_STATIC_DATA_HPP
 
-#include "TestStructs.hpp"
-#include <boost/tti/TTIntrospection.hpp>
+#include "test_structs.hpp"
+#include <boost/tti/tti.hpp>
 
-TTI_HAS_STATIC_MEMBER(DSMember)
-TTI_HAS_STATIC_MEMBER(SomeStaticData)
+BOOST_TTI_HAS_STATIC_MEMBER(DSMember)
+BOOST_TTI_HAS_STATIC_MEMBER(SomeStaticData)
 
-TTI_MTFC_HAS_STATIC_MEMBER(DSMember)
-TTI_MTFC_HAS_STATIC_MEMBER(SomeStaticData)
+BOOST_TTI_MTFC_HAS_STATIC_MEMBER(DSMember)
+BOOST_TTI_MTFC_HAS_STATIC_MEMBER(SomeStaticData)
 
 #endif // TEST_MF_HAS_STATIC_DATA_HPP
