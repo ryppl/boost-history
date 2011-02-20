c11361ba813dae680a927d99c85b4927f188cdc5
Lorenzo Caminiti <lorcaminiti@gmail.com>
2011-02-20 19:33:41 +0000

    Adding boost/ dir with new variadic syntax.
    SVN-Revision: 69095

---
+++ local/boost/local/aux_/file.hpp
@@ -0,0 +1,18 @@
+
+// Copyright (C) 2009-2011 Lorenzo Caminiti
+// Use, modification, and distribution is subject to the
+// Boost Software License, Version 1.0
+// (see accompanying file LICENSE_1_0.txt or a copy at
+// http://www.boost.org/LICENSE_1_0.txt).
+
+#ifndef BOOST_LOCAL_AUX_FILE_HPP_
+#define BOOST_LOCAL_AUX_FILE_HPP_
+
+#define BOOST_LOCAL_AUX_FILE_ABSTRACT_FUNCTION_HPP \
+    "boost/local/aux_/abstract_function.hpp"
+
+#define BOOST_LOCAL_AUX_FILE_FUNCTION_HPP \
+    "boost/local/aux_/function.hpp"
+
+#endif // #include guard
+
