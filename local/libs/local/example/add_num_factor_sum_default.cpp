9df7eb8b20bb3ff1701d44069e1eb943551a2706
Lorenzo Caminiti <lorcaminiti@gmail.com>
2011-03-06 02:06:26 +0000

    Added WITH_DEFAULT examples.
    SVN-Revision: 69598

---
+++ local/libs/local/example/add_num_factor_sum_default.cpp
@@ -0,0 +1,25 @@
+
+// Copyright (C) 2009-2011 Lorenzo Caminiti
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0 (see accompanying file LICENSE_1_0.txt or a
+// copy at http://www.boost.org/LICENSE_1_0.txt).
+
+//[ add_num_factor_sum_default_cpp
+#include <boost/local/function.hpp>
+#include <iostream>
+
+int main() {
+    double sum = 0.0;
+    int factor = 10;
+
+    void BOOST_LOCAL_FUNCTION_PARAMS( (double num)(default +1.23)
+            (const bind factor) (bind& sum) ) {
+        sum += factor * num;
+        std::clog << "Summed: " << sum << std::endl;
+    } BOOST_LOCAL_FUNCTION_NAME(add)
+
+    add();
+    return 0;
+}
+//]
+
