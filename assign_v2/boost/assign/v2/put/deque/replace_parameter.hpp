a7eb571502c51e69a80f871b18891d04df2857c5
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-08 17:30:27 +0000

    upd assign_v2
    SVN-Revision: 69717

---
+++ assign_v2/boost/assign/v2/put/deque/replace_parameter.hpp
@@ -0,0 +1,39 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_PUT_DEQUE_REPLACE_PARAMETER_ER_2010_HPP
+#define BOOST_ASSIGN_V2_PUT_DEQUE_REPLACE_PARAMETER_ER_2010_HPP
+#include <boost/assign/v2/put/frame/replace_parameter.hpp>
+#include <boost/assign/v2/put/deque/fwd.hpp>
+
+namespace boost{
+namespace assign{
+namespace v2{
+namespace put_aux{
+
+    template<typename T, typename F, typename Tag>
+    struct replace_fun< deque_cont<T, F, Tag> >
+    {
+        template<typename F1>
+        struct apply{ typedef deque_cont<T, F1, Tag> type; };
+    };
+
+    template<typename T, typename F, typename Tag>
+    struct replace_modifier_tag< deque_cont<T, F, Tag> >
+    {
+        template<typename Tag1>
+        struct apply{ typedef deque_cont<T, F, Tag1> type; };
+    };
+
+}// put_aux
+}// v2
+}// assign
+}// boost
+
+#endif // BOOST_ASSIGN_V2_PUT_DEQUE_REPLACE_PARAMETER_ER_2010_HPP
