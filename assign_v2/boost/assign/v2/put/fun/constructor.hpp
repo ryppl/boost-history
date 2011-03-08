a3139b79bc7dc8756ad6877a2d7352deb89b54d2
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-08 15:59:14 +0000

    upd assign_v2
    SVN-Revision: 69692

---
+++ assign_v2/boost/assign/v2/put/fun/constructor.hpp
@@ -0,0 +1,17 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_PUT_FUN_CONSTRUCTOR_ER_2010_HPP
+#define BOOST_ASSIGN_V2_PUT_FUN_CONSTRUCTOR_ER_2010_HPP
+#include <boost/assign/v2/detail/functor/constructor.hpp>
+#include <boost/assign/v2/put/modulo/fun/keyword.hpp>
+
+BOOST_ASSIGN_V2_MODULO_FUN_KEYWORD(constructor, keyword_aux::constructor<T>)
+
+#endif
---
+++ assign_v2/boost/assign/v2/ref/array/conversion.hpp
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_REF_ARRAY_CONVERSION_ER_2010_HPP
+#define BOOST_ASSIGN_V2_REF_ARRAY_CONVERSION_ER_2010_HPP
+#include <boost/assign/v2/ref/array/interface.hpp>
+#include <boost/assign/v2/utility/conversion/name_lookup.hpp>
+
+namespace boost{
+namespace assign{
+namespace v2{
+namespace ref{
+namespace array_aux{
+
+#define BOOST_ASSIGN_V2_SEQ (Impl)(D)
+#define BOOST_ASSIGN_V2_R interface<Impl, D>
+BOOST_ASSIGN_V2_UTILITY_CONVERSION_NAME_LOOKUP(BOOST_ASSIGN_V2_SEQ, BOOST_ASSIGN_V2_R)
+#undef BOOST_ASSIGN_V2_SEQ
+#undef BOOST_ASSIGN_V2_R
+
+}// array_aux
+}// ref
+}// v2
+}// assign
+}// boost
+
+#endif
