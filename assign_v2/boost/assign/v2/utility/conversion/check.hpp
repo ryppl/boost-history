3b36b32b6fe8d4b6750ddd999474aa553d12657d
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-08 15:58:25 +0000

    upd assign_v2
    SVN-Revision: 69691

--- assign_v2/boost/assign/v2/utility/conversion/check.hpp
+++ assign_v2/boost/assign/v2/utility/conversion/check.hpp
@@ -7,8 +7,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_V2_CONVERT_CHECK_ER_2010_HPP
-#define BOOST_ASSIGN_V2_CONVERT_CHECK_ER_2010_HPP
+#ifndef BOOST_ASSIGN_V2_UTILITY_CONVERSION_CHECK_ER_2010_HPP
+#define BOOST_ASSIGN_V2_UTILITY_CONVERSION_CHECK_ER_2010_HPP
 #include <deque>
 #include <list>
 #include <map>
