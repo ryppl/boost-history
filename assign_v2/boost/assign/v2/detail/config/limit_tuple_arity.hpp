58a9a569f1f45924d40e4c1e6e3a6a68ac893ded
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-09 03:27:42 +0000

    upd assign_v2
    SVN-Revision: 69752

---
+++ assign_v2/boost/assign/v2/detail/config/limit_tuple_arity.hpp
@@ -0,0 +1,19 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_DETAIL_CONFIG_LIMIT_TUPLE_ARITY_ER_2010_HPP
+#define BOOST_ASSIGN_V2_DETAIL_CONFIG_LIMIT_TUPLE_ARITY_ER_2010_HPP
+#include <boost/assign/v2/detail/config/enable_cpp0x.hpp>
+#ifndef BOOST_ASSIGN_V2_LIMIT_CPP03_TUPLE_ARITY
+//[limit_tuple_arity
+/*<<Do not override. Source: boost/tuple/detail/tuple_basic.hpp>>*/
+#define BOOST_ASSIGN_V2_LIMIT_CPP03_TUPLE_ARITY 10
+//]
+#endif // BOOST_ASSIGN_V2_LIMIT_CPP03_TUPLE_ARITY
+#endif // BOOST_ASSIGN_V2_DETAIL_CONFIG_LIMIT_CPP03_TUPLE_ARITY_ER_2010_HPP
