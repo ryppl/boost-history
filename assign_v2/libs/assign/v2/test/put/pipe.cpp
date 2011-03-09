f063142623a9f7a4c600033b114c1243d5f4e334
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-09 21:08:32 +0000

    upd assign_v2
    SVN-Revision: 69785

--- assign_v2/libs/assign/v2/test/put/pipe.cpp
+++ assign_v2/libs/assign/v2/test/put/pipe.cpp
@@ -7,11 +7,11 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#include <libs/assign/v2/test/put/pipe/fun.h>
 #include <libs/assign/v2/test/put/pipe/args_list.h>
+#include <libs/assign/v2/test/put/pipe/fun.h>
 #include <libs/assign/v2/test/put/pipe/modifier.h>
-#include <libs/assign/v2/test/put/pipe/operator.h>
-#include <libs/assign/v2/test/put/pipe/range.h>
+#include <libs/assign/v2/test/put/pipe/put.h>
+#include <libs/assign/v2/test/put/pipe/put_range.h>
 #include <libs/assign/v2/test/put/pipe.h>
 
 namespace test_assign_v2{
@@ -21,10 +21,10 @@ namespace xxx_pipe{
     void test()
     {
         xxx_args_list::test();
-        xxx_modifier::test();
         xxx_fun::test();
-        xxx_operator::test();
-        xxx_range::test();
+        xxx_modifier::test();
+        xxx_put::test();
+        xxx_put_range::test();
     }
 
 }// xxx_pipe
