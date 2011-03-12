ed7751632da5aeff13f489a5ae8c7a7274f01fa5
Barend Gehrels <barend@xs4all.nl>
2011-03-12 23:53:18 +0000

    Moved wkt (used in nearly all tests) from extension to domains tree for release
    SVN-Revision: 69906

--- libs/geometry/test/views/reversible_view.cpp
+++ libs/geometry/test/views/reversible_view.cpp
@@ -12,9 +12,9 @@
 
 #include <geometry_test_common.hpp>
 
-#include <boost/geometry/util/reversible_view.hpp>
+#include <boost/geometry/views/reversible_view.hpp>
 
-#include <boost/geometry/extensions/gis/io/wkt/wkt.hpp>
+#include <boost/geometry/domains/gis/io/wkt/wkt.hpp>
 #include <boost/geometry/util/write_dsv.hpp>
 #include <boost/geometry/geometries/geometries.hpp>
 #include <boost/geometry/geometries/adapted/tuple_cartesian.hpp>
