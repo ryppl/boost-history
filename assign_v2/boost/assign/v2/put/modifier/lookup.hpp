2a7625606b0684f04afe4396fb356cf4ecadfac2
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-13 14:05:59 +0000

    upd assign_v2
    SVN-Revision: 69930

--- assign_v2/boost/assign/v2/put/modifier/lookup.hpp
+++ assign_v2/boost/assign/v2/put/modifier/lookup.hpp
@@ -9,8 +9,63 @@
 //////////////////////////////////////////////////////////////////////////////
 #ifndef BOOST_ASSIGN_V2_PUT_MODIFIER_LOOKUP_ER_2010_HPP
 #define BOOST_ASSIGN_V2_PUT_MODIFIER_LOOKUP_ER_2010_HPP
-#include <boost/assign/v2/put/modifier/lookup/impl.hpp>
-#include <boost/assign/v2/put/modifier/lookup/tag.hpp>
+#include <boost/assign/v2/detail/keyword/ignore.hpp>
+#include <boost/assign/v2/put/adapter/modifier.hpp>
+#include <boost/call_traits.hpp>
+#include <boost/shared_ptr.hpp>
+
+namespace boost{
+namespace assign{
+namespace v2{
+namespace modifier_tag{ 
+
+    template<typename Arg> struct lookup; 
+
+}// modifier_tag
+namespace put_aux{
+                
+    template<typename Arg>
+    class adapter_modifier<modifier_tag::lookup<Arg> >
+    {
+
+        typedef Arg arg_;
+        // storing a copy of lambda::something has caused pbs, hence ptr
+        typedef boost::shared_ptr<arg_> ptr_; 
+        typedef keyword_aux::ignore ignore_;
+        
+        public:
+
+        adapter_modifier() : ptr( new arg_() ){}
+        explicit adapter_modifier(
+            ignore_,
+            typename boost::call_traits<arg_>::param_type arg
+        ) : ptr( new arg_( arg ) )
+        {}
+
+#if BOOST_ASSIGN_V2_ENABLE_CPP0X
+        template<typename C, typename T>
+        void impl(C& cont, T&& t )const
+        {
+            cont[ key ] = (*this->ptr)( cont[ std::forward<T>( key ) ] );
+        }
+
+#else
+
+        template<typename C, typename T>
+        void impl(C& cont, T& key )const{
+            cont[ key ] = (*this->ptr)( cont[ key ] );
+        }
+
+#endif
+
+        private:
+        ptr_ ptr;
+    };
+
+}// put_aux
+}// v2
+}// assign
+}// boost
 
 #include <boost/assign/v2/put/modifier/modulo.hpp>
 BOOST_ASSIGN_V2_PUT_MODIFIER_MODULO_KEYWORD(lookup)
