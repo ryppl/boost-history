adb3d5976061492d9c04c6f3d0e86f58b0f4cab8
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-13 12:46:50 +0000

    upd assign_v2
    SVN-Revision: 69919

---
+++ assign_v2/boost/assign/v2/detail/pp/ignore.hpp
@@ -0,0 +1,15 @@
+////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                       //
+//                                                                        //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                              //
+//  Copyright (C) 2010 Erwann Rogard                                      //
+//  Use, modification and distribution are subject to the                 //
+//  Boost Software License, Version 1.0. (See accompanying file           //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)      //
+////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_DETAIL_PP_IGNORE_ER_2010_HPP
+#define BOOST_ASSIGN_V2_DETAIL_PP_IGNORE_ER_2010_HPP
+
+#define BOOST_ASSIGN_V2_IGNORE(x)
+
+#endif // BOOST_ASSIGN_V2_DETAIL_PP_IGNORE_ER_2010_HPP
