4193bcd77a877a3b9ae5ca59999a9fdbeea5c2f6
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-18 23:58:10 +0000

    upd assign_v2
    SVN-Revision: 70167

---
+++ assign_v2/libs/assign/v2/test/ref/aux_.cpp
@@ -0,0 +1,28 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#include <libs/assign/v2/test/ref/aux_/convert_traits.h>
+#include <libs/assign/v2/test/ref/aux_/list.h>
+#include <libs/assign/v2/test/ref/aux_/list_tuple.h>
+#include <libs/assign/v2/test/ref/aux_.h>
+
+namespace test_assign_v2{
+namespace xxx_ref{
+namespace xxx_aux_{
+
+    void test()
+    {
+        xxx_convert_traits::test();
+        xxx_list::test();
+        xxx_list_tuple::test();
+    }
+
+}// xxx_aux_
+}// xxx_ref
+}// test_assign_v2
