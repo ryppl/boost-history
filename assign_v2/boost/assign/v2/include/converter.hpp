7d61e7cb5419b0063c7f690230d67bcbdb0f479c
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-18 22:05:10 +0000

    upd assign_v2
    SVN-Revision: 70146

---
+++ assign_v2/boost/assign/v2/include/converter.hpp
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_INCLUDE_CONVERTER_ER_2010_HPP
+#define BOOST_ASSIGN_V2_INCLUDE_CONVERTER_ER_2010_HPP
+#include <boost/assign/v2/utility/conversion/converter.hpp>
+
+#endif // BOOST_ASSIGN_V2_INCLUDE_CONVERTER_ER_2010_HPP
