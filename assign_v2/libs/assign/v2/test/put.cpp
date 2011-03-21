7d19f4800aba28f3c81c1b48aed917d845c052c9
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-21 02:22:07 +0000

    upd assign_v2
    SVN-Revision: 70285

---
+++ assign_v2/libs/assign/v2/test/put.cpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#include <libs/assign/v2/test/put/pipe.h>
+#include <libs/assign/v2/test/put/put.h>
+#include <libs/assign/v2/test/put.h>
+
+namespace test_assign_v2{
+namespace xxx_put{
+
+    void test(){
+
+        xxx_put::test();
+        xxx_pipe::test();
+
+    }
+
+}// xxx_put
+}// test_assign_v2
