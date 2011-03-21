53def81619719243d8d563b4361435a59efab7ae
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-21 19:30:43 +0000

    upd assign_v2
    SVN-Revision: 70347

---
+++ assign_v2/boost/assign/v2/include/optional.hpp
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_INCLUDE_OPTIONAL_ER_2010_HPP
+#define BOOST_ASSIGN_V2_INCLUDE_OPTIONAL_ER_2010_HPP
+#include <boost/assign/v2/include/optional/fun.hpp>
+#include <boost/assign/v2/include/optional/insert.hpp>
+#include <boost/assign/v2/include/optional/iterate.hpp>
+#include <boost/assign/v2/include/optional/lookup.hpp>
+#include <boost/assign/v2/include/optional/push.hpp>
+#include <boost/assign/v2/include/optional/push_back.hpp>
+#include <boost/assign/v2/include/optional/push_front.hpp>
+#include <boost/assign/v2/include/optional/repeat.hpp>
+
+#endif // BOOST_ASSIGN_V2_INCLUDE_OPTIONAL_ER_2010_HPP
