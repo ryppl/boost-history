e6f8223c282cab077522d8c052b57857db03af29
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-21 01:58:13 +0000

    upd assign_v2
    SVN-Revision: 70282

--- assign_v2/boost/assign/v2/put.hpp
+++ assign_v2/boost/assign/v2/put.hpp
@@ -9,6 +9,7 @@
 //////////////////////////////////////////////////////////////////////////////
 #ifndef BOOST_ASSIGN_V2_PUT_ER_2010_HPP
 #define BOOST_ASSIGN_V2_PUT_ER_2010_HPP
-#include <boost/assign/v2/framework/adapter.hpp>
+#include <boost/assign/v2/put/pipe.hpp>
+#include <boost/assign/v2/put/put.hpp>
 
 #endif // BOOST_ASSIGN_V2_PUT_ER_2010_HPP
