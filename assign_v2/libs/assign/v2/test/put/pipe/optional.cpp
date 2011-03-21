e5d1d6a969df35dd79848ead30865e52762f710b
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-21 18:28:56 +0000

    upd assign_v2
    SVN-Revision: 70340

--- assign_v2/libs/assign/v2/test/put/pipe/optional.cpp
+++ assign_v2/libs/assign/v2/test/put/pipe/optional.cpp
@@ -7,6 +7,7 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
+#include <libs/assign/v2/test/put/pipe/optional/fun.h>
 #include <libs/assign/v2/test/put/pipe/optional/iterate.h>
 #include <libs/assign/v2/test/put/pipe/optional/lookup.h>
 #include <libs/assign/v2/test/put/pipe/optional/repeat.h>
@@ -16,17 +17,18 @@
 namespace test_assign_v2{
 namespace xxx_put{
 namespace xxx_pipe{
-namespace xxx_modifier{
+namespace xxx_optional{
 
     void test()
     {
+        xxx_fun::test();
         xxx_iterate::test();
         xxx_lookup::test();
         xxx_repeat::test();
         xxx_standard::test();
     }
 
-}// xxx_modifier
+}// xxx_optional
 }// xxx_pipe
 }// xxx_put
 }// xxx_test_assign
