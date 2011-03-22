0715c02c986ca56e7496214919ec6cb5c6854433
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-22 21:11:19 +0000

    upd assign_v2
    SVN-Revision: 70426

---
+++ assign_v2/boost/assign/v2/include/keyword.hpp
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2010 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_INCLUDE_KEYWORD_ER_2010_HPP
+#define BOOST_ASSIGN_V2_INCLUDE_KEYWORD_ER_2010_HPP
+#include <boost/assign/v2/include/keyword/chain.hpp>
+#include <boost/assign/v2/include/keyword/convert.hpp>
+#include <boost/assign/v2/include/keyword/csv_put.hpp>
+#include <boost/assign/v2/include/keyword/data.hpp>
+#include <boost/assign/v2/include/keyword/insert.hpp>
+#include <boost/assign/v2/include/keyword/iterate.hpp>
+#include <boost/assign/v2/include/keyword/key.hpp>
+#include <boost/assign/v2/include/keyword/mapped.hpp>
+#include <boost/assign/v2/include/keyword/push.hpp>
+#include <boost/assign/v2/include/keyword/push_back.hpp>
+#include <boost/assign/v2/include/keyword/push_front.hpp>
+#include <boost/assign/v2/include/keyword/repeat.hpp>
+
+#endif // BOOST_ASSIGN_V2_INCLUDE_KEYWORD_ER_2010_HPP
--- assign_v2/boost/assign/v2/include/keyword/push_back.hpp
+++ assign_v2/boost/assign/v2/include/keyword/push_back.hpp
@@ -7,8 +7,8 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_V2_INCLUDE_MODULO_PUSH_BACK_ER_2010_HPP
-#define BOOST_ASSIGN_V2_INCLUDE_MODULO_PUSH_BACK_ER_2010_HPP
+#ifndef BOOST_ASSIGN_V2_INCLUDE_KEYWORD_PUSH_BACK_ER_2010_HPP
+#define BOOST_ASSIGN_V2_INCLUDE_KEYWORD_PUSH_BACK_ER_2010_HPP
 #include <boost/assign/v2/option/push_back.hpp>
 
-#endif // BOOST_ASSIGN_V2_INCLUDE_MODULO_PUSH_BACK_ER_2010_HPP
+#endif // BOOST_ASSIGN_V2_INCLUDE_KEYWORD_PUSH_BACK_ER_2010_HPP
