d35a12b51edaa2df542331f573ff912f45d799f4
Lorenzo Caminiti <lorcaminiti@gmail.com>
2011-03-25 16:06:51 +0000

    Fixed copyright info to all files.
    SVN-Revision: 70541

--- local/libs/local/example/nesting_va.cpp
+++ local/libs/local/example/nesting_va.cpp
@@ -1,4 +1,9 @@
 
+// Copyright (C) 2009-2011 Lorenzo Caminiti
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0 (see accompanying file LICENSE_1_0.txt or a
+// copy at http://www.boost.org/LICENSE_1_0.txt).
+
 #include <boost/config.hpp>
 #if defined(BOOST_NO_VARIADIC_MACROS) || defined(BOOST_LOCAL_CONFIG_COMPLIANT)
 #include <iostream>
--- local/libs/local/example/scope_guards_except.cpp
+++ local/libs/local/example/scope_guards_except.cpp
@@ -1,4 +1,9 @@
 
+// Copyright (C) 2009-2011 Lorenzo Caminiti
+// Use, modification, and distribution is subject to the Boost Software
+// License, Version 1.0 (see accompanying file LICENSE_1_0.txt or a
+// copy at http://www.boost.org/LICENSE_1_0.txt).
+
 //[scope_guards_except_cpp
 #include <boost/local/exit.hpp>
 #include <iostream>
