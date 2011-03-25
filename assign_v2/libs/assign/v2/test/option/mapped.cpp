af8b8e72dfe6f8db93daddc0bdec81f5d935e12d
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-25 22:15:06 +0000

    upd assign_v2
    SVN-Revision: 70546

--- assign_v2/libs/assign/v2/test/option/mapped.cpp
+++ assign_v2/libs/assign/v2/test/option/mapped.cpp
@@ -15,7 +15,7 @@
 #include <boost/assign/v2/deque.hpp>
 // Options come next
 #include <boost/assign/v2/option/key.hpp>
-#include <boost/assign/v2/option/mapped.hpp> 
+#include <boost/assign/v2/option/mapped.hpp>
 #include <boost/lambda/lambda.hpp>
 #include <boost/mpl/assert.hpp>
 #include <boost/mpl/apply.hpp>
@@ -34,33 +34,17 @@ namespace xxx_mapped{
         using namespace boost;
         namespace as2 = assign::v2;
         {
-            //[test_option_mapped_meta
-            using namespace lambda;
-            typedef BOOST_TYPEOF(_1) arg_;
-            typedef as2:: interpreter_aux::keyword_mapped keyword_;
-            typedef as2:: interpreter_aux::option_modifier<keyword_, arg_> modulo_;
-            typedef std::map<std::string, int> cont_;
-            typedef as2::result_of::put<cont_>::type put_;
-            typedef as2::result_of::option_modifier<put_> meta1_;
-            typedef ::boost::mpl::apply2<meta1_, keyword_, arg_>::type result1_;
-            typedef as2::modifier_tag::mapped<arg_> tag1_;
-            typedef as2:: interpreter_aux::replace_modifier_tag<put_> meta2_;
-            typedef ::boost::mpl::apply1<meta2_, tag1_>::type result2_;
-            BOOST_MPL_ASSERT((boost::is_same<result1_, result2_>));
-            //]
-        }
-        {
             //[test_option_mapped_map
             using namespace lambda;
-            typedef std::string str_; typedef std::map<str_, int> C; C year; 
-            ( 
-                as2::put( year )( "feb", 28 )( "apr", 30 )( "jun", 30 )( "sep", 30 )( "nov", 30 ) 
-                    % as2::_key %  ( as2::_mapped = ( _1 = 31 ) ) 
+            typedef std::string str_; typedef std::map<str_, int> C; C year;
+            (
+                as2::put( year )( "feb", 28 )( "apr", 30 )( "jun", 30 )( "sep", 30 )( "nov", 30 )
+                    % as2::_key %  ( as2::_mapped = ( _1 = 31 ) )
             )/*<<Calls `year["jan"] = 31` etc.>>*/( "jan" )( "mar" )( "may" )( "jul" )( "aug" )( "oct" )( "dec" );
-            
+
             BOOST_ASSIGN_V2_CHECK( year["jan"] == 31 );
             BOOST_ASSIGN_V2_CHECK( year["dec"] == 31 );
-            //]        
+            //]
         }
         {
             //[test_option_mapped_meta_deque
@@ -82,9 +66,9 @@ namespace xxx_mapped{
             using namespace lambda;
             BOOST_AUTO(
                 days_in_first_quater,
-                ( 
-                    as2::deque<int>( 31 )( 31 )( 31 ) % ( as2::_mapped = ( _1 -= 3 ) ) 
-                )( 1 ) 
+                (
+                    as2::deque<int>( 31 )( 31 )( 31 ) % ( as2::_mapped = ( _1 -= 3 ) )
+                )( 1 )
             );
             BOOST_ASSIGN_V2_CHECK( days_in_first_quater[0] == 31 );
             BOOST_ASSIGN_V2_CHECK( days_in_first_quater[1] == 28 );
