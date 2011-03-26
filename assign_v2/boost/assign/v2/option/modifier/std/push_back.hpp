72a067dde1c0cf5f6b2f01bbc28ed1816a394826
Erwann Rogard <erwann.rogard@gmail.com>
2011-03-26 20:47:21 +0000

    upd assign_v2
    SVN-Revision: 70594

--- assign_v2/boost/assign/v2/option/modifier/std/push_back.hpp
+++ assign_v2/boost/assign/v2/option/modifier/std/push_back.hpp
@@ -1,16 +1,1 @@
-//////////////////////////////////////////////////////////////////////////////
-//  Boost.Assign v2                                                         //
-//                                                                          //
-//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
-//  Copyright (C) 2010 Erwann Rogard                                        //
-//  Use, modification and distribution are subject to the                   //
-//  Boost Software License, Version 1.0. (See accompanying file             //
-//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
-//////////////////////////////////////////////////////////////////////////////
-#ifndef BOOST_ASSIGN_V2_OPTION_PUSH_BACK_ER_2010_HPP
-#define BOOST_ASSIGN_V2_OPTION_PUSH_BACK_ER_2010_HPP
-#include <boost/assign/v2/option/std/modifier/framework.hpp>
-BOOST_ASSIGN_V2_OPTION_STD_MODIFIER_TAG(push_back)
-BOOST_ASSIGN_V2_OPTION_STD_MODIFIER(push_back)
-
-#endif // BOOST_ASSIGN_V2_OPTION_PUSH_BACK_ER_2010_HPP
+// TODO remove file
