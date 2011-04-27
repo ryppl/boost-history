b1971e729861255902aaf347f905417593838090
Erwann Rogard <erwann.rogard@gmail.com>
2011-04-27 22:15:18 +0000

    upd assign_v2
    SVN-Revision: 71550

--- assign_v2/libs/assign/v2/test/put/ptr.cpp
+++ assign_v2/libs/assign/v2/test/put/ptr.cpp
@@ -72,7 +72,7 @@ namespace xxx_ptr{
             const char y[4] = { 'b', 'a', 'r', '\0' };
             word_ z = "***baz";
             boost::ptr_unordered_map<int, word_> map;
-            as2::put( map )( 1, x, 3 )( 2, y )( 3, z, 3, 3 )( 4, "qux");
+            as2::put( map )( 1, x, 3 )( 2, y )( 3, z, 3, 3 )( 4, "qux" );
 
             assert( map[1] == "foo" ); assert( map[2] == "bar" );
             assert( map[3] == "baz" ); assert( map[4] == "qux" );
