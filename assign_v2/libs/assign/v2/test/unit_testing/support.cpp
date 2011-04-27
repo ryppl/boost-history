fbb96bd6dc5689697985190ad42cabdd6e908208
Erwann Rogard <erwann.rogard@gmail.com>
2011-04-27 18:56:37 +0000

    upd assign_v2
    SVN-Revision: 71547

--- assign_v2/libs/assign/v2/test/unit_testing/support.cpp
+++ assign_v2/libs/assign/v2/test/unit_testing/support.cpp
@@ -7,12 +7,12 @@
 //  Boost Software License, Version 1.0. (See accompanying file             //
 //  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
 //////////////////////////////////////////////////////////////////////////////
-#ifndef LIBS_ASSIGN_V2_TEST_UNIT_TEST_DETAIL_ER_2010_CPP
-#define LIBS_ASSIGN_V2_TEST_UNIT_TEST_DETAIL_ER_2010_CPP
+#ifndef LIBS_ASSIGN_V2_TEST_UNIT_TEST_SUPPORT_ER_2010_CPP
+#define LIBS_ASSIGN_V2_TEST_UNIT_TEST_SUPPORT_ER_2010_CPP
 #include <boost/test/test_tools.hpp>
 #define BOOST_ASSIGN_V2_CHECK( p ) BOOST_CHECK( p )
-#include <libs/assign/v2/test/detail/functor.cpp>
-#include <libs/assign/v2/test/detail/traits.cpp>
+#include <libs/assign/v2/test/support/functor.cpp>
+#include <libs/assign/v2/test/support/traits.cpp>
 
 #include <boost/test/unit_test.hpp>
 using boost::unit_test::test_suite;
@@ -21,7 +21,7 @@ test_suite* init_unit_test_suite( int argc, char* argv[] )
     test_suite* test = BOOST_TEST_SUITE( "BOOST_ASSIGN_V2" );
     using namespace test_assign_v2;
     {
-        using namespace xxx_detail;
+        using namespace xxx_support;
         test->add( BOOST_TEST_CASE( &xxx_functor::test ) );
         test->add( BOOST_TEST_CASE( &xxx_traits::test ) );
     }
