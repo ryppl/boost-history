2ba8e0f675adfc4b493431b52181c6b52bf197c2
Lorenzo Caminiti <lorcaminiti@gmail.com>
2011-04-29 03:24:41 +0000

    Removed LD_LIBRARY_PATH set for cygwin.
    SVN-Revision: 71586

--- local/tools/setup/cygwin-boost-env.sh
+++ local/tools/setup/cygwin-boost-env.sh
@@ -2,7 +2,6 @@
 # Source this files `. <FILE>`.
 
 export BOOST_ROOT="/cygdrive/e/sandbox/boost-trunk.cygwin"
-export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$BOOST_ROOT/stage/lib"
 export BOOST_BUILD_PATH="$BOOST_ROOT/tools/build/v2"
 export PATH="$BOOST_BUILD_PATH/engine/bin.cygwinx86:$PATH"
 
