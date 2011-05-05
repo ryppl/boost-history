387a6afb7aef79f84cd68392316b550b05be2280
Phil Bouchard <phil@fornux.com>
2011-05-05 23:47:29 +0000

    * Added multithreaded test
    SVN-Revision: 71753

---
+++ block_ptr/libs/smart_ptr/example/thread_test.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <boost/thread.hpp>
+#include <boost/block_ptr.hpp>
+
+using namespace std;
+using namespace boost;
+
+
+block_ptr< pair<int, int> > p;
+
+
+void worker(int id)  
+{         
+    std::cout << "Worker: running" << std::endl;  
+       
+    for (int i = 0; i < 100000; ++ i)
+    	p = make_block< pair<int, int> >(make_pair(id, i));
+       
+    std::cout << "Worker: finished" << std::endl;  
+}  
+   
+int main(int argc, char* argv[])  
+{  
+    std::cout << "main: startup" << std::endl;  
+    
+   	boost::thread t0(worker, 0);  
+   	boost::thread t1(worker, 1);  
+   	//boost::thread t2(worker, 2);  
+   	//boost::thread t3(worker, 3);  
+       
+    std::cout << "main: waiting for thread" << std::endl;  
+       
+    t0.join();  
+    t1.join();  
+    //t2.join();  
+    //t3.join();  
+       
+    std::cout << "main: done" << std::endl;  
+       
+    return 0;  
+}  
