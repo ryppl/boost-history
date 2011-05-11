e620f0275a56e1eab1f7d5a879cf09fd53d71ef6
Daniel James <daniel@calamity.org.uk>
2011-05-11 23:45:33 +0000

    Website: Remove the dev branch.
    SVN-Revision: 71883

--- public_html/dev/common/code/update-6x6.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd ${HOME}/www.boost.org/testing
-for result in `find incoming -mmin +5 -name '*.zip'` ; do
-  f=`basename ${result}`
-  mv -f live/${f} old
-  mv -f ${result} live
-  rm -f old/${f}
-done
