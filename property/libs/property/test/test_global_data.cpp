470ff0738759998d953cc0b5918c11987fdda2c5
Edward Diener <eldiener@tropicsoft.com>
2011-05-14 13:56:49 +0000

    One more test file added.
    SVN-Revision: 71938

---
+++ property/libs/property/test/test_global_data.cpp
@@ -0,0 +1,10 @@
+#include "test_enum.hpp"
+#include "test_pod.hpp"
+
+char gld_char(' ');
+int gld_int(0);
+double gld_double(0.0);
+bool gld_bool(false);
+int * gld_pointer(0);
+test_enum gld_enum(e_test_default);
+test_pod gld_pod;
