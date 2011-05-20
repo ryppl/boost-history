a581cfed2fd5b93f131aa035afe787595928a16b
Vicente J. Botet Escriba <vicente.botet@wanadoo.fr>
2011-05-20 17:08:28 +0000

    Added global include file
    SVN-Revision: 72055

---
+++ quantization/boost/quanta.hpp
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// (C) Copyright Vicente J. Botet Escriba 2011.
+// Distributed under the Boost
+// Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or
+// copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+// See http://www.boost.org/libs/quanta for documentation.
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef BOOST_QUANTA__HPP
+#define BOOST_QUANTA__HPP
+
+#include <boost/config.hpp>
+#include <boost/quanta/include.hpp>
+
+
+#endif 
