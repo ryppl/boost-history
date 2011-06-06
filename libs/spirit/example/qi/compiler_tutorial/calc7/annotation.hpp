bb5365f10edc9c8a4f458347ea1151a2a84a34ce
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2011-06-06 12:42:19 +0000

    Spirit: merging minor fixes for examples from trunk
    SVN-Revision: 72434

--- libs/spirit/example/qi/compiler_tutorial/calc7/annotation.hpp
+++ libs/spirit/example/qi/compiler_tutorial/calc7/annotation.hpp
@@ -9,6 +9,8 @@
 
 #include <map>
 #include <boost/variant/apply_visitor.hpp>
+#include <boost/type_traits/is_base_of.hpp>
+#include <boost/mpl/bool.hpp>
 #include "ast.hpp"
 
 namespace client
@@ -24,10 +26,9 @@ namespace client
         template <typename, typename>
         struct result { typedef void type; };
 
-        std::map<int, Iterator>& iters;
-        int& current_id;
-        annotation(int& current_id, std::map<int, Iterator>& iters)
-          : current_id(current_id), iters(iters) {}
+        std::vector<Iterator>& iters;
+        annotation(std::vector<Iterator>& iters)
+          : iters(iters) {}
 
         struct set_id
         {
@@ -39,15 +40,36 @@ namespace client
             template <typename T>
             void operator()(T& x) const
             {
+                this->dispatch(x, boost::is_base_of<ast::tagged, T>());
+            }
+
+            // This will catch all nodes except those inheriting from ast::tagged
+            template <typename T>
+            void dispatch(T& x, boost::mpl::false_) const
+            {
+                // (no-op) no need for tags
+            }
+
+            // This will catch all nodes inheriting from ast::tagged
+            template <typename T>
+            void dispatch(T& x, boost::mpl::true_) const
+            {
                 x.id = id;
             }
         };
 
         void operator()(ast::operand& ast, Iterator pos) const
         {
-            int id = current_id++;
-            iters[id] = pos;
+            int id = iters.size();
+            iters.push_back(pos);
             boost::apply_visitor(set_id(id), ast);
+        }
+
+        void operator()(ast::assignment& ast, Iterator pos) const
+        {
+            int id = iters.size();
+            iters.push_back(pos);
+            ast.lhs.id = id;
         }
     };
 }
