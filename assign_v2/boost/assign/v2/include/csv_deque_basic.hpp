a1588f4c780b2c731c47d235f33b9cf3750becf3
Erwann Rogard <erwann.rogard@gmail.com>
2011-06-07 01:25:19 +0000

    upd assign_v2
    SVN-Revision: 72448

---
+++ assign_v2/boost/assign/v2/include/csv_deque_basic.hpp
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2011 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_INCLUDE_CSV_DEQUE_BASIC_ER_2011_HPP
+#define BOOST_ASSIGN_V2_INCLUDE_CSV_DEQUE_BASIC_ER_2011_HPP
+#include <boost/assign/v2/deque/csv_deque/csv_deque_basic.hpp>
+
+#endif // BOOST_ASSIGN_V2_INCLUDE_CSV_DEQUE_BASIC_ER_2011_HPP
---
+++ assign_v2/boost/assign/v2/include/csv_put_basic.hpp
@@ -0,0 +1,14 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2011 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef BOOST_ASSIGN_V2_INCLUDE_CSV_PUT_BASIC_ER_2011_HPP
+#define BOOST_ASSIGN_V2_INCLUDE_CSV_PUT_BASIC_ER_2011_HPP
+#include <boost/assign/v2/put/csv_put/csv_put_basic.hpp>
+
+#endif // BOOST_ASSIGN_V2_INCLUDE_CSV_PUT_BASIC_ER_2011_HPP
