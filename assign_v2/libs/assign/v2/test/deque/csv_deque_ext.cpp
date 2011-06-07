e2a5a0248ac7411211592c7787e153ff22ade995
Erwann Rogard <erwann.rogard@gmail.com>
2011-06-07 01:29:24 +0000

    upd assign_v2
    SVN-Revision: 72452

---
+++ assign_v2/libs/assign/v2/test/deque/csv_deque_ext.cpp
@@ -0,0 +1,75 @@
+////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                       //
+//                                                                        //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                              //
+//  Copyright (C) 2011 Erwann Rogard                                      //
+//  Use, modification and distribution are subject to the                 //
+//  Boost Software License, Version 1.0. (See accompanying file           //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)      //
+////////////////////////////////////////////////////////////////////////////
+#include <string>
+#include <boost/assign/v2/include/csv_deque_ext.hpp>
+#include <boost/assign/v2/support/config/check.hpp>
+
+#include <boost/assign/v2/support/config/enable_cpp0x.hpp>
+#if BOOST_ASSIGN_V2_ENABLE_CPP0X
+#include <tuple>
+#else
+#include <boost/tuple/tuple.hpp>
+#endif
+
+#include <libs/assign/v2/test/deque/csv_deque_ext.h>
+
+namespace test_assign_v2{
+namespace xxx_deque{
+namespace xxx_csv_deque_ext{
+
+    void test()
+    {
+        namespace as2 = boost::assign::v2;
+        {
+            //[test_csv_deque_ext1
+            typedef const char state_ [3]; typedef int code_;
+            state_ ct = "CT", nj = "NJ", ny = "NY";
+
+//<-
+#if BOOST_ASSIGN_V2_ENABLE_CPP0X
+//->
+            typedef std::tuple<state_/*<<Notice the reference>>*/&,  code_> data_;
+//<-
+#else
+            typedef boost::tuple<state_&,  code_> data_;
+#endif
+//->
+            as2::result_of::deque<
+                data_
+            >::type region = as2::csv_deque<data_, 2>(
+                ny, 212, ny, 718, ny, 516, ny, 914,
+                nj, 210, nj, 908, nj, 609,
+                ct, 203
+            );
+
+//<-
+#if BOOST_ASSIGN_V2_ENABLE_CPP0X
+//->
+            BOOST_ASSIGN_V2_CHECK( std::get<0>( region.front() ) == ny );
+            BOOST_ASSIGN_V2_CHECK( std::get<1>( region.back() ) == 203 );
+            BOOST_ASSIGN_V2_CHECK( std::get<1>( region.front() ) == 212 );
+            BOOST_ASSIGN_V2_CHECK( std::get<0>( region.back() ) == ct );
+//<-
+#endif
+//->
+            //]
+#if! BOOST_ASSIGN_V2_ENABLE_CPP0X
+            BOOST_ASSIGN_V2_CHECK( boost::get<0>( region.front() ) == ny );
+            BOOST_ASSIGN_V2_CHECK( boost::get<1>( region.back() ) == 203 );
+            BOOST_ASSIGN_V2_CHECK( boost::get<1>( region.front() ) == 212 );
+            BOOST_ASSIGN_V2_CHECK( boost::get<0>( region.back() ) == ct );
+#endif
+        }
+
+    }
+
+}// xxx_csv_deque_ext
+}// xxx_deque
+}// test_assign_v2
---
+++ assign_v2/libs/assign/v2/test/deque/csv_deque_ext.h
@@ -0,0 +1,23 @@
+//////////////////////////////////////////////////////////////////////////////
+//  Boost.Assign v2                                                         //
+//                                                                          //
+//  Copyright (C) 2003-2004 Thorsten Ottosen                                //
+//  Copyright (C) 2011 Erwann Rogard                                        //
+//  Use, modification and distribution are subject to the                   //
+//  Boost Software License, Version 1.0. (See accompanying file             //
+//  LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)        //
+//////////////////////////////////////////////////////////////////////////////
+#ifndef LIBS_ASSIGN_V2_TEST_DEQUE_CSV_DEQUE_EXT_ER_2010_H
+#define LIBS_ASSIGN_V2_TEST_DEQUE_CSV_DEQUE_EXT_ER_2010_H
+
+namespace test_assign_v2{
+namespace xxx_deque{
+namespace xxx_csv_deque_ext{
+
+    void test();
+
+}// xxx_csv_deque_ext
+}// xxx_deque
+}// test_assign_v2
+
+#endif // LIBS_ASSIGN_V2_TEST_DEQUE_CSV_DEQUE_EXT_ER_2010_H
