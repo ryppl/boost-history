86eb4fe27f6136df4595809a7987e2f893d56402
Steven Watanabe <steven@providere-consulting.com>
2011-07-07 04:57:37 +0000

    Merge [72825] and [72826] from the trunk.  Authorized by Beman.
    SVN-Revision: 72951

--- boost/random/generate_canonical.hpp
+++ boost/random/generate_canonical.hpp
@@ -14,13 +14,14 @@
 #ifndef BOOST_RANDOM_GENERATE_CANONICAL_HPP
 #define BOOST_RANDOM_GENERATE_CANONICAL_HPP
 
-#include <cassert>
 #include <algorithm>
+#include <boost/assert.hpp>
 #include <boost/config/no_tr1/cmath.hpp>
 #include <boost/limits.hpp>
 #include <boost/type_traits/is_integral.hpp>
 #include <boost/math/special_functions.hpp>
 #include <boost/random/detail/signed_unsigned_tools.hpp>
+#include <boost/random/detail/generator_bits.hpp>
 
 namespace boost {
 namespace random {
@@ -33,9 +34,11 @@ RealType generate_canonical_impl(URNG& g, boost::mpl::true_ /*is_integral*/)
     using std::pow;
     typedef typename URNG::result_type base_result;
     std::size_t digits = std::numeric_limits<RealType>::digits;
-    RealType R = RealType(g.max()) - RealType(g.min()) + 1;
+    RealType R = RealType((g.max)()) - RealType((g.min)()) + 1;
     RealType mult = R;
-    RealType limit = pow(RealType(2), RealType((std::min)(bits, digits)));
+    RealType limit =
+        pow(RealType(2),
+            RealType((std::min)(static_cast<std::size_t>(bits), digits)));
     RealType S = RealType(detail::subtract<base_result>()(g(), (g.min)()));
     while(mult < limit) {
         RealType inc = RealType(detail::subtract<base_result>()(g(), (g.min)()));
@@ -50,17 +53,16 @@ RealType generate_canonical_impl(URNG& g, boost::mpl::false_ /*is_integral*/)
 {
     using std::pow;
     using std::floor;
-    assert((g.min)() == 0);
-    assert((g.max)() == 1);
+    BOOST_ASSERT((g.min)() == 0);
+    BOOST_ASSERT((g.max)() == 1);
     typedef typename URNG::result_type base_result;
     std::size_t digits = std::numeric_limits<RealType>::digits;
-    std::size_t engine_bits = g.precision();
+    std::size_t engine_bits = detail::generator_bits<URNG>::value();
     std::size_t b = (std::min)(bits, digits);
-    std::size_t k = (b  + engine_bits - 1) / engine_bits;
     RealType R = pow(RealType(2), RealType(engine_bits));
     RealType mult = R;
     RealType limit = pow(RealType(2), RealType(b));
-    RealType S = g() - (g.min)();
+    RealType S = RealType(g() - (g.min)());
     while(mult < limit) {
         RealType inc(floor((RealType(g()) - RealType((g.min)())) * R));
         S += inc * mult;
@@ -80,11 +82,11 @@ RealType generate_canonical(URNG& g)
 {
     RealType result = detail::generate_canonical_impl<RealType, bits>(
         g, boost::is_integral<typename URNG::result_type>());
-    assert(result >= 0);
-    assert(result <= 1);
+    BOOST_ASSERT(result >= 0);
+    BOOST_ASSERT(result <= 1);
     if(result == 1) {
         result -= std::numeric_limits<RealType>::epsilon() / 2;
-        assert(result != 1);
+        BOOST_ASSERT(result != 1);
     }
     return result;
 }
--- libs/random/test/concepts.hpp
+++ libs/random/test/concepts.hpp
@@ -9,18 +9,38 @@
  *
  */
 
+#ifndef BOOST_RANDOM_TEST_CONCEPTS_HPP
+#define BOOST_RANDOM_TEST_CONCEPTS_HPP
+
+#include <boost/config.hpp>
+
+#ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable:4100)
+#endif
+
 #include <boost/concept_check.hpp>
+
+#ifdef BOOST_MSVC
+#pragma warning(pop)
+#endif
+
 #include <boost/concept_archetype.hpp>
-# include <boost/concept/requires.hpp>
+#include <boost/concept/requires.hpp>
 #include <boost/mpl/assert.hpp>
 #include <boost/type_traits/is_arithmetic.hpp>
+#include <boost/type_traits/is_integral.hpp>
+#include <boost/type_traits/is_same.hpp>
 #include <boost/cstdint.hpp>
 #include <boost/static_assert.hpp>
 #include <istream>
 #include <ostream>
 
-#ifndef BOOST_RANDOM_TEST_CONCEPTS_HPP
-#define BOOST_RANDOM_TEST_CONCEPTS_HPP
+#ifdef BOOST_MSVC
+#pragma warning(push)
+#pragma warning(disable:4510)
+#pragma warning(disable:4610)
+#endif
 
 namespace boost {
 namespace random {
@@ -34,7 +54,16 @@ struct seed_seq_archetype : Base
         ((Mutable_RandomAccessIterator<Iter>))
         ((UnsignedInteger<typename Mutable_RandomAccessIterator<Iter>::value_type>)),
     (void))
-    generate(Iter, Iter);
+    generate(Iter, Iter) {}
+};
+
+template<class R = unsigned, class Base = null_archetype<> >
+struct uniform_random_number_generator_archetype : Base
+{
+    typedef R result_type;
+    static R min BOOST_PREVENT_MACRO_SUBSTITUTION () { return 0; }
+    static R max BOOST_PREVENT_MACRO_SUBSTITUTION () { return 0; }
+    R operator()() { return 0; }
 };
 
 template<class SSeq>
@@ -71,6 +100,10 @@ private:
     std::wostream wos;
 };
 
+// Type deduction will fail unless the arguments have the same type.
+template <typename T>
+void same_type(T const&, T const&) {}
+
 template <class E>
 struct RandomNumberEngine :
     DefaultConstructible<E>,
@@ -79,7 +112,7 @@ struct RandomNumberEngine :
     EqualityComparable<E>,
     Streamable<E>
 {
- public:
+public:
     typedef typename E::result_type result_type;
 
     // relaxed from the standard
@@ -94,12 +127,12 @@ struct RandomNumberEngine :
     BOOST_CONCEPT_USAGE(RandomNumberEngine)
     {
         same_type(e(), result_type());
-        same_type(E::min(), result_type());
-        same_type(E::max(), result_type());
+        same_type((E::min)(), result_type());
+        same_type((E::max)(), result_type());
 
-        E();
-        E(s);
-        E(q);
+        (void)E();
+        (void)E(s);
+        (void)E(q);
 
         e.seed();
         e.seed(s);
@@ -108,27 +141,70 @@ struct RandomNumberEngine :
         e.discard(z);
 
         // extension
-        E(sb, se);
+        (void)E(sb, se);
         e.seed(sb, se);
     }
     
- private:
+private:
     E e;
     E v;
     const E x;
     seed_seq_archetype<> q;
-    result_type s;
+    typename detail::seed_type<result_type>::type s;
     unsigned long long z;
     
     input_iterator_archetype<boost::uint32_t> sb, se;
+};
 
-    // Type deduction will fail unless the arguments have the same type.
-    template <typename T>
-    void same_type(T const&, T const&);
+template<class D>
+struct RandomNumberDistribution :
+    DefaultConstructible<D>,
+    CopyConstructible<D>,
+    Assignable<D>,
+    EqualityComparable<D>,
+    Streamable<D>
+{
+public:
+    typedef typename D::result_type result_type;
+    typedef typename D::param_type param_type;
+    // backwards compatibility
+    typedef typename D::input_type input_type;
+
+    typedef param_type P;
+
+    BOOST_CONCEPT_ASSERT((DefaultConstructible<P>));
+    BOOST_CONCEPT_ASSERT((CopyConstructible<P>));
+    BOOST_CONCEPT_ASSERT((Assignable<P>));
+    BOOST_CONCEPT_ASSERT((EqualityComparable<P>));
+    BOOST_CONCEPT_ASSERT((Streamable<P>));
+
+    BOOST_MPL_ASSERT((boost::is_same<typename P::distribution_type, D>));
+
+    BOOST_CONCEPT_USAGE(RandomNumberDistribution)
+    {
+        (void)D(p);
+        d.reset();
+        same_type(x.param(), p);
+        d.param(p);
+        same_type(d(g), result_type());
+        same_type(d(g, p), result_type());
+        same_type((x.min)(), result_type());
+        same_type((x.max)(), result_type());
+    }
+
+private:
+    D d;
+    const D x;
+    const P p;
+    uniform_random_number_generator_archetype<> g;
 };
 
 }
 }
 }
+
+#ifdef BOOST_MSVC
+#pragma warning(pop)
+#endif
 
 #endif
